e815638ff2470b7a8d22802fc1090b69aedccdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:03:22 +0000

    Add package djangocms-icon/2.1.0/djangocms-icon-2.1.0.tar.gz

---
+++ packages/djangocms-icon/djangocms-icon-2.1.0.tar.gz/djangocms-icon-2.1.0/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+from djangocms_icon import __version__
+
+
+REQUIREMENTS = [
+    'django-cms>=3.10',
+    'djangocms-attributes-field>=1',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Framework :: Django',
+    'Framework :: Django :: 3.2',
+    'Framework :: Django :: 4.0',
+    'Framework :: Django :: 4.1',
+    'Framework :: Django :: 4.2',
+    'Framework :: Django CMS',
+    'Framework :: Django CMS :: 3.10',
+    'Framework :: Django CMS :: 3.11',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+
+setup(
+    name='djangocms-icon',
+    version=__version__,
+    author='Divio AG',
+    author_email='info@divio.com',
+    maintainer='Django CMS Association and contributors',
+    maintainer_email='info@django-cms.org',
+    url='https://github.com/django-cms/djangocms-icon',
+    license='BSD-3-Clause',
+    description='Adds icon plugin to django CMS.',
+    long_description=open('README.rst').read(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
+)
---
+++ packages/djangocms-icon/djangocms-icon-2.1.0.tar.gz/djangocms-icon-2.1.0/tests/settings.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+HELPER_SETTINGS = {
+    'INSTALLED_APPS': [],
+    'CMS_LANGUAGES': {
+        1: [{
+            'code': 'en',
+            'name': 'English',
+        }]
+    },
+    'LANGUAGE_CODE': 'en',
+    'ALLOWED_HOSTS': ['localhost'],
+    'CMS_CONFIRM_VERSION4': True,
+}
+
+try:  # V4 test?
+    import djangocms_versioning  # noqa
+
+    HELPER_SETTINGS["INSTALLED_APPS"] += [
+        "djangocms_versioning",
+    ]
+except ImportError:  # Nope
+    pass
+
+
+def run():
+    from app_helper import runner
+    runner.cms('djangocms_icon')
+
+
+if __name__ == '__main__':
+    run()
