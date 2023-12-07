d4ba0619aa99f31e706858e7f66a1473641945d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:25 +0000

    Add package pypetal/0.2.1/pypetal-0.2.1.tar.gz

---
+++ packages/pypetal/pypetal-0.2.1.tar.gz/pypetal-0.2.1/src/pypetal/utils/defaults.py
@@ -0,0 +1,291 @@
+import numpy as np
+from astropy.table import Table
+
+
+def set_general(input_args, fnames):
+
+    default_kwargs = {
+        'verbose': False,
+        'plot': False,
+        'time_unit': 'd',
+        'lc_unit': 'Arbitrary Units',
+        'file_fmt': 'csv',
+        'lag_bounds': 'baseline',
+        'threads': 1
+    }
+
+    params = { **default_kwargs, **input_args }
+
+    verbose = params['verbose']
+    plot = params['plot']
+    time_unit = params['time_unit']
+    lc_unit = params['lc_unit']
+    file_fmt = params['file_fmt']
+    lag_bounds = params['lag_bounds']
+    threads = params['threads']
+
+    if isinstance(lag_bounds, list):
+        if len(lag_bounds) == 1:
+            assert isinstance(lag_bounds[0], list) or isinstance(lag_bounds[0], str) or ( lag_bounds[0] is None )
+
+        elif len(lag_bounds) == 2:
+            pass
+
+        else:
+            for i in range(len(lag_bounds)):
+                if isinstance(lag_bounds[i], list):
+                    assert ( len(lag_bounds[i]) == 2 ) | ( lag_bounds[i] is None ) | ( lag_bounds[i] == 'baseline' )
+                else:
+                    assert ( lag_bounds[i] is None ) | ( lag_bounds[i] == 'baseline' )
+
+
+    if isinstance(lc_unit, str):
+        lc_unit = list( np.full( len(fnames), lc_unit ) )
+
+
+
+
+    len_lag_bounds = len(lag_bounds)
+    if len_lag_bounds == 2:
+        if not ( isinstance(lag_bounds[0], list) or ( lag_bounds[0] == 'baseline' ) ):
+            len_lag_bounds = 1
+        else:
+            len_lag_bounds = 2
+
+
+    if len_lag_bounds != len(fnames)-1:
+
+        lag_bounds_og = lag_bounds
+        lag_bounds = []
+
+        for i in range(len(fnames)-1):
+            lag_bounds.append(lag_bounds_og)
+
+    if (len(fnames) == 2) and ( not isinstance(lag_bounds[0], list) ):
+        if not isinstance(lag_bounds[0], str):
+            lag_bounds = [lag_bounds]
+
+
+
+    xvals_tot = []
+    baselines = []
+    for i in range(len(fnames)):
+
+        try:
+            dat = Table.read(fnames[i], format=file_fmt)
+        except:
+            dat = Table.read(fnames[i], format='ascii')
+
+        colnames = dat.colnames
+        x = np.array( dat[colnames[0]] )
+
+        sort_ind = np.argsort(x)
+        x = x[sort_ind]
+
+        xvals_tot.append(x)
+
+    for i in range(len(fnames)-1):
+        baseline = np.max([ xvals_tot[0].max(), xvals_tot[i+1].max() ]) - np.min([ xvals_tot[0].min(), xvals_tot[i+1].min() ])
+        baselines.append(baseline)
+
+    for i in range(len(fnames)-1):
+        if (lag_bounds[i] is None) | (lag_bounds[i] == 'baseline'):
+            lag_bounds[i] = [-baselines[i], baselines[i]]
+
+
+    #Return dict so it can be passed to other functions
+    output = {
+        'verbose': verbose,
+        'plot': plot,
+        'time_unit': time_unit,
+        'lc_unit': lc_unit,
+        'file_fmt': file_fmt,
+        'lag_bounds': lag_bounds,
+        'threads': threads
+    }
+
+    return output
+
+
+
+
+
+def set_drw_rej(input_args, fnames):
+
+    default_kwargs = {
+        'jitter' : True,
+        'nsig' : 3,
+        'nwalker': 100,
+        'nburn': 300,
+        'nchain': 1000,
+        'clip': np.full( len(fnames), True),
+        'reject_data': np.hstack([ [True], np.full( len(fnames)-1, False) ]),
+        'use_for_javelin': False
+    }
+
+    params = { **default_kwargs, **input_args }
+
+    jitter = params['jitter']
+    nsig = params['nsig']
+    nwalker = params['nwalker']
+    nburn = params['nburn']
+    nchain = params['nchain']
+    clip = params['clip']
+    reject_data = params['reject_data']
+    use_for_javelin = params['use_for_javelin']
+
+    if isinstance(clip, bool):
+        clip = np.full( len(fnames), clip)
+
+    if isinstance(reject_data, bool):
+        reject_data = np.full( len(fnames), reject_data)
+
+    if (use_for_javelin) & (not reject_data[0]):
+        raise Exception('Cannot use continuum for Javelin without fitting it to a DRW first')
+
+
+    return jitter, nsig, nwalker, nburn, nchain, clip, \
+        reject_data, use_for_javelin
+
+
+def set_detrend(input_args):
+
+    default_kwargs = {
+        'K': 2,
+        'nchain': 4,
+        'miniter': 5000,
+        'maxiter': 10000
+    }
+
+    params = { **default_kwargs, **input_args }
+
+    K = params['K']
+    nchain = params['nchain']
+    miniter = params['miniter']
+    maxiter = params['maxiter']
+
+    return K, nchain, miniter, maxiter
+
+
+def set_pyccf(input_args):
+
+    default_kwargs = {
+        'interp': 2 + 1e-10,
+        'nsim': 3000,
+        'mcmode': 0,
+        'sigmode': 0.2,
+        'thres': 0.8,
+        'nbin': 50
+    }
+
+    params = { **default_kwargs, **input_args }
+
+    interp = params['interp']
+    nsim = params['nsim']
+    mcmode = params['mcmode']
+    sigmode = params['sigmode']
+    thres = params['thres']
+    nbin = params['nbin']
+
+    return interp, nsim, mcmode, sigmode, thres, nbin
+
+
+
+def set_pyzdcf(input_args, fnames):
+
+    default_kwargs = {
+        'nsim': 1000,
+        'minpts': 0,
+        'uniform_sampling': False,
+        'omit_zero_lags': True,
+        'sparse': 'auto',
+        'prefix': 'zdcf',
+        'run_plike': False,
+        'plike_dir': None,
+    }
+
+    params = { **default_kwargs, **input_args }
+
+    nsim = params['nsim']
+    minpts = params['minpts']
+    uniform_sampling = params['uniform_sampling']
+    omit_zero_lags = params['omit_zero_lags']
+    sparse = params['sparse']
+    prefix = params['prefix']
+    run_plike = params['run_plike']
+    plike_dir = params['plike_dir']
+
+
+    return nsim, minpts, uniform_sampling, omit_zero_lags, \
+        sparse, prefix, run_plike, plike_dir
+
+
+def set_pyroa(input_args, nlc):
+
+    default_kwargs = {
+        'nchain': 20000,
+        'nburn': 15000,
+        'init_tau': None,
+        'subtract_mean': True,
+        'div_mean': False,
+        'add_var': True,
+        'delay_dist': True,
+        'psi_types': 'Gaussian',
+        'together': True,
+        'objname': None,
+        'prior_func': None
+    }
+
+    params = { **default_kwargs, **input_args }
+
+    nchain = params['nchain']
+    nburn = params['nburn']
+    init_tau = params['init_tau']
+    subtract_mean = params['subtract_mean']
+    div_mean = params['div_mean']
+    add_var = params['add_var']
+    delay_dist = params['delay_dist']
+    psi_types = params['psi_types']
+    together = params['together']
+    objname = params['objname']
+
+    if init_tau is None:
+        init_tau = [10.] * (nlc-1)
+
+    if objname is None:
+        objname = 'pyroa'
+
+    if isinstance(psi_types, str):
+        psi_types = [psi_types] * (nlc-1)
+
+    if not together:
+        if isinstance(delay_dist, bool):
+            delay_dist = [delay_dist] * (nlc-1)
+        if isinstance(add_var, bool):
+            add_var = [add_var] * (nlc-1)
+
+
+    return nchain, nburn, init_tau, subtract_mean, div_mean, \
+            add_var, delay_dist, psi_types, together, objname
+
+
+
+def set_weighting(input_args):
+
+    default_kwargs = {
+        'gap_size': 30,
+        'k': 2,
+        'width': 15,
+        'rel_height': .99,
+        'zoom': True
+    }
+
+    params = { **default_kwargs, **input_args }
+
+    gap_size = params['gap_size']
+    k = params['k']
+    width = params['width']
+    rel_height = params['rel_height']
+    zoom = params['zoom']
+
+    return gap_size, k, width, rel_height, zoom
---
+++ packages/pypetal/pypetal-0.2.1.tar.gz/pypetal-0.2.1/src/pypetal/utils/petalio.py
@@ -0,0 +1,317 @@
+import os
+import re
+
+import astropy.units as u
+import numpy as np
+from astropy.io import fits
+from astropy.table import Table
+
+
+def make_directories(output_dir, fnames, line_names,
+                     run_drw_rej, run_pyccf, run_pyzdcf, run_pyroa,
+                     reject_data, together_pyroa):
+
+    #Create subdirectories for each line
+    for i in range(len(fnames)):
+        os.makedirs( output_dir + line_names[i], exist_ok=True )
+
+        if (run_drw_rej) & (reject_data[i]):
+            os.makedirs( output_dir + line_names[i] + '/drw_rej', exist_ok=True )
+
+    for i in range(len(fnames)-1):
+        if run_pyccf:
+            os.makedirs( output_dir + line_names[i+1] + '/pyccf', exist_ok=True )
+        if run_pyzdcf:
+            os.makedirs( output_dir + line_names[i+1] + '/pyzdcf', exist_ok=True )
+
+    if run_pyroa:
+        if together_pyroa:
+            os.makedirs( output_dir + 'pyroa', exist_ok=True )
+        else:
+            for i in range(len(fnames)-1):
+                os.makedirs( output_dir + line_names[i+1] + '/pyroa', exist_ok=True )
+
+
+    #Make subdirectories for light curves
+    os.makedirs( output_dir + 'light_curves/', exist_ok=True )
+
+    return
+
+
+
+
+
+
+def str2unit(x):
+
+    if (x == 'Arbitrary Units'):
+        unit = u.dimensionless_unscaled
+    else:
+        unit = u.Unit(x)
+
+    return unit
+
+
+#From https://stackoverflow.com/questions/5807952/removing-trailing-zeros-in-python/5808661#5808661
+def number_shaver(ch,
+                  regx = re.compile('(?<![\d.])0*(?:'
+                                    '(\d+)\.?|\.(0)'
+                                    '|(\.\d+?)|(\d+\.\d+?)'
+                                    ')0*(?![\d.])')  ,
+                  repl = lambda mat: mat.group(mat.lastindex)
+                                     if mat.lastindex!=3
+                                     else '0' + mat.group(3) ):
+    return regx.sub(repl,ch)
+
+
+def err2str(val, up_err, lo_err, dec=2):
+    val = number_shaver( str(round(val, dec)) )
+    up_err = number_shaver( str(round(up_err, dec)) )
+    lo_err = number_shaver( str(round(lo_err, dec)) )
+
+    return val + '^{+' + up_err + '}_{-' + lo_err + '}'
+
+
+def write_data(arr, fname, header=None):
+    arr = np.array(arr, dtype=object)
+
+    ndim = len( arr.shape )
+    assert ndim <= 2
+
+
+    if ndim == 2:
+        cols = len(arr)
+        rows = len(arr[0])
+
+        with open(fname, "w") as file:
+
+            if header is not None:
+                file.write(header + "\n")
+
+            for i in range(rows):
+
+                string = "{},".format(arr[0][i])
+                for j in range(1, cols-1):
+                    string += "{},".format(arr[j][i])
+
+                string += "{}\n".format(arr[-1][i])
+
+                file.write(string)
+
+    elif ndim == 1:
+        rows = len(arr)
+
+        with open(fname, "w") as file:
+
+            if header is not None:
+                file.write(header + "\n")
+
+            for i in range(rows):
+                file.write( "{}\n".format(arr[i]) )
+
+    return
+
+
+
+def write_weighting_summary(fname, res, run_pyccf, run_javelin, run_pyroa):
+
+    run_arr = [run_pyccf, run_javelin, run_pyroa]
+    names = ['pyccf', 'javelin', 'pyroa']
+    mod_ncols = 6
+
+    colnames = ['n0', 'peak_bounds', 'peak', 'lag', 'lag_err', 'frac_rejected']
+    colnames_tot = []
+    for name in names:
+        for col in colnames:
+            colnames_tot.append( col + '_' + name )
+
+    weighting_dat = []
+    weighting_dat.append( res['k'] )
+    for i, run in enumerate(run_arr):
+        if run:
+            for i in range( i*mod_ncols, (i+1)*mod_ncols ):
+                weighting_dat.append( res[ colnames_tot[i] ] )
+
+        else:
+            for i in range(mod_ncols):
+                if i in [1, 4]:
+                    weighting_dat.append( [np.nan, np.nan] )
+                else:
+                    weighting_dat.append( np.nan )
+
+    if run_pyccf:
+        weighting_dat.append( res['rmax_pyccf'] )
+
+        if run_javelin & run_pyroa:
+            weighting_dat.append( res['rmax_javelin'] )
+            weighting_dat.append( res['rmax_pyroa'] )
+        elif run_javelin:
+            weighting_dat.append( res['rmax_javelin'] )
+            weighting_dat.append( np.nan )
+        elif run_pyroa:
+            weighting_dat.append( np.nan )
+            weighting_dat.append( res['rmax_pyroa'] )
+        else:
+            weighting_dat.append( np.nan )
+            weighting_dat.append( np.nan )
+
+    else:
+        weighting_dat.append(np.nan)
+        weighting_dat.append(np.nan)
+        weighting_dat.append(np.nan)
+
+
+    tot_fits_cols = []
+
+    fits_colnames = []
+    fits_colnames.append('k')
+    for i in range(len(colnames_tot)):
+        fits_colnames.append( colnames_tot[i] )
+
+    fits_colnames.append('rmax_pyccf')
+    fits_colnames.append('rmax_javelin')
+    fits_colnames.append('rmax_pyroa')
+
+
+
+    col_fmts = []
+    col_fmts.append('E')
+    for _ in range(len(run_arr)):
+        for j in range(mod_ncols):
+            if j in [1,4]:
+                col_fmts.append('2E')
+            else:
+                col_fmts.append('E')
+
+    for _ in range(3):
+        col_fmts.append('E')
+
+
+    for i in range(len(weighting_dat)):
+        fits_col_i = fits.Column(name=fits_colnames[i], format=col_fmts[i], array=[ weighting_dat[i] ])
+        tot_fits_cols.append(fits_col_i)
+
+    table = fits.BinTableHDU.from_columns(tot_fits_cols)
+    table.writeto(fname, overwrite=True)
+
+    return
+
+
+def combine_weight_summary(filenames, output_fname, line_names=None):
+
+    if line_names is None:
+        line_names = []
+
+        for i in range(len(filenames)):
+            line_names.append('Line {}'.format(i+1))
+
+
+    #---------------------------
+    #Get header
+    hdr = fits.open(filenames[0]).header
+
+    #---------------------------
+    #Get data
+
+    k = np.zeros(len(filenames))
+
+    n0_pyccf = np.zeros(len(filenames))
+    peak_bounds_pyccf = np.zeros( ( len(filenames),2 ) )
+    peak_pyccf = np.zeros(len(filenames))
+    lag_pyccf = np.zeros(len(filenames))
+    lag_err_pyccf = np.zeros( ( len(filenames),2 ) )
+    frac_rejected_pyccf = np.zeros(len(filenames))
+
+    n0_javelin = np.zeros(len(filenames))
+    peak_bounds_javelin = np.zeros( (len(filenames),2) )
+    peak_javelin = np.zeros(len(filenames))
+    lag_javelin = np.zeros(len(filenames))
+    lag_err_javelin = np.zeros( (len(filenames),2) )
+    frac_rejected_javelin = np.zeros(len(filenames))
+
+    n0_pyroa = np.zeros(len(filenames))
+    peak_bounds_pyroa = np.zeros( (len(filenames),2) )
+    peak_pyroa = np.zeros(len(filenames))
+    lag_pyroa = np.zeros(len(filenames))
+    lag_err_pyroa = np.zeros( (len(filenames),2) )
+    frac_rejected_pyroa = np.zeros(len(filenames))
+
+    rmax_pyccf = np.zeros(len(filenames))
+    rmax_jav = np.zeros(len(filenames))
+    rmax_pyroa = np.zeros(len(filenames))
+
+
+    for i, fname in enumerate(filenames):
+        table = Table.read(fname)
+
+        k[i] = table['k'][0]
+
+        n0_pyccf[i] = table['n0_pyccf'][0]
+        peak_bounds_pyccf[i,:] = table['peak_bounds_pyccf'][0]
+        peak_pyccf[i] = table['peak_pyccf'][0]
+        lag_pyccf[i] = table['lag_pyccf'][0]
+        lag_err_pyccf[i,:] = table['lag_err_pyccf'][0]
+        frac_rejected_pyccf[i] = table['frac_rejected_pyccf'][0]
+
+        n0_javelin[i] = table['n0_javelin'][0]
+        peak_bounds_javelin[i,:] = table['peak_bounds_javelin'][0]
+        peak_javelin[i] = table['peak_javelin'][0]
+        lag_javelin[i] = table['lag_javelin'][0]
+        lag_err_javelin[i,:] = table['lag_err_javelin'][0]
+        frac_rejected_javelin[i] = table['frac_rejected_javelin'][0]
+
+        n0_pyroa[i] = table['n0_pyroa'][0]
+        peak_bounds_pyroa[i,:] = table['peak_bounds_pyroa'][0]
+        peak_pyroa[i] = table['peak_pyroa'][0]
+        lag_pyroa[i] = table['lag_pyroa'][0]
+        lag_err_pyroa[i,:] = table['lag_err_pyroa'][0]
+        frac_rejected_pyroa[i] = table['frac_rejected_pyroa'][0]
+
+        rmax_pyccf[i] = table['rmax_pyccf'][0]
+        rmax_jav[i] = table['rmax_javelin'][0]
+        rmax_pyroa[i] = table['rmax_pyroa'][0]
+
+    #---------------------------
+    #Make columns
+
+    name_col = fits.Column(name='name', format='20A', array=line_names)
+    k_col = fits.Column(name='k', format='E', array=k)
+
+    n0_pyccf_col = fits.Column(name='n0_pyccf', format='E', array=n0_pyccf)
+    peak_bounds_pyccf_col = fits.Column(name='peak_bounds_pyccf', format='2E', array=peak_bounds_pyccf)
+    peak_pyccf_col = fits.Column(name='peak_pyccf', format='E', array=peak_pyccf)
+    lag_pyccf_col = fits.Column(name='lag_pyccf', format='E', array=lag_pyccf)
+    lag_err_pyccf_col = fits.Column(name='lag_err_pyccf', format='2E', array=lag_err_pyccf)
+    frac_rejected_pyccf_col = fits.Column(name='frac_rejected_pyccf', format='E', array=frac_rejected_pyccf)
+
+    n0_javelin_col = fits.Column(name='n0_javelin', format='E', array=n0_javelin)
+    peak_bounds_javelin_col = fits.Column(name='peak_bounds_javelin', format='2E', array=peak_bounds_javelin)
+    peak_javelin_col = fits.Column(name='peak_javelin', format='E', array=peak_javelin)
+    lag_javelin_col = fits.Column(name='lag_javelin', format='E', array=lag_javelin)
+    lag_err_javelin_col = fits.Column(name='lag_err_javelin', format='2E', array=lag_err_javelin)
+    frac_rejected_javelin_col = fits.Column(name='frac_rejected_javelin', format='E', array=frac_rejected_javelin)
+
+    n0_pyroa_col = fits.Column(name='n0_pyroa', format='E', array=n0_pyroa)
+    peak_bounds_pyroa_col = fits.Column(name='peak_bounds_pyroa', format='2E', array=peak_bounds_pyroa)
+    peak_pyroa_col = fits.Column(name='peak_pyroa', format='E', array=peak_pyroa)
+    lag_pyroa_col = fits.Column(name='lag_pyroa', format='E', array=lag_pyroa)
+    lag_err_pyroa_col = fits.Column(name='lag_err_pyroa', format='2E', array=lag_err_pyroa)
+    frac_rejected_pyroa_col = fits.Column(name='frac_rejected_pyroa', format='E', array=frac_rejected_pyroa)
+
+    rmax_pyccf_col = fits.Column(name='rmax_pyccf', format='E', array=rmax_pyccf)
+    rmax_jav_col = fits.Column(name='rmax_javelin', format='E', array=rmax_jav)
+    rmax_pyroa_col = fits.Column(name='rmax_pyroa', format='E', array=rmax_pyroa)
+
+    cols = [name_col, k_col,
+            n0_pyccf_col, peak_bounds_pyccf_col, peak_pyccf_col, lag_pyccf_col, lag_err_pyccf_col, frac_rejected_pyccf_col,
+            n0_javelin_col, peak_bounds_javelin_col, peak_javelin_col, lag_javelin_col, lag_err_javelin_col, frac_rejected_javelin_col,
+            n0_pyroa_col, peak_bounds_pyroa_col, peak_pyroa_col, lag_pyroa_col, lag_err_pyroa_col, frac_rejected_pyroa_col,
+            rmax_pyccf_col, rmax_jav_col, rmax_pyroa_col]
+
+    #---------------------------
+    #Make table
+
+    table_hdu = fits.BinTableHDU.from_columns(cols, header=hdr)
+    table_hdu.writeto(output_fname, overwrite=True)
+
+    return
