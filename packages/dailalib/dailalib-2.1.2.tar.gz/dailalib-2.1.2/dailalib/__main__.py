9fe5b1ebdb8a5e88e129137ea7f665789f7d97b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 06:04:12 +0000

    Add package dailalib/2.1.2/dailalib-2.1.2.tar.gz

---
+++ packages/dailalib/dailalib-2.1.2.tar.gz/dailalib-2.1.2/dailalib/__main__.py
@@ -0,0 +1,39 @@
+import argparse
+
+from .installer import DAILAInstaller
+import dailalib
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="""
+            The DAILA CLI is used to install, run, and host the DAILA plugin.
+            """,
+        epilog="""
+            Examples:
+            daila install
+            """
+    )
+    parser.add_argument(
+        "-i", "--install", action="store_true", help="Install DAILA into your decompilers"
+    )
+    parser.add_argument(
+        "-s", "--server", help="Run a a headless server for DAILA", choices=["ghidra"]
+    )
+    parser.add_argument(
+        "-v", "--version", action="version", version=f"DAILA {dailalib.__version__}"
+    )
+    args = parser.parse_args()
+
+    if args.install:
+        DAILAInstaller().install()
+    elif args.server:
+        if args.server != "ghidra":
+            raise NotImplementedError("Only Ghidra is supported for now")
+
+        from dailalib import create_plugin
+        create_plugin(force_decompiler="ghidra")
+
+
+if __name__ == "__main__":
+    main()
