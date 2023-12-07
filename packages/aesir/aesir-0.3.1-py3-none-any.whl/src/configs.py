e4d5e1c8851aad86c360b31b110c7c98e8389da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:03:39 +0000

    Add package aesir/0.3.1/aesir-0.3.1-py3-none-any.whl

---
+++ packages/aesir/aesir-0.3.1-py3-none-any.whl/src/configs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3.8
+# coding:utf-8
+# Copyright (C) 2022-2023 All rights reserved.
+# FILENAME:    ~~/src/configs.py
+# VERSION: 	   0.3.1
+# CREATED: 	   2023-12-01 05:31
+# AUTHOR: 	   Sitt Guruvanich <aekasitt.g+github@siamintech.co.th>
+# DESCRIPTION:
+#
+# HISTORY:
+# *************************************************************
+
+### Standard packages ###
+from pathlib import Path
+from typing import Any, Dict, List, Optional
+
+### Standard packages ###
+from pydantic import TypeAdapter
+from yaml import Loader, load
+
+### Local modules ###
+from src.schemas import (
+    Build,
+    BuildEnum,
+    ClusterEnum,
+    ImageAlias,
+    ImageEnum,
+    PeripheralEnum,
+    Service,
+    ServiceName,
+)
+
+BUILDS: Dict[BuildEnum, Build]
+CLUSTERS: Dict[ClusterEnum, Dict[ServiceName, Service]]
+DEPRECATED: List[str]
+IMAGES: Dict[ImageEnum, Dict[ImageAlias, str]]
+NETWORK: str
+PERIPHERALS: Dict[PeripheralEnum, Dict[ServiceName, Service]]
+
+file_path: Path = Path(__file__).resolve()
+with open(str(file_path).replace("configs.py", "constants.yaml"), "rb") as stream:
+    constants: Optional[Dict[str, Any]] = load(stream, Loader=Loader)
+    if constants:
+        BUILDS = TypeAdapter(Dict[BuildEnum, Build]).validate_python(constants["builds"])
+        CLUSTERS = TypeAdapter(Dict[ClusterEnum, Dict[ServiceName, Service]]).validate_python(
+            constants["clusters"]
+        )
+        DEPRECATED = constants.get("deprecated", [])
+        IMAGES = TypeAdapter(Dict[ImageEnum, Dict[ImageAlias, str]]).validate_python(
+            constants["images"]
+        )
+        NETWORK = constants.get("network", "aesir")
+        PERIPHERALS = TypeAdapter(Dict[PeripheralEnum, Dict[ServiceName, Service]]).validate_python(
+            constants["peripherals"]
+        )
+
+__all__ = ["BUILDS", "CLUSTERS", "DEPRECATED", "IMAGES", "NETWORK", "PERIPHERALS"]
