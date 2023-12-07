fa60f1b8213a21aa930fd612547ce2c39e6f8ef2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 14:03:07 +0000

    Add package citation-rabbot/0.6/citation-rabbot-0.6.tar.gz

---
+++ packages/citation-rabbot/citation-rabbot-0.6.tar.gz/citation-rabbot-0.6/citation_rabbot/jump.py
@@ -0,0 +1,23 @@
+from typing import Callable, Tuple, Dict, List
+from neo4j import Session, Result
+from telegram import Update
+from telegram.ext import Application, ContextTypes, CommandHandler
+
+
+class Rabbot:
+    def __init__(self, bot: Application, session: Session) -> None:
+        self.bot = bot
+        self.session = session
+
+    def add_jump(self, name: str, message2querys: Callable[[str], List[Tuple[str, Dict]]], results2message: Callable[[List[Result]], str]):
+        async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+            text = update.message
+            querys = message2querys(text)
+            results = []
+            for query, kwargs in querys:
+                results.append(self.session.execute_read(lambda tx: tx.run(query, **kwargs).values()))
+            message = results2message(results)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+        command_handler = CommandHandler(name, handler)
+        self.bot.add_handler(command_handler)
