efeb2834b594fc175e4e17ddbda8ac1ac777b870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:03:49 +0000

    Add package freerec/0.5.1/freerec-0.5.1.tar.gz

---
+++ packages/freerec/freerec-0.5.1.tar.gz/freerec-0.5.1/freerec/criterions.py
@@ -0,0 +1,130 @@
+
+
+from typing import Iterable, Union
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+__all__ = [
+    "BaseCriterion", 
+    "CrossEntropy4Logits", "KLDivLoss4Logits",
+    "BPRLoss", "BCELoss4Logits",
+    "MSELoss", "L1Loss"
+]
+
+
+class BaseCriterion(nn.Module):
+    r"""
+    Criterion.
+
+    Parameters:
+    -----------
+    reduction : str, default 'mean'
+        Reduction method. Choices are 'none', 'sum', and 'mean'.
+
+    Attributes:
+    -----------
+    reduction : str
+        Reduction method.
+
+    Methods:
+    --------
+    regularize(params: Union[torch.Tensor, Iterable[torch.Tensor]], rtype: str = 'l2') -> torch.Tensor
+        Regularizes the given parameters with the specified regularization method.
+
+    """
+
+    def __init__(self, reduction: str = 'mean') -> None:
+        super().__init__()
+        self.reduction = reduction
+
+    @staticmethod
+    def regularize(params: Union[torch.Tensor, Iterable[torch.Tensor]], rtype: str = 'l2'):
+        r"""
+        Add regularization for given parameters.
+
+        Parameters:
+        -----------
+        params : Union[torch.Tensor, Iterable[torch.Tensor]]
+            List of parameters for regularization.
+        rtype : str, default 'l2'
+            The type of regularization to use. Options include 'l1' and 'l2'.
+
+        Returns:
+        --------
+        reg_loss : torch.Tensor
+            The regularization loss tensor.
+        """
+        params = [params] if isinstance(params, torch.Tensor) else params
+        if rtype == 'l1':
+            return sum(param.abs().sum() for param in params)
+        elif rtype == 'l2':
+            return sum(param.pow(2).sum() for param in params) / 2
+        else:
+            raise NotImplementedError(f"{rtype} regularization is not supported ...")
+
+
+class CrossEntropy4Logits(BaseCriterion):
+    """Cross entropy loss with logits."""
+
+    def forward(self, logits: torch.Tensor, targets: torch.Tensor):
+        return F.cross_entropy(logits, targets, reduction=self.reduction)
+
+
+class BCELoss4Logits(BaseCriterion):
+    """Binary Cross Entropy with logits !!!"""
+
+    def forward(self, logits: torch.Tensor, targets: torch.Tensor):
+        return F.binary_cross_entropy_with_logits(logits, targets.to(logits.dtype), reduction=self.reduction)
+
+
+class KLDivLoss4Logits(BaseCriterion):
+    """KLDivLoss with logits"""
+
+    def __init__(self, reduction: str = 'batchmean') -> None:
+        super().__init__(reduction)
+
+    def forward(self, logits: torch.Tensor, targets: torch.Tensor):
+        assert logits.size() == targets.size()
+        inputs = F.log_softmax(logits, dim=-1)
+        targets = F.softmax(targets, dim=-1)
+        return F.kl_div(inputs, targets, reduction=self.reduction)
+        
+
+class BPRLoss(BaseCriterion):
+
+    def forward(self, pos_scores: torch.Tensor, neg_scores: torch.Tensor):
+        r"""
+        Parameters:
+        -----------
+        pos_scores: torch.Tensor
+            Positive scores
+        neg_scores: torch.Tensor
+            Negative scores
+        
+        Returns:
+        --------
+        loss: torch.Tensor
+        """
+        loss = F.softplus(neg_scores - pos_scores)
+        if self.reduction == 'none':
+            return loss
+        elif self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+
+
+class MSELoss(BaseCriterion):
+    """Mean Square Loss."""
+
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
+        return F.mse_loss(inputs, targets, reduction=self.reduction)
+
+
+class L1Loss(BaseCriterion):
+    """L1 Loss."""
+    def forward(self, inputs: torch.Tensor, targets: torch.Tensor):
+        return F.l1_loss(inputs, targets, reduction=self.reduction)
---
+++ packages/freerec/freerec-0.5.1.tar.gz/freerec-0.5.1/freerec/models/base.py
@@ -0,0 +1,180 @@
+
+
+from typing import Optional, Union, overload, Tuple, Dict
+
+import torch, inspect
+import torch.nn as nn
+
+
+__all__ = ['RecSysArch']
+
+
+class RecSysArch(nn.Module):
+    r"""
+    A PyTorch Module for recommendation system architecture.
+    This module contains methods for broadcasting tensors and initializing the module parameters.
+
+    Parameters:
+    -----------
+    nn.Module : PyTorch Module
+        The base class of this module.
+
+    Methods:
+    --------
+    to(device, dtype, non_blocking)
+        Moves and/or casts the parameters and buffers.
+
+    initialize()
+        Initializes the module parameters.
+
+    broadcast(*tensors)
+        Broadcasts the given tensors according to broadcasting semantics.
+
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+        self._check()
+
+    @classmethod
+    def _check(cls):
+        signature = inspect.signature(cls.recommend_from_pool)
+        if 'pool' not in signature.parameters:
+            raise NotImplementedError(
+                f"`pool` must be a argument of `recommend_from_pool` ..."
+            )
+
+    def to(
+        self, device: Optional[Union[int, torch.device]] = None,
+        dtype: Optional[Union[torch.dtype, str]] = None,
+        non_blocking: bool = False
+    ):
+        r"""
+        Moves and/or casts the parameters and buffers.
+
+        Parameters:
+        -----------
+        device : Union[int, torch.device], optional
+            The destination device of the parameters and buffers, by default None.
+        dtype : Union[torch.dtype, str], optional
+            The desired data type of the parameters and buffers, by default None.
+        non_blocking : bool, optional
+            Whether the copy should be asynchronous or not, by default False.
+
+        Returns:
+        --------
+        nn.Module
+            The module with parameters and buffers moved and/or cast.
+        """
+        if device:
+            self.device = device
+        return super().to(device, dtype, non_blocking)
+
+    def reset_parameters(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0.)
+            elif isinstance(m, nn.Embedding):
+                nn.init.xavier_normal_(m.weight)
+            elif isinstance(m, nn.GRU):
+                nn.init.xavier_uniform_(m.weight_hh_l0)
+                nn.init.xavier_uniform_(m.weight_ih_l0)
+            elif isinstance(m, (nn.BatchNorm1d, nn.BatchNorm2d)):
+                nn.init.constant_(m.weight, 1.)
+                nn.init.constant_(m.bias, 0.)
+
+    @staticmethod
+    def broadcast(*tensors: torch.Tensor):
+        r"""
+        Broadcasts the given tensors according to Broadcasting semantics.
+        See [here](https://pytorch.org/docs/stable/generated/torch.broadcast_tensors.html#torch.broadcast_tensors) for details.
+
+        Parameters:
+        -----------
+        tensors : torch.Tensor
+            The tensors to broadcast.
+
+        Returns:
+        --------
+        Tuple[torch.Tensor, ...]
+            The broadcasted tensors.
+
+        Examples:
+        ---------
+        >>> users = torch.rand(4, 1, 4)
+        >>> items = torch.rand(4, 2, 1)
+        >>> users, items = RecSysArch.broadcast(users, items)
+        >>> users.size()
+        torch.Size(4, 2, 4)
+        >>> items.size()
+        torch.Size(4, 2, 4)
+        """
+        return torch.broadcast_tensors(*tensors)
+
+    @overload
+    def recommend_from_full(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        r"""
+        Returns:
+        --------
+        user features: torch.Tensor
+        item features: torch.Tensor
+        """
+
+    @overload
+    def recommend_from_full(self, seqs: torch.Tensor) -> torch.Tensor:
+        r"""
+        Full ranking recommendation.
+
+        Parameters:
+        -----------
+        seqs: torch.Tensor, (B, S)
+            A batch of sequences.
+        
+        Returns:
+        --------
+        scores: torch.Tensor, (B, N)
+            `N' denotes the number of items.
+        """
+
+    @overload
+    def recommend_from_pool(self, seqs: torch.Tensor, pool: torch.Tensor) -> torch.Tensor:
+        r"""
+        Sampling-based recommendation.
+
+        Parameters:
+        -----------
+        seqs: torch.Tensor, (B, S)
+            A batch of sequences.
+        pool: torch.Tensor, (B, K)
+            A batch of items.
+
+        Returns:
+        --------
+        scores: torch.Tensor, (B, K)
+        """
+
+    def recommend_from_full(self, **kwargs):
+        raise NotImplementedError()
+
+    def recommend_from_pool(self, *, pool: torch.Tensor, **kwargs):
+        raise NotImplementedError()
+
+    def _kwargs4full(self, kwargs: Dict):
+        parameters = inspect.signature(self.recommend_from_full).parameters
+        if any(param.kind == param.VAR_KEYWORD for param in parameters.values()):
+            return kwargs
+        return {key: kwargs[key] for key in parameters}
+
+    def _kwargs4pool(self, kwargs: Dict):
+        parameters = inspect.signature(self.recommend_from_pool).parameters
+        if any(param.kind == param.VAR_KEYWORD for param in parameters.values()):
+            return kwargs
+        return {key: kwargs[key] for key in parameters}
+
+    def recommend(self, **kwargs):
+        if kwargs.get('pool', None) is None:
+            return self.recommend_from_full(**self._kwargs4full(kwargs))
+        else:
+            return self.recommend_from_pool(**self._kwargs4pool(kwargs))
