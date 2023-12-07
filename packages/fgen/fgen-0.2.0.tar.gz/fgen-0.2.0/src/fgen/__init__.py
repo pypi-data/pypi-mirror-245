9a39e3ebd5872defdb2a64c8f52422ccfcedc9bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 04:04:02 +0000

    Add package fgen/0.2.0/fgen-0.2.0.tar.gz

---
+++ packages/fgen/fgen-0.2.0.tar.gz/fgen-0.2.0/src/fgen/__init__.py
@@ -0,0 +1,10 @@
+"""
+An opinionated framework for wrapping Fortran-based code with Python.
+"""
+from importlib.metadata import version as _version
+
+try:
+    __version__ = _version("fgen")
+except Exception:  # pylint: disable=broad-except  # pragma: no cover
+    # Local copy, not installed with setuptools
+    __version__ = "unknown"
