8674db96558b57cecc44ae971c5df4018b683055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:03:48 +0000

    Add package synapse-selector/0.1.16b9/synapse_selector-0.1.16b9.tar.gz

---
+++ packages/synapse-selector/synapse_selector-0.1.16b9.tar.gz/synapse_selector-0.1.16b9/synapse_selector/utils/pytorch_models.py
@@ -0,0 +1,28 @@
+import torch
+import numpy as np
+
+from synapse_selector.utils.iglu_cnn import PeakDetectionModel_convout
+
+
+class torch_cnn_model:
+    def __init__(
+        self,
+        model_path: str = "./models/peak_detection-conv_output.pt",
+        window_len: int = 50,
+    ) -> None:
+        model = PeakDetectionModel_convout()
+        model.load_state_dict(torch.load(model_path))
+        model.eval()
+
+        self.model = model
+        self.window_len = window_len
+        self.to_pad = 0
+
+    def predict(self, arr: np.ndarray, threshold: float = 0.5) -> list[int]:
+        # reshape input to (1, 1, length)
+        input_tensor = torch.reshape(
+            torch.tensor(arr, dtype=torch.float), (1, 1, arr.shape[0])
+        )
+        preds = np.array(self.model(input_tensor).detach().flatten())
+        infered_peaks = list(np.argwhere(preds > threshold).flatten())
+        return infered_peaks
