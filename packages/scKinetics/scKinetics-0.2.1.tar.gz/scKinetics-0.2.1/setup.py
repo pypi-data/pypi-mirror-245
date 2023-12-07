f0cfb529a65db98aeb1a1bacd4a5fe7693d13393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:04:48 +0000

    Add package scKinetics/0.2.1/scKinetics-0.2.1.tar.gz

---
+++ packages/scKinetics/scKinetics-0.2.1.tar.gz/scKinetics-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'scKinetics',
+    version = '0.2.1',
+    description = 'Biological prior guided single-cell kinetics inference.',
+    author = 'Yanshuo Chen',
+    author_email = 'poseidonchan@icloud.com',
+    url = 'https://github.com/poseidonchan/scKinetics',
+    license = 'GPL-3.0 license',
+    packages = find_packages(),
+    python_requires='>=3.9',
+    platforms = 'any',
+    install_requires = [
+        'diffrax',
+        'jax >= 0.4.13',
+        'equinox >= 0.10.11',
+        'optax',
+        'anndata>=0.7.6',
+        'tqdm',
+        'matplotlib',
+        'seaborn',
+        'numpy',
+    ],
+)
