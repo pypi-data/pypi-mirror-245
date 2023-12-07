a31d7db49e51f88cbda44c0e7bdce4362d64f9b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:03:29 +0000

    Add package mara-cli/0.3.0/mara-cli-0.3.0.tar.gz

---
+++ packages/mara-cli/mara-cli-0.3.0.tar.gz/mara-cli-0.3.0/tests/test_mara_main.py
@@ -0,0 +1,11 @@
+from mara_cli.cli import cli
+import re
+
+
+def test_without_argument(cli_runner):
+
+    result = cli_runner.invoke(cli)
+    assert result.exit_code == 0
+    # here we get the name as 'cli' instead of 'mara'
+    assert 'Usage: cli [OPTIONS] COMMAND [ARGS]' in result.output
+    assert re.search(r'--debug\s+Show debug output',result.output) is not None
