dc5452b865d4b1eb6ebb121ffa7141f033033e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:32 +0000

    Add package enzope/0.0.9/enzope-0.0.9-py3-none-any.whl

---
+++ packages/enzope/enzope-0.0.9-py3-none-any.whl/enzope/graphs/graph_class.py
@@ -0,0 +1,200 @@
+import os
+
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+from IPython.display import display
+
+from .custom_gtg import geographical_threshold_graph_custom
+
+# ---------------------- #
+#        Classes         #
+# ---------------------- #
+
+
+class BaseGraph:
+    def __init__(
+        self,
+        n_nodes,
+        upd_w=np.inf,
+        upd_graph=np.inf,
+        upd_plot=np.inf,
+        figpath=os.getcwd(),
+        plotable=False,
+    ):
+        self.n_nodes = n_nodes
+        self.upd_w = upd_w
+        self.upd_graph = upd_graph
+        self.upd_plot = upd_plot
+        self.G = None
+
+        if plotable:
+            self.fig, self.ax = plt.subplots(dpi=150)
+            self.ax.set_xlim([-0.03, 1.03])
+            self.ax.set_ylim([-0.03, 1.03])
+            plt.close(self.fig)
+
+        if figpath is not None:
+            self.figpath = figpath
+            self.temppath = os.path.join(self.figpath, "temp")
+
+    def plot_snapshot(self, *args, **kwargs):
+        pass
+
+    def update_weights(self, *args, **kwargs):
+        pass
+
+    def update_graph(self, *args, **kwargs):
+        pass
+
+    def get_opponents_cpu(self, *args, **kwargs):
+        pass
+
+    def get_mean_connectivity(self):
+        pass
+
+    def get_average_distance(self):
+        pass
+
+
+class GTG(BaseGraph):
+    def __init__(
+        self,
+        n_nodes,
+        theta,
+        metric=None,
+        join="add",
+        w0=None,
+        posi=None,
+        seed=None,
+        p_dist=None,
+        **kwargs,
+    ):
+        super().__init__(n_nodes, **kwargs)
+        self.theta = theta
+        self.metric = metric
+        self.posi = posi
+        self.join = join
+        self.seed = seed
+
+        self.G = geographical_threshold_graph_custom(
+            n_nodes,
+            theta,
+            dim=2,
+            pos=posi,
+            weight=w0,
+            metric=metric,
+            p_dist=p_dist,
+            seed=seed,
+            join=join,
+        )
+
+        if self.posi is None:
+            self.posi = [self.G.nodes[i]["pos"] for i in range(n_nodes)]
+
+        self.w = dict(enumerate(self.G.nodes[i]["weight"] for i in range(self.n_nodes)))
+
+    def __call__(self, *args, **kwds):
+        return self.G
+
+    # Modificar la funcion para que o bien guarde en temp o bien muestre en pantalla
+    def plot_snapshot(
+        self, w_min=1e-17, new_w=None, mcs=None, mode="show", *args, **kwargs
+    ):
+        # Esto se tiene que escribir mejor
+        if new_w is not None:
+            self.update_weights(new_w)
+
+        a = np.array(list(self.w.values()))
+
+        dead_nodes = [node for node, weight in self.w.items() if weight < w_min]
+
+        node_size = 25 * (a)
+        node_colors = ["r" if n in dead_nodes else "royalblue" for n in self.G.nodes]
+        edge_colors = [
+            "r" if (e[0] in dead_nodes or e[1] in dead_nodes) else "black"
+            for e in self.G.edges
+        ]
+
+        self.ax.clear()
+        if mcs is not None:
+            self.ax.set_title(f"t = {mcs}")
+        nx.draw(
+            self.G,
+            node_size=node_size,
+            width=0.2,
+            pos=self.posi,
+            node_color=node_colors,
+            edge_color=edge_colors,
+            ax=self.ax,
+        )
+
+        if mode == "save":
+            if mcs:
+                filename = os.path.join(self.temppath, "test_{:05d}.png".format(mcs))
+            else:
+                filename = os.path.join(self.temppath, "test.png")
+            self.fig.savefig(filename, format="PNG")
+
+        if mode == "show":
+            display(self.fig)
+
+    def update_weights(self, wealths, *args, **kwargs):
+        self.w = dict(enumerate(wealths))
+        nx.set_node_attributes(self.G, self.w, "weight")
+
+    def update_graph(self, *args, **kwargs):
+        self.G = geographical_threshold_graph_custom(
+            self.n_nodes,
+            self.theta,
+            dim=2,
+            pos=self.posi,
+            weight=self.w,
+            metric=None,
+            p_dist=None,
+            seed=self.seed,
+            join=self.join,
+        )
+
+    def get_opponents_cpu(self, *args, **kwargs):
+        opponents = np.full(self.n_nodes, fill_value=-1)
+        for i in range(self.n_nodes):
+            if neighbors := list(nx.all_neighbors(self.G, i)):
+                opponents[i] = np.random.choice(neighbors)
+
+        return opponents
+
+    # Function inspired by cuTradeNet's toLL() function in:
+    # https://github.com/Qsanti/cuTradeNet/blob/e20f29e65bcac448d7fcd17fac45746f90e8538e/cuTradeNet/Models/Utils/GraphManager.py
+    def get_neighbors_array_gpu(self, *args, **kwargs):
+        """ """
+        neighs = [(list(self.G.neighbors(i))) for i in range(self.n_nodes)]
+        n_neighs = [0] + [len(x) for x in neighs]
+        n_neighs = np.array(n_neighs, dtype=np.int32)
+        # c_neighs has N+1 components, first is 0.
+        c_neighs = np.cumsum(n_neighs)
+        neighs = np.hstack(neighs).astype(np.int32)
+        return c_neighs, neighs
+
+    # Some networkx functions for comfort
+    def get_mean_connectivity(self):
+        return np.mean(list(dict(nx.degree(self.G)).values()))
+
+    def get_average_distance(self):
+        Gcc = sorted(nx.connected_components(self.G), key=len, reverse=True)
+        G0 = self.G.subgraph(Gcc[0])
+        return nx.average_shortest_path_length(G0)
+    
+    def get_clustering(self):
+        return nx.average_clustering(self.G)
+    
+    def get_assortativity(self):
+        return nx.degree_assortativity_coefficient(self.G)
+
+
+# Acá iría la clase del multigraph
+
+
+# ---------------------- #
+#       Funciones        #
+# ---------------------- #
---
+++ packages/enzope/enzope-0.0.9-py3-none-any.whl/enzope/metrics/measures.py
@@ -0,0 +1,39 @@
+import networkx as nx
+import numpy as np
+
+
+def gini(w):
+    n = len(w)
+    w_sorted = np.sort(w)
+    p_cumsum = np.cumsum(w_sorted) / np.sum(w_sorted)
+    B = np.sum(p_cumsum) / n
+    return 1 + 1 / n - 2 * B
+
+
+def liquidity(w, w_old):
+    n = len(w)
+    return np.sum(np.abs(w - w_old)) / (2 * n)
+
+
+def num_actives(w, w_min):
+    return np.mean(w > w_min)
+
+
+def num_frozen(w, w_min, graph):
+    n = len(w)
+    n_frozen = 0
+    actives = [i for i in range(n) if w[i] > w_min]
+    for i in actives:
+        is_frozen = 1
+        neighbors = list(nx.all_neighbors(graph.G, i))
+        for neigh in neighbors:
+            if w[neigh] > w_min:
+                is_frozen = 0
+        n_frozen += is_frozen
+    return n_frozen / n
+
+def r1(x):
+    return 1/x
+
+# def movility(w, w_old):
+#     return np.mean(np.abs(w - w_old) > 0)
