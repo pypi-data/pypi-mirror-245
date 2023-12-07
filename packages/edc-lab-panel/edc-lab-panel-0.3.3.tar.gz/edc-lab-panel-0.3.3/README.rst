a02a379440c6b0e0f813538a40582a52634b7a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 04:04:02 +0000

    Add package edc-lab-panel/0.3.3/edc-lab-panel-0.3.3.tar.gz

---
+++ packages/edc-lab-panel/edc-lab-panel-0.3.3.tar.gz/edc-lab-panel-0.3.3/README.rst
@@ -0,0 +1,13 @@
+edc-lab-panel
+-------------
+
+A collection of common lab panels used in edc deployments
+
+* FBC
+* RFT
+* LFT
+* LIPIDS
+* HbA1c
+* Glucose
+
+Also included are normal and graded ranges for the test results in these panels.
---
+++ packages/edc-lab-panel/edc-lab-panel-0.3.3.tar.gz/edc-lab-panel-0.3.3/runtests.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import logging
+import sys
+from os.path import abspath, dirname, join
+
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+
+base_dir = dirname(abspath(__file__))
+app_name = "edc_lab_panel"
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=join(base_dir, app_name, "tests", "etc"),
+    SUBJECT_SCREENING_MODEL="edc_metadata.subjectscreening",
+    SUBJECT_CONSENT_MODEL="edc_metadata.subjectconsent",
+    SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
+    SUBJECT_VISIT_MISSED_MODEL="edc_metadata.subjectvisitmissed",
+    LIST_MODEL_APP_LABEL="edc_transfer",
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.sessions",
+        "django.contrib.messages",
+        "django.contrib.staticfiles",
+        "django.contrib.sites",
+        "django_crypto_fields.apps.AppConfig",
+        "django_revision.apps.AppConfig",
+        "multisite",
+        "edc_appointment.apps.AppConfig",
+        "edc_action_item.apps.AppConfig",
+        "edc_crf.apps.AppConfig",
+        # "edc_device.apps.AppConfig",
+        # "edc_dashboard.apps.AppConfig",
+        # "edc_facility.apps.AppConfig",
+        "edc_lab.apps.AppConfig",
+        "edc_metadata.apps.AppConfig",
+        # "edc_offstudy.apps.AppConfig",
+        "edc_registration.apps.AppConfig",
+        "edc_identifier.apps.AppConfig",
+        "edc_notification.apps.AppConfig",
+        "edc_sites.apps.AppConfig",
+        # "edc_timepoint.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
+        "edc_visit_tracking.apps.AppConfig",
+        "edc_lab_results.apps.AppConfig",
+    ],
+    RANDOMIZATION_LIST_PATH=join(base_dir, app_name, "tests", "test_randomization_list.csv"),
+    add_dashboard_middleware=True,
+    use_test_urls=True,
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
+    failures = DiscoverRunner(failfast=False, tags=tags).run_tests([f"{app_name}.tests"])
+    sys.exit(failures)
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
