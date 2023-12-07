f951e50e26b4770cce7d51b52848c9884c3ef3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:04:15 +0000

    Add package renumics-spotlight/1.5.14/renumics_spotlight-1.5.14-py3-none-any.whl

---
+++ packages/renumics-spotlight/renumics_spotlight-1.5.14-py3-none-any.whl/renumics/spotlight/backend/statics/widgets/MetricsWidget/types.d.ts
@@ -0,0 +1,6 @@
+import { DataKind } from '../../datatypes';
+export type ValueArray = number[] | Int32Array | boolean[];
+export interface Metric {
+    signature: Record<string, DataKind | DataKind[]>;
+    compute: (values: ValueArray[]) => number;
+}
