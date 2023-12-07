d8add0e450c2735e1bb809233ba73546f27394b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:03:48 +0000

    Add package qlispc/1.0.0/qlispc-1.0.0-cp310-cp310-win_amd64.whl

---
+++ packages/qlispc/qlispc-1.0.0-cp310-cp310-win_amd64.whl/qlispc/__init__.py
@@ -0,0 +1,4 @@
+from .matricies import (CR, CX, CZ, SWAP, H, S, Sdag, SQiSWAP, T, Tdag, U,
+                        fSim, iSWAP, make_immutable, rfUnitary, sigmaI, sigmaX,
+                        sigmaY, sigmaZ, synchronize_global_phase)
+from .simple import applySeq, regesterGateMatrix, seq2mat
