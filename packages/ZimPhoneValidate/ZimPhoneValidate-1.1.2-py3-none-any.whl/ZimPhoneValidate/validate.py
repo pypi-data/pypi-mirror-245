3ca0d97e5029ae7f2a9fc13c5b9ad8f94306d0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:03:22 +0000

    Add package ZimPhoneValidate/1.1.2/ZimPhoneValidate-1.1.2-py3-none-any.whl

---
+++ packages/ZimPhoneValidate/ZimPhoneValidate-1.1.2-py3-none-any.whl/ZimPhoneValidate/validate.py
@@ -0,0 +1,36 @@
+import re
+
+
+def number_is_valid(number):
+		
+	'''
+	Returns True if number is a valid zim phone number with country code
+	 number: 263773738925
+	 '''
+	if re.search(r'^263(?:\s?)?(\d{3})(?:\s?)?(\d{3})(?:\s?)(\d{3})$', number):
+			
+		return True
+			
+	else: 
+			
+		return False
+	
+ 	
+	
+class example:
+
+	def show():
+
+		eg='''
+		from ZimPhoneValidate.validate import is_number_valid
+		
+		number = "263778040497"
+
+		if number_is_valid(number):
+			print("Number is Valid")
+		else:
+			print("Not a valid number")
+
+		'''
+
+		print(eg)
