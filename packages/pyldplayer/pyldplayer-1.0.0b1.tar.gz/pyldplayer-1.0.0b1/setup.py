93aeb8410034f5d06f6eebe00e3526d447179b7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 14:03:20 +0000

    Add package pyldplayer/1.0.0b1/pyldplayer-1.0.0b1.tar.gz

---
+++ packages/pyldplayer/pyldplayer-1.0.0b1.tar.gz/pyldplayer-1.0.0b1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+# walk file in pyldplayer
+folders = []
+for root, dirs, files in os.walk("pyldplayer"):
+    for dir in dirs:
+        if dir == "__pycache__":
+            continue
+        folders.append(os.path.join(root, dir))
+
+
+setup(
+    name="pyldplayer",
+    version="1.0.0b1",
+    author="Zackary W",
+    description="python wrapper for ldplayer",
+    packages=folders,
+    install_requires=[
+        "psutil",
+        "pydantic",
+        "pygetwindow",
+        "pyautogui"
+    ],
+    python_requires=">=3.6",
+    long_description=open("README_PYPI.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZackaryW/pyldplayer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
