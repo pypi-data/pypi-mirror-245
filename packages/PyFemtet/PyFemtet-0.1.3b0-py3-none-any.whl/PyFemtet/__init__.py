d4fc3efc89cb8b18832d19ac398d0d984e6b2c10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:03:59 +0000

    Add package PyFemtet/0.1.3b0/PyFemtet-0.1.3b0-py3-none-any.whl

---
+++ packages/PyFemtet/PyFemtet-0.1.3b0-py3-none-any.whl/PyFemtet/__init__.py
@@ -0,0 +1,9 @@
+from ._version import __version__
+
+__copyright__    = 'Copyright (C) 2023 Kazuma Naito'
+__license__      = 'GPLv3'
+__author__       = 'Kazuma Naito'
+__author_email__ = 'kazuma.naito@murata.com'
+__url__          = ''
+
+__all__ = ['公開モジュール', ...]
