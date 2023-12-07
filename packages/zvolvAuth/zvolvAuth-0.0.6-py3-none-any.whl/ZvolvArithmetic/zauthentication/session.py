4e4dd27678dc82dca0a404543b4255b7b6725795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:04:15 +0000

    Add package zvolvAuth/0.0.6/zvolvAuth-0.0.6-py3-none-any.whl

---
+++ packages/zvolvAuth/zvolvAuth-0.0.6-py3-none-any.whl/ZvolvArithmetic/zauthentication/session.py
@@ -0,0 +1,38 @@
+import copy
+import os
+from ZvolvArithmetic.zauthentication.zsession import generate_request_headers
+
+class Session:
+    """
+    A session stores configuration state and allows you to create service
+
+    :type zvolv_access_key_id: string
+    :param zvolv_access_key_id: Email access key ID
+    :type zvolv_secret_access_key: string
+    :param zvolv_secret_access_key: password secret access key
+    :type zvolv_domain_context: string
+    :param zvolv_domain_context: zvolv business domain
+    """
+
+    def __init__(
+        self,
+        zvolv_access_key_id=None,
+        zvolv_secret_access_key=None,
+        zvolv_domain_context =None,
+    ):
+        self.zvolv_access_key_id = zvolv_access_key_id
+        self.zvolv_secret_access_key = zvolv_secret_access_key
+        self.zvolv_domain_context = zvolv_domain_context
+
+        # if zvolv_access_key_id or zvolv_secret_access_key or zvolv_domain_context:
+        #     self.set_credentials(
+        #        zvolv_access_key_id, zvolv_secret_access_key, zvolv_domain_context,
+        #     )
+    
+    def set_credentials(self,zvolv_access_key_id, zvolv_secret_access_key, zvolv_domain_context):
+        return generate_request_headers(zvolv_access_key_id, zvolv_secret_access_key, zvolv_domain_context)
+
+    def __repr__(self):
+        return '{}'.format(
+            repr(self.set_credentials(self.zvolv_access_key_id, self.zvolv_secret_access_key, self.zvolv_domain_context)),
+        )
