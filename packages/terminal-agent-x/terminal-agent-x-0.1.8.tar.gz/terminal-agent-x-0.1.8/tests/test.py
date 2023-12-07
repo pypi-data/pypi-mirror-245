aaaca00e67382625f71f312777b5bf4b46a3d72a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:03:51 +0000

    Add package terminal-agent-x/0.1.8/terminal-agent-x-0.1.8.tar.gz

---
+++ packages/terminal-agent-x/terminal-agent-x-0.1.8.tar.gz/terminal-agent-x-0.1.8/tests/test.py
@@ -0,0 +1,28 @@
+# curl https://api.openai.com/v1/chat/completions \
+#   -H "Content-Type: application/json" \
+#   -H "Authorization: Bearer $OPENAI_API_KEY" \
+#   -d '{
+#     "model": "gpt-4-vision-preview",
+#     "messages": [
+#       {
+#         "role": "user",
+#         "content": [
+#           {
+#             "type": "text",
+#             "text": "What’s in this image?"
+#           },
+#           {
+#             "type": "image_url",
+#             "image_url": {
+#               "url": "URL"
+#             }
+#           }
+#         ]
+#       }
+#     ],
+#     "max_tokens": 300
+#   }'
+
+
+
+def ask_gpt4_vision(prompt, image_url):
