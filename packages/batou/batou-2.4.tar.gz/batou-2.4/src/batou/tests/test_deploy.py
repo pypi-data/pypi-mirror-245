43a1fce4900232c2ed58fd7c8ad6de445534a844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:04:21 +0000

    Add package batou/2.4/batou-2.4.tar.gz

---
+++ packages/batou/batou-2.4.tar.gz/batou-2.4/src/batou/tests/test_deploy.py
@@ -0,0 +1,62 @@
+import os
+
+import pytest
+
+from batou.tests.ellipsis import Ellipsis
+
+
+def test_main_with_errors(capsys):
+    os.chdir("examples/errors")
+
+    from batou.deploy import main
+
+    with pytest.raises(SystemExit) as r:
+        main(
+            environment="errors",
+            platform=None,
+            timeout=None,
+            dirty=False,
+            consistency_only=False,
+            predict_only=False,
+            jobs=None,
+            provision_rebuild=False,
+        )
+
+    assert r.value.code == 1
+
+    out, err = capsys.readouterr()
+    assert err == ""
+    assert out == Ellipsis(
+        """\
+batou/2... (cpython 3...)
+================================== Preparing ===================================
+main: Loading environment `errors`...
+main: Verifying repository ...
+main: Loading secrets ...
+
+ERROR: Failed loading component file
+           File: .../examples/errors/components/component5/component.py
+      Exception: invalid syntax (component.py, line 1)
+
+ERROR: Failed loading component file
+           File: .../examples/errors/components/component6/component.py
+      Exception: No module named 'asdf'
+
+ERROR: Missing component
+      Component: missingcomponent
+
+ERROR: Superfluous section in environment configuration
+        Section: superfluoussection
+
+ERROR: Override section for unknown component found
+      Component: nonexisting-component-section
+
+ERROR: Attribute override found both in environment and secrets
+      Component: component1
+      Attribute: my_attribute
+
+ERROR: Secrets section for unknown component found
+      Component: another-nonexisting-component-section
+======================= DEPLOYMENT FAILED (during load) ========================
+"""
+    )  # noqa: E501 line too long
