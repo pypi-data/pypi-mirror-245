7ff266b9756997fcdfc27e8b5cd78e432fd91a07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:03:34 +0000

    Add package camera-API/0.0.2/camera_API-0.0.2.tar.gz

---
+++ packages/camera-API/camera_API-0.0.2.tar.gz/camera_API-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='camera_API',
+    version='0.0.2',
+    packages=find_packages(),
+    install_requires=[
+        'opencv-python==4.5.5.62', 'hidapi==0.13.1', 'numpy==1.24.3', 'prettytable==3.8.0',
+        # Add other dependencies as needed
+    ],
+
+    author='Sivabalan T',
+    author_email='sivabalan.t@e-consystems.com',
+    description='Used to accessing the camera in friendly way',
+    classifiers=['Programming Language :: Python :: 3',],
+)
