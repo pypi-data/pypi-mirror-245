83c15273ceb21f249ba1419bbc9ae70bab4d94a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:03:38 +0000

    Add package digsim-logic-simulator/0.0.1/digsim-logic-simulator-0.0.1.tar.gz

---
+++ packages/digsim-logic-simulator/digsim-logic-simulator-0.0.1.tar.gz/digsim-logic-simulator-0.0.1/src/digsim/circuit/components/_button.py
@@ -0,0 +1,62 @@
+# Copyright (c) Fredrik Andersson, 2023
+# All rights reserved
+
+""" A PushButton component """
+
+from .atoms import CallbackComponent, PortOutImmediate
+
+
+class PushButton(CallbackComponent):
+    """PushButton component class"""
+
+    def __init__(self, circuit, name=None, inverted=False):
+        super().__init__(circuit, name)
+        portout = PortOutImmediate(self, "O")
+        self.add_port(portout)
+        portout.update_parent(True)
+        self.parameter_set("inverted", inverted)
+
+    def default_state(self):
+        self.release()
+
+    def push(self):
+        """Push pushbutton"""
+        if self.parameter_get("inverted"):
+            self.O.value = 0
+        else:
+            self.O.value = 1
+
+    def release(self):
+        """Release pushbutton"""
+        if self.parameter_get("inverted"):
+            self.O.value = 1
+        else:
+            self.O.value = 0
+
+    def reconfigure(self):
+        self.release()
+
+    @property
+    def has_action(self):
+        return True
+
+    @property
+    def active(self):
+        return self.O.value == 1
+
+    def onpress(self):
+        self.push()
+
+    def onrelease(self):
+        self.release()
+
+    @classmethod
+    def get_parameters(cls):
+        return {
+            "inverted": {
+                "type": bool,
+                "default": False,
+                "description": "Button output is inverted",
+                "reconfigurable": True,
+            },
+        }
---
+++ packages/digsim-logic-simulator/digsim-logic-simulator-0.0.1.tar.gz/digsim-logic-simulator-0.0.1/src/digsim/synth/_synthesis.py
@@ -0,0 +1,91 @@
+# Copyright (c) Fredrik Andersson, 2023
+# All rights reserved
+
+"""Helper module for yosys synthesis"""
+
+import os
+import subprocess
+import tempfile
+
+
+class SynthesisException(Exception):
+    """Exception class for yosys synthesis"""
+
+
+class Synthesis:
+    """Helper class for yosys synthesis"""
+
+    @classmethod
+    def list_modules(cls, verilog_files):
+        """List available moduels in verilog files"""
+        if isinstance(verilog_files, str):
+            verilog_files = [verilog_files]
+
+        script_file = None
+        with tempfile.NamedTemporaryFile(
+            mode="w", encoding="utf-8", suffix=".ys", delete=False
+        ) as stream:
+            script_file = stream.name
+            stream.write(f"read -sv {' '.join(verilog_files)}\n")
+            stream.write("ls\n")
+            stream.flush()
+
+        with subprocess.Popen(
+            ["yosys", script_file], stdout=subprocess.PIPE, stdin=None
+        ) as process:
+            modules = []
+            ls_output_found = False
+            while process.poll() is None:
+                line = process.stdout.readline().decode("utf-8").rstrip()
+                if "modules:" in line:
+                    ls_output_found = True
+                if ls_output_found and "$abstract" in line:
+                    modules.append(line.replace("$abstract\\", "").strip())
+            if process.returncode != 0:
+                raise SynthesisException("Yosys execution failed...")
+        os.unlink(script_file)
+        return modules
+
+    def __init__(self, verilog_files, json_output_file, verilog_top_module):
+        if isinstance(verilog_files, str):
+            self._verilog_files = [verilog_files]
+        else:
+            self._verilog_files = verilog_files
+        self._json_output_file = json_output_file
+        self._verilog_top_module = verilog_top_module
+        self._yosys_log = []
+
+    def execute(self, silent=False):
+        """Execute yosys with generated synthesis script"""
+        success = False
+        script_file = None
+        with tempfile.NamedTemporaryFile(
+            mode="w", encoding="utf-8", suffix=".ys", delete=False
+        ) as stream:
+            script_file = stream.name
+            stream.write(f"read -sv {' '.join(self._verilog_files)}\n")
+            stream.write(f"hierarchy -top {self._verilog_top_module}\n")
+            stream.write("proc; flatten\n")
+            stream.write("memory_dff\n")
+            stream.write("proc; opt; techmap; opt\n")
+            stream.write(f"synth -top {self._verilog_top_module}\n")
+            stream.write(f"write_json {self._json_output_file}\n")
+            stream.flush()
+
+        with subprocess.Popen(
+            ["yosys", stream.name], stdout=subprocess.PIPE, stdin=None
+        ) as process:
+            while process.poll() is None:
+                line = process.stdout.readline().decode("utf-8").rstrip()
+                if len(line) == 0:
+                    continue
+                self._yosys_log.append(line)
+                if not silent:
+                    print("Yosys: ", line)
+            success = process.returncode == 0
+        os.unlink(script_file)
+        return success
+
+    def get_log(self):
+        """Get the yosys output"""
+        return self._yosys_log
