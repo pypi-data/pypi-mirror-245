97b39cf90c4123303332b03fc63c276d2765145e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:03:48 +0000

    Add package synapse-selector/0.1.16b9/synapse_selector-0.1.16b9-py3-none-any.whl

---
+++ packages/synapse-selector/synapse_selector-0.1.16b9-py3-none-any.whl/synapse_selector/main.py
@@ -0,0 +1,16 @@
+from synapse_selector.utils.gui import ui_window
+from synapse_selector.utils.configuration import gui_settings
+from PyQt6.QtWidgets import QApplication
+import sys
+
+
+def main():
+    settings = gui_settings()
+    app = QApplication(sys.argv)
+    main = ui_window(settings)
+    main.show()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
