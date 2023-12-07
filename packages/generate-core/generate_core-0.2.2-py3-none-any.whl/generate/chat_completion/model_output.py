91985822cdfb77de05f22e2ab60f7eea35937c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:03:48 +0000

    Add package generate-core/0.2.2/generate_core-0.2.2-py3-none-any.whl

---
+++ packages/generate-core/generate_core-0.2.2-py3-none-any.whl/generate/chat_completion/model_output.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import Generic, Literal, Optional, TypeVar
+
+from pydantic import BaseModel
+
+from generate.chat_completion.message import AssistantMessage, UnionAssistantMessage
+from generate.model import ModelOutput
+
+M = TypeVar('M', bound=UnionAssistantMessage)
+
+
+class ChatCompletionOutput(ModelOutput, Generic[M]):
+    message: M
+    finish_reason: Optional[str] = None
+
+    @property
+    def reply(self) -> str:
+        if self.message and isinstance(self.message, AssistantMessage):
+            return self.message.content
+        return ''
+
+    @property
+    def is_finish(self) -> bool:
+        return self.finish_reason is not None
+
+
+class Stream(BaseModel):
+    delta: str = ''
+    control: Literal['start', 'continue', 'finish']
+
+
+class ChatCompletionStreamOutput(ChatCompletionOutput, Generic[M]):
+    stream: Stream
