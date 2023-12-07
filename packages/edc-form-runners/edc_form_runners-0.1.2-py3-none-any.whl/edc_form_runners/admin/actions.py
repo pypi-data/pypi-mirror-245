534187a23d6fc1b6162324d42f18826a28f4bfda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:03:58 +0000

    Add package edc-form-runners/0.1.2/edc_form_runners-0.1.2-py3-none-any.whl

---
+++ packages/edc-form-runners/edc_form_runners-0.1.2-py3-none-any.whl/edc_form_runners/admin/actions.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from edc_constants.constants import DONE, IN_PROGRESS, NEW
+
+from ..get_form_runner_by_src_id import get_form_runner_by_src_id
+
+
+@admin.action(description="Refresh selected issues")
+def issue_refresh(modeladmin, request, queryset):
+    for issue_obj in queryset:
+        runner = get_form_runner_by_src_id(
+            src_id=issue_obj.src_id, model_name=issue_obj.label_lower
+        )
+        runner.run_one()
