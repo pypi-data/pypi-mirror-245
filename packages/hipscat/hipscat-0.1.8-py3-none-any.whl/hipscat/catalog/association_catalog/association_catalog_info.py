88e02b4892a4ca9dec9c308318cf1a65d9a9078c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:00 +0000

    Add package hipscat/0.1.8/hipscat-0.1.8-py3-none-any.whl

---
+++ packages/hipscat/hipscat-0.1.8-py3-none-any.whl/hipscat/catalog/association_catalog/association_catalog_info.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from hipscat.catalog.catalog_type import CatalogType
+from hipscat.catalog.dataset.base_catalog_info import BaseCatalogInfo
+
+
+@dataclass
+class AssociationCatalogInfo(BaseCatalogInfo):
+    """Catalog Info for a HiPSCat Association Catalog"""
+
+    primary_catalog: str | None = None
+    """Catalog name for the primary (left) side of association"""
+
+    primary_column: str | None = None
+    """Column name in the primary (left) side of join"""
+
+    join_catalog: str | None = None
+    """Catalog name for the joining (right) side of association"""
+
+    join_column: str | None = None
+    """Column name in the joining (right) side of join"""
+
+    required_fields = BaseCatalogInfo.required_fields + [
+        "primary_catalog",
+        "join_catalog",
+    ]
+
+    DEFAULT_TYPE = CatalogType.ASSOCIATION
+    REQUIRED_TYPE = CatalogType.ASSOCIATION
