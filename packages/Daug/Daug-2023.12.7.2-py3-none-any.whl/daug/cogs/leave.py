94a747805e5382b3f6b1c34d6d8957efe968a2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:17 +0000

    Add package Daug/2023.12.7.2/Daug-2023.12.7.2-py3-none-any.whl

---
+++ packages/Daug/Daug-2023.12.7.2-py3-none-any.whl/daug/cogs/leave.py
@@ -0,0 +1,22 @@
+from discord.ext import commands
+from daug.utils.dpyexcept import excepter
+
+
+class Leave(commands.Cog):
+    """メンバー退出時"""
+    def __init__(self, bot, guild_id, channel_id = None):
+        self.bot = bot
+        self.guild_id = guild_id
+        self.channel_id = channel_id
+
+    @commands.Cog.listener()
+    @excepter
+    async def on_member_remove(self, member):
+        guild = member.guild
+        if guild.id != self.guild_id:
+            return
+        text = f'{member.mention} が退出しました'
+        if self.channel_id is None:
+            await guild.system_channel.send(text)
+        else:
+            await self.bot.get_channel(self.channel_id).send(text)
