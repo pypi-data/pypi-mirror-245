78268fde43ac8e692a0d8561f32c343a5cc76d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:04:00 +0000

    Add package TamilLexicheck/1.0.5/TamilLexicheck-1.0.5.tar.gz

---
+++ packages/TamilLexicheck/TamilLexicheck-1.0.5.tar.gz/TamilLexicheck-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='TamilLexicheck',
+    version='1.0.5',
+    packages=['TamilLexicheck'],
+    install_requires=[
+        # Your dependencies here
+    ],
+     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
