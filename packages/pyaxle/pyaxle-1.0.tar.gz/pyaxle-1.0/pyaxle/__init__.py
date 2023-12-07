8dc612ef90d506e72fae96121b593084aa617f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 14:03:29 +0000

    Add package pyaxle/1.0/pyaxle-1.0.tar.gz

---
+++ packages/pyaxle/pyaxle-1.0.tar.gz/pyaxle-1.0/pyaxle/__init__.py
@@ -0,0 +1,1 @@
+from ._pyaxle import *  # _pyaxle.py
---
+++ packages/pyaxle/pyaxle-1.0.tar.gz/pyaxle-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyaxle",
+    version="1.0",
+    packages=find_packages()
+    # There are no dependencies for pyaxle
+    # as of now
+)
