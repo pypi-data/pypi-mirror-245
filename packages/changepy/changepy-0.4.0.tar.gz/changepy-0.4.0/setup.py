d22528e396302db7a21a27bb541ab04a28ffdd43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 06:03:50 +0000

    Add package changepy/0.4.0/changepy-0.4.0.tar.gz

---
+++ packages/changepy/changepy-0.4.0.tar.gz/changepy-0.4.0/setup.py
@@ -0,0 +1,21 @@
+"""
+Setup script
+"""
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='changepy',
+    version='0.4.0',
+    description='Changepoint detection on time series, in Python',
+    author='Rui Gil',
+    author_email='ruipgil@gmail.com',
+    url='https://github.com/ruipgil/changepy',
+    packages=['changepy'],
+    download_url='https://github.com/ruipgil/changepy/archive/master.zip',
+    keywords=['changepoint', 'timeseries', 'time', 'series', 'math', 'stats', 'probabilities'],
+    classifiers=[],
+    install_requires=['numpy']
+)
