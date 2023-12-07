edc6eb243ba8965d33679bb39934a645369d78c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:04:27 +0000

    Add package siqc/0.0.1/siqc-0.0.1.tar.gz

---
+++ packages/siqc/siqc-0.0.1.tar.gz/siqc-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='siqc',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Reardon, Fonville, Avants',
+    author_email='areardon@invicro.com',
+    description='siqc',
+    url='https://github.com/alex-reardon/siqc',
+)
