94f383fbdb579511dd3ec245091c6dd63afb737a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:16 +0000

    Add package premeide-package-test2/0.0.4/premeide_package_test2-0.0.4.tar.gz

---
+++ packages/premeide-package-test2/premeide_package_test2-0.0.4.tar.gz/premeide_package_test2-0.0.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.4'
+DESCRIPTION = 'DESCRIPTION 0.0.4'
+LONG_DESCRIPTION = 'LONG_DESCRIPTION 0.0.4'
+
+# Setting up
+setup(
+    name="premeide_package_test2",
+    version=VERSION,
+    author="premeide",
+    author_email="premeide@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
