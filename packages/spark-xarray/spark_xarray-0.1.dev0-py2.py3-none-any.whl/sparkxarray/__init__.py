f65a1906fe31890f521ca87106d4e763787bd2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:04:10 +0000

    Add package spark-xarray/0.1.dev0/spark_xarray-0.1.dev0-py2.py3-none-any.whl

---
+++ packages/spark-xarray/spark_xarray-0.1.dev0-py2.py3-none-any.whl/sparkxarray/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+# List to define the behaviour of imports of the form:
+#           from sparkxarray import *
+
+__all__ = []
+
+# Package version number.
+__version__ = version = '0.1.dev0'
+
+
