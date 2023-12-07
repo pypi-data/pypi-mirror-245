5f9af0b30bb0bb5e884b7ce3731e57aa39c2910b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:04:47 +0000

    Add package python-datapath/0.1.2/python-datapath-0.1.2.tar.gz

---
+++ packages/python-datapath/python-datapath-0.1.2.tar.gz/python-datapath-0.1.2/datapath/__init__.py
@@ -0,0 +1,48 @@
+# datapath python module
+
+from ._base import (
+    is_path,
+    validate_path,
+    split,
+    join,
+    leaf,
+    get,
+    iterate,
+    put,
+    delete,
+    discard,
+)
+from .collection import collection
+from .folding import (
+    unfold_path_dict,
+    fold_path_dict,
+    complete_collection,
+    UnfoldProcessor,
+)
+from .types import (
+    DatapathError,
+    ValidationError,
+    InvalidIterationError,
+    PathLookupError,
+)
+
+__all__ = [
+    'get',
+    'iterate',
+    'put',
+    'delete',
+    'discard',
+    'is_path',
+    'validate_path',
+    'split',
+    'join',
+    'leaf',
+    'unfold_path_dict',
+    'fold_path_dict',
+    'collection',
+    'UnfoldProcessor',
+    'DatapathError',
+    'ValidationError',
+    'InvalidIterationError',
+    'PathLookupError',
+]
