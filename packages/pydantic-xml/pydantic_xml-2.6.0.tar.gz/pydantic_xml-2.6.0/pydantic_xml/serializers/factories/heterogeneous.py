7640fed52911f121fa46f941567438713c91e469
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:15 +0000

    Add package pydantic-xml/2.6.0/pydantic_xml-2.6.0.tar.gz

---
+++ packages/pydantic-xml/pydantic_xml-2.6.0.tar.gz/pydantic_xml-2.6.0/pydantic_xml/serializers/factories/heterogeneous.py
@@ -0,0 +1,91 @@
+from typing import Any, Dict, List, Optional, Tuple
+
+from pydantic_core import core_schema as pcs
+
+from pydantic_xml import errors
+from pydantic_xml.element import XmlElementReader, XmlElementWriter
+from pydantic_xml.serializers.serializer import TYPE_FAMILY, SchemaTypeFamily, Serializer
+from pydantic_xml.typedefs import EntityLocation
+
+
+class ElementSerializer(Serializer):
+    @classmethod
+    def from_core_schema(cls, schema: pcs.TuplePositionalSchema, ctx: Serializer.Context) -> 'ElementSerializer':
+        computed = ctx.field_computed
+        inner_serializers: List[Serializer] = []
+        for item_schema in schema['items_schema']:
+            inner_serializers.append(Serializer.parse_core_schema(item_schema, ctx))
+
+        return cls(computed, tuple(inner_serializers))
+
+    def __init__(self, computed: bool, inner_serializers: Tuple[Serializer, ...]):
+        self._computed = computed
+        self._inner_serializers = inner_serializers
+
+    def serialize(
+            self, element: XmlElementWriter, value: List[Any], encoded: List[Any], *, skip_empty: bool = False,
+    ) -> Optional[XmlElementWriter]:
+        if value is None:
+            return element
+
+        if skip_empty and len(value) == 0:
+            return element
+
+        if len(value) != len(self._inner_serializers):
+            raise errors.SerializationError("value length is incorrect")
+
+        for serializer, val, enc in zip(self._inner_serializers, value, encoded):
+            serializer.serialize(element, val, enc, skip_empty=skip_empty)
+
+        return element
+
+    def deserialize(
+            self,
+            element: Optional[XmlElementReader],
+            *,
+            context: Optional[Dict[str, Any]],
+    ) -> Optional[List[Any]]:
+        if self._computed:
+            return None
+
+        if element is None:
+            return None
+
+        result = [
+            serializer.deserialize(element, context=context)
+            for serializer in self._inner_serializers
+        ]
+        if all((value is None for value in result)):
+            return None
+        else:
+            return result
+
+
+def from_core_schema(schema: pcs.TuplePositionalSchema, ctx: Serializer.Context) -> Serializer:
+    for item_schema in schema['items_schema']:
+        item_schema, ctx = Serializer.preprocess_schema(item_schema, ctx)
+
+        items_type_family = TYPE_FAMILY.get(item_schema['type'])
+        if items_type_family not in (
+                SchemaTypeFamily.PRIMITIVE,
+                SchemaTypeFamily.MODEL,
+                SchemaTypeFamily.MAPPING,
+                SchemaTypeFamily.TYPED_MAPPING,
+                SchemaTypeFamily.UNION,
+                SchemaTypeFamily.IS_INSTANCE,
+        ):
+            raise errors.ModelFieldError(
+                ctx.model_name, ctx.field_name, "collection item must be of primitive, model, mapping or union type",
+            )
+
+        if items_type_family not in (SchemaTypeFamily.MODEL, SchemaTypeFamily.UNION) and ctx.entity_location is None:
+            raise errors.ModelFieldError(ctx.model_name, ctx.field_name, "entity name is not provided")
+
+    if ctx.entity_location is EntityLocation.ELEMENT:
+        return ElementSerializer.from_core_schema(schema, ctx)
+    elif ctx.entity_location is None:
+        return ElementSerializer.from_core_schema(schema, ctx)
+    elif ctx.entity_location is EntityLocation.ATTRIBUTE:
+        raise errors.ModelFieldError(ctx.model_name, ctx.field_name, "attributes of collection types are not supported")
+    else:
+        raise AssertionError("unreachable")
---
+++ packages/pydantic-xml/pydantic_xml-2.6.0.tar.gz/pydantic_xml-2.6.0/pydantic_xml/utils.py
@@ -0,0 +1,94 @@
+import dataclasses as dc
+import itertools as it
+import re
+from collections import ChainMap
+from typing import Iterable, Optional, cast
+
+from .element.native import etree
+from .typedefs import NsMap
+
+
+@dc.dataclass(frozen=True)
+class QName:
+    """
+    XML entity qualified name.
+
+    :param tag: entity tag
+    :param ns: entity namespace
+    """
+
+    tag: str
+    ns: Optional[str]
+
+    @classmethod
+    def from_uri(cls, uri: str) -> 'QName':
+        """
+        Creates `QName` from uri.
+
+        :param uri: entity uri in format '{namespace}tag'
+        :return: qualified name
+        """
+
+        if (m := re.match(r'({(.*)})?(.*)', uri)) is None:
+            raise ValueError
+
+        return cls(tag=m[3], ns=m[2])
+
+    @classmethod
+    def from_alias(
+            cls, tag: str, ns: Optional[str] = None, nsmap: Optional[NsMap] = None, is_attr: bool = False,
+    ) -> 'QName':
+        """
+        Creates `QName` from namespace alias.
+
+        :param tag: entity tag
+        :param ns: xml namespace alias
+        :param nsmap: xml namespace mapping
+        :param is_attr: is the tag of attribute type
+        :return: qualified name
+        """
+
+        if not is_attr or ns is not None:
+            ns = nsmap.get(ns or '') if nsmap else None
+
+        return QName(tag=tag, ns=ns)
+
+    @property
+    def uri(self) -> str:
+        return '{%s}%s' % (self.ns, self.tag) if self.ns else self.tag
+
+    def __str__(self) -> str:
+        return self.uri
+
+
+def merge_nsmaps(*maps: Optional[NsMap]) -> NsMap:
+    """
+    Merges multiple namespace maps into s single one respecting provided order.
+
+    :param maps: namespace maps
+    :return: merged namespace
+    """
+
+    return cast(NsMap, ChainMap(*(nsmap for nsmap in maps if nsmap)))
+
+
+def register_nsmap(nsmap: NsMap) -> None:
+    """
+    Registers namespaces prefixes from the map.
+    """
+
+    for prefix, uri in nsmap.items():
+        if prefix != '':  # skip default namespace
+            etree.register_namespace(prefix, uri)
+
+
+def get_slots(o: object) -> Iterable[str]:
+    return it.chain.from_iterable(getattr(cls, '__slots__', []) for cls in o.__class__.__mro__)
+
+
+def select_ns(*nss: Optional[str]) -> Optional[str]:
+    for ns in nss:
+        if ns is not None:
+            return ns
+
+    return None
