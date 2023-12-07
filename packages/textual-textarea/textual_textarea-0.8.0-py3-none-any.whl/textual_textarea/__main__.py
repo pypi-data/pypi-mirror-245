d04cf1c39fa01d8ee34814c8f0a880cc7ef4d5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 14:03:30 +0000

    Add package textual-textarea/0.8.0/textual_textarea-0.8.0-py3-none-any.whl

---
+++ packages/textual-textarea/textual_textarea-0.8.0-py3-none-any.whl/textual_textarea/__main__.py
@@ -0,0 +1,27 @@
+import sys
+
+from textual.app import App, ComposeResult
+
+from textual_textarea import TextArea
+
+
+class TextApp(App, inherit_bindings=False):
+    def compose(self) -> ComposeResult:
+        try:
+            language = sys.argv[1]
+        except IndexError:
+            language = "python"
+        yield TextArea(
+            language=language,
+            theme="monokai",
+            use_system_clipboard=True,
+            id="ta",
+        )
+
+    def on_mount(self) -> None:
+        ta = self.query_one("#ta", expect_type=TextArea)
+        ta.focus()
+
+
+app = TextApp()
+app.run()
