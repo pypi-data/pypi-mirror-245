96c681a28700d2e083c375c99af4fc3168bac0a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:03:24 +0000

    Add package ramodels/32.16.0/ramodels-32.16.0-py3-none-any.whl

---
+++ packages/ramodels/ramodels-32.16.0-py3-none-any.whl/ramodels/mo/details/owner.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+from uuid import UUID
+
+from pydantic import Field
+
+from .._shared import MOBase
+from .._shared import Validity
+
+
+class OwnerBase(MOBase):
+    """A MO owner object."""
+
+    type_: str = Field("owner", alias="type", description="The object type.")
+    validity: Validity = Field(description="Validity of the owner object.")
+
+
+class OwnerRead(OwnerBase):
+    """A MO OwnerRead object."""
+
+    owner_uuid: UUID | None = Field(description="UUID of the owner.")
+    org_unit_uuid: UUID | None = Field(
+        description="UUID of the organisation unit related to the owner."
+    )
+    employee_uuid: UUID | None = Field(
+        description="UUID of the employee related to the owner."
+    )
+    owner_inference_priority: str | None = Field(
+        description="Inference priority, if set: `engagement_priority` or `association_priority`"
+    )
