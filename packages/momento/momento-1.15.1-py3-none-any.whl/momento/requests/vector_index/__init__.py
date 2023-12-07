799b2cd8b5f9fb67225fe04d1a8b335c73b19075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 14:03:34 +0000

    Add package momento/1.15.1/momento-1.15.1-py3-none-any.whl

---
+++ packages/momento/momento-1.15.1-py3-none-any.whl/momento/requests/vector_index/__init__.py
@@ -0,0 +1,6 @@
+from momento.common_data.vector_index.item import Item
+
+from .search import ALL_METADATA, AllMetadata
+from .similarity_metric import SimilarityMetric
+
+__all__ = ["Item", "AllMetadata", "ALL_METADATA", "SimilarityMetric"]
