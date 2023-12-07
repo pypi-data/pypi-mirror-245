e04fa72240936e23b5a233ba1ced0782da9ed5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:02:44 +0000

    Add package Ani-Encrypt/1.0.0/Ani_Encrypt-1.0.0-py3-none-any.whl

---
+++ packages/Ani-Encrypt/Ani_Encrypt-1.0.0-py3-none-any.whl/enc_dec/encoder_decoder.py
@@ -0,0 +1,423 @@
+
+
+
+
+def encryption(string):
+    if string == 1 or string == "1":
+        return "rte"        
+    if string == 2 or string == "2":
+        return "gjk"    
+    if string == 3 or string == "3":        
+        return "rw3"        
+    if string == 4 or string == "4":
+        return "54y"        
+    if string == 5 or string == "5":
+        return "h4e"        
+    if string == 6 or string == "6":
+        return "21f"        
+    if string == 7 or string == "7":
+        return "ka;"
+    if string == 8 or string == "8":
+        return "*kd"
+    if string == 9 or string == "9":
+        return ")s$"
+    if string == 0 or string == "0":
+        return "#@j"
+
+    if string == " ":
+        return "Ir&"
+
+    if string == "!":
+        return "5ug"
+    if string == "@":
+        return "0em"
+    if string == "#":
+        return "r2="
+    if string == "$":
+        return "0:*"
+    if string == "%":
+        return "{*l"
+    if string == "^":
+        return "r1n"
+    if string == "&":
+        return "w[6"
+    if string == "*":
+        return ";uo"
+    if string == "(":
+        return "3vh"
+    if string == ")":
+        return "12r"
+    if string == "-":
+        return "rgh"
+    if string == "_":
+        return "etg"
+    if string == "=":
+        return "1?p"
+    if string == "+":
+        return "/{;"
+    if string == "[":
+        return "t?,"
+    if string == "]":
+        return "w=o"
+    if string == "{":
+        return "j9-"        
+    if string == "}":
+        return "r7:"
+    if string == "<":
+        return "(ji"
+    if string == ">":
+        return ")el"
+    if string == "*":
+        return "???"
+    if string == ":":
+        return "r3>"
+    if string == ";":
+        return ".kL"
+    if string == "/":
+        return "Fk6"
+    if string == "?":
+        return "L7*"
+    if string == ",":
+        return "i6x"
+    if string == ".":
+        return "/8N"
+    if string == "|":
+        return "4ms"
+    if string == "`":
+        return "n*;"
+    if string == "~":
+        return "m7["
+
+
+    if string == "a":
+        return "srj"
+    if string == "b":
+        return "her"
+    if string == "c":
+        return "tsk"
+    if string == "d":
+        return "kpi"
+    if string == "e":
+        return "tyd"
+    if string == "f":
+        return "lhy"
+    if string == "g":
+        return "wfg"
+    if string == "h":
+        return "fkt"
+    if string == "i":
+        return "uer"
+    if string == "j":
+        return "wr="
+    if string == "k":
+        return "er5"
+    if string == "l":
+        return "45-"
+    if string == "m":
+        return "23="
+    if string == "n":
+        return "tb6"
+    if string == "o":
+        return "dhe"
+    if string == "p":
+        return "8g0"
+    if string == "q":
+        return "eks"
+    if string == "r":
+        return "dft"
+    if string == "s":
+        return "she"
+    if string == "t":
+        return "s5:"
+    if string == "u":
+        return "735"
+    if string == "v":
+        return "924"
+    if string == "w":
+        return "h30"
+    if string == "x":
+        return "4ek"
+    if string == "y":
+        return "sd1"
+    if string == "z":
+        return "836"
+
+
+    if string == "A":
+        return "^hG"
+    if string == "B":
+        return ")KD"
+    if string == "C":
+        return "5H@"
+    if string == "D":
+        return "BW,"
+    if string == "E":
+        return "2H*"
+    if string == "F":
+        return "3n{"
+    if string == "G":
+        return "=RB"
+    if string == "H":
+        return "+k;"
+    if string == "I":
+        return "e*["
+    if string == "J":
+        return "6&#"
+    if string == "K":
+        return "%19"
+    if string == "L":
+        return "#f_"
+    if string == "M":
+        return "/le"
+    if string == "N":
+        return "*k,"
+    if string == "O":
+        return "ery"
+    if string == "P":
+        return "!2d"
+    if string == "Q":
+        return "vD,"
+    if string == "R":
+        return "f%6"
+    if string == "S":
+        return ":i5"
+    if string == "T":
+        return "X{6"
+    if string == "U":
+        return "2J?"
+    if string == "V":
+        return "1m7"
+    if string == "W":
+        return "cT@"
+    if string == "X":
+        return "ztd"
+    if string == "Y":
+        return "e(*"
+    if string == "Z":
+        return "w?9"
+
+def decryption(string):
+    if string == "rte":
+        return "1"        
+    if string == "gjk":
+        return "2"    
+    if string == "rw3":        
+        return "3"        
+    if string == "54y":
+        return "4"        
+    if string == "h4e":
+        return "5"        
+    if string == "21f":
+        return "6"        
+    if string == "ka;":
+        return "7"
+    if string == "*kd":
+        return "8"
+    if string == ")s$":
+        return "9"
+    if string == "#@j":
+        return "0"
+
+    if string == "Ir&":
+        return " "
+
+    if string == "5ug":
+        return "!"
+    if string == "0em":
+        return "@"
+    if string == "r2=":
+        return "#"
+    if string == "0:*":
+        return "$"
+    if string == "{*l":
+        return "%"
+    if string == "r1n":
+        return "^"
+    if string == "w[6":
+        return "&"
+    if string == ";uo":
+        return "*"
+    if string == "3vh":
+        return "("
+    if string == "12r":
+        return ")"
+    if string == "rgh":
+        return "-"
+    if string == "etg":
+        return "_"
+    if string == "1?p":
+        return "="
+    if string == "/{;":
+        return "+"    
+    if string == "t?,":
+        return "["
+    if string == "w=o":
+        return "]"
+    if string == "j9-":
+        return "{"        
+    if string == "r7:":
+        return "}"
+    if string == "(ji":
+        return "<"
+    if string == ")el":
+        return ">"
+    if string == "???":
+        return "*"
+    if string == "r3>":
+        return ":"
+    if string == ".kL":
+        return ";"
+    if string == "Fk6":
+        return "/"
+    if string == "L7*":
+        return "?"
+    if string == "i6x":
+        return ","
+    if string == "/8N":
+        return "."
+    if string == "4ms":
+        return "|"
+    if string == "n*;":
+        return "`"
+    if string == "m7[":
+        return "~"
+
+
+    if string == "srj":
+        return "a"
+    if string == "her":
+        return "b"
+    if string == "tsk":
+        return "c"
+    if string == "kpi":
+        return "d"
+    if string == "tyd":
+        return "e"
+    if string == "lhy":
+        return "f"    
+    if string == "wfg":
+        return "g"
+    if string == "fkt":
+        return "h"
+    if string == "uer":
+        return "i"
+    if string == "wr=":
+        return "j"
+    if string == "er5":
+        return "k"
+    if string == "45-":
+        return "l"
+    if string == "23=":
+        return "m"
+    if string == "tb6":
+        return "n"
+    if string == "dhe":
+        return "o"
+    if string == "8g0":
+        return "p"
+    if string == "eks":
+        return "q"
+    if string == "dft":
+        return "r"
+    if string == "she":
+        return "s"
+    if string == "s5:":
+        return "t"
+    if string == "735":
+        return "u"
+    if string == "924":
+        return "v"
+    if string == "h30":
+        return "w"
+    if string == "4ek":
+        return "x"
+    if string == "sd1":
+        return "y"
+    if string == "836":
+        return "z"
+
+
+    if string == "^hG":
+        return "A"
+    if string == ")KD":
+        return "B"
+    if string == "5H@":
+        return "C"
+    if string == "BW,":
+        return "D"
+    if string == "2H*":
+        return "E"
+    if string == "3n{":
+        return "F"
+    if string == "=RB":
+        return "G"
+    if string == "+k;":
+        return "H"
+    if string == "e*[":
+        return "I"
+    if string == "6&#":
+        return "J"    
+    if string == "%19":
+        return "K"
+    if string == "#f_":
+        return "L"
+    if string == "/le":
+        return "M"
+    if string == "*k,":
+        return "N"
+    if string == "ery":
+        return "O"
+    if string == "!2d":
+        return "P"
+    if string == "vD,":
+        return "Q"
+    if string == "f%6":
+        return "R"
+    if string == ":i5":
+        return "S"
+    if string == "X{6":
+        return "T"
+    if string == "2J?":
+        return "U"
+    if string == "1m7":
+        return "V"
+    if string == "cT@":
+        return "W"
+    if string == "ztd":
+        return "X"
+    if string == "e(*":
+        return "Y"
+    if string == "w?9":
+        return "Z"
+
+
+
+
+
+
+def my_encoder(encd_str):
+    encd_list = []
+    for enc in encd_str:
+        slice_enc = encryption(enc)
+        encd_list.append(slice_enc)
+    encd_list = list(reversed(encd_list))
+    merge_str = ''.join(encd_list)
+    return merge_str
+
+
+
+
+
+def my_decoder(encded_string):
+    #encded_string = "^hGtb6uerer5tyds5:"
+    encded_string_reversed = [encded_string[i:i+3] for i in range(0, len(encded_string), 3)]
+    #print(encded_string)
+    encded_string = list(reversed(encded_string_reversed))
+    decd_list = []
+    for dec in encded_string:
+        slice_dec = (decryption(dec))
+        decd_list.append(slice_dec)
+    decd_string = ''.join(decd_list)
+    return decd_string
+
+
