58fa788ab812b9c75e5a2bf3c354f78c85a2563d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 14:03:37 +0000

    Add package synthetix/0.1.0rc1/synthetix-0.1.0rc1.tar.gz

---
+++ packages/synthetix/synthetix-0.1.0rc1.tar.gz/synthetix-0.1.0rc1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="synthetix",
+    version="0.1.0-rc.1",
+    description="Synthetix protocol SDK",
+    long_description="A library containing helpful functions for interacting with the Synthetix protocol",
+    author="Synthetix DAO",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "requests",
+        "requests_toolbelt",
+        "web3>=6.4.0",
+        "gql",
+    ],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8",
+    include_package_data=True,
+)
