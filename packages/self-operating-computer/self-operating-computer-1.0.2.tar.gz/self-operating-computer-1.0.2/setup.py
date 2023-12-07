ed7779938eb67eef21cd7c7992c12232836d7abf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 04:04:10 +0000

    Add package self-operating-computer/1.0.2/self-operating-computer-1.0.2.tar.gz

---
+++ packages/self-operating-computer/self-operating-computer-1.0.2.tar.gz/self-operating-computer-1.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your requirements.txt file
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+setup(
+    name="self-operating-computer",
+    version="1.0.2",
+    packages=find_packages(),
+    install_requires=required,  # Add dependencies here
+    entry_points={
+        "console_scripts": [
+            "operate=operate.main:main_entry",
+        ],
+    },
+    # include any other necessary setup options here
+)
