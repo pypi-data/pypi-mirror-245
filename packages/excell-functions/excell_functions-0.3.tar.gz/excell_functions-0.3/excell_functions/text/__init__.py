ccde38f0f5dc1b46cf6b40b4bfdbc1c493d65d68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:03:15 +0000

    Add package excell-functions/0.3/excell_functions-0.3.tar.gz

---
+++ packages/excell-functions/excell_functions-0.3.tar.gz/excell_functions-0.3/excell_functions/text/__init__.py
@@ -0,0 +1,302 @@
+# text
+# ASC function
+def ASC():
+    '''Changes full-width (double-byte) English letters or katakana within a character string to half-width (single-byte) characters'''
+    pass
+    return
+
+
+# ARRAYTOTEXT function
+def ARRAYTOTEXT():
+    '''Returns an array of text values from any specified range'''
+    pass
+    return
+
+
+# BAHTTEXT function
+def BAHTTEXT():
+    '''Converts a number to text, using the ß (baht) currency format'''
+    pass
+    return
+
+
+# CHAR function
+def CHAR():
+    '''Returns the character specified by the code number'''
+    pass
+    return
+
+
+# CLEAN function
+def CLEAN():
+    '''Removes all nonprintable characters from text'''
+    pass
+    return
+
+
+# CODE function
+def CODE():
+    '''Returns a numeric code for the first character in a text string'''
+    pass
+    return
+
+
+# CONCAT function
+def CONCAT():
+    '''Combines the text from multiple ranges and/or strings, but it doesn't provide the delimiter or IgnoreEmpty arguments.'''
+    pass
+    return
+
+
+# CONCATENATE function
+def CONCATENATE():
+    '''Joins several text items into one text item'''
+    pass
+    return
+
+
+# DBCS function
+def DBCS():
+    '''Changes half-width (single-byte) English letters or katakana within a character string to full-width (double-byte) characters'''
+    pass
+    return
+
+
+# DOLLAR function
+def DOLLAR():
+    '''Converts a number to text, using the $ (dollar) currency format'''
+    pass
+    return
+
+
+# EXACT function
+def EXACT():
+    '''Checks to see if two text values are identical'''
+    pass
+    return
+
+
+# FIND, FINDB functions
+def FIND():
+    '''Finds one text value within another (case-sensitive)'''
+    pass
+    return
+
+
+def FINDB():
+    '''Finds one text value within another (case-sensitive)'''
+    pass
+    return
+
+
+# FIXED function
+def FIXED():
+    '''Formats a number as text with a fixed number of decimals'''
+    pass
+    return
+
+
+# LEFT, LEFTB functions
+def LEFT():
+    '''Returns the leftmost characters from a text value'''
+    pass
+    return
+
+
+def LEFTB():
+    '''Returns the leftmost characters from a text value'''
+    pass
+    return
+
+
+# LEN, LENB functions
+def LEN():
+    '''Returns the number of characters in a text string'''
+    pass
+    return
+
+
+def LENB():
+    '''Returns the number of characters in a text string'''
+    pass
+    return
+
+
+# LOWER function
+def LOWER():
+    '''Converts text to lowercase'''
+    pass
+    return
+
+
+# MID, MIDB functions
+def MID():
+    '''Returns a specific number of characters from a text string starting at the position you specify'''
+    pass
+    return
+
+
+def MIDB():
+    '''Returns a specific number of characters from a text string starting at the position you specify'''
+    pass
+    return
+
+
+# NUMBERVALUE function
+def NUMBERVALUE():
+    '''Converts text to number in a locale-independent manner'''
+    pass
+    return
+
+
+# PHONETIC function
+def PHONETIC():
+    '''Extracts the phonetic (furigana) characters from a text string'''
+    pass
+    return
+
+
+# PROPER function
+def PROPER():
+    '''Capitalizes the first letter in each word of a text value'''
+    pass
+    return
+
+
+# REPLACE, REPLACEB functions
+def REPLACE():
+    '''Replaces characters within text'''
+    pass
+    return
+
+
+def REPLACEB():
+    '''Replaces characters within text'''
+    pass
+    return
+
+
+# REPT function
+def REPT():
+    '''Repeats text a given number of times'''
+    pass
+    return
+
+
+# RIGHT, RIGHTB functions
+def RIGHT():
+    '''Returns the rightmost characters from a text value'''
+    pass
+    return
+
+
+def RIGHTB():
+    '''Returns the rightmost characters from a text value'''
+    pass
+    return
+
+
+# SEARCH, SEARCHB functions
+def SEARCH():
+    '''Finds one text value within another (not case-sensitive)'''
+    pass
+    return
+
+
+def SEARCHB():
+    '''Finds one text value within another (not case-sensitive)'''
+    pass
+    return
+
+
+# SUBSTITUTE function
+def SUBSTITUTE():
+    '''Substitutes new text for old text in a text string'''
+    pass
+    return
+
+
+# T function
+def T():
+    '''Converts its arguments to text'''
+    pass
+    return
+
+
+# TEXT function
+def TEXT():
+    '''Formats a number and converts it to text'''
+    pass
+    return
+
+
+# TEXTAFTER function
+def TEXTAFTER():
+    '''Returns text that occurs after given character or string'''
+    pass
+    return
+
+
+# TEXTBEFORE function
+def TEXTBEFORE():
+    '''Returns text that occurs before a given character or string'''
+    pass
+    return
+
+
+# TEXTJOIN function
+def TEXTJOIN():
+    '''Text:    Combines the text from multiple ranges and/or strings'''
+    pass
+    return
+
+
+# TEXTSPLIT function
+def TEXTSPLIT():
+    '''Splits text strings by using column and row delimiters'''
+    pass
+    return
+
+
+# TRIM function
+def TRIM():
+    '''Removes spaces from text'''
+    pass
+    return
+
+
+# UNICHAR function
+def UNICHAR():
+    '''Returns the Unicode character that is references by the given numeric value'''
+    pass
+    return
+
+
+# UNICODE function
+def UNICODE():
+    '''Returns the number (code point) that corresponds to the first character of the text'''
+    pass
+    return
+
+
+# UPPER function
+def UPPER():
+    '''Converts text to uppercase'''
+    pass
+    return
+
+
+# VALUE function
+def VALUE():
+    '''Converts a text argument to a number'''
+    pass
+    return
+
+
+# VALUETOTEXT function
+def VALUETOTEXT():
+    '''Returns text from any specified value'''
+    pass
+    return
+
+
