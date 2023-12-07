fcc80a56944f0d11cb574ebbe7ef7203890ae147
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:04:32 +0000

    Add package webfinder/1.4.1/webfinder-1.4.1.tar.gz

---
+++ packages/webfinder/webfinder-1.4.1.tar.gz/webfinder-1.4.1/finder/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+project desc
+"""
+import pkgutil
+import sys
+
+__author__ = "hyxf"
+
+# Syntax sugar.
+_ver = sys.version_info
+
+#: Python 3.x?
+is_py3 = (_ver[0] == 3)
