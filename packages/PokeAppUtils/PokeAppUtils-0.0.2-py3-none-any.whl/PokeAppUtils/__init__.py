0b6b0c34de1cddf5b2426b446633ae0d3ad2224d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 04:04:27 +0000

    Add package PokeAppUtils/0.0.2/PokeAppUtils-0.0.2-py3-none-any.whl

---
+++ packages/PokeAppUtils/PokeAppUtils-0.0.2-py3-none-any.whl/PokeAppUtils/__init__.py
@@ -0,0 +1,3 @@
+# needed to be able to import the utils folder correctly
+from datas import pokeDexes, damageRelations, NatIDtoPkn, PknToNatID, PknWeaknesses, PokemonNamesList
+from teamSynergies import teamSynergy
