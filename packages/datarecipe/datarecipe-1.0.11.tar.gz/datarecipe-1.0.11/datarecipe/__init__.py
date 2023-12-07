616fbc5dfb6fa850ba9453cba1359c61ecf1096e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:04:02 +0000

    Add package datarecipe/1.0.11/datarecipe-1.0.11.tar.gz

---
+++ packages/datarecipe/datarecipe-1.0.11.tar.gz/datarecipe-1.0.11/datarecipe/__init__.py
@@ -0,0 +1,24 @@
+name = "datarecipe"
+moudle = "chat, model_list, send_email, update_data, local_to_df, df_to_xlsx"
+
+from .open_ai_api import(
+    chat,
+    model_list
+)
+
+from .common_tools import(
+    send_email,
+    local_to_df,
+    df_to_xlsx,
+    df_to_csv
+)
+
+from .mysql import(
+    update_data,
+    clear_db,
+    fetch_table_data
+)
+
+from .examine import(
+    check_empty
+)
