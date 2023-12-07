1e76220132eba85e2ed73547ba75bac79c3ab59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:16 +0000

    Add package zuul/9.3.0/zuul-9.3.0.tar.gz

---
+++ packages/zuul/zuul-9.3.0.tar.gz/zuul-9.3.0/doc/source/drivers/timer.rst
@@ -0,0 +1,46 @@
+:title: Timer Driver
+
+Timer
+=====
+
+The timer driver supports triggers only.  It is used for configuring
+pipelines so that jobs run at scheduled times.  No connection
+configuration is required.
+
+Trigger Configuration
+---------------------
+
+Timers don't require a special connection or driver. Instead they can
+simply be used by listing ``timer`` as the trigger.
+
+This trigger will run based on a cron-style time specification.  It
+will enqueue an event into its pipeline for every project and branch
+defined in the configuration.  Any job associated with the pipeline
+will run in response to that event.
+
+Zuul implements the timer using `apscheduler`_, Please check the
+`apscheduler documentation`_ for more information about the syntax.
+
+.. attr:: pipeline.trigger.timer
+
+   The timer trigger supports the following attributes:
+
+   .. attr:: time
+      :required:
+
+      The time specification in cron syntax.  Only the 5 part syntax
+      is supported, not the symbolic names.  Example: ``0 0 * * *``
+      runs at midnight.
+      An optional 6th part specifies seconds.  The optional 7th part specifies
+      a jitter in seconds. This delays the trigger randomly, limited by
+      the specified value.  Example ``0 0 * * * * 60`` runs at
+      midnight or randomly up to 60 seconds later.  The jitter is
+      applied individually to each project-branch combination.
+
+   .. warning::
+       Be aware the day-of-week value differs from from cron.
+       The first weekday is Monday (0), and the last is Sunday (6).
+
+
+.. _apscheduler: https://apscheduler.readthedocs.io/
+.. _apscheduler documentation: https://apscheduler.readthedocs.io/en/3.x/modules/triggers/cron.html#module-apscheduler.triggers.cron
---
+++ packages/zuul/zuul-9.3.0.tar.gz/zuul-9.3.0/zuul/ansible/8/paths.py
@@ -0,0 +1,71 @@
+# Copyright 2016 Red Hat, Inc.
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+import imp
+import os
+
+import ansible.plugins.action
+
+
+def _full_path(path):
+    return os.path.realpath(os.path.abspath(os.path.expanduser(path)))
+
+
+def _fail_dict(path, prefix='Accessing files from'):
+    return dict(
+        failed=True,
+        path=path,
+        msg="{prefix} outside the working dir {curdir} is prohibited".format(
+            prefix=prefix,
+            curdir=os.path.abspath(os.path.curdir)))
+
+
+def _import_ansible_action_plugin(name):
+    # Ansible forces the import of our action plugins
+    # (zuul.ansible.action.foo) as ansible.plugins.action.foo, which
+    # is the import path of the ansible implementation.  Our
+    # implementations need to subclass that, but if we try to import
+    # it with that name, we will get our own module.  This bypasses
+    # Python's module namespace to load the actual ansible modules.
+    # We need to give it a name, however.  If we load it with its
+    # actual name, we will end up overwriting our module in Python's
+    # namespace, causing infinite recursion.  So we supply an
+    # otherwise unused name for the module:
+    # zuul.ansible.protected.action.foo.
+
+    return imp.load_module(
+        'zuul.ansible.protected.action.' + name,
+        *imp.find_module(name, ansible.plugins.action.__path__))
+
+
+def _sanitize_filename(name):
+    return ''.join(c for c in name if c.isalnum())
+
+
+# Ansible assigns a unique id to every task (Task._uuid).  However, if
+# a role is included more than once, the task object is re-used.  In
+# order to provide unique log ids for the Zuul command log streaming
+# system, this global dictionary is used to map keys that are derived
+# from tasks (task._uuid concatenated with the host name) to a counter
+# which is incremented each time the task+host combination is
+# encountered.  Ansible will not run more than one task on a host
+# simultaneously, so this should be sufficiently unique to avoid
+# collisions.
+#
+# We use a global dictionary defined here so that zuul_stream can
+# write to it and zuul.ansible.command modules can read it.  Note that
+# the command module operates after a fork and therefore it should be
+# treated as read-only there.
+ZUUL_LOG_ID_MAP = {}
