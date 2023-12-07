f290b7ce65ba4ee44068cd1039cdc4deff4dcde8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:03:29 +0000

    Add package ZimPhoneValidate/1.1.0/ZimPhoneValidate-1.1.0-py3-none-any.whl

---
+++ packages/ZimPhoneValidate/ZimPhoneValidate-1.1.0-py3-none-any.whl/ZimPhoneValidate/__init__.py
@@ -0,0 +1,18 @@
+'''
+ZimPhoneValidate module is for validating
+Zimbabwean phone numbers in your applications
+it supports both formating with spaces and one with no spacing 
+
+eg: 263778040497 or 263 778 040 497
+NB: The + in country codes is not supported and the moment
+    this format wont validate: +263778040497 or +263 778 040 497
+
+developed by: Tanaka Chinengundu
+
+'''
+from ZimPhoneValidate.validate import isvalid, example
+
+
+example = example
+is_number_valid = is_number_valid 
+versiom= "1.0.0"
