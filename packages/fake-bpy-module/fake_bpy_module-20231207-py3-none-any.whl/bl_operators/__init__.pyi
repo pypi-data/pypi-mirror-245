77ee458477c03f71fa22b8882007440ccf4f2799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:03:55 +0000

    Add package fake-bpy-module/20231207/fake_bpy_module-20231207-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20231207-py3-none-any.whl/bl_operators/__init__.pyi
@@ -0,0 +1,49 @@
+import sys
+import typing
+from . import rigidbody
+from . import presets
+from . import freestyle
+from . import geometry_nodes
+from . import clip
+from . import mesh
+from . import node
+from . import screen_play_rendered_anim
+from . import object_randomize_transform
+from . import spreadsheet
+from . import console
+from . import view3d
+from . import file
+from . import vertexpaint_dirt
+from . import object
+from . import object_quick_effects
+from . import assets
+from . import uvcalc_follow_active
+from . import userpref
+from . import add_mesh_torus
+from . import image
+from . import anim
+from . import object_align
+from . import wm
+from . import constraint
+from . import uvcalc_lightmap
+from . import sequencer
+from . import bmesh
+from . import uvcalc_transform
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def register():
+    ''' 
+
+    '''
+
+    ...
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    ...
---
+++ packages/fake-bpy-module/fake_bpy_module-20231207-py3-none-any.whl/freestyle/__init__.pyi
@@ -0,0 +1,10 @@
+import sys
+import typing
+from . import functions
+from . import shaders
+from . import chainingiterators
+from . import utils
+from . import predicates
+from . import types
+
+GenericType = typing.TypeVar("GenericType")
