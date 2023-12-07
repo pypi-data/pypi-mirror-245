08c1c682d65c04a59971bb27d6c1e9c493397722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:04:14 +0000

    Add package aicloud-lp-new/0.6.0/aicloud_lp_new-0.6.0-py3-none-any.whl

---
+++ packages/aicloud-lp-new/aicloud_lp_new-0.6.0-py3-none-any.whl/aicloud/__init__.py
@@ -0,0 +1,11 @@
+# aicloudlp/__init__.py
+
+from .users import *
+from .modeling import *
+from .prediction import *
+from .status import *
+from .monitoring import *
+from .plots import *
+from .reports import *
+
+__all__ = ['user', 'modeling', 'prediction', 'status', 'monitoring', 'plots', 'reports']
