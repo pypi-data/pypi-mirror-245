b30a941865853f07ef9b3d52b6bd8e324882b320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 16:03:50 +0000

    Add package astronomer-providers/1.18.4/astronomer_providers-1.18.4-py3-none-any.whl

---
+++ packages/astronomer-providers/astronomer_providers-1.18.4-py3-none-any.whl/astronomer/providers/package.py
@@ -0,0 +1,15 @@
+from typing import Any, Dict
+
+
+def get_provider_info() -> Dict[str, Any]:
+    """Return provider metadata to Airflow"""
+    return {
+        # Required.
+        "package-name": "astronomer-providers",
+        "name": "Astronomer Providers",
+        "description": "Apache Airflow Providers containing Deferrable Operators & Sensors from Astronomer",
+        "versions": "1.18.4",
+        # Optional.
+        "hook-class-names": [],
+        "extra-links": [],
+    }
