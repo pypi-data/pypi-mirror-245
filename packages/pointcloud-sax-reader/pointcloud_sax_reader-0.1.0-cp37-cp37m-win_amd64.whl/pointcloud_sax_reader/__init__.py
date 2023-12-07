eeea1df5129b208eb7ca458d64e23869299e5670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 04:04:30 +0000

    Add package pointcloud-sax-reader/0.1.0/pointcloud_sax_reader-0.1.0-cp37-cp37m-win_amd64.whl

---
+++ packages/pointcloud-sax-reader/pointcloud_sax_reader-0.1.0-cp37-cp37m-win_amd64.whl/pointcloud_sax_reader/__init__.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from ._core import (
+    __doc__,
+    __version__,
+    add,
+    bbox2d_of_pointcloud,
+    bbox3d_of_pointcloud,
+    subtract,
+)
+
+__all__ = [
+    "__doc__",
+    "__version__",
+    "add",
+    "subtract",
+    "bbox2d_of_pointcloud",
+    "bbox3d_of_pointcloud",
+]
