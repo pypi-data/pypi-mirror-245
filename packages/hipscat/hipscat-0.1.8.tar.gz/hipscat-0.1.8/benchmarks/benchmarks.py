ec9bbdf3eda2a2ed4bedba375dbde105f15bb899
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:00 +0000

    Add package hipscat/0.1.8/hipscat-0.1.8.tar.gz

---
+++ packages/hipscat/hipscat-0.1.8.tar.gz/hipscat-0.1.8/benchmarks/benchmarks.py
@@ -0,0 +1,52 @@
+"""Two sample benchmarks to compute runtime and memory usage.
+
+For more information on writing benchmarks:
+https://asv.readthedocs.io/en/stable/writing_benchmarks.html."""
+
+import healpy as hp
+import numpy as np
+
+import hipscat.pixel_math as hist
+from hipscat.catalog import Catalog
+from hipscat.catalog.catalog_info import CatalogInfo
+from hipscat.pixel_math import HealpixPixel
+from hipscat.pixel_tree.pixel_tree_builder import PixelTreeBuilder
+
+
+def time_test_alignment_even_sky():
+    """Create alignment from an even distribution at order 7"""
+    initial_histogram = np.full(hp.order2npix(7), 40)
+    result = hist.generate_alignment(initial_histogram, highest_order=7, threshold=1_000)
+    # everything maps to order 5, given the density
+    for mapping in result:
+        assert mapping[0] == 5
+
+
+def time_test_cone_filter_multiple_order():
+    """Create a catalog cone filter where we have multiple orders in the catalog"""
+    catalog_info = CatalogInfo(
+        **{
+            "catalog_name": "test_name",
+            "catalog_type": "object",
+            "total_rows": 10,
+            "epoch": "J2000",
+            "ra_column": "ra",
+            "dec_column": "dec",
+        }
+    )
+    pixels = [HealpixPixel(6, 30), HealpixPixel(7, 124), HealpixPixel(7, 5000)]
+    catalog = Catalog(catalog_info, pixels)
+    filtered_catalog = catalog.filter_by_cone(47.1, 6, 30)
+    assert filtered_catalog.get_healpix_pixels() == [HealpixPixel(6, 30), HealpixPixel(7, 124)]
+
+
+class Suite:
+    def __init__(self) -> None:
+        """Just initialize things"""
+        self.pixel_list = None
+
+    def setup(self):
+        self.pixel_list = [HealpixPixel(8, pixel) for pixel in np.arange(100000)]
+
+    def time_pixel_tree_creation(self):
+        PixelTreeBuilder.from_healpix(self.pixel_list)
---
+++ packages/hipscat/hipscat-0.1.8.tar.gz/hipscat-0.1.8/src/hipscat/pixel_math/hipscat_id.py
@@ -0,0 +1,101 @@
+"""
+Compute the hipscat index field
+
+This index is defined as a 64-bit integer which has two parts:
+    - healpix pixel (at order 19)
+    - incrementing counter (within same healpix, for uniqueness)
+    
+::
+
+    |------------------------------------------|-------------------|
+    |<-----    healpixel at order 19    ------>|<--   counter   -->|
+
+This provides us with an increasing index, that will not overlap
+between spatially partitioned data files.
+
+See the README in this directory for more information on the fiddly pixel math.
+"""
+
+import healpy as hp
+import numpy as np
+
+HIPSCAT_ID_COLUMN = "_hipscat_index"
+HIPSCAT_ID_HEALPIX_ORDER = 19
+
+
+def compute_hipscat_id(ra_values, dec_values):
+    """Compute the hipscat ID field.
+
+    Args:
+        ra_values (list[float]): celestial coordinates, right ascension
+        dec_values (list[float]): celestial coordinates, declination
+    Returns:
+        one-dimensional array of int64s with hipscat IDs for the sky positions.
+    Raises:
+        ValueError: if the length of the input lists don't match.
+    """
+    if len(ra_values) != len(dec_values):
+        raise ValueError("ra and dec arrays should have the same length")
+
+    ## Construct the bit-shifted healpix segment
+    value_count = len(ra_values)
+    mapped_pixels = hp.ang2pix(2 ** HIPSCAT_ID_HEALPIX_ORDER, ra_values, dec_values, nest=True, lonlat=True)
+
+    ## We sort to put pixels next to each other that will need to be counted.
+    ## This simplifies the counter logic, as we can subtract the index where
+    ## we first see the pixel value from the current index to get the offset counter.
+    sort_index = np.argsort(mapped_pixels, kind="stable")
+    mapped_pixels = mapped_pixels[sort_index]
+
+    ## Construct the counter.
+    _, unique_indices, unique_inverse = np.unique(mapped_pixels, return_inverse=True, return_index=True)
+    unique_indices = unique_indices.astype(np.uint64)
+    boring_number_index = np.arange(value_count, dtype=np.uint64)
+    offset_counter = boring_number_index - unique_indices[unique_inverse]
+
+    ## Add counter to shifted pixel, and map back to the original, unsorted, values
+    shifted_pixels = _compute_hipscat_id_from_mapped_pixels(mapped_pixels, offset_counter)
+
+    unsort_index = np.argsort(sort_index, kind="stable")
+    return shifted_pixels[unsort_index]
+
+
+def _compute_hipscat_id_from_mapped_pixels(mapped_pixels, offset_counter):
+    shifted_pixels = mapped_pixels.astype(np.uint64) << np.uint64(64 - (4 + 2 * HIPSCAT_ID_HEALPIX_ORDER))
+    shifted_pixels = shifted_pixels + offset_counter
+    return shifted_pixels
+
+
+def hipscat_id_to_healpix(ids, target_order=HIPSCAT_ID_HEALPIX_ORDER):
+    """Convert some hipscat ids to the healpix pixel at the specified order
+    This is just bit-shifting the counter away.
+
+    Args:
+        ids (list[int64]): list of well-formatted hipscat ids
+    Returns:
+        list of target_order pixels from the hipscat id
+    """
+    return np.asarray(ids, dtype=np.uint64) >> (64 - (4 + 2 * target_order))
+
+
+def healpix_to_hipscat_id(order: int, pixel: int, counter: int = 0) -> int:
+    """Convert a healpix pixel to a hipscat_id
+
+    This maps the healpix pixel to the lowest pixel number within that pixel at order 19,
+    then shifts and adds the given counter to get a hipscat_id.
+
+    Useful for operations such as filtering by hipscat_id.
+
+    Args:
+        order (int64 | list[int64]): order of pixel to convert
+        pixel (int64 | list[int64]): pixel number in nested ordering of pixel to convert
+        counter (int64 | list[int64]) (Default: 0): counter value in converted hipscat id
+    Returns:
+        list of hipscat ids
+    """
+    order = np.uint64(order)
+    pixel = np.uint64(pixel)
+    counter = np.uint64(counter)
+    pixel_higher_order = pixel * (4 ** (HIPSCAT_ID_HEALPIX_ORDER - order))
+    hipscat_id = _compute_hipscat_id_from_mapped_pixels(pixel_higher_order, counter)
+    return hipscat_id
