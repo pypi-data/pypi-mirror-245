30397032ba67e03c943d67dcba29b2828f7261d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 14:03:30 +0000

    Add package citation-rabbot/0.3/citation-rabbot-0.3.tar.gz

---
+++ packages/citation-rabbot/citation-rabbot-0.3.tar.gz/citation-rabbot-0.3/citation_rabbot/__main__.py
@@ -0,0 +1,32 @@
+import argparse
+import logging
+from neo4j import GraphDatabase
+from telegram.ext import CommandHandler, ApplicationBuilder
+from .arg import add_argument_jump, parse_args_jump
+from .jump import Rabbot
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--auth", type=str, default=None, help=f'Auth to neo4j database.')
+parser.add_argument("--uri", type=str, required=True, help=f'URI to neo4j database.')
+parser.add_argument("--token", type=str, required=True, help=f'Telegram bot token.')
+add_argument_jump(parser)
+
+
+args = parser.parse_args()
+jump_list = parse_args_jump(parser)
+
+
+async def start(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a rabbit jumping in citation database")
+
+application = ApplicationBuilder().token(args.token).build()
+application.add_handler(CommandHandler('start', start))
+with GraphDatabase.driver(args.uri, auth=args.auth) as driver:
+    with driver.session() as session:
+        rabbot = Rabbot(bot=application, session=session)
+        application.run_polling()
