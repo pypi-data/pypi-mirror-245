c5e9a26d76e54c0ef5296102ac2c4512f2141707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:02:41 +0000

    Add package defog/0.54.2/defog-0.54.2.tar.gz

---
+++ packages/defog/defog-0.54.2.tar.gz/defog-0.54.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+extras = {
+    "postgres": ["psycopg2-binary"],
+    "mysql": ["mysql-connector-python"],
+    "snowflake": ["snowflake-connector-python"],
+    "bigquery": ["google-cloud-bigquery"],
+    "redshift": ["psycopg2-binary"],
+}
+
+setup(
+    name="defog",
+    packages=find_packages(),
+    package_data={"defog": ["gcp/*", "aws/*", "static/*"]},
+    version="0.54.2",
+    description="Defog is a Python library that helps you generate data queries from natural language questions.",
+    author="Full Stack Data Pte. Ltd.",
+    license="MIT",
+    # install_requires=requirements,
+    install_requires=[
+        "requests>=2.28.2",
+        "psycopg2-binary>=2.9.5",
+        "prompt-toolkit>=3.0.38",
+        "fastapi",
+        "uvicorn",
+    ],
+    entry_points={
+        "console_scripts": [
+            "defog=defog.cli:main",
+        ],
+    },
+    author_email="founders@defog.ai",
+    url="https://github.com/defog-ai/defog-python",
+    long_description="Defog is a Python library that helps you generate data queries from natural language questions.",
+    long_description_content_type="text/markdown",
+    extras_require=extras,
+)
