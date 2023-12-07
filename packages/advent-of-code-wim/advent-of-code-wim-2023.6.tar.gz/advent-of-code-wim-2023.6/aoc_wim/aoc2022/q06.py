e3f9c470b31cc2f2de4094873d9401692294e055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:03:47 +0000

    Add package advent-of-code-wim/2023.6/advent-of-code-wim-2023.6.tar.gz

---
+++ packages/advent-of-code-wim/advent-of-code-wim-2023.6.tar.gz/advent-of-code-wim-2023.6/aoc_wim/aoc2022/q06.py
@@ -0,0 +1,20 @@
+"""
+--- Day 6: Tuning Trouble ---
+https://adventofcode.com/2022/day/6
+"""
+from aocd import data
+from collections import Counter
+
+
+def marker(data, n):
+    counts = Counter(data[:n])
+    i = n
+    while len(counts) < n:
+        if data[i] != data[i - n]:
+            counts += {data[i]: 1, data[i - n]: -1}
+        i += 1
+    return i
+
+
+print("answer_a:", marker(data, 4))
+print("answer_b:", marker(data, 14))
