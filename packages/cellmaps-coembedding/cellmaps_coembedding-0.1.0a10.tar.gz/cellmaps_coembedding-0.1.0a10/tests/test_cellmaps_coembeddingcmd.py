804f4b484b13745b9b4f4e83fd3b5a03fa405425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:34 +0000

    Add package cellmaps-coembedding/0.1.0a10/cellmaps_coembedding-0.1.0a10.tar.gz

---
+++ packages/cellmaps-coembedding/cellmaps_coembedding-0.1.0a10.tar.gz/cellmaps_coembedding-0.1.0a10/tests/test_cellmaps_coembeddingcmd.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `cellmaps_coembedding` package."""
+
+import os
+import tempfile
+import shutil
+
+import unittest
+from cellmaps_coembedding import cellmaps_coembeddingcmd
+
+
+class TestCellmapsCoEmbedding(unittest.TestCase):
+    """Tests for `cellmaps_coembedding` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_parse_arguments(self):
+        """Tests parse arguments"""
+        res = cellmaps_coembeddingcmd._parse_arguments('hi', ['foo',
+                                                               '--ppi_embeddingdir',
+                                                               'apms',
+                                                               '--image_embeddingdir',
+                                                               'image'])
+
+        self.assertEqual(res.verbose, 1)
+        self.assertEqual(res.logconf, None)
+
+        someargs = ['-vv', '--logconf', 'hi',
+                    'foo',
+                    '--ppi_embeddingdir',
+                    'apms',
+                    '--image_embeddingdir',
+                    'image']
+        res = cellmaps_coembeddingcmd._parse_arguments('hi', someargs)
+
+        self.assertEqual(3, res.verbose)
+        self.assertEqual('hi', res.logconf)
+        self.assertEqual('foo', res.outdir)
+        self.assertEqual('apms', res.ppi_embeddingdir)
+        self.assertEqual('image', res.image_embeddingdir)
+
+    def test_main(self):
+        """Tests main function"""
+
+        # try where loading config is successful
+        try:
+            temp_dir = tempfile.mkdtemp()
+            res = cellmaps_coembeddingcmd.main(['myprog.py',
+                                                'foo',
+                                                '--ppi_embeddingdir',
+                                                'apms',
+                                                '--image_embeddingdir',
+                                                'image'])
+            self.assertEqual(2, res)
+        finally:
+            shutil.rmtree(temp_dir)
