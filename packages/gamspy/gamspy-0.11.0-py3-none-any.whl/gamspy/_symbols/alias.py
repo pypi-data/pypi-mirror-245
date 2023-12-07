593b3b61dbe24fc43e3a32f1f618442212ab9403
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 04:03:30 +0000

    Add package gamspy/0.11.0/gamspy-0.11.0-py3-none-any.whl

---
+++ packages/gamspy/gamspy-0.11.0-py3-none-any.whl/gamspy/_symbols/alias.py
@@ -0,0 +1,227 @@
+#
+# GAMS - General Algebraic Modeling System Python API
+#
+# Copyright (c) 2023 GAMS Development Corp. <support@gams.com>
+# Copyright (c) 2023 GAMS Software GmbH <support@gams.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+from __future__ import annotations
+
+from typing import Literal
+from typing import TYPE_CHECKING
+from typing import Union
+
+import gams.transfer as gt
+
+import gamspy as gp
+import gamspy._algebra.condition as condition
+import gamspy._algebra.expression as expression
+import gamspy._algebra.operable as operable
+import gamspy._symbols.implicits as implicits
+import gamspy.utils as utils
+from gamspy._symbols.symbol import Symbol
+
+if TYPE_CHECKING:
+    from gamspy._symbols.implicits.implicit_set import ImplicitSet
+    from gamspy import Set, Container
+    from gamspy._algebra.expression import Expression
+
+
+class Alias(gt.Alias, operable.Operable, Symbol):
+    """
+    Represents an Alias symbol in GAMS.
+    https://www.gams.com/latest/docs/UG_SetDefinition.html#UG_SetDefinition_TheAliasStatementMultipleNamesForASet
+
+    Parameters
+    ----------
+    container : Container
+    name : str
+    alias_with : Set
+
+    Examples
+    --------
+    >>> import gamspy as gp
+    >>> m = gp.Container()
+    >>> i = gp.Set(m, "i")
+    >>> j = gp.Alias(m, "j", i)
+
+    """
+
+    def __new__(cls, container: Container, name: str, alias_with: Set):
+        if not isinstance(container, gp.Container):
+            raise TypeError(
+                "Container must of type `Container` but found"
+                f" {type(container)}"
+            )
+
+        if not isinstance(name, str):
+            raise TypeError(f"Name must of type `str` but found {type(name)}")
+
+        try:
+            symobj = container[name]
+            if isinstance(symobj, cls):
+                return symobj
+            else:
+                raise TypeError(
+                    f"Cannot overwrite symbol `{name}` in container"
+                    " because it is not an Alias object)"
+                )
+        except KeyError:
+            return object.__new__(Alias)
+
+    def __init__(self, container: Container, name: str, alias_with: Set):
+        # enable load on demand
+        self._is_dirty = False
+
+        # check if the name is a reserved word
+        name = utils._reservedCheck(name)
+
+        super().__init__(container, name, alias_with)
+
+        self._container_check(self.domain)
+
+        # allow conditions
+        self.where = condition.Condition(self)
+
+        # add statement
+        self.container._addStatement(self)
+
+        # iterator index
+        self._current_index = 0
+
+        # for records and setRecords
+        self._is_assigned = True
+
+    def __len__(self):
+        if self.records is not None:
+            return len(self.records.index)
+
+        return 0
+
+    def __next__(self):
+        if self._current_index < len(self):
+            row = self.records.iloc[self._current_index]
+            self._current_index += 1
+            return row
+
+        self._current_index = 0
+        raise StopIteration
+
+    def __iter__(self):
+        return self
+
+    def lag(
+        self,
+        n: Union[int, Symbol, Expression],
+        type: Literal["linear", "circular"] = "linear",
+    ) -> ImplicitSet:
+        """Lag operation shifts the values of a Set or Alias by one to the left
+
+        Parameters
+        ----------
+        n : int | Symbol | Expression
+        type : 'linear' or 'circular', optional
+
+        Returns
+        -------
+        ImplicitSet
+
+        Raises
+        ------
+        ValueError
+            When type is not circular or linear
+        """
+        jump = n if isinstance(n, int) else n.gamsRepr()  # type: ignore
+
+        if type == "circular":
+            return implicits.ImplicitSet(self, name=f"{self.name} -- {jump}")
+        elif type == "linear":
+            return implicits.ImplicitSet(self, name=f"{self.name} - {jump}")
+
+        raise ValueError("Lag type must be linear or circular")
+
+    def lead(
+        self,
+        n: Union[int, Symbol, Expression],
+        type: Literal["linear", "circular"] = "linear",
+    ) -> ImplicitSet:
+        """
+        Lead shifts the values of a Set or Alias by one to the right
+
+        Parameters
+        ----------
+        n : int | Symbol | Expression
+        type : 'linear' or 'circular', optional
+
+        Returns
+        -------
+        ImplicitSet
+
+        Raises
+        ------
+        ValueError
+            When type is not circular or linear
+        """
+        jump = n if isinstance(n, int) else n.gamsRepr()  # type: ignore
+
+        if type == "circular":
+            return implicits.ImplicitSet(self, name=f"{self.name} ++ {jump}")
+        elif type == "linear":
+            return implicits.ImplicitSet(self, name=f"{self.name} + {jump}")
+
+        raise ValueError("Lead type must be linear or circular")
+
+    def sameAs(self, other: Union[Set, Alias]) -> Expression:
+        return expression.Expression(
+            "sameAs(", ",".join([self.gamsRepr(), other.gamsRepr()]), ")"
+        )
+
+    def gamsRepr(self) -> str:
+        """
+        Representation of this Alias in GAMS language.
+
+        Returns
+        -------
+        str
+        """
+        return self.name
+
+    def getStatement(self) -> str:
+        """
+        Statement of the Alias definition
+
+        Returns
+        -------
+        str
+        """
+        return f"Alias({self.alias_with.name},{self.name});"
+
+    @property
+    def records(self):
+        return super().records
+
+    @records.setter
+    def records(self, records):
+        self.alias_with._is_assigned = True
+        self.alias_with.records = records
+
+    def setRecords(self, records, uels_on_axes=False):
+        self.alias_with._is_assigned = True
+        super().setRecords(records, uels_on_axes)
