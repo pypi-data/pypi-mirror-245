56b55db79a94336eeb0b50ff7bb032ed65f3f622
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:03:15 +0000

    Add package RefractDQ/1.3.1/RefractDQ-1.3.1.tar.gz

---
+++ packages/RefractDQ/RefractDQ-1.3.1.tar.gz/RefractDQ-1.3.1/data_quality/init_script.sh
@@ -0,0 +1,5 @@
+echo "Picking dependency packages from NAS /custom_plugin/data_quality";
+pip install pandas-profiling==3.6.6;
+pip install bs4==0.0.1;
+pip install refractio[all]==2.0.5.9;
+echo "Done with dependency installtion!";
