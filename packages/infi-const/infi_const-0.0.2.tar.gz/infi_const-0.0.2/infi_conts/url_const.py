cb27ce608bfd03c1b72126cca920d8435cb1b8d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 03:02:39 +0000

    Add package infi-const/0.0.2/infi_const-0.0.2.tar.gz

---
+++ packages/infi-const/infi_const-0.0.2.tar.gz/infi_const-0.0.2/infi_conts/url_const.py
@@ -0,0 +1,17 @@
+PREPROCESS_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8081/activate'
+
+TRIGGER_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8082/docs#/'
+
+EMAIL_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8083/emails/send_email'
+
+TGV_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8084/tgv_process'
+
+FLIGHTS_RECORD_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8085/docs#/'
+
+AZCOPY_SERVICE_URL: str = 'http://infinity-serivces.westeurope.cloudapp.azure.com:8087/'
+
+SKYLINE_SERVICE_URL: str = 'https://pmapi.skylineglobe.com/startproject/Mipui/poda/a247acd3-977c-4a77-b93f-e713a8632d57'
+
+WALL_E_SOLUTION_SKYLINE_SERVICE: str = 'https://pmapi.skylineglobe.com/startproject/Mipui/poda/a247acd3-977c-4a77-b93f-e713a8632d57'
+
+WAR_E_SOLUTION_SKYLINE_SERVICE: str = 'https://pmapi.skylineglobe.com/startproject/Mipui/podb/a247acd3-977c-4a77-b93f-e713a8632d57'
---
+++ packages/infi-const/infi_const-0.0.2.tar.gz/infi_const-0.0.2/infi_conts/utils_consts.py
@@ -0,0 +1,9 @@
+EMAIL_LIST_NAVAD: list[str] = ["liorfranko587@gmail.com, amit.shwartz01@gmail.com, itay@skylinesoft.com,\
+                           ronik1917@gmail.com, Daniel@iit.co.il, Bason_tehila@hotmail.com,\
+                           Ronnie@skylinesoft.com, liorcliorc@gmail.com, razraz2711@gmail.com,\
+                           shmuelmorr@gmail.com, shacharba@matrix.co.il, stamdvarim1000@gmail.com,\
+                           Rotembh20000@gmail.com"]
+
+EMAIL_TEST_LIST: list[str] = ["infinitymailbox9900@gmail.com,stamdvarim1000@gmail.com"]
+
+RABBIT_HOST: str = 'localhost'
