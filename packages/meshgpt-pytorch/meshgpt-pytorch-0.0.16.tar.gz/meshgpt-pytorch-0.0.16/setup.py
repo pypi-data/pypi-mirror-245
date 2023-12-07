78147a81f5e9d85bf769083f080f5cbf754f651c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 04:03:50 +0000

    Add package meshgpt-pytorch/0.0.16/meshgpt-pytorch-0.0.16.tar.gz

---
+++ packages/meshgpt-pytorch/meshgpt-pytorch-0.0.16.tar.gz/meshgpt-pytorch-0.0.16/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'meshgpt-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.16',
+  license='MIT',
+  description = 'MeshGPT Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/meshgpt-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'attention mechanisms',
+    'transformers',
+    'mesh generation'
+  ],
+  install_requires=[
+    'accelerate',
+    'beartype',
+    'classifier-free-guidance-pytorch>=0.1.4',
+    'einops>=0.7.0',
+    'ema-pytorch',
+    'torch>=2.0',
+    'torch_geometric',
+    'torchtyping',
+    'vector-quantize-pytorch>=1.11.8',
+    'x-transformers>=1.26.0',
+    'tqdm'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
