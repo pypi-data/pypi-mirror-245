144327e09aa76b14819f0efb13715a6a8cdb3549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:04:47 +0000

    Add package brainpy/2.4.6.post2/brainpy-2.4.6.post2-py3-none-any.whl

---
+++ packages/brainpy/brainpy-2.4.6.post2-py3-none-any.whl/brainpy/math/others.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from brainpy._src.math.others import (
+  shared_args_over_time as shared_args_over_time,
+  remove_diag as remove_diag,
+  clip_by_norm as clip_by_norm,
+)
+
+from brainpy._src.math.object_transform.naming import (
+  clear_name_cache,
+)
