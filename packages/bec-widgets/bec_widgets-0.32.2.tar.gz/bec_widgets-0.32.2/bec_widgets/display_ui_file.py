1504751db4c46f95f1c80376573f1a49140f592f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:16 +0000

    Add package bec-widgets/0.32.2/bec_widgets-0.32.2.tar.gz

---
+++ packages/bec-widgets/bec_widgets-0.32.2.tar.gz/bec_widgets-0.32.2/bec_widgets/display_ui_file.py
@@ -0,0 +1,33 @@
+import os
+import sys
+
+from qtpy import QtWidgets, uic
+
+
+class UI(QtWidgets.QWidget):
+    def __init__(self, uipath):
+        super().__init__()
+
+        self.ui = uic.loadUi(uipath, self)
+
+        _, fname = os.path.split(uipath)
+        self.setWindowTitle(fname)
+
+        self.show()
+
+
+def main():
+    """A basic script to display UI file
+
+    Run the script, passing UI file path as an argument, e.g.
+    $ python bec_widgets/display_ui_file.py bec_widgets/line_plot.ui
+    """
+    app = QtWidgets.QApplication(sys.argv)
+
+    UI(sys.argv[1])
+
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main()
