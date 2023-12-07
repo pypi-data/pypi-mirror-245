7b5670a16f101ab8cd2aac37516172d473dcbd7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 04:04:02 +0000

    Add package rk-extractor/0.3.6/rk_extractor-0.3.6.tar.gz

---
+++ packages/rk-extractor/rk_extractor-0.3.6.tar.gz/rk_extractor-0.3.6/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+import os
+import glob
+
+#----------------------------------------------
+def get_scripts(dir_path):
+    l_obj = glob.glob(f'{dir_path}/*')
+    l_scr = [ obj for obj in l_obj if os.path.isfile(obj)]
+
+    return l_scr
+#----------------------------------------------
+setup(
+        name              = 'rk_extractor',
+        version           = '0.3.6',
+        description       = 'Used to extract RK from simultaneous fits',
+        scripts           = get_scripts('scripts/jobs') + get_scripts('scripts/offline'),
+        long_description  = '',
+        package_dir       = {'' : 'src'},
+        packages          = [''],
+        install_requires  = open('requirements.txt').read().splitlines()
+        )
+
