18a78971d73b45019ec1fe6f5902252beb8bb683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:04:26 +0000

    Add package sphinx-summaries/0.0.4/sphinx_summaries-0.0.4-py3-none-any.whl

---
+++ packages/sphinx-summaries/sphinx_summaries-0.0.4-py3-none-any.whl/sphinx-summaries/__init__.py
@@ -0,0 +1,29 @@
+import json
+import hashlib
+
+import docutils
+import transformers
+
+summarizer = transformers.pipeline('summarization', model='google/pegasus-xsum')
+data = {}
+
+def generate_summary(app, doctree, docname):
+    text = doctree.astext()
+    hash = hashlib.md5(text.encode('utf-8')).hexdigest()
+    summary = summarizer(text)['summary_text']
+    data[docname] = {
+        'hash': hash,
+        'summary': summary
+    }
+
+def dump(app, exception):
+    print(json.dumps(data, indent=4))
+
+def setup(app):
+    app.connect('doctree-resolved', generate_summary)
+    app.connect('build-finished', dump)
+    return {
+        'version': '0.0.4',
+        'parallel_read_safe': True,
+        'parallel_write_safe': True,
+    }
