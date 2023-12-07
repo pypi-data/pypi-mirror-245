ff8b96b021bd4d910fe57c77886f1d764766f59e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 15:04:32 +0000

    Add package fincookie/0.2.1/fincookie-0.2.1.tar.gz

---
+++ packages/fincookie/fincookie-0.2.1.tar.gz/fincookie-0.2.1/fincookie/__init__.py
@@ -0,0 +1,10 @@
+from fincookie.api import set_dev, get_cookies, get_logs, get_loads, cookie_format, headers_format, proxy_format
+from importlib.metadata import version as __version__
+
+__title__ = "fincookie"
+__description__ = "大智慧远程获取Cookies集群API"
+__url__ = "https://gitee.com/yl153977/finchina-remote-cookies"
+__version__ = __version__(__name__)
+__author__ = "胡永乐"
+__author_email__ = "huyl3@finchina.com"
+__license__ = "Apache 2.0"
