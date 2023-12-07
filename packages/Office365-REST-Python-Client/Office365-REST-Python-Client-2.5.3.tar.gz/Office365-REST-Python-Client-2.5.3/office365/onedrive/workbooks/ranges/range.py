5f70cf1d3c21240ab253955eefe2b11a2fdfe54a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 06:04:21 +0000

    Add package Office365-REST-Python-Client/2.5.3/Office365-REST-Python-Client-2.5.3.tar.gz

---
+++ packages/Office365-REST-Python-Client/Office365-REST-Python-Client-2.5.3.tar.gz/Office365-REST-Python-Client-2.5.3/office365/onedrive/workbooks/ranges/range.py
@@ -0,0 +1,76 @@
+from typing import Optional
+
+from office365.entity import Entity
+from office365.onedrive.workbooks.ranges.format import WorkbookRangeFormat
+from office365.onedrive.workbooks.ranges.sort import WorkbookRangeSort
+from office365.onedrive.workbooks.ranges.view import WorkbookRangeView
+from office365.runtime.paths.resource_path import ResourcePath
+from office365.runtime.queries.function import FunctionQuery
+
+
+class WorkbookRange(Entity):
+    """Range represents a set of one or more contiguous cells such as a cell, a row, a column, block of cells, etc."""
+
+    def visible_view(self):
+        """"""
+        return_type = WorkbookRangeView(self.context)
+        qry = FunctionQuery(self, "visibleView", return_type=return_type)
+        self.context.add_query(qry)
+        return return_type
+
+    @property
+    def address(self):
+        # type: () -> Optional[str]
+        """
+        Represents the range reference in A1-style. Address value will contain the Sheet reference
+        (e.g. Sheet1!A1:B4)
+        """
+        return self.properties.get("address", None)
+
+    @property
+    def address_local(self):
+        # type: () -> Optional[str]
+        """Represents range reference for the specified range in the language of the user."""
+        return self.properties.get("addressLocal", None)
+
+    @property
+    def cell_count(self):
+        # type: () -> Optional[int]
+        """Number of cells in the range. Read-only."""
+        return self.properties.get("cellCount", None)
+
+    @property
+    def column_count(self):
+        # type: () -> Optional[int]
+        """Represents the total number of columns in the range. Read-only."""
+        return self.properties.get("columnCount", None)
+
+    @property
+    def format(self):
+        """Returns a format object, encapsulating the range's font, fill, borders, alignment, and other properties"""
+        return self.properties.get(
+            "format",
+            WorkbookRangeFormat(
+                self.context, ResourcePath("format", self.resource_path)
+            ),
+        )
+
+    @property
+    def sort(self):
+        """The worksheet containing the current range."""
+        return self.properties.get(
+            "sort",
+            WorkbookRangeSort(self.context, ResourcePath("sort", self.resource_path)),
+        )
+
+    @property
+    def worksheet(self):
+        """The worksheet containing the current range"""
+        from office365.onedrive.workbooks.worksheets.worksheet import WorkbookWorksheet
+
+        return self.properties.get(
+            "worksheet",
+            WorkbookWorksheet(
+                self.context, ResourcePath("worksheet", self.resource_path)
+            ),
+        )
