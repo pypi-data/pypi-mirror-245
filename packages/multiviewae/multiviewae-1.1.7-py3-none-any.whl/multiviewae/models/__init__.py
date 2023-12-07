aad092f81225789789be0e793669d4a23218d8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-07 05:04:19 +0000

    Add package multiviewae/1.1.7/multiviewae-1.1.7-py3-none-any.whl

---
+++ packages/multiviewae/multiviewae-1.1.7-py3-none-any.whl/multiviewae/models/__init__.py
@@ -0,0 +1,22 @@
+from .ae import *
+from .maae import *
+from .mwae import *
+from .mcvae import *
+from .mvae import *
+from .mmvae import *
+from .mvtcae import *
+from .dvcca import *
+from .me_mvae import *
+from .jmvae import *
+from .mopoevae import *
+from .mmJSD import *
+from .weighted_mvae import *
+from .vae_barlow import *
+from .ae_barlow import *
+from .DMVAE import *
+from .weighted_DMVAE import *
+from .mmvaeplus import *
+__all__ = ["AE", "mAAE", "mWAE", "mcVAE", "mVAE", "JMVAE", "me_mVAE", "mmVAE", "mvtCAE", "DVCCA", "MoPoEVAE", "mmJSD", "weighted_mVAE",
+            "VAE_barlow", "AE_barlow", "DMVAE", "weighted_DMVAE", "mmVAEPlus"]
+classes = __all__ 
+
