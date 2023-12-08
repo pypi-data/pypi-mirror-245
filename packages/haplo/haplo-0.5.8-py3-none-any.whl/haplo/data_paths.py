c7a62c0563654beff39865cb7c5f4d82d64fd653
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 05:04:14 +0000

    Add package haplo/0.5.8/haplo-0.5.8-py3-none-any.whl

---
+++ packages/haplo/haplo-0.5.8-py3-none-any.whl/haplo/data_paths.py
@@ -0,0 +1,28 @@
+import re
+import shutil
+import socket
+from pathlib import Path
+
+from filelock import FileLock
+
+def move_path_to_nvme(path: Path) -> Path:
+    match = re.match(r"gpu\d{3}", socket.gethostname())
+    if match is not None:
+        nvme_path = Path("/lscratch/golmsche").joinpath(path)
+        if not nvme_path.exists():
+            nvme_path.parent.mkdir(exist_ok=True, parents=True)
+            nvme_lock_path = nvme_path.parent.joinpath(nvme_path.name + '.lock')
+            lock = FileLock(str(nvme_lock_path))
+            with lock.acquire():
+                if not nvme_path.exists():
+                    nvme_tmp_path = nvme_path.parent.joinpath(nvme_path.name + '.tmp')
+                    shutil.copy(path, nvme_tmp_path)
+                    nvme_tmp_path.rename(nvme_path)
+        return nvme_path
+    else:
+        return path
+
+rotated_dataset_path = Path('data/640m_rotated_parameters_and_phase_amplitudes.arrow')
+constantinos_kalapotharakos_format_rotated_dataset_path = Path('data/mcmc_vac_all_640m_A.dat')
+unrotated_dataset_path = Path('data/800k_parameters_and_phase_amplitudes.arrow')
+constantinos_kalapotharakos_format_unrotated_dataset_path = Path('data/mcmc_vac_all_800k.dat')
