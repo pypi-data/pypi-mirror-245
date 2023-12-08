c9813abfb222da6a2d2b7fd228d2221415bcdffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:50 +0000

    Add package flexmeasures/0.17.1/flexmeasures-0.17.1-py3-none-any.whl

---
+++ packages/flexmeasures/flexmeasures-0.17.1-py3-none-any.whl/flexmeasures/data/models/planning/exceptions.py
@@ -0,0 +1,22 @@
+class MissingAttributeException(Exception):
+    pass
+
+
+class UnknownMarketException(Exception):
+    pass
+
+
+class UnknownForecastException(Exception):
+    pass
+
+
+class UnknownPricesException(Exception):
+    pass
+
+
+class WrongTypeAttributeException(Exception):
+    pass
+
+
+class InfeasibleProblemException(Exception):
+    pass
