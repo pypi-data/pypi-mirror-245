3f773cf4e45503cbfc9d16a6f1c681261769ac62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 05:03:32 +0000

    Add package openlineage-airflow/1.6.2/openlineage-airflow-1.6.2.tar.gz

---
+++ packages/openlineage-airflow/openlineage-airflow-1.6.2.tar.gz/openlineage-airflow-1.6.2/openlineage/airflow/__init__.py
@@ -0,0 +1,26 @@
+# Copyright 2018-2023 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
+#
+# -*- coding: utf-8 -*-
+import logging
+
+from openlineage.airflow.version import __version__
+from pkg_resources import parse_version
+
+from airflow.version import version as AIRFLOW_VERSION
+
+__author__ = """OpenLineage"""
+
+if parse_version(AIRFLOW_VERSION) < parse_version("2.0.0"):  # type: ignore
+    logging.warning(
+        f"""
+        OpenLineage support for Airflow version {AIRFLOW_VERSION} is REMOVED.
+        Please make sure to upgrade your Airflow version to minimum of 2.0.0
+        in order to continue using OpenLineage.
+        """
+    )
+
+from airflow.models import DAG
+
+__all__ = ["DAG", "__version__"]
---
+++ packages/openlineage-airflow/openlineage-airflow-1.6.2.tar.gz/openlineage-airflow-1.6.2/tests/test_openlineage_adapter.py
@@ -0,0 +1,72 @@
+# Copyright 2018-2023 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+import os
+from unittest import mock
+from unittest.mock import MagicMock, patch
+
+from openlineage.airflow.adapter import OpenLineageAdapter
+
+
+@patch.dict(os.environ, {"MARQUEZ_URL": "http://marquez:5000", "MARQUEZ_API_KEY": "api-key"})
+def test_create_client_from_marquez_url():
+    client = OpenLineageAdapter().get_or_create_openlineage_client()
+    assert client.transport.url == "http://marquez:5000"
+
+
+@patch.dict(
+    os.environ,
+    {"OPENLINEAGE_URL": "http://ol-api:5000", "OPENLINEAGE_API_KEY": "api-key"},
+)
+def test_create_client_from_ol_env():
+    client = OpenLineageAdapter().get_or_create_openlineage_client()
+    assert client.transport.url == "http://ol-api:5000"
+
+
+def test_setting_ol_adapter_log_level() -> None:
+    # DEBUG level set for `openlineage` logger in tests setup
+    default_log_level = logging.DEBUG
+    # without environment variable
+    OpenLineageAdapter()
+    parent_logger = logging.getLogger("openlineage.airflow")
+    logger = logging.getLogger("openlineage.airflow.adapter")
+    assert parent_logger.getEffectiveLevel() == default_log_level
+    assert logger.getEffectiveLevel() == default_log_level
+    with patch.dict(os.environ, {"OPENLINEAGE_AIRFLOW_LOGGING": "CRITICAL"}):
+        assert parent_logger.getEffectiveLevel() == default_log_level
+        assert logger.getEffectiveLevel() == default_log_level
+        OpenLineageAdapter()
+        assert parent_logger.getEffectiveLevel() == logging.CRITICAL
+        assert logger.getEffectiveLevel() == logging.CRITICAL
+
+
+@patch("openlineage.airflow.adapter.OpenLineageAdapter.get_or_create_openlineage_client")
+@patch("openlineage.airflow.adapter.redact_with_exclusions")
+@patch("openlineage.airflow.adapter.Stats.incr")
+@patch("openlineage.airflow.adapter.Stats.timer")
+def test_openlineage_adapter_stats_emit_success(
+    mock_stats_timer, mock_stats_incr, mock_redact, mock_get_client
+):
+    adapter = OpenLineageAdapter()
+
+    adapter.emit(MagicMock())
+
+    mock_stats_incr.assert_not_called()
+    mock_stats_timer.assert_called_with("ol.emit.attempts")
+
+
+@patch("openlineage.airflow.adapter.OpenLineageAdapter.get_or_create_openlineage_client")
+@patch("openlineage.airflow.adapter.redact_with_exclusions")
+@patch("openlineage.airflow.adapter.Stats.incr")
+@patch("openlineage.airflow.adapter.Stats.timer")
+def test_openlineage_adapter_stats_emit_failed(
+    mock_stats_timer, mock_stats_incr, mock_redact, mock_get_client
+):
+    adapter = OpenLineageAdapter()
+    mock_get_client.return_value.emit.side_effect = Exception()
+
+    adapter.emit(MagicMock())
+
+    mock_stats_timer.assert_called_with("ol.emit.attempts")
+    mock_stats_incr.assert_has_calls([mock.call("ol.emit.failed")])
