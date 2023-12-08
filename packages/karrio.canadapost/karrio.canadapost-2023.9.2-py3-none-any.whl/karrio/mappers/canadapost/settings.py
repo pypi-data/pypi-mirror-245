b754fae736d688f2feba0788102d06c43f533baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:28 +0000

    Add package karrio.canadapost/2023.9.2/karrio.canadapost-2023.9.2-py3-none-any.whl

---
+++ packages/karrio.canadapost/karrio.canadapost-2023.9.2-py3-none-any.whl/karrio/mappers/canadapost/settings.py
@@ -0,0 +1,22 @@
+"""Karrio Canada post client settings."""
+
+import attr
+from karrio.providers.canadapost import Settings as BaseSettings
+
+
+@attr.s(auto_attribs=True)
+class Settings(BaseSettings):
+    """Canada post connection settings."""
+
+    username: str
+    password: str
+    customer_number: str = None
+    contract_id: str = None
+    language: str = "en"
+
+    id: str = None
+    test_mode: bool = False
+    carrier_id: str = "canadapost"
+    account_country_code: str = "CA"
+    metadata: dict = {}
+    config: dict = {}
