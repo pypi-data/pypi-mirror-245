bc51f821a75fc01c0d02d20076aeff2e7622ae8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:04:13 +0000

    Add package CS2APY/0.5/CS2APY-0.5.tar.gz

---
+++ packages/CS2APY/CS2APY-0.5.tar.gz/CS2APY-0.5/CS2APY/__init__.py
@@ -0,0 +1,3 @@
+from . import CS2APY
+from .scoreboard_pb2 import ScoreLeaderboardData
+from .CS2APY import fetch_all, fetch_global, fetch_europe, fetch_africa, fetch_asia, fetch_australia, fetch_china, fetch_northamerica, fetch_southamerica
