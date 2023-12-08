426c09ee25e736a2032a0f9a3a7612bc5ddfe56c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:19 +0000

    Add package sonic-engine/2.1.12/sonic_engine-2.1.12.tar.gz

---
+++ packages/sonic-engine/sonic_engine-2.1.12.tar.gz/sonic_engine-2.1.12/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+VERSION = "2.1.12"
+
+__authors__ = ["Soufiane Oualil", "Ahmed Bargady"]
+__contact__ = ["https://github.com/sooualil", "https://github.com/AhmedCoolProjects"]
+__copyright__ = "Copyright 2022, AtlasSonic"
+__date__ = "2023/07/17"
+__deprecated__ = False
+__email__ = ["someone@um6p.ma", "ahmed.bargady@outlook.com"]
+__license__ = "MIT"
+__maintainer__ = "developer"
+__status__ = "Development"
+__version__ = VERSION
+
+import sys
+import setuptools
+
+if not sys.version_info >= (3, 7):
+    sys.exit('Sorry, python3.7+ is required for this package')
+
+setuptools.setup(
+    name="sonic_engine",
+    version=VERSION,
+    author="Soufiane Oualil, Ahmed Bargady",
+    author_email="someone@um6p.ma, ahmed.bargady@outlook.com",
+    description="Sonic Engine is a python package for the AtlasSonic project",
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    install_requires=["pyaml == 23.7.0", "redis == 4.2.2",
+                      "numpy", "pytest", "yapsy", "flask"]
+)
