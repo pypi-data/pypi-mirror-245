5ba0a15658a6f704a3232584833b07b23b980066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:04:02 +0000

    Add package aws-cf/0.0.8/aws-cf-0.0.8.tar.gz

---
+++ packages/aws-cf/aws-cf-0.0.8.tar.gz/aws-cf-0.0.8/aws-cf/diff.py
@@ -0,0 +1,29 @@
+from utils.logging import logger
+from utils.config import Config
+import sys
+from utils.common import create_change_set, remove_change_set, format_diff
+
+
+if __name__ == "__main__":
+    config_path = sys.argv[1]
+    root_path = sys.argv[2]
+
+    logger.warning(f"Checking difference for stacks from file {config_path}")
+
+    config = Config.parse(config_path)
+    services = config.Stacks
+
+    logger.info(f"* Found {len(services)} services checking differences...")
+
+    for service in services:
+        change_set = create_change_set(service.name, service.path, root_path, config)
+        diffs = [format_diff(change)for change in change_set["Changes"]]
+
+        if len(diffs):
+            logger.warning(f"Found {len(diffs)} differences for the stack {service.name}")
+            for diff in diffs:
+                logger.warning(f"> {diff}")
+        else:
+            logger.info(f"Found no differences for the stack {service.name}")
+
+        remove_change_set(service.name, change_set["ChangeSetName"])
---
+++ packages/aws-cf/aws-cf-0.0.8.tar.gz/aws-cf-0.0.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='aws-cf',
+    version='0.0.8',    
+    description='Simple way to deploy AWS stacks',
+    long_description='Simple way to deploy AWS stacks',
+    url='https://github.com/erikschmutz/aws-cf/',
+    author='Erik Rehn',
+    author_email='erik.rehn98@gmail.com',
+    license='BSD 2-clause',
+    packages=['aws-cf'],
+    install_requires=['pydantic', 'boto3'],
+
+    classifiers=[],
+)
