f6b484e9d4103805b9e05d5e5f0845e66486af2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:32 +0000

    Add package dbdesigner/2.0.3/dbdesigner-2.0.3.tar.gz

---
+++ packages/dbdesigner/dbdesigner-2.0.3.tar.gz/dbdesigner-2.0.3/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# created by marco on 2023/12/8
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dbdesigner",
+    version="2.0.3",
+    packages=find_packages(),
+    install_requires=[
+        'mysqlclient',
+        'psycopg2',
+        'pytz',
+        'sqlparse'
+    ],
+
+    author="chaos-ma",
+    author_email="630950058@qq.com",
+    description="DBDesigner",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
