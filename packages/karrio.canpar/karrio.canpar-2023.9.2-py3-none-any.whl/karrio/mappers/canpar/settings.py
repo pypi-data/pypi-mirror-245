49ab9b26cd959bd0619dbd1ae1ef2c6d5cf741e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:28 +0000

    Add package karrio.canpar/2023.9.2/karrio.canpar-2023.9.2-py3-none-any.whl

---
+++ packages/karrio.canpar/karrio.canpar-2023.9.2-py3-none-any.whl/karrio/mappers/canpar/settings.py
@@ -0,0 +1,20 @@
+"""Karrio Canpar client settings."""
+
+import attr
+from karrio.providers.canpar import Settings as BaseSettings
+
+
+@attr.s(auto_attribs=True)
+class Settings(BaseSettings):
+    """Canpar connection settings."""
+
+    username: str
+    password: str
+    language: str = "en"
+
+    id: str = None
+    test_mode: bool = False
+    carrier_id: str = "canpar"
+    account_country_code: str = "CA"
+    metadata: dict = {}
+    config: dict = {}
