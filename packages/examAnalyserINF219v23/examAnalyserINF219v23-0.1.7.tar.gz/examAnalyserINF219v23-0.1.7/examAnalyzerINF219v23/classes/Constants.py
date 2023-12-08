318e7beb77336d57568b31193efc97c7fa8e14d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:04:05 +0000

    Add package examAnalyserINF219v23/0.1.7/examAnalyserINF219v23-0.1.7.tar.gz

---
+++ packages/examAnalyserINF219v23/examAnalyserINF219v23-0.1.7.tar.gz/examAnalyserINF219v23-0.1.7/examAnalyzerINF219v23/classes/Constants.py
@@ -0,0 +1,40 @@
+class Constants:
+
+    JSON_FILE_PATH_A = 'src/data/forkunnskapstest-A.json'
+    JSON_FILE_PATH_B = 'src/data/forkunnskapstest-B.json'
+    JSON_FILE_PATH_C = 'src/data/21H_anonymised.json'
+
+    class EXPLANATIONS:
+
+        BOX = ("DUMMY TEXT: This box plot represents the portion of students "
+               "who answered each question correctly. The horizontal "
+               "bars indicate the percentage of correct answers for "
+               "each question. The color of each bar signifies the "
+               "difficulty level of the question, with different "
+               "shades representing varying levels of difficulty. "
+               "The legend on the right provides a clear breakdown "
+               "of these difficulty levels. This visualization is "
+               "helpful in understanding which questions were more "
+               "challenging for students and which ones were easier.")
+
+        PBC = ("DUMMY TEXT: The PBC (Probability By Category) chart showcases "
+               "the likelihood of events occurring within distinct categories. "
+               "It provides a comprehensive view of data distribution, enabling "
+               "readers to draw inferences about the prominence of specific categories.")
+
+        PBC_GROUPED = ("DUMMY TEXT: The PBC_GROUPED chart displays grouped categories of data "
+                       "based on their probability scores. This representation allows "
+                       "for a more segmented analysis, helping in discerning patterns "
+                       "within subgroups of the data. It's particularly useful when "
+                       "the overall category has subdivisions with distinct characteristics.")
+
+        TaskDuration = ("DUMMY TEXT: The TaskDuration graph illustrates the time taken "
+                        "for different tasks. The x-axis represents the tasks, "
+                        "while the y-axis signifies the duration. This graph is "
+                        "useful for identifying tasks that consume disproportionate "
+                        "amounts of time.")
+
+        GraphDistribution = ("DUMMY TEXT: The GraphDistribution plot provides insights into "
+                             "how data points are dispersed across various ranges or intervals. "
+                             "By analyzing the density and spread of the data, one can "
+                             "gauge the central tendency and variability of the dataset.")
