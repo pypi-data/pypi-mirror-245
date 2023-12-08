9aa23f852fee7367151bb74195dd8c4411c1c419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 06:04:04 +0000

    Add package st-stepper/0.0.2/st_stepper-0.0.2-py3-none-any.whl

---
+++ packages/st-stepper/st_stepper-0.0.2-py3-none-any.whl/st_stepper/__init__.py
@@ -0,0 +1,55 @@
+import os
+import streamlit.components.v1 as components
+import streamlit as st
+from typing import Tuple
+from streamlit.components.v1.components import CustomComponent
+from typing import List
+
+# Create a _RELEASE constant. We'll set this to False while we're developing
+# the component, and True when we're ready to package and distribute it.
+# (This is, of course, optional - there are innumerable ways to manage your
+# release process.)
+_RELEASE = True
+
+# Declare a Streamlit component. `declare_component` returns a function
+# that is used to create instances of the component. We're naming this
+# function "_component_func", with an underscore prefix, because we don't want
+# to expose it directly to users. Instead, we will create a custom wrapper
+# function, below, that will serve as our component's public API.
+
+# It's worth noting that this call to `declare_component` is the
+# *only thing* you need to do to create the binding between Streamlit and
+# your component frontend. Everything else we do in this file is simply a
+# best practice.
+
+if not _RELEASE:
+    _st_stepper = components.declare_component(
+        # We give the component a simple, descriptive name ("my_component"
+        # does not fit this bill, so please choose something better for your
+        # own component :)
+        "st_triggers",
+        # Pass `url` here to tell Streamlit that the component will be served
+        # by the local dev server that you run via `npm run start`.
+        # (This is useful while your component is in development.)
+        url="http://localhost:3000",
+    )
+else:
+    # When we're distributing a production version of the component, we'll
+    # replace the `url` param with `path`, and point it to the component's
+    # build directory:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _st_stepper = components.declare_component("st_stepper", path=build_dir)
+
+
+# Create a wrapper function for the component. This is an optional
+# best practice - we could simply expose the component function returned by
+# `declare_component` and call it done. The wrapper allows us to customize
+# our component's API: we can pre-process its input args, post-process its
+# output value, and add a docstring for users.
+def st_stepper(steps: List[str], is_vertical: bool = False, lock_sequence: bool = True) -> CustomComponent:
+    component_value = _st_stepper(steps=steps, is_vertical=is_vertical, lock_sequence=lock_sequence, default=0)
+    return component_value
+    
+
+
