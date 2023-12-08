0b11faee6819b65474585d1f25e065a0196c2a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:04:36 +0000

    Add package dataadderDB/0.1.0/dataadderDB-0.1.0.tar.gz

---
+++ packages/dataadderDB/dataadderDB-0.1.0.tar.gz/dataadderDB-0.1.0/dataadderDB/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dataadderDB',
+    version='0.1.0',
+    packages=['dataadderDB'],
+    install_requires=[
+        # List dependencies here
+        'sqlite3',
+        'csv'
+        'json'
+        'sqlite3'
+        'datetime'  
+    ],
+)
