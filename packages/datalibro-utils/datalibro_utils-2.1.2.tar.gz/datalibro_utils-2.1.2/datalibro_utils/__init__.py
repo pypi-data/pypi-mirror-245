3225bc1c536031dfbd7d7b01d8be353022405caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:04:03 +0000

    Add package datalibro-utils/2.1.2/datalibro_utils-2.1.2.tar.gz

---
+++ packages/datalibro-utils/datalibro_utils-2.1.2.tar.gz/datalibro_utils-2.1.2/datalibro_utils/__init__.py
@@ -0,0 +1,20 @@
+
+from .mapping import (
+    get_sku_extra,
+    amz_merge
+)
+
+from .email import(
+    send_email
+)
+
+from .openai import(
+    ask,
+    memy_ask
+)
+
+from .import dlbi, datalibro_auth
+
+from .backend import(
+    read_config_file
+)
