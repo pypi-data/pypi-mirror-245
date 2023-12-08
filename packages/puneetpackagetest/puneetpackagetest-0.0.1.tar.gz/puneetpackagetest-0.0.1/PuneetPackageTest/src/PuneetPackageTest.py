eee32a758abdddc23e055fc32af29b63c4c8a0ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:04:04 +0000

    Add package puneetpackagetest/0.0.1/puneetpackagetest-0.0.1.tar.gz

---
+++ packages/puneetpackagetest/puneetpackagetest-0.0.1.tar.gz/puneetpackagetest-0.0.1/PuneetPackageTest/src/PuneetPackageTest.py
@@ -0,0 +1,8 @@
+def SayLoud() :
+    print("This greet is from Puneet Package ")
+def SayLouder(message) :
+    print("This louder message from puneet package "+ message)
+def WhatIsYourName(message) :
+    print("Your Name is : "+ message)
+def PrintHello() :
+    print("Hello")
