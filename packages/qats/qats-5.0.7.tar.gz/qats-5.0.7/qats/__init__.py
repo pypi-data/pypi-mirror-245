7f48cd370feb23897f724237e2d271a2068565f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:42 +0000

    Add package qats/5.0.7/qats-5.0.7.tar.gz

---
+++ packages/qats/qats-5.0.7.tar.gz/qats-5.0.7/qats/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Library for efficient processing and visualization of time series.
+"""
+from .ts import TimeSeries
+from .tsdb import TsDB
+from ._version import __version__, __version_tuple__
+
+# summary
+__summary__ = __doc__
