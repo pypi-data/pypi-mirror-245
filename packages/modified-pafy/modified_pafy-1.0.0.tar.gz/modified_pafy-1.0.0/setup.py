07c9d0403ebdf8d8c5fc6ebeaedf58d1da277039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:52 +0000

    Add package modified-pafy/1.0.0/modified_pafy-1.0.0.tar.gz

---
+++ packages/modified-pafy/modified_pafy-1.0.0.tar.gz/modified_pafy-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    packages=find_packages(),
+    name = "modified_pafy",
+    author = "NA",
+    description = "NA",
+    install_requires = [],
+    version = "1.0.0"
+)
