e545cc5f2ef99ee162d2848d56f6c39d23fba7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 05:04:12 +0000

    Add package pbipy/2.6.0/pbipy-2.6.0.tar.gz

---
+++ packages/pbipy/pbipy-2.6.0.tar.gz/pbipy-2.6.0/pbipy/dashboards.py
@@ -0,0 +1,71 @@
+from requests import Session
+from pbipy.resources import Resource
+
+
+class Dashboard(Resource):
+    _REPR = [
+        "id",
+        "display_name",
+        "group_id",
+        "app_id",
+    ]
+
+    def __init__(
+        self,
+        id: str,
+        session: Session,
+        group_id=None,
+        raw=None,
+    ) -> None:
+        super().__init__(id, session)
+
+        if group_id:
+            self.group_id = group_id
+        else:
+            self.group_id = None
+
+        if self.group_id:
+            self.resource_path = f"/groups/{self.group_id}/dashboards/{self.id}"
+        else:
+            self.resource_path = f"/dashboards/{self.id}"
+
+        self.base_path = f"{self.BASE_URL}{self.resource_path}"
+
+        if raw:
+            self._load_from_raw(raw)
+
+
+class Tile(Resource):
+    _REPR = [
+        "id",
+        "title",
+        "dashboard_id",
+        "group_id",
+        "report_id",
+        "dataset_id",
+    ]
+
+    def __init__(
+        self,
+        id: str,
+        dashboard_id: str,
+        session: Session,
+        group_id=None,
+        raw=None,
+    ) -> None:
+        super().__init__(id, session)
+
+        self.dashboard_id = dashboard_id
+
+        if group_id:
+            self.group_id = group_id
+        else:
+            self.group_id = None
+
+        if self.group_id:
+            self.resource_path = f"/groups/{self.group_id}/dashboards/{self.dashboard_id}/tiles/{self.id}"
+        else:
+            self.resource_path = f"/dashboards/{self.dashboard_id}/tiles/{self.id}"
+
+        if raw:
+            self._load_from_raw(raw)
