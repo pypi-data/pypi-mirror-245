0f12690074fe13aea9fc282973827613ec4bb34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:03:51 +0000

    Add package typora-upload-util/1.0.0/typora_upload_util-1.0.0.tar.gz

---
+++ packages/typora-upload-util/typora_upload_util-1.0.0.tar.gz/typora_upload_util-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'typora_upload_util',
+    version          = '1.0.0',
+    description      = 'Uploader Plugins for Typora',
+    author           = 'Revi1337',
+    author_email     = 'david122123@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['requests'],
+	include_package_data=True,
+	packages=find_packages(),
+    keywords         = ['TYPORA UPLOADER', 'typora uploader'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
