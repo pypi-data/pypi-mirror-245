823b00f32793df3678b5968a11f8ca5fd23e14c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:03:51 +0000

    Add package prisjakt-fis-entity-functions/0.0.1/prisjakt_fis_entity_functions-0.0.1.tar.gz

---
+++ packages/prisjakt-fis-entity-functions/prisjakt_fis_entity_functions-0.0.1.tar.gz/prisjakt_fis_entity_functions-0.0.1/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='prisjakt_fis_entity_functions',
+    version='0.0.1',
+    description='PyPi package created by Schibsted\'s Product & Application Security team.',
+    long_description='',
+    url='https://schibsted.com',
+    author='Application Security Team',
+    author_email='application-security+pypi@schibsted.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
