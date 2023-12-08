5bb521aa4eeabc4c391e94e011b2d44acd5eab83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:05:13 +0000

    Add package Warg/1.3.5/Warg-1.3.5.tar.gz

---
+++ packages/Warg/Warg-1.3.5.tar.gz/Warg-1.3.5/warg/metas/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Christian Heider Lindbjerg"
+__doc__ = r"""
+
+           Created on 07/03/2020
+           """
+
+from pathlib import Path
+
+with open(Path(__file__).parent / "README.md", "r") as this_init_file:
+    __doc__ += this_init_file.read()
+# del Path
+
+from .post_init import *
+from .singleton import *
---
+++ packages/Warg/Warg-1.3.5.tar.gz/Warg-1.3.5/warg/mixins/dict_mixins.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+__author__ = "Christian Heider Lindbjerg"
+__doc__ = ""
+
+__all__ = [
+    "IterDictItemsMixin",
+    "IterDictKeysMixin",
+    "IterDictValuesMixin",
+]
+
+from typing import Any, Tuple
+
+
+class IterDictItemsMixin:
+    """
+    Mixin class for iterating kw pairs in a class instance __dict__"""
+
+    def __iter__(self) -> Tuple[Any, Any]:
+        for attr, value in self.__dict__.items():
+            yield attr, value
+
+
+class IterDictKeysMixin:
+    """
+    Mixin class for iterating only the keys of a class instance __dict__"""
+
+    def __iter__(self) -> Any:
+        for attr in self.__dict__.keys():
+            yield attr
+
+
+class IterDictValuesMixin:
+    """
+    Mixin class for iterating only the values of a class instance __dict__"""
+
+    def __iter__(self) -> Any:
+        for value in self.__dict__.values():
+            yield value
+
+
+if __name__ == "__main__":
+
+    def asdij() -> None:
+        """
+        :rtype: None
+        """
+
+        class IASD(IterDictValuesMixin):
+            pass
+
+        a = IASD()
+        a.b = 1
+        a.c = 2
+        a.d = 3
+
+        for ca in a:
+            print(ca)
+
+    asdij()
