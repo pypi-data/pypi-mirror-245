eb8ccc30184dfae2e0e231f85ef22e1143ba3fc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:00 +0000

    Add package cu-cat/0.6.7/cu-cat-0.6.7.tar.gz

---
+++ packages/cu-cat/cu-cat-0.6.7.tar.gz/cu-cat-0.6.7/cu_cat/_dep_manager.py
@@ -0,0 +1,36 @@
+import importlib
+
+class DepManager:
+    def __init__(self):
+        self.pkgs = {}
+
+    def __getattr__(self, pkg:str):
+        if '.' in pkg:
+            module = '.'.join(pkg.split('.')[:-1])
+            name = pkg.split('.')[-1]
+            self.import_from(module, name)
+            try:
+                return self.pkgs[name]
+            except KeyError:
+                return None
+        else:
+            self._add_deps(pkg)
+            try:
+                return self.pkgs[pkg]
+            except KeyError:
+                return None
+
+    def _add_deps(self, pkg:str):
+        try:
+            pkg_val = importlib.import_module(pkg)
+            self.pkgs[pkg] = pkg_val
+            setattr(self, pkg, pkg_val)
+        except:
+            pass
+
+    def import_from(self,pkg:str, name:str):
+        try:
+            module = __import__(pkg, fromlist=[name])
+            self.pkgs[name] = module
+        except:
+            pass
