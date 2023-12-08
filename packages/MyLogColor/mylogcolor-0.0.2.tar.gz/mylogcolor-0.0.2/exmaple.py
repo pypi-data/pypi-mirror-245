d2c5f5fec9ded86711f876256a4d7ca5c7ade8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:07 +0000

    Add package MyLogColor/0.0.2/mylogcolor-0.0.2.tar.gz

---
+++ packages/MyLogColor/mylogcolor-0.0.2.tar.gz/mylogcolor-0.0.2/exmaple.py
@@ -0,0 +1,10 @@
+from MyLogColor import log,LogLevel
+
+
+log('Hello,错误', LogLevel.ERROR)
+log('Hello,通过', LogLevel.PASS)
+log('Hello,信息', LogLevel.INFO)
+log('Hello,至关重要的', LogLevel.CRITICAL)
+log('Hello,警告', LogLevel.WARN)
+log('Hello,愚蠢的', LogLevel.SILLY)
+log('Hello,成功', LogLevel.SUCCESS)
