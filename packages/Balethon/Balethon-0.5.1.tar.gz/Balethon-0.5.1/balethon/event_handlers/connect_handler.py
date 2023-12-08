874119e7dab5f1b51773eec9c5a2dc8fd020512b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 06:04:07 +0000

    Add package Balethon/0.5.1/Balethon-0.5.1.tar.gz

---
+++ packages/Balethon/Balethon-0.5.1.tar.gz/Balethon-0.5.1/balethon/event_handlers/connect_handler.py
@@ -0,0 +1,16 @@
+from .event_handler import EventHandler
+
+
+class ConnectHandler(EventHandler):
+
+    def __init__(self, callback):
+        super().__init__(callback)
+
+    async def __call__(self, client, *args, **kwargs):
+        await super().__call__(client)
+
+    async def check(self, client, update):
+        try:
+            return isinstance(self, update)
+        except TypeError:
+            return False
---
+++ packages/Balethon/Balethon-0.5.1.tar.gz/Balethon-0.5.1/balethon/objects/user.py
@@ -0,0 +1,41 @@
+from . import Object
+from ..states import StateMachine
+
+
+class User(Object):
+    state_machine = StateMachine()
+
+    def __init__(
+            self,
+            id: int = None,
+            username: str = None,
+            first_name: str = None,
+            last_name: str = None,
+            language_code: str = None,
+            is_bot: bool = None,
+            **kwargs
+    ):
+        super().__init__(**kwargs)
+        self.id: int = id
+        self.username: str = username
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+        self.language_code: str = language_code
+        self.is_bot: bool = is_bot
+
+    @property
+    def full_name(self):
+        if self.first_name and self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        if self.first_name:
+            return self.first_name
+        return ""
+
+    def set_state(self, state):
+        self.state_machine[self.id] = state
+
+    def get_state(self):
+        return self.state_machine[self.id]
+
+    def del_state(self):
+        del self.state_machine[self.id]
