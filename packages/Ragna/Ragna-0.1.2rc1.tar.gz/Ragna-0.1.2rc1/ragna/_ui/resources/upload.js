1430b7d81180998d680ff00075afd245617148b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:32 +0000

    Add package Ragna/0.1.2rc1/Ragna-0.1.2rc1.tar.gz

---
+++ packages/Ragna/Ragna-0.1.2rc1.tar.gz/Ragna-0.1.2rc1/ragna/_ui/resources/upload.js
@@ -0,0 +1,29 @@
+function upload(files, token, informationsEndpoint, final_callback) {
+  Promise.all(
+    Array.from(files).map((file) => {
+      return uploadFile(file, token, informationsEndpoint);
+    }),
+  ).then(final_callback);
+}
+
+async function uploadFile(file, token, informationEndpoint) {
+  const response = await fetch(informationEndpoint, {
+    method: "POST",
+    headers: { "Content-Type": "application/json", Authorization: `Bearer ${token}` },
+    body: JSON.stringify({ name: file.name }),
+  });
+  const documentInfo = await response.json();
+
+  var body = new FormData();
+  for (const [key, value] of Object.entries(documentInfo.data)) {
+    body.append(key, value);
+  }
+  body.append("file", file);
+
+  await fetch(documentInfo.url, {
+    method: "PUT",
+    body: body,
+  });
+
+  return documentInfo.document;
+}
