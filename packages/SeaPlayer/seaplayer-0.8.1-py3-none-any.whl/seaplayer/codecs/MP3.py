5c806e86bec4c84fa80c2d08087c8e028d17b977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 06:04:24 +0000

    Add package SeaPlayer/0.8.1/seaplayer-0.8.1-py3-none-any.whl

---
+++ packages/SeaPlayer/seaplayer-0.8.1-py3-none-any.whl/seaplayer/codecs/MP3.py
@@ -0,0 +1,18 @@
+import aiofiles
+# > Local Imports
+from .Any import AnyCodec
+
+# ! Main Class
+class MP3Codec(AnyCodec):
+    codec_name: str = "MP3"
+    
+    # ! Testing
+    @staticmethod
+    def is_this_codec(path: str) -> bool:
+        with open(path, "rb") as file:
+            return file.read(3) == b'ID3'
+    
+    @staticmethod
+    async def aio_is_this_codec(path: str) -> bool:
+        async with aiofiles.open(path, "rb") as file:
+            return await file.read(3) == b'ID3'
