ef75b7daf602b54ffd0ed5d6576c98a31525580c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 05:04:12 +0000

    Add package pbipy/2.6.0/pbipy-2.6.0-py2.py3-none-any.whl

---
+++ packages/pbipy/pbipy-2.6.0-py2.py3-none-any.whl/pbipy/resources.py
@@ -0,0 +1,55 @@
+from requests import Session
+
+from pbipy import settings
+from pbipy.utils import RequestsMixin, to_identifier, to_snake_case
+
+
+class Resource(RequestsMixin):
+    BASE_URL = settings.BASE_URL
+
+    def __init__(
+        self,
+        id: str,
+        session: Session,
+        **kwargs,
+    ) -> None:
+        self.id = id
+        self.session = session
+        self.raw = None
+
+    def __repr__(
+        self,
+    ) -> str:
+        """
+        Provide a readable representation of the class. Looks to the class'
+        `self._REPR` to determine what to show.
+
+        Returns
+        -------
+        `str`
+            Formatted, readable representation of the class.
+        """
+
+        name = self.__class__.__name__
+
+        attrs = []
+        for attr in self._REPR:
+            if attr in self.__dict__.keys():
+                attr_str = "{}={!r}".format(attr, self.__dict__.get(attr))
+                attrs.append(attr_str)
+
+        return f"<{name} {', '.join(attrs)}>"
+
+    def _load_from_raw(self, raw):
+        self.raw = raw
+
+        for k, v in raw.items():
+            attr = to_identifier(k)
+            attr = to_snake_case(attr)
+            setattr(self, attr, v)
+
+        return self
+
+    def load(self):
+        raw = self.get_raw(self.base_path, self.session)
+        self._load_from_raw(raw)
