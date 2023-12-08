3be20dceb9221183d781f161d7db7f242f7c43d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 05:03:26 +0000

    Add package aws-cf/0.0.28/aws-cf-0.0.28.tar.gz

---
+++ packages/aws-cf/aws-cf-0.0.28.tar.gz/aws-cf-0.0.28/aws_cf/diff.py
@@ -0,0 +1,35 @@
+from .utils.logging import logger
+from .utils.config import Config
+import sys
+from .utils.common import create_change_set, get_yml, remove_change_set, format_diff
+
+
+def diff(config_path, root_path):
+    config = Config.parse(config_path)
+    config.setup_env()
+
+    services = config.Stacks
+
+    logger.warning(f"Checking difference for stacks from file {config_path}")
+
+    logger.info(f"* Found {len(services)} services checking differences...")
+
+    for service in services:
+        change_set = create_change_set(service.name, service.path, root_path, config)
+
+        if change_set:
+            diffs = [format_diff(change)for change in change_set["Changes"]]
+
+            if len(diffs):
+                logger.warning(f"Found {len(diffs)} differences for the stack {service.name}")
+                for diff in diffs:
+                    logger.warning(f"> {diff}")
+            else:
+                logger.info(f"Found no differences for the stack {service.name}")
+
+            remove_change_set(service.name, change_set["ChangeSetName"])
+        
+        else:
+            yml = get_yml(service.path, config, root_path)
+            logger.info(f"Not able to find stack {service.name}: Creating new stack")
+            logger.warn(yml)
