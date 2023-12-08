4c6687466c61376394915c47cfe1a15953e2a586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:04:02 +0000

    Add package apifish/0.6.4.dev0/apifish-0.6.4.dev0-py3-none-any.whl

---
+++ packages/apifish/apifish-0.6.4.dev0-py3-none-any.whl/apifish/detection/tests/test_cluster_detection.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Author: Arthur Imbert <arthur.imbert.pro@gmail.com>
+# License: BSD 3 clause
+
+"""
+Unitary tests for apifish.detection.cluster_detection module.
+"""
+
+# TODO test apifish.detection.detect_clusters
