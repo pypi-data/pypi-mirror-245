be09a6b9260daca4f8a234de4a89d0b9d358aaa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 05:04:06 +0000

    Add package Fcolor/0.3/Fcolor-0.3.tar.gz

---
+++ packages/Fcolor/Fcolor-0.3.tar.gz/Fcolor-0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name='Fcolor',
+    version='0.3',
+    description='Fcolor pèrmet de facilement gérer les couleures rgb,hex et hsl',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
