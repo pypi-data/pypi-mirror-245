6b94a53450c9bb04d62f39d8a28e9f11b818dfe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:22 +0000

    Add package grabbygram/0.0.2/grabbygram-0.0.2.tar.gz

---
+++ packages/grabbygram/grabbygram-0.0.2.tar.gz/grabbygram-0.0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='grabbygram',
+    version='0.0.2',
+    author='Kavidu Dilhara',
+    author_email='contact@kavidudilhara.eu.org',
+    description='A Python package for downloading media from Telegram',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kavidu-dilhara/GrabbyGram',
+    packages=find_packages(),
+    install_requires=[
+        'telethon',
+        'tqdm',
+        'python-dotenv',
+        'pyfiglet'
+    ],
+    entry_points={
+        'console_scripts': [
+            'grabbygram = grabbygram.grabbygram:main'
+        ]
+    },
+classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    keywords='telegram downloader media python scrapping bulk bulkdownload',
+    project_urls={
+        'Bug Reports': 'https://github.com/kavidu-dilhara/GrabbyGram/issues',
+        'Source': 'https://github.com/kavidu-dilhara/GrabbyGram',
+    },
+)
