3131dad82e977b437e18a67bddc6affc4610cdee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:29 +0000

    Add package pds-epitome/3.3.1/pds_epitome-3.3.1-py3-none-any.whl

---
+++ packages/pds-epitome/pds_epitome-3.3.1-py3-none-any.whl/pds/epitome/main.py
@@ -0,0 +1,19 @@
+# encoding: utf-8
+"""Main.
+
+(Shut up, Flake8.)
+"""
+
+
+def main():
+    """Main entry point.
+
+    (Shut up, Flake8.)
+    """
+    print("🎤 Is this thing on? Yeah, I think this thing is on.")
+    print("We are pretty sure it is.")
+    print('Thu Dec  7 08:38:25 CST 2023')
+
+
+if __name__ == "__main__":
+    main()
