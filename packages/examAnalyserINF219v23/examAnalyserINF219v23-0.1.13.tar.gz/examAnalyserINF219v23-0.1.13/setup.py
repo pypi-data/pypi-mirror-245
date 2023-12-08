7fa84fc01222bf2924a97dd4223ff045e1182b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:05:18 +0000

    Add package examAnalyserINF219v23/0.1.13/examAnalyserINF219v23-0.1.13.tar.gz

---
+++ packages/examAnalyserINF219v23/examAnalyserINF219v23-0.1.13.tar.gz/examAnalyserINF219v23-0.1.13/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.13'
+DESCRIPTION = 'Exam analyzer'
+
+# Setting up
+setup(
+    name="examAnalyserINF219v23",
+    version=VERSION,
+    author="premeide",
+    author_email="premeide@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'examAnalyserINF219v23': ['templates/*.html']},
+    install_requires=['Flask'],
+    keywords=['python', 'education', 'exam analysis'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
