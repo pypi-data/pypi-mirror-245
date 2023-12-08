663f0d1b2362a15c3918752a3bffbbc963a34e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:04:04 +0000

    Add package tesp-support/1.3.5/tesp_support-1.3.5.tar.gz

---
+++ packages/tesp-support/tesp_support-1.3.5.tar.gz/tesp_support-1.3.5/tesp_support/sgip1/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2017-2023 Battelle Memorial Institute
+# file: __init__.py
+""" Transactive Energy Simulation Platform (TESP)
+Contains the python files for the SGIP1 analysis
+Plotting all SGIP1 variants on the same graph
+"""
