8bc74989977a6fd6fd375b8868ac648101d30da1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:08 +0000

    Add package zrb/0.0.123/zrb-0.0.123-py3-none-any.whl

---
+++ packages/zrb/zrb-0.0.123-py3-none-any.whl/zrb/task_input/choice_input.py
@@ -0,0 +1,55 @@
+from zrb.helper.typing import Any, Optional, Iterable, Union
+from zrb.helper.typecheck import typechecked
+from zrb.task_input.base_input import BaseInput
+import click
+
+
+@typechecked
+class ChoiceInput(BaseInput):
+
+    def __init__(
+        self,
+        name: str,
+        shortcut: Optional[str] = None,
+        choices: Iterable[Any] = [],
+        default: Optional[Any] = None,
+        description: Optional[str] = None,
+        show_default: Union[bool, str, None] = None,
+        prompt: Union[bool, str] = True,
+        confirmation_prompt: Union[bool, str] = False,
+        prompt_required: bool = True,
+        hide_input: bool = False,
+        is_flag: Optional[bool] = None,
+        flag_value: Optional[Any] = None,
+        multiple: bool = False,
+        count: bool = False,
+        allow_from_autoenv: bool = True,
+        hidden: bool = False,
+        show_choices: bool = True,
+        show_envvar: bool = False,
+        nargs: int = 1,
+        should_render: bool = True
+    ):
+        BaseInput.__init__(
+            self,
+            name=name,
+            shortcut=shortcut,
+            default=default,
+            description=description,
+            show_default=show_default,
+            prompt=prompt,
+            confirmation_prompt=confirmation_prompt,
+            prompt_required=prompt_required,
+            hide_input=hide_input,
+            is_flag=is_flag,
+            flag_value=flag_value,
+            multiple=multiple,
+            count=count,
+            allow_from_autoenv=allow_from_autoenv,
+            type=click.Choice(choices),
+            hidden=hidden,
+            show_choices=show_choices,
+            show_envvar=show_envvar,
+            nargs=nargs,
+            should_render=should_render
+        )
---
+++ packages/zrb/zrb-0.0.123-py3-none-any.whl/zrb/task_input/task_input.py
@@ -0,0 +1,5 @@
+from zrb.task_input.base_input import BaseInput
+
+
+class Input(BaseInput):
+    pass
