9fd253f5e8a2b83124dda2bb7d7a3623806ccecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:03:26 +0000

    Add package premeidePackageTest/0.1.20/premeidePackageTest-0.1.20-py3-none-any.whl

---
+++ packages/premeidePackageTest/premeidePackageTest-0.1.20-py3-none-any.whl/premeidePackageTest/classes/json/__init__.py
@@ -0,0 +1,4 @@
+from premeidePackageTest.classes.json import Exam
+from premeidePackageTest.classes.json import Question
+from premeidePackageTest.classes.json import Result
+from premeidePackageTest.classes.json import ResultWrapper
