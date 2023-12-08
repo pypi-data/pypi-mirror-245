0ab25c4a93df75c4c857417f0fdf94479de3e807
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:34 +0000

    Add package pdm-dotenv/0.3.1/pdm_dotenv-0.3.1-py3-none-any.whl

---
+++ packages/pdm-dotenv/pdm_dotenv-0.3.1-py3-none-any.whl/pdm_dotenv/__init__.py
@@ -0,0 +1,27 @@
+"""
+    pdm-dotenv
+
+    A pdm plugin that automatically loads .env files
+    :author: Zane Dufour <zane@znd4.me>
+    :license: MIT
+"""
+from typing import Any
+import dotenv
+from pdm.project import ConfigItem, Project
+from pdm.signals import pre_invoke
+from pdm.core import Core
+from pdm.termui import Verbosity
+
+
+def load_dotenv(project: Project, **_: Any) -> None:
+    dotenv_path = project.root / project.config["dotenv.path"]
+    project.core.ui.echo(f"Loading dotenv file {dotenv_path}", err=True, verbosity=Verbosity.NORMAL)
+    dotenv.load_dotenv(dotenv_path)
+
+
+def plugin(core: Core) -> None:
+    core.add_config(
+        "dotenv.path",
+        ConfigItem("Path to your .env file", ".env", env_var="PDM_DOTENV_PATH"),
+    )
+    pre_invoke.connect(load_dotenv)
