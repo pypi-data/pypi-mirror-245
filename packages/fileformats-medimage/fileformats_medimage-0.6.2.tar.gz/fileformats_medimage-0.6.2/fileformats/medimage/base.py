3d76c61d9f5360b66632e659ff011cd314d532c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:19 +0000

    Add package fileformats-medimage/0.6.2/fileformats_medimage-0.6.2.tar.gz

---
+++ packages/fileformats-medimage/fileformats_medimage-0.6.2.tar.gz/fileformats_medimage-0.6.2/fileformats/medimage/base.py
@@ -0,0 +1,36 @@
+import typing as ty
+import logging
+from fileformats.generic import FileSet
+from fileformats.core import hook
+
+logger = logging.getLogger("fileformats")
+
+
+# =====================================================================
+# Custom loader functions for different image types
+# =====================================================================
+
+
+class MedicalImage(FileSet):
+
+    iana_mime: ty.Optional[str] = None
+    INCLUDE_HDR_KEYS = None
+    IGNORE_HDR_KEYS = None
+    binary = True
+
+    @hook.extra
+    def read_array(self) -> "numpy.ndarray":  # noqa
+        """
+        Returns the binary data of the image in a numpy array
+        """
+        raise NotImplementedError
+
+    @hook.extra
+    def vox_sizes(self) -> ty.Tuple[float, float, float]:
+        """The length of the voxels along each dimension"""
+        raise NotImplementedError
+
+    @hook.extra
+    def dims(self) -> ty.Tuple[int, int, int]:
+        """The dimensions of the image"""
+        raise NotImplementedError
