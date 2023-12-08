ca611928c2f4987cb0ddace80a0b427f03f2a488
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:42 +0000

    Add package advent-of-code-wim/2023.8/advent_of_code_wim-2023.8-py3-none-any.whl

---
+++ packages/advent-of-code-wim/advent_of_code_wim-2023.8-py3-none-any.whl/aoc_wim/aoc2022/q03.py
@@ -0,0 +1,23 @@
+"""
+--- Day 3: Rucksack Reorganization ---
+https://adventofcode.com/2022/day/3
+"""
+from aocd import data
+from wimpy import chunks
+
+a = 0
+for line in data.splitlines():
+    i = len(line) // 2  # midpoint
+    left, right = line[:i], line[i:]
+    [c] = set(left).intersection(right)
+    a += c.isupper() * 26
+    a += ord(c.lower()) - ord("a") + 1
+
+b = 0
+for s1, s2, s3 in chunks(data.splitlines()):
+    [c] = set(s1).intersection(s2, s3)
+    b += c.isupper() * 26
+    b += ord(c.lower()) - ord("a") + 1
+
+print("answer_a:", a)
+print("answer_b:", b)
---
+++ packages/advent-of-code-wim/advent_of_code_wim-2023.8-py3-none-any.whl/aoc_wim/aoc2023/q07.py
@@ -0,0 +1,24 @@
+"""
+--- Day 7: Camel Cards ---
+https://adventofcode.com/2023/day/7
+"""
+from aocd import data
+from collections import Counter
+
+
+tr = str.maketrans("TQKA", "IKLM")
+
+
+def key_a(hand):
+    return sorted(Counter(hand).values(), reverse=True), hand.translate(tr)
+
+
+def key_b(hand):
+    c = Counter(hand.replace("J", "") or "J")
+    k0, _ = key_a(hand.replace("J", max(c, key=c.get)))
+    return k0, hand.translate(tr).replace("J", "0")
+
+
+d = dict(h.split() for h in data.splitlines())
+print("answer_a", sum(i * int(d[k]) for i, k in enumerate(sorted(d, key=key_a), 1)))
+print("answer_b", sum(i * int(d[k]) for i, k in enumerate(sorted(d, key=key_b), 1)))
