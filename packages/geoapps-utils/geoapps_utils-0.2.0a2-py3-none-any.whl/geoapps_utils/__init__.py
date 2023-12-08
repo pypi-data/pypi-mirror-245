3ab83dfa6a80fa001361db2eb8b4b409a2e51440
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 06:04:16 +0000

    Add package geoapps-utils/0.2.0a2/geoapps_utils-0.2.0a2-py3-none-any.whl

---
+++ packages/geoapps-utils/geoapps_utils-0.2.0a2-py3-none-any.whl/geoapps_utils/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) 2023 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps-utils package.
+#
+#  geoapps-utils is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+#
+
+__version__ = "0.2.0-alpha.2"
