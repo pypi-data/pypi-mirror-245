9813b180ddce086f7c2f1825769280a1826a3add
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 05:03:27 +0000

    Add package datasus-db/0.1.0/datasus_db-0.1.0-py3-none-any.whl

---
+++ packages/datasus-db/datasus_db-0.1.0-py3-none-any.whl/datasus_db/__init__.py
@@ -0,0 +1,6 @@
+from .datasources.auxiliar import import_auxiliar_tables
+from .datasources.po import import_po
+from .datasources.ibge_pop import import_ibge_pop
+from .datasources.ibge_pop_tcu import import_ibge_pop_tcu
+from .datasources.sih_rd import import_sih_rd
+from .datasources.sim_do import import_sim_do
