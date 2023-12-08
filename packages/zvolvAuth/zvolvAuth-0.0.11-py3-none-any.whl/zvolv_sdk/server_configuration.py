a78d045316d655cfaccd6fa49bbb93cc2248fcc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:04:16 +0000

    Add package zvolvAuth/0.0.11/zvolvAuth-0.0.11-py3-none-any.whl

---
+++ packages/zvolvAuth/zvolvAuth-0.0.11-py3-none-any.whl/zvolv_sdk/server_configuration.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+
+class EnvVariables(object):
+    # if(os.path.exists("../../.env")):
+    load_dotenv()
+
+    @staticmethod
+    def get_val(key):
+        return os.environ[key] if key in os.environ else None
+
+    @staticmethod
+    def get_localhost_url():
+        return EnvVariables.get_val('LOCALHOST_URL')
+
+    @staticmethod
+    def get_api_base_url():
+        return EnvVariables.get_val('BASE_URL')
+
+    @staticmethod
+    def get_api_version():
+        return EnvVariables.get_val('API_VERSION')
+
+    @staticmethod
+    def get_api_17_version():
+        return EnvVariables.get_val('API_VERSION_17')
+
+    @staticmethod
+    def get_admin_email():
+        return EnvVariables.get_val('ADMIN_EMAIL')
+
+    @staticmethod
+    def get_admin_password():
+        return EnvVariables.get_val('ADMIN_PASSWORD')
+
+    @staticmethod
+    def get_business_tag_id():
+        return EnvVariables.get_val('BUSINESS_TAG_ID')
+
+    @staticmethod
+    def get_business_domain():
+        return EnvVariables.get_val('BUSINESS_DOMAIN')
