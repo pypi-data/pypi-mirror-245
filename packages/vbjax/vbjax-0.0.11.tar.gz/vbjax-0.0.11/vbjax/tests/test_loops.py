a3056ce36b4dbb3a6a3a3526bda780f62e41d6d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:37 +0000

    Add package vbjax/0.0.11/vbjax-0.0.11.tar.gz

---
+++ packages/vbjax/vbjax-0.0.11.tar.gz/vbjax-0.0.11/vbjax/tests/test_loops.py
@@ -0,0 +1,45 @@
+import jax
+import jax.numpy as np
+import vbjax as vb
+
+
+def test_sde():
+    f = lambda x,_: -x
+    g = lambda *_: 1e-2
+    dt = 0.1
+    _, run = vb.make_sde(dt, f, g)
+    zs = vb.randn(100, 32)
+    xs = run(zs[0]+1, zs, None)
+    assert xs.shape == zs.shape
+
+
+def test_ode():
+    f = lambda x, _: -x
+    dt = 0.1
+    _, run = vb.make_ode(dt, f)
+    x0 = np.r_[:32].astype('f')
+    xs = run(x0, np.r_[:64], None)
+    assert xs.shape == (64, 32)
+
+
+def test_dde():
+    def dfun(xt, x, t, p):
+        xd = xt[0, t-100]
+        dx = x - x**3/3 + p*xt[0, t-100]
+        return dx
+
+    _, loop = vb.make_dde(0.1, 100, dfun)
+    xt0 = np.ones((1, 200))
+    xt1,t = loop(xt0, 0.2)
+    assert xt1.shape == (1, 200)
+
+
+def test_sdde():
+    import vbjax as vb, jax.numpy as np
+    def dfun(xt, x, t, p):
+        return -xt[t - 5]
+    _, sdde = vb.make_sdde(1.0, 5, dfun, 0.01)
+    sdde(vb.randn(20)+10, None)
+
+
+# TODO theta method? https://gist.github.com/maedoc/c47acb9d346e31017e05324ffc4582c1
