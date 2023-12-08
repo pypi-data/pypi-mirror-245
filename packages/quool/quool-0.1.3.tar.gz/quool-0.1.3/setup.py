01b8b8bec18b5b8d9f8d4a065618b031fa34e366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:04:35 +0000

    Add package quool/0.1.3/quool-0.1.3.tar.gz

---
+++ packages/quool/quool-0.1.3.tar.gz/quool-0.1.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "quool",
+    packages = find_packages(),
+    author = "ppoak",
+    author_email = "ppoak@foxmail.com",
+    description = "Quantitative Toolkit - a helper in quant developping",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    keywords = ['quant', 'framework', 'finance'],
+    url = "https://github.com/ppoak/quool",
+    version = '0.1.3',
+    install_requires = [
+        'bs4',
+        'numpy',
+        'joblib',
+        'pandas',
+        'matplotlib',
+    ],
+    extras_require={
+        'backtest': ['backtrader'],
+        'crawler': ['akshare'],
+        'all': ['backtrader', 'akshare'],
+    }
+)
