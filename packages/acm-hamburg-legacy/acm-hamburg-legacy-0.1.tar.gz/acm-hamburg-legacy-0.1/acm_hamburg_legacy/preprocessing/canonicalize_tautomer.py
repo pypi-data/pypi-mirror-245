03ba528c1a62af84da2a1e2960ba7b6fe324e11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:04:59 +0000

    Add package acm-hamburg-legacy/0.1/acm-hamburg-legacy-0.1.tar.gz

---
+++ packages/acm-hamburg-legacy/acm-hamburg-legacy-0.1.tar.gz/acm-hamburg-legacy-0.1/acm_hamburg_legacy/preprocessing/canonicalize_tautomer.py
@@ -0,0 +1,49 @@
+from typing import List, Tuple
+
+from rdkit import RDLogger
+
+# disable rdkit logging messages
+# importing rdMolStandardize already logs messages
+logger = RDLogger.logger()
+logger.setLevel(RDLogger.ERROR)
+
+from nerdd_module.preprocessing import Step
+from rdkit.Chem import KekulizeException, Mol, SanitizeMol
+from rdkit.Chem.MolStandardize import rdMolStandardize
+
+__all__ = ["CanonicalizeTautomer"]
+
+
+class CanonicalizeTautomer(Step):
+    """Canonalizes the molecules by removing stereochemistry and
+    enumerating tautomers.
+    """
+
+    def __init__(self, remove_stereo=False, remove_invalid_molecules=False):
+        super().__init__()
+        self.remove_stereo = remove_stereo
+        self.remove_invalid_molecules = remove_invalid_molecules
+
+    def _run(self, mol: Mol) -> Tuple[Mol, List[str]]:
+        errors = []
+
+        # generating a canonical tautomer might ignore stereochemistry
+        taut_params = rdMolStandardize.CleanupParameters()
+        if not self.remove_stereo:
+            taut_params.tautomerRemoveSp3Stereo = False
+            taut_params.tautomerRemoveBondStereo = False
+            taut_params.tautomerRemoveIsotopicHs = False
+        canon = rdMolStandardize.TautomerEnumerator(taut_params)
+
+        SanitizeMol(mol)
+        try:
+            molc = canon.Canonicalize(mol)
+            if molc is None:
+                raise KekulizeException()
+            return molc, errors
+        except KekulizeException:
+            errors.append("C1")
+            if self.remove_invalid_molecules:
+                return None, errors
+            else:
+                return mol, errors
