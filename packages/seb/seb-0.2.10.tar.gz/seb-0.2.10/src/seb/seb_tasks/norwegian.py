4eb125c954b9e799435414d97a5496f1ec7c569d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:04:30 +0000

    Add package seb/0.2.10/seb-0.2.10.tar.gz

---
+++ packages/seb/seb-0.2.10.tar.gz/seb-0.2.10/src/seb/seb_tasks/norwegian.py
@@ -0,0 +1,22 @@
+from seb.registries import tasks
+from seb.tasks_interface import MTEBTask, Task
+
+
+@tasks.register("NoReC")
+def create_norec() -> Task:
+    from mteb import NoRecClassification
+
+    task = MTEBTask(NoRecClassification())
+    task.name = "NoReC"
+    task.domain = ["reviews"]
+    return task
+
+
+@tasks.register("Norwegian parliament")
+def create_norwegian_parliament() -> Task:
+    from mteb import NorwegianParliamentClassification
+
+    task = MTEBTask(NorwegianParliamentClassification())
+    task.name = "Norwegian parliament"
+    task.domain = ["spoken"]
+    return task
