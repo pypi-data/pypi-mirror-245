37d5649afd24704047a99678b72e110b4b86331a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 06:04:43 +0000

    Add package nbconvert-a11y/2023.12.6/nbconvert_a11y-2023.12.6-py3-none-any.whl

---
+++ packages/nbconvert-a11y/nbconvert_a11y-2023.12.6-py3-none-any.whl/nbconvert_a11y/exporter.py
@@ -0,0 +1,307 @@
+"""a formal html5 approach to building static notebooks.
+
+this design assumes __notebooks are a feed of forms__.
+"""
+
+import builtins
+from io import StringIO
+import json
+from contextlib import suppress
+from datetime import datetime
+from functools import lru_cache
+from pathlib import Path
+
+import bs4
+from nbconvert import Exporter
+import nbformat.v4
+import pygments
+from bs4 import BeautifulSoup
+from nbconvert.exporters.html import HTMLExporter
+from traitlets import Bool, CUnicode, Enum, Unicode
+
+singleton = lru_cache(1)
+
+HERE = Path(__file__).parent
+TEMPLATES = HERE / "templates"
+
+AXE_VERSION = "4.8.2"
+AXE = f"https://cdnjs.cloudflare.com/ajax/libs/axe-core/{AXE_VERSION}/axe.min.js"
+SCHEMA = nbformat.validator._get_schema_json(nbformat.v4)
+
+
+THEMES = {
+    "a11y": "a11y-{}",
+    "a11y-high-contrast": "a11y-high-contrast-{}",
+    "gh": "github-{}",
+    "gh-colorblind": "github-{}-colorblind",
+    "gh-high": "github-{}-high-contrast",
+    "gotthard": "gotthard-{}",
+    "blinds": "blinds-{}",
+}
+
+
+class PostProcess(Exporter):
+    """an exporter that allows post processing after the templating step
+
+    this class introduces the `post_process_html` protocol that can be used to modify
+    exported html.
+    """
+
+    def from_notebook_node(self, nb, resources=None, **kw):
+        html, resources = super().from_notebook_node(nb, resources, **kw)
+        html = self.post_process_html(html) or html
+        return html, resources
+
+    def post_process_html(self, body):
+        ...
+
+
+class A11yExporter(PostProcess, HTMLExporter):
+    """an accessible reference implementation for computational notebooks implemented for ipynb files.
+
+    this template provides a flexible screen reader experience with settings to control and customize the reading experience.
+    """
+
+    template_file = Unicode("a11y/table.html.j2").tag(config=True)
+    include_axe = Bool(False, help="include axe auditing tools in the rendered page.").tag(
+        config=True
+    )
+    axe_url = CUnicode(AXE, help="the remote source for the axe resources.").tag(config=True)
+    include_settings = Bool(False, help="include configurable accessibility settings dialog.").tag(
+        config=True
+    )
+    include_help = Bool(
+        False, help="include help and supplementary descriptions about notebooks and cells"
+    ).tag(config=True)
+    include_toc = Bool(
+        True, help="collect a table of contents of the headings in the document"
+    ).tag(config=True)
+    wcag_priority = Enum(
+        ["AAA", "AA", "A"], "AA", help="the default inital wcag priority to start with"
+    ).tag(config=True)
+    accesskey_navigation = Bool(
+        True, help="use numeric accesskeys to access the first 10 cells"
+    ).tag(config=True)
+    include_cell_index = Bool(
+        True, help="show the ordinal cell index, typically this is ignored from notebooks."
+    ).tag(config=True)
+    exclude_anchor_links = Bool(True).tag(config=True)
+    code_theme = Enum(list(THEMES), "gh-high", help="an accessible pygments dark/light theme").tag(
+        config=True
+    )
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        from nbconvert.filters import strings
+
+        for k, v in vars(strings).items():
+            if callable(v) and not k.startswith("_"):
+                self.environment.filters.setdefault(k, v)
+        self.environment.globals.update(vars(builtins))
+        import html
+
+        self.environment.globals.update(json=json, markdown=get_markdown, highlight=highlight)
+        self.environment.filters.update(escape_html=html.escape)
+        self.environment.globals.update(
+            formatter=pygments.formatters,
+            count_loc=count_loc,
+            count_outputs=count_outputs,
+            count_code_cells=count_code_cells,
+            ordered=ordered,
+            schema=SCHEMA,
+            datetime=datetime,
+        )
+
+    @property
+    def default_config(self):
+        c = super().default_config
+        c.merge(
+            {
+                "CSSHTMLHeaderPreprocessor": {"enabled": False},
+            }
+        )
+        return c
+
+    def from_notebook_node(self, nb, resources=None, **kw):
+        resources = resources or {}
+        resources["include_axe"] = self.include_axe
+        resources["include_settings"] = self.include_settings
+        resources["include_help"] = self.include_help
+        resources["include_toc"] = self.include_toc
+        resources["wcag_priority"] = self.wcag_priority
+        resources["accesskey_navigation"] = self.accesskey_navigation
+        resources["code_theme"] = THEMES[self.code_theme]
+        resources["axe_url"] = self.axe_url
+
+        return super().from_notebook_node(nb, resources, **kw)
+
+    def post_process_html(self, body):
+        """a final pass at the exported html to add table of contents, heading links, and other a11y affordances."""
+        soup = soupify(body)
+        describe_main(soup)
+        heading_links(soup)
+        details = soup.select_one("""[aria-labelledby="nb-toc"] details""")
+        if details:
+            details.extend(soupify(toc(soup)).body.children)
+            for x in details.select("ul"):
+                x.name = "ol"
+            details.select_one("ol").attrs["aria-labelledby"] = "nb-toc"
+        return soup.prettify(formatter="html5")
+
+
+class SectionExporter(A11yExporter):
+    template_file = Unicode("a11y/section.html.j2").tag(config=True)
+
+
+class ListExporter(A11yExporter):
+    template_file = Unicode("a11y/list.html.j2").tag(config=True)
+
+
+def strip_comments(tag):
+    for child in getattr(tag, "children", ()):
+        with suppress(AttributeError):
+            if isinstance(child, bs4.Comment):
+                child.extract()
+        strip_comments(child)
+    return tag
+
+
+@lru_cache
+def get_markdown_renderer():
+    from markdown_it import MarkdownIt
+    from mdit_py_plugins.anchors import anchors_plugin
+
+    md = MarkdownIt("gfm-like", options_update={"inline_definitions": True, "langPrefix": ""})
+    md.use(anchors_plugin)
+    md.options.update(highlight=highlight)
+    return md
+
+
+def get_markdown(md, **kwargs):
+    """exporter markdown as html"""
+    return get_markdown_renderer().render("".join(md), **kwargs)
+
+
+def highlight(code, lang="python", attrs=None, experimental=True):
+    """highlight code blocks"""
+    import html
+
+    import pygments
+
+    if lang == "code":
+        lang = "python"
+    elif lang == "raw":
+        return ""
+
+    lang = lang or pygments.lexers.get_lexer_by_name(lang or "python")
+
+    formatter = pygments.formatters.get_formatter_by_name(
+        "html", debug_token_types=True, title=f"{lang} code", wrapcode=True
+    )
+    try:
+        return pygments.highlight(
+            code, pygments.lexers.get_lexer_by_name(lang or "python"), formatter
+        )
+    except BaseException as e:
+        print(code, e)
+
+    return f"""<pre><code>{html.escape(code)}</code></pre>"""
+
+
+def soupify(body: str) -> BeautifulSoup:
+    """Convert a string of html to an beautiful soup object."""
+    return BeautifulSoup(body, features="html5lib")
+
+
+def mdtoc(html):
+    """create a table of contents in markdown that will be converted to html"""
+    import io
+
+    toc = io.StringIO()
+    for header in html.select("#cells :is(h1,h2,h3,h4,h5,h6)"):
+        id = header.attrs.get("id")
+        if not id:
+            from slugify import slugify
+
+            id = slugify(header.string)
+
+        # there is missing logistics for managely role=heading
+        # adding code group semantics will motivate this addition
+
+        level = int(header.name[-1])
+        toc.write("  " * (level - 1) + f"* [{header.string}](#{id})\n")
+    return toc.getvalue()
+
+
+def toc(html):
+    """create an html table of contents"""
+    return get_markdown(mdtoc(html))
+
+
+def heading_links(html):
+    """convert headings into links"""
+    for header in html.select(":is(h1,h2,h3,h4,h5,h6):not([role])"):
+        id = header.attrs.get("id")
+        if not id:
+            from slugify import slugify
+
+            id = slugify(header.string)
+
+        link = soupify(f"""<a href="#{id}">{header.string}</a>""").body.a
+        header.clear()
+        header.append(link)
+
+
+# * navigate links
+# * navigate headers
+# * navigate table
+# * navigate landmarks
+
+
+def count_cell_loc(cell):
+    lines = 0
+    for line in StringIO("".join(cell.source)):
+        if not line:
+            continue
+        if line.strip():
+            lines += 1
+    return lines
+
+
+def count_loc(nb):
+    """count total significant lines of code in the document"""
+    return sum(map(count_cell_loc, nb.cells))
+
+
+def count_outputs(nb):
+    """count total number of cell outputs"""
+    return sum(map(len, (x.get("outputs", "") for x in nb.cells)))
+
+
+def count_code_cells(nb):
+    """count total number of code cells"""
+    return len([None for x in nb.cells if x["cell_type"] == "code"])
+
+
+def describe_main(soup):
+    """add REFIDs to aria-describedby"""
+    x = soup.select_one("#toc > details > summary")
+    if x:
+        x.attrs["aria-describedby"] = soup.select_one("main").attrs[
+            "aria-describedby"
+        ] = "nb-cells-count-label nb-cells-label nb-code-cells nb-code-cells-label nb-ordered nb-loc nb-loc-label"
+
+
+def ordered(nb) -> str:
+    """measure if the notebook is ordered"""
+    start = 0
+    for cell in nb.cells:
+        if cell["cell_type"] == "code":
+            if any("".join(cell.source).strip()):
+                continue
+            start += 1
+            if start != cell["execution_count"] and start:
+                return "executed out of order"
+    if start:
+        return "executed in order"
+    return "unexecuted"
---
+++ packages/nbconvert-a11y/nbconvert_a11y-2023.12.6-py3-none-any.whl/nbconvert_a11y/pytest_axe.py
@@ -0,0 +1,279 @@
+"""report axe violations in html content
+
+* an axe fixture to use in pytest
+* a command line application for auditing files.
+
+"""
+
+# requires node and axe
+# requires playwright
+from ast import Not
+from collections import defaultdict
+import dataclasses
+from functools import lru_cache, partial
+from json import dumps, loads
+from pathlib import Path
+from shlex import quote, split
+from subprocess import CalledProcessError, check_output
+from typing import Any
+
+import exceptiongroup
+from pytest import fixture, mark, param
+
+# selectors for regions of the notebook
+MATHJAX = "[id^=MathJax]"
+JUPYTER_WIDGETS = ".jupyter-widgets"
+OUTPUTS = ".jp-OutputArea-output"
+NO_ALT = "img:not([alt])"
+PYGMENTS = ".highlight"
+
+# axe test tags
+# https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#axe-core-tags
+TEST_TAGS = [
+    "ACT",
+    "best-practice",
+    "experimental",
+    "wcag2a",
+    "wcag2aa",
+    "wcag2aaa",
+    "wcag21a",
+    "wcag21aa",
+    "wcag22aa",
+    "TTv5",
+]
+
+
+class Base:
+    """base class for exceptions and models"""
+
+    def __init_subclass__(cls) -> None:
+        dataclasses.dataclass(cls)
+
+    def dict(self):
+        return {k: v for k, v in dataclasses.asdict(self).items() if v is not None}
+
+    def dump(self):
+        return dumps(self.dict())
+
+
+# https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#api-name-axeconfigure
+class AxeConfigure(Base):
+    """axe configuration model"""
+
+    branding: str = None
+    reporter: str = None
+    checks: list = None
+    rules: list = None
+    standards: list = None
+    disableOtherRules: bool = None
+    local: str = None
+    axeVersion: str = None
+    noHtml: bool = False
+    allowedOrigins: list = dataclasses.field(default_factory=["<same_origin>"].copy)
+
+
+# https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter
+class AxeOptions(Base):
+    """axe options model"""
+
+    runOnly: list = dataclasses.field(default_factory=TEST_TAGS.copy)
+    rules: list = None
+    reporter: str = None
+    resultTypes: Any = None
+    selectors: bool = None
+    ancestry: bool = None
+    xpath: bool = None
+    absolutePaths: bool = None
+    iframes: bool = True
+    elementRef: bool = None
+    frameWaitTime: int = None
+    preload: bool = None
+    performanceTimer: bool = None
+    pingWaitTime: int = None
+
+
+def get_npm_directory(package, data=False):
+    """get the path of an npm package in the environment"""
+    try:
+        info = loads(check_output(split(f"npm ls --long --depth 0 --json {quote(package)}")))
+    except CalledProcessError:
+        return None
+    if data:
+        return info
+    return Path(info.get("dependencies").get(package).get("path"))
+
+
+class AxeResults(Base):
+    data: Any
+
+    def exception(self):
+        if self.data["violations"]:
+            return Violation.from_violations(self.data)
+
+    def raises(self):
+        exc = self.exception()
+        if exc:
+            raise exc
+
+    def dump(self, file: Path):
+        if file.is_dir():
+            file /= "axe-results.json"
+        file.parent.mkdir(exist_ok=True, parents=True)
+        file.write_text(dumps(self.data))
+        return self
+
+
+class NotAllOf(Exception):
+    ...
+
+
+class AllOf(Exception):
+    ...
+
+
+class NoAllOfMember(Exception):
+    ...
+
+
+@dataclasses.dataclass
+class Axe(Base):
+    """the Axe class is a fluent api for configuring and running accessibility tests."""
+
+    page: Any = None
+    url: str = None
+    results: Any = None
+
+    def __post_init__(self):
+        self.page.goto(self.url)
+        self.page.evaluate(get_axe())
+
+    def configure(self, **config):
+        self.page.evaluate(f"window.axe.configure({AxeConfigure(**config).dump()})")
+        return self
+
+    def reset(self):
+        self.page.evaluate(f"""window.axe.reset()""")
+        return self
+
+    def __enter__(self):
+        self.reset()
+
+    def __exit__(self, *e):
+        None
+
+    def run(self, test=None, options=None):
+        self.results = AxeResults(
+            self.page.evaluate(
+                f"""window.axe.run({test and dumps(test) or "document"}, {AxeOptions(**options or {}).dump()})"""
+            )
+        )
+        return self
+
+    def raises(self, allof=None):
+        if allof:
+            self.raises_allof(allof)
+        else:
+            self.results.raises()
+
+    def raises_allof(self, *types, extra=False):
+        found = set()
+        allof = set()
+        exc = self.results.exception()
+        if exc:
+            for t in list(types):
+                for e in exc.exceptions:
+                    allof.add(type(e))
+                    if isinstance(e, t):
+                        found.add(t)
+        not_found = set(types).difference(found)
+        if not_found:
+            raise NotAllOf(f"""{",".join(map(str, not_found))} not raised""")
+        elif not extra:
+            excess = allof.difference(found)
+            if excess:
+                raise NoAllOfMember(f"""{",".join(map(str, excess))} """)
+        result = AllOf(f"""{",".join(map(str, allof))} exceptions raised""")
+        result.__cause__ = exc
+        return result
+
+
+class Violation(Exception, Base):
+    id: str = dataclasses.field(repr=False)
+    impact: str | None = dataclasses.field(repr=False)
+    tags: list = dataclasses.field(default=None, repr=False)
+    description: str = ""
+    help: str = ""
+    helpUrl: str = ""
+    nodes: list = dataclasses.field(default=None, repr=False)
+    elements: dict = dataclasses.field(default_factory=partial(defaultdict, list))
+    map = {}
+
+    def __class_getitem__(cls, id):
+        if id in cls.map:
+            return cls.map[id]
+        return cls.map.setdefault(id, type(id, (Violation,), {}))
+
+    def __new__(cls, **kwargs):
+        if cls is Violation:
+            target = cls.cast(kwargs)
+            return target(**kwargs)
+        self = super().__new__(cls, **kwargs)
+        self.__init__(**kwargs)
+        return self
+
+    @classmethod
+    def cast(cls, data):
+        object = {"__doc__": f"""{data.get("help")} {data.get("helpUrl")}"""}
+        name = "-".join((data["impact"], data["id"]))
+        if name in cls.map:
+            return cls.map.get(name)
+        bases = ()
+        # these generate types primitves
+        if data["impact"]:
+            bases += (Violation[data["impact"]],)
+        for tag in data["tags"]:
+            bases += (Violation[tag],)
+        return cls.map.setdefault(name, type(name, bases, object))
+
+    def get_elements(self, N=150):
+        for node in self.nodes:
+            key = node["html"]
+            if len(key) > N:
+                key = key[:N] + "..."
+            self.elements[key].extend(node["target"])
+
+    def __str__(self):
+        try:
+            self.get_elements()
+            return repr(self)
+        except BaseException as e:
+            print(e)
+            raise e
+
+    @classmethod
+    def from_violations(cls, data):
+        out = []
+        for violation in (violations := data.get("violations")):
+            out.append(Violation(**violation))
+
+        return exceptiongroup.ExceptionGroup(f"{len(violations)} accessibility violations", out)
+
+
+@mark.parametrize("package", ["axe-core", param("axe-core-doesnt-ship-this", marks=mark.xfail)])
+def test_non_package(package):
+    assert get_npm_directory(package), "package not found."
+
+
+@lru_cache(1)
+def get_axe():
+    return (get_npm_directory("axe-core") / "axe.js").read_text()
+
+
+@fixture()
+def axe(page):
+    def go(url, **axe_config):
+        axe = Axe(page=page, url=url)
+        axe.configure(**axe_config)
+        return axe
+
+    return go
