3c333809c597530f149ddbb6f6fcf8181742011b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:21 +0000

    Add package defi-state-querier/0.4.2/defi-state-querier-0.4.2.tar.gz

---
+++ packages/defi-state-querier/defi-state-querier-0.4.2.tar.gz/defi-state-querier-0.4.2/src/defi_services/utils/init_services.py
@@ -0,0 +1,10 @@
+from defi_services.constants.entities.lending_services import LendingServices
+from defi_services.jobs.queriers.state_querier import StateQuerier
+
+
+def init_services(state_querier: StateQuerier, chain_id: str):
+    services = {}
+    for protocol, value in LendingServices.mapping.get(chain_id, {}).items():
+        services[protocol] = value(state_querier, chain_id)
+
+    return services
