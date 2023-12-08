26aa422afd8273b88fb1b848a9e7b801b39b79ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:05:19 +0000

    Add package spectrum.py/0.0.2/spectrum.py-0.0.2-py3-none-any.whl

---
+++ packages/spectrum.py/spectrum.py-0.0.2-py3-none-any.whl/spectrum/__init__.py
@@ -0,0 +1,11 @@
+from .httpclient import HTTPClient
+from .client import Client
+from .models import *
+from .errors import *
+
+from . import client
+from . import models
+from . import errors
+from . import util
+
+__version__ = "0.0.2"
