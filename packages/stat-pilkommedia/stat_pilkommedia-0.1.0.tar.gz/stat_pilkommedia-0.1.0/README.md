3191708b60c8fab3858c499ec03d9cc13b53e560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:04:03 +0000

    Add package stat-pilkommedia/0.1.0/stat_pilkommedia-0.1.0.tar.gz

---
+++ packages/stat-pilkommedia/stat_pilkommedia-0.1.0.tar.gz/stat_pilkommedia-0.1.0/README.md
@@ -0,0 +1,12 @@
+# Modul Statistika Pilkommedia
+
+Modul ini berisikan data serta fungsi-fungsi yang digunakan untuk keperluan perkuliahan di Program Studi Pendidikan Komputer FKIP ULM Banjarmasin.
+
+Untuk saat ini teradpat satu sub modul, yakni data statistik iwak papuyu dengan beberapa fungsi yang dapat digunakan, yakni sebagai berikut.
+
+- get_data(header=False)
+- get_header()
+- get_data_by_column(column_name)
+- average(column_name)
+- median(column_name)
+- modes(column_name)
---
+++ packages/stat-pilkommedia/stat_pilkommedia-0.1.0.tar.gz/stat_pilkommedia-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stat_pilkommedia",
+    version="0.1.0",
+    description="Modul statistika untuk keperluan perkuliahan Program Studi Pendidikan Komputer ULM",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    url="https://github.com/ahmadyogi543/stat_pilkommedia",
+    author="Ahmad Yogi",
+    author_email="ahmadyogi543@gmail.com",
+)
