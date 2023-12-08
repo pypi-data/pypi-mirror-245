223d3e12e165955057a52d0628e4a3d2c49038b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:03:51 +0000

    Add package afeng-py-tools/0.0.4/afeng-py-tools-0.0.4.tar.gz

---
+++ packages/afeng-py-tools/afeng-py-tools-0.0.4.tar.gz/afeng-py-tools-0.0.4/src/afeng_tools/template_tool/__init__.py
@@ -0,0 +1,9 @@
+"""
+模板工具
+"""
+from .core import calmjs_tools
+from .core import html_tools
+from .core import js_tools
+from .core import less_tools
+from .core import scss_tools
+from .core import vue_tools
