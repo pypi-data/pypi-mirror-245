730527e0b195600174481745e1615817c3076309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:37 +0000

    Add package nalibs-utility/1.0.0/nalibs-utility-1.0.0.tar.gz

---
+++ packages/nalibs-utility/nalibs-utility-1.0.0.tar.gz/nalibs-utility-1.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+# from pathlib import Path
+
+
+# def get_install_requires() -> list[str]:
+#     """Returns requirements.txt parsed to a list"""
+#     fname = Path(__file__).parent / 'requirements.txt'
+#     targets = []
+#     if fname.exists():
+#         with open(fname, 'r') as f:
+#             targets = f.read().splitlines()
+#     return targets
+
+
+# setuptools.setup(
+#     install_requires=get_install_requires(),
+#     setup_requires=get_install_requires(),
+# )
+
+setuptools.setup()
