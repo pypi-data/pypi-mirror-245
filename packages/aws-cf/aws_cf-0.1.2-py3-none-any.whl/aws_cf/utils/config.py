2b1c3ca4a1378ce8ca718debc597d6bd512ce183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:03:58 +0000

    Add package aws-cf/0.1.2/aws_cf-0.1.2-py3-none-any.whl

---
+++ packages/aws-cf/aws_cf-0.1.2-py3-none-any.whl/aws_cf/utils/config.py
@@ -0,0 +1,33 @@
+from typing import List
+from pydantic import BaseModel
+import yaml
+import os
+
+class Stack(BaseModel):
+    path: str
+    name: str
+
+class Enviroment(BaseModel):
+    name: str
+    profile: str
+    region: str
+    artifacts: str
+
+
+
+class Config(BaseModel):
+    Enviroments: List[Enviroment]
+    Stacks: List[Stack]
+
+    @staticmethod
+    def parse(path: str = None):
+        try:
+            data = yaml.safe_load(open(path))
+            return Config(**data)
+        except:
+            raise IOError("Not able to find file at path: " + '"' + path + '"')
+
+    def setup_env(self, env=None):
+        if not env:
+            os.environ["AWS_PROFILE"] = self.Enviroments[0].profile
+            os.environ["AWS_DEFAULT_REGION"] = self.Enviroments[0].region
