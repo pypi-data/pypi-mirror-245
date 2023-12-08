baa5feff46aa2704d405e053cb24341ac19f0979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:04:05 +0000

    Add package tesp-support/1.3.4/tesp_support-1.3.4.tar.gz

---
+++ packages/tesp-support/tesp_support-1.3.4.tar.gz/tesp_support-1.3.4/tesp_support/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2017-2023 Battelle Memorial Institute
+# file: __init__.py
+""" Transactive Energy Simulation Platform (TESP)
+Contains the python packages for the tesp_support
+
+Example:
+    To start PYPOWER for connection to FNCS::
+
+        import tesp_support.original.tso_PYPOWER_f as tesp
+        tesp.tso_pypower_loop_f('te30_pp.json','TE_Challenge')
+
+    To start PYPOWER for connection to HELICS::
+
+        import tesp_support.api.tso_PYPOWER as tesp
+        tesp.tso_pypower_loop('te30_pp.json','TE_Challenge', helicsConfig='tso.json')
+"""
---
+++ packages/tesp-support/tesp_support-1.3.4.tar.gz/tesp_support-1.3.4/tesp_support/matpower/__init__.py
@@ -0,0 +1,5 @@
+# Copyright (c) 2017-2023 Battelle Memorial Institute
+# file: __init__.py
+"""Support files for using MATPOWER - also see instructions on
+readthedocs for building the FNCS wrapper around the MATLAB run-time
+"""
