fd3c38668a1d088fe4c63e1cb3d7590f029c2fa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:31 +0000

    Add package karrio.freightcom/2023.9.2/karrio.freightcom-2023.9.2-py3-none-any.whl

---
+++ packages/karrio.freightcom/karrio.freightcom-2023.9.2-py3-none-any.whl/karrio/mappers/freightcom/__init__.py
@@ -0,0 +1,21 @@
+from karrio.core.metadata import Metadata
+
+from karrio.mappers.freightcom.mapper import Mapper
+from karrio.mappers.freightcom.proxy import Proxy
+from karrio.mappers.freightcom.settings import Settings
+import karrio.providers.freightcom.units as units
+
+
+METADATA = Metadata(
+    id="freightcom",
+    label="Freightcom",
+    is_hub=True,
+    # Integrations
+    Mapper=Mapper,
+    Proxy=Proxy,
+    Settings=Settings,
+    # Data Units
+    options=units.ShippingOption,
+    services=units.ShippingService,
+    hub_carriers=units.CARRIER_IDS,
+)
