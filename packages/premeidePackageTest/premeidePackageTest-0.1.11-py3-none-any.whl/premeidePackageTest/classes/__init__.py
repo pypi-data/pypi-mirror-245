ee25e1f7884006374f6e547414b191b395d2ff3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:04:33 +0000

    Add package premeidePackageTest/0.1.11/premeidePackageTest-0.1.11-py3-none-any.whl

---
+++ packages/premeidePackageTest/premeidePackageTest-0.1.11-py3-none-any.whl/premeidePackageTest/classes/__init__.py
@@ -0,0 +1,2 @@
+import Constants
+import File_gen
---
+++ packages/premeidePackageTest/premeidePackageTest-0.1.11-py3-none-any.whl/premeidePackageTest/classes/json/ResultWrapper.py
@@ -0,0 +1,6 @@
+from classes.json.Result import Result
+
+
+class ResultWrapper:
+    def __init__(self, result):
+        self.result = Result(**result)
