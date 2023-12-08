1f2c643617afed8f12f291f4f1e654940bafa7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:04:15 +0000

    Add package nothion/0.1.6/nothion-0.1.6.tar.gz

---
+++ packages/nothion/nothion-0.1.6.tar.gz/nothion-0.1.6/src/nothion/_config.py
@@ -0,0 +1,6 @@
+import os
+
+NT_AUTH = os.getenv("NT_AUTH", default="")
+NT_TASKS_DB_ID = os.getenv("NT_TASKS_DB_ID", default="")
+NT_STATS_DB_ID = os.getenv("NT_STATS_DB_ID", default="")
+NT_EXPENSES_DB_ID = os.getenv("NT_EXPENSES_DB_ID", default="")
