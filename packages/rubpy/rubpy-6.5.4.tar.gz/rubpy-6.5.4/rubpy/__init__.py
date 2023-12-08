917c803b49e872f754e89f3661cdaba6507523e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:04:37 +0000

    Add package rubpy/6.5.4/rubpy-6.5.4.tar.gz

---
+++ packages/rubpy/rubpy-6.5.4.tar.gz/rubpy-6.5.4/rubpy/__init__.py
@@ -0,0 +1,8 @@
+from .client import Client
+from .structs import handlers, models
+from .structs.struct import Struct as Message
+from .gadgets import exceptions, methods
+from . import emoji
+
+__version__ = '6.5.4'
+__author__ = 'Shayan Heidari'
