1000637b7fdc4e92c36e08fb7bb3be8dba4796f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:04:33 +0000

    Add package vstt/1.4.0/vstt-1.4.0-py3-none-any.whl

---
+++ packages/vstt/vstt-1.4.0-py3-none-any.whl/vstt/__main__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import logging
+from typing import Optional
+
+import click
+from psychopy.gui.qtgui import ensureQtApp
+from qtpy import QtWidgets
+from vstt.gui import Gui
+
+
+@click.command()
+@click.argument("filename", required=False)
+def main(filename: Optional[str]) -> None:
+    logging.basicConfig(
+        format="%(levelname)s %(module)s.%(funcName)s.%(lineno)d :: %(message)s"
+    )
+    ensureQtApp()
+    app = QtWidgets.QApplication.instance()
+    assert app is not None
+    gui = Gui(filename=filename)
+    gui.show()
+    app.exec()
+
+
+if __name__ == "__main__":
+    main()
