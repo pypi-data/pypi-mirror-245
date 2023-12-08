1f2380c5430eb696974160ea44e411e16939dba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:04:05 +0000

    Add package aeom/0.0.8/aeom-0.0.8.tar.gz

---
+++ packages/aeom/aeom-0.0.8.tar.gz/aeom-0.0.8/aeom/__init__.py
@@ -0,0 +1,9 @@
+
+__version__ = '0.0.8'                  
+__enable_cython__ = True 
+
+
+from .cluster import AEOM
+from .cluster import calculate_cluster_centers
+from .cluster import normalization
+
