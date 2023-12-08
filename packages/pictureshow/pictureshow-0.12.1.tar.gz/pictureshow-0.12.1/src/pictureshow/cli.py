f3da89890c18c90e47c108d108c6abb49cd6e4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 16:04:03 +0000

    Add package pictureshow/0.12.1/pictureshow-0.12.1.tar.gz

---
+++ packages/pictureshow/pictureshow-0.12.1.tar.gz/pictureshow-0.12.1/src/pictureshow/cli.py
@@ -0,0 +1,189 @@
+import argparse
+import contextlib
+import io
+import sys
+from pathlib import Path
+
+from . import __version__
+from .core import PAGE_SIZES, PictureShow
+
+
+def get_args(parser):
+    parser.add_argument(
+        'pictures',
+        nargs='+',
+        metavar='PICTURE',
+        help='one or more picture paths or URLs',
+    )
+    parser.add_argument(
+        '-a',
+        '--fill-area',
+        action='store_true',
+        help="fill drawing area with picture, ignoring the picture's aspect ratio",
+    )
+    parser.add_argument(
+        '-b',
+        '--bg-color',
+        metavar='COLOR',
+        help='specify page background color as 6-digit hexadecimal RGB, e.g. ff8c00',
+    )
+    parser.add_argument(
+        '-f',
+        '--force-overwrite',
+        action='store_true',
+        help='save to output filename even if file exists',
+    )
+    parser.add_argument(
+        '-F',
+        '--fail',
+        choices=('skipped', 'no-output', 'no'),
+        default='no-output',
+        metavar='MODE',
+        help='If set to `skipped`, fail (exit with code 2) if at least one file '
+             'was skipped due to an error. '
+             'If set to `no-output` (default), fail if all files were skipped '
+             'and no PDF file was created; succeed (exit with code 0) if at '
+             'least one file was successfully saved. '
+             'If set to `no`, succeed even if all files were skipped.',
+    )
+    parser.add_argument(
+        '-L',
+        '--landscape',
+        action='store_true',
+        help='set landscape orientation of page; default is portrait',
+    )
+    parser.add_argument(
+        '-l',
+        '--layout',
+        default='1x1',
+        help='specify grid layout (columns x rows) of pictures on page, '
+             'e.g. 2x3 or 2,3; default is 1x1',
+    )
+    parser.add_argument(
+        '-m',
+        '--margin',
+        type=float,
+        default=72,
+        help='set width of empty space around drawing areas; '
+             'default is 72 (72 points = 1 inch)',
+    )
+    parser.add_argument(
+        '-o',
+        '--output-file',
+        required=True,
+        metavar='PATH',
+        help='path of the output PDF file (required)',
+    )
+    parser.add_argument(
+        '-p',
+        '--page-size',
+        choices=PAGE_SIZES,
+        default='A4',
+        metavar='SIZE',
+        help='specify page size; default is A4 '
+             f'(available sizes: {", ".join(PAGE_SIZES)})',
+    )
+    verbosity_group = parser.add_mutually_exclusive_group()
+    verbosity_group.add_argument(
+        '-q',
+        '--quiet',
+        action='store_true',
+        help='suppress printing to stdout',
+    )
+    parser.add_argument(
+        '-s',
+        '--stretch-small',
+        action='store_true',
+        help='scale small pictures up to fit drawing areas',
+    )
+    verbosity_group.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help='show details on files skipped due to error',
+    )
+    parser.add_argument('-V', '--version', action='version')
+
+    return parser.parse_args()
+
+
+def report_results(result, output_file, verbose=False):
+    unique_errors = dict(result.errors)
+    num_errors = len(unique_errors)
+    if num_errors > 0:
+        print(f'{_number(num_errors, "file")} skipped due to error.')
+        if verbose:
+            for pic_file, error in unique_errors.items():
+                print(f'{pic_file}:\n{type(error).__name__}: {error}\n')
+
+    if result.num_ok > 0:
+        print(
+            f'Saved {_number(result.num_ok, "picture")} '
+            f'({_number(result.num_pages, "page")}) to {output_file!r}'
+        )
+    else:
+        print('Nothing to save.')
+
+
+def _number(number, noun):
+    """Return a repr of amount in correct grammatical number."""
+    suffix = 's' if number > 1 else ''
+    return f'{number} {noun}{suffix}'
+
+
+def _ensure_suffix(file_path):
+    """If `file_path` has no suffix, add '.pdf' to it."""
+    path = Path(file_path)
+    if path.suffix:
+        return file_path
+    return str(path.with_suffix('.pdf'))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [options] PICTURE [PICTURE ...] -o PATH',
+        description='Save pictures to PDF.',
+        epilog='https://pypi.org/project/pictureshow/',
+    )
+    parser.version = __version__
+
+    # handle special case before parsing args
+    if not sys.argv[1:]:
+        parser.print_usage(file=sys.stderr)
+        parser.exit(2, f"Try '{parser.prog} --help' for more information.\n")
+
+    args = get_args(parser)
+    output_file = _ensure_suffix(args.output_file)
+
+    stdout_context = (
+        contextlib.redirect_stdout(io.StringIO()) if args.quiet
+        else contextlib.nullcontext()
+    )
+    with stdout_context:
+        try:
+            pic_show = PictureShow(*args.pictures)
+            for ok_flag in pic_show._save_pdf(
+                    output_file=output_file,
+                    force_overwrite=args.force_overwrite,
+                    page_size=args.page_size,
+                    landscape=args.landscape,
+                    bg_color=args.bg_color,
+                    layout=args.layout,
+                    margin=args.margin,
+                    stretch_small=args.stretch_small,
+                    fill_area=args.fill_area,
+            ):
+                print('.' if ok_flag else '!', end='', flush=True)
+            print()
+            result = pic_show.result
+        except Exception as err:
+            parser.error(f'{type(err).__name__}: {err}')
+        else:
+            report_results(result, output_file, args.verbose)
+            if args.fail == 'skipped':
+                exit_code = 2 if result.errors else 0
+            elif args.fail == 'no-output':
+                exit_code = 2 if result.num_ok == 0 else 0
+            else:
+                exit_code = 0
+            parser.exit(exit_code)
---
+++ packages/pictureshow/pictureshow-0.12.1.tar.gz/pictureshow-0.12.1/src/pictureshow/core.py
@@ -0,0 +1,264 @@
+import itertools
+import re
+from collections import namedtuple
+from pathlib import Path
+
+from reportlab.lib import pagesizes
+
+from .backends import ReportlabBackend
+from .exceptions import LayoutError, MarginError, PageSizeError, RGBColorError
+
+PAGE_SIZES = {
+    name: size
+    for name, size in vars(pagesizes).items()
+    # use isupper() to exclude deprecated names and function names
+    if name.isupper()
+}
+
+DELIMITER = re.compile('[x,]')
+
+DrawingArea = namedtuple('DrawingArea', 'x y width height')
+
+Result = namedtuple('Result', 'num_ok errors num_pages')
+
+
+class PictureShow:
+    def __init__(self, *pic_files):
+        self._pic_files = pic_files
+        self._backend = ReportlabBackend()
+
+    def save_pdf(
+            self,
+            output_file,
+            *,
+            force_overwrite=False,
+            page_size='A4',
+            landscape=False,
+            bg_color=None,
+            layout=(1, 1),
+            margin=72,
+            stretch_small=False,
+            fill_area=False,
+    ):
+        """Save pictures stored in `self._pic_files` to a PDF document.
+
+        Return a named tuple of three values:
+        `num_ok` - number of successfully saved pictures
+        `errors` - list of items skipped due to error
+        `num_pages` - number of pages of the resulting PDF document
+        """
+        for _ in self._save_pdf(
+                output_file,
+                force_overwrite=force_overwrite,
+                page_size=page_size,
+                landscape=landscape,
+                bg_color=bg_color,
+                layout=layout,
+                margin=margin,
+                stretch_small=stretch_small,
+                fill_area=fill_area,
+        ):
+            pass
+        return self.result
+
+    def _save_pdf(
+            self,
+            output_file,
+            *,
+            force_overwrite,
+            page_size,
+            landscape,
+            bg_color,
+            layout,
+            margin,
+            stretch_small,
+            fill_area,
+    ):
+        output_file = self._validate_target_path(output_file, force_overwrite)
+        page_size = self._validate_page_size(page_size, landscape)
+        bg_color = self._validate_color(bg_color)
+        layout = self._validate_layout(layout)
+
+        self._backend.init(output_file, page_size, bg_color)
+        valid_pics = self._valid_pictures()
+        self.num_ok = 0
+        areas = tuple(self._areas(layout, page_size, margin))
+        while True:
+            for area in areas:
+                try:
+                    while True:
+                        picture = next(valid_pics)
+                        if picture is not None:
+                            break
+                        yield False
+                except StopIteration:
+                    if self.num_ok > 0:
+                        self._backend.save()
+                    self.num_pages = self._backend.num_pages
+                    return
+                x, y, pic_width, pic_height = self._position_and_size(
+                    self._backend.get_picture_size(picture),
+                    area[2:],    # short for (area.width, area.height)
+                    stretch_small,
+                    fill_area,
+                )
+                self._backend.add_picture(
+                    picture, area.x + x, area.y + y, pic_width, pic_height
+                )
+                self.num_ok += 1
+                yield True
+            self._backend.add_page()
+
+    @property
+    def result(self):
+        return Result(self.num_ok, self.errors, self.num_pages)
+
+    @staticmethod
+    def _validate_target_path(path, force_overwrite):
+        target_str = str(path)
+        target_path = Path(path)
+
+        if target_path.exists() and not force_overwrite:
+            raise FileExistsError(f'file {target_str!r} exists')
+
+        return target_str
+
+    @staticmethod
+    def _validate_page_size(page_size, landscape):
+        if isinstance(page_size, str):
+            try:
+                page_size = PAGE_SIZES[page_size.upper()]
+            except KeyError as err:
+                raise PageSizeError(
+                    f'unknown page size {page_size!r},'
+                    f' please use one of: {", ".join(PAGE_SIZES)}'
+                ) from err
+
+        page_size_error = PageSizeError('two positive numbers expected')
+        try:
+            page_width, page_height = page_size
+            if not (page_width > 0 and page_height > 0):
+                raise page_size_error
+        except (ValueError, TypeError) as err:
+            raise page_size_error from err
+
+        if page_width < page_height and landscape:
+            page_size = page_height, page_width
+        return page_size
+
+    @staticmethod
+    def _validate_color(color):
+        if color is None:
+            return None
+        try:
+            r, g, b = bytes.fromhex(color)
+        except (ValueError, TypeError) as err:
+            raise RGBColorError('6-digit hex value expected') from err
+        return r, g, b
+
+    @staticmethod
+    def _validate_layout(layout):
+        layout_error = LayoutError('two positive integers expected')
+        try:
+            if isinstance(layout, str):
+                layout = tuple(int(s) for s in DELIMITER.split(layout))
+            columns, rows = layout
+            if not (
+                    isinstance(columns, int) and isinstance(rows, int)
+                    and columns > 0 and rows > 0
+            ):
+                raise layout_error
+        except (ValueError, TypeError) as err:
+            raise layout_error from err
+
+        return columns, rows
+
+    def _valid_pictures(self):
+        self.errors = []
+        for pic_file in self._pic_files:
+            try:
+                picture = self._backend.read_picture(pic_file)
+            except self._backend.read_errors as err:
+                self.errors.append((pic_file, err))
+                yield None
+            else:
+                yield picture
+
+    @staticmethod
+    def _position_and_size(pic_size, area_size, stretch_small, fill_area):
+        """Calculate position and size of the picture in the area."""
+        area_width, area_height = area_size
+        if fill_area:
+            return 0, 0, area_width, area_height
+
+        pic_width, pic_height = pic_size
+        pic_is_big = pic_width > area_width or pic_height > area_height
+        pic_is_wide = pic_width / pic_height > area_width / area_height
+
+        # calculate scale factor to fit picture to area
+        if pic_is_big or stretch_small:
+            scale = area_width / pic_width if pic_is_wide else area_height / pic_height
+            pic_width *= scale
+            pic_height *= scale
+
+        # center picture to area
+        x = (area_width - pic_width) / 2
+        y = (area_height - pic_height) / 2
+
+        return x, y, pic_width, pic_height
+
+    @staticmethod
+    def _areas(layout, page_size, margin):
+        num_columns, num_rows = layout
+        page_width, page_height = page_size
+
+        area_width = (page_width - (num_columns + 1) * margin) / num_columns
+        area_height = (page_height - (num_rows + 1) * margin) / num_rows
+        if area_width < 1 or area_height < 1:
+            raise MarginError(f'margin value too high: {margin}')
+
+        areas_y_coords = (
+            page_height - row * (area_height + margin)
+            for row in range(1, num_rows + 1)
+        )
+        areas_x_coords = (
+            margin + col * (area_width + margin)
+            for col in range(num_columns)
+        )
+        # yield areas row-wise
+        for y, x in itertools.product(areas_y_coords, areas_x_coords):
+            yield DrawingArea(x, y, area_width, area_height)
+
+
+def pictures_to_pdf(
+        *pic_files,
+        output_file,
+        force_overwrite=False,
+        page_size='A4',
+        landscape=False,
+        bg_color=None,
+        layout=(1, 1),
+        margin=72,
+        stretch_small=False,
+        fill_area=False,
+):
+    """Save one or more pictures to a PDF document.
+
+    Return a named tuple of three values:
+    `num_ok` - number of successfully saved pictures
+    `errors` - list of items skipped due to error
+    `num_pages` - number of pages of the resulting PDF document
+    """
+    pic_show = PictureShow(*pic_files)
+
+    return pic_show.save_pdf(
+        output_file,
+        force_overwrite=force_overwrite,
+        page_size=page_size,
+        landscape=landscape,
+        bg_color=bg_color,
+        layout=layout,
+        margin=margin,
+        stretch_small=stretch_small,
+        fill_area=fill_area,
+    )
