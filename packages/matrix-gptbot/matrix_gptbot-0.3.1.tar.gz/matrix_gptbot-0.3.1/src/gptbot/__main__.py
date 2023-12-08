f6c6d0e41e96092f25892a9211855db4d3b47883
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:47 +0000

    Add package matrix-gptbot/0.3.1/matrix_gptbot-0.3.1.tar.gz

---
+++ packages/matrix-gptbot/matrix_gptbot-0.3.1.tar.gz/matrix_gptbot-0.3.1/src/gptbot/__main__.py
@@ -0,0 +1,48 @@
+from .classes.bot import GPTBot
+
+from argparse import ArgumentParser
+from configparser import ConfigParser
+
+import signal
+import asyncio
+
+
+def sigterm_handler(_signo, _stack_frame):
+    exit()
+
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--config",
+        "-c",
+        help="Path to config file (default: config.ini in working directory)",
+        default="config.ini",
+    )
+    parser.add_argument(
+        "--version",
+        "-v",
+        help="Print version and exit",
+        action="version",
+        version="GPTBot v0.1.1",
+    )
+    args = parser.parse_args()
+
+    # Read config file
+    config = ConfigParser()
+    config.read(args.config)
+
+    # Create bot
+    bot = GPTBot.from_config(config)
+
+    # Listen for SIGTERM
+    signal.signal(signal.SIGTERM, sigterm_handler)
+
+    # Start bot
+    try:
+        asyncio.run(bot.run())
+    except KeyboardInterrupt:
+        print("Received KeyboardInterrupt - exiting...")
+    except SystemExit:
+        print("Received SIGTERM - exiting...")
