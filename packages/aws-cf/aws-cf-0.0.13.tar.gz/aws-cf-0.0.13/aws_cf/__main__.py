32bb998f65df2616b7639e0177961ab71a6fbbe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:58 +0000

    Add package aws-cf/0.0.13/aws-cf-0.0.13.tar.gz

---
+++ packages/aws-cf/aws-cf-0.0.13.tar.gz/aws-cf-0.0.13/aws_cf/__main__.py
@@ -0,0 +1,23 @@
+import argparse
+from .deploy import deploy
+from .utils.logging import logger
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("action", choices=['diff', 'deploy', 'package', "version"], help='what action to preform')
+parser.add_argument("path", help='path to the file describing the services')
+parser.add_argument("-v", '--version', action='version', help='path to the file describing the services', version='aws-cf: 0.0.0')
+parser.add_argument("-r", "--root")
+
+args = parser.parse_args()
+
+if args.action:
+    print("Version: ")
+
+if args.action == "deploy":
+    try:
+        deploy(args.path, args.root or "")
+    except Exception as e:
+        logger.error(str(e))
+
+
