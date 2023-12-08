1e3473b137fc30acb12570f59389ab4918b7d17b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:47 +0000

    Add package TaoWa/1.0.0/TaoWa-1.0.0.tar.gz

---
+++ packages/TaoWa/TaoWa-1.0.0.tar.gz/TaoWa-1.0.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='TaoWa',
+    version='1.0.0',
+    description='中文模块',
+    long_description_content_type='text/markdown',
+    author='陈炳强',
+    author_email='99396686@qq.com',
+    url='https://github.com/ChenBingQiangi/-',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.1',
+    # 如果有依赖包，可以在此处添加
+    install_requires=[
+        'wxPython==4.1.0',
+        'requests',
+        'PyExecjs',
+        'web3',
+        'pywin32',
+    ],
+)
