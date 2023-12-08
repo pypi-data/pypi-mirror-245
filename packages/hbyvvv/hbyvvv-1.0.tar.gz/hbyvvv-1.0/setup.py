37d14353d398be7e653cd18f45eec9c333da2905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 03:04:45 +0000

    Add package hbyvvv/1.0/hbyvvv-1.0.tar.gz

---
+++ packages/hbyvvv/hbyvvv-1.0.tar.gz/hbyvvv-1.0/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+
+setuptools.setup(
+    name="hbyvvv",
+    version="1.0",
+    author="hk",
+    author_email="1301016428@qq.com.com",
+    description="",
+    long_description="spider tools",
+    long_description_content_type="spider tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
