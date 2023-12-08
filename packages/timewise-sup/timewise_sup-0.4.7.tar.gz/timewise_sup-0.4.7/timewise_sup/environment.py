cf3ec0379f28af795e9d3d7da23e27d0a32dc46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:04:03 +0000

    Add package timewise-sup/0.4.7/timewise_sup-0.4.7.tar.gz

---
+++ packages/timewise-sup/timewise_sup-0.4.7.tar.gz/timewise_sup-0.4.7/timewise_sup/environment.py
@@ -0,0 +1,24 @@
+import os
+import logging
+from pathlib import Path
+
+from timewise.general import data_dir
+
+
+logger = logging.getLogger(__name__)
+
+environment_variable_defaults = {
+    "TIMEWISE_SUP_DATA": str(Path(data_dir) / "subtraction_pipeline"),
+    "TIMEWISE_SUP_MONGODB_PORT": "27017",
+    "ZTF_FPH": None,
+    "ZTF_NUCLEAR_SAMPLE_DB_PATH": None
+}
+
+
+def load_environment(key):
+    try:
+        return os.environ[key]
+    except KeyError:
+        default_val = environment_variable_defaults[key]
+        logger.debug(f"environment variable {key} not specified. Using default {default_val}")
+        return default_val
