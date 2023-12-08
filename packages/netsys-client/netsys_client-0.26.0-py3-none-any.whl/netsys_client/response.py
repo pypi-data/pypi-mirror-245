e68b9c6d83d5a836c08a511c30b73bb18f384f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:03:54 +0000

    Add package netsys-client/0.26.0/netsys_client-0.26.0-py3-none-any.whl

---
+++ packages/netsys-client/netsys_client-0.26.0-py3-none-any.whl/netsys_client/response.py
@@ -0,0 +1,31 @@
+import socket
+import tkinter as tk
+
+class Response:
+
+    _host = 'localhost'
+    _port = 12345
+    _message = 0
+
+    def __init__(self):
+
+        self._client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._client.connect((self._host, self._port))
+
+    def send_message(self, message):
+
+        self._client.sendall(str.encode(message))
+        data = self._client.recv(4096)
+        self._message = data.decode()
+        root = tk.Tk()
+        root.title("Test Screen")
+        label = tk.Label(root, text=self._message)
+        label.pack()
+        root.mainloop()
+        self._client.close()
+
+    def get_message(self):
+
+        return self._message
+
+    
