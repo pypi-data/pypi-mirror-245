dd6aca6c909fdab212d2c8c23498b07b4d8086c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:03:56 +0000

    Add package pymor/2023.2.0/pymor-2023.2.0.tar.gz

---
+++ packages/pymor/pymor-2023.2.0.tar.gz/pymor-2023.2.0/src/pymordemos/delay.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# This file is part of the pyMOR project (https://www.pymor.org).
+# Copyright pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
+import matplotlib.pyplot as plt
+import numpy as np
+from typer import Argument, run
+
+from pymor.models.transfer_function import TransferFunction
+from pymor.reductors.h2 import TFIRKAReductor
+from pymordemos.heat import fom_properties, run_mor_method
+
+
+def main(
+        tau: float = Argument(0.1, help='The time delay.'),
+        r: int = Argument(10, help='Order of the TF-IRKA ROM.'),
+):
+    """Delay demo.
+
+    Cascade of delay and integrator
+    """
+    plt.rcParams['axes.grid'] = True
+
+    # Transfer function
+    def H(s):
+        return np.array([[np.exp(-s) / (tau * s + 1)]])
+
+    def dH(s):
+        return np.array([[-(tau * s + tau + 1) * np.exp(-s) / (tau * s + 1) ** 2]])
+
+    tf = TransferFunction(1, 1, H, dH)
+
+    # Bode plot
+    w = (1e-1, 1e3)
+    fom_properties(tf, w)
+
+    # Transfer function IRKA (TF-IRKA)
+    run_mor_method(tf, w, TFIRKAReductor(tf), 'TF-IRKA', r, maxit=1000)
+
+
+if __name__ == '__main__':
+    run(main)
---
+++ packages/pymor/pymor-2023.2.0.tar.gz/pymor-2023.2.0/src/pymortests/algorithms/rand_la.py
@@ -0,0 +1,105 @@
+# This file is part of the pyMOR project (https://www.pymor.org).
+# Copyright pyMOR developers and contributors. All rights reserved.
+# License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
+
+import numpy as np
+import pytest
+import scipy.linalg as spla
+from numpy.random import uniform
+
+from pymor.algorithms.rand_la import adaptive_rrf, random_generalized_svd, random_ghep, rrf
+from pymor.operators.constructions import VectorArrayOperator
+from pymor.operators.numpy import NumpyMatrixOperator
+
+pytestmark = pytest.mark.builtin
+
+
+def test_adaptive_rrf():
+    A = uniform(low=-1.0, high=1.0, size=(100, 100))
+    A = A @ A.T
+    range_product = NumpyMatrixOperator(A)
+
+    B = uniform(low=-1.0, high=1.0, size=(10, 10))
+    B = B.dot(B.T)
+    source_product = NumpyMatrixOperator(B)
+
+    C = range_product.range.random(10)
+    op = VectorArrayOperator(C)
+
+    D = range_product.range.random(10)
+    D += 1j*range_product.range.random(10)
+    op_complex = VectorArrayOperator(D)
+
+    Q1 = adaptive_rrf(op, source_product, range_product)
+    assert Q1 in op.range
+
+    Q2 = adaptive_rrf(op_complex, iscomplex=True)
+    assert np.iscomplexobj(Q2.to_numpy())
+    assert Q2 in op.range
+
+
+def test_rrf():
+    A = uniform(low=-1.0, high=1.0, size=(100, 100))
+    A = A @ A.T
+    range_product = NumpyMatrixOperator(A)
+
+    B = uniform(low=-1.0, high=1.0, size=(10, 10))
+    B = B @ B.T
+    source_product = NumpyMatrixOperator(B)
+
+    C = range_product.range.random(10)
+    op = VectorArrayOperator(C)
+
+    D = range_product.range.random(10)
+    D += 1j*range_product.range.random(10)
+    op_complex = VectorArrayOperator(D)
+
+    Q1 = rrf(op, source_product, range_product)
+    assert Q1 in op.range
+    assert len(Q1) == 8
+
+    Q2 = rrf(op_complex, iscomplex=True)
+    assert np.iscomplexobj(Q2.to_numpy())
+    assert Q2 in op.range
+    assert len(Q2) == 8
+
+
+def test_random_generalized_svd():
+    E = uniform(low=-1.0, high=1.0, size=(5, 5))
+    E_op = NumpyMatrixOperator(E)
+
+    modes = 3
+    U, s, Vh = random_generalized_svd(E_op, modes=modes, p=1)
+    U_real, s_real, Vh_real = spla.svd(E)
+
+    assert abs(np.linalg.norm(s-s_real[:modes])) <= 1e-2
+    assert len(U) == modes
+    assert len(Vh) == modes
+    assert len(s) == modes
+    assert U in E_op.range
+    assert Vh in E_op.source
+
+
+def test_random_ghep():
+    D = uniform(low=-1.0, high=1.0, size=(5, 5))
+    D = D @ D.T
+    D_op = NumpyMatrixOperator(D)
+
+    modes = 3
+    w1, V1 = random_ghep(D_op, modes=modes, p=1, single_pass=False)
+    w2, V2 = random_ghep(D_op, modes=modes, p=1, single_pass=True)
+    w_real, V_real = spla.eigh(D)
+    w_real = w_real[::-1]
+    V_real = V_real[:, ::-1]
+
+    assert abs(np.linalg.norm(w1-w_real[:modes])) <= 1e-2
+    assert abs(np.linalg.norm(w2-w_real[:modes])) <= 1
+
+    for i in range(0, modes):
+        assert np.linalg.norm(abs(V1.to_numpy()[i, :]) - abs(V_real[:, i])) <= 1
+    for i in range(0, modes):
+        assert np.linalg.norm(abs(V2.to_numpy()[i, :]) - abs(V_real[:, i])) <= 1
+
+    assert len(w1) == modes
+    assert len(V1) == modes
+    assert V1.dim == D_op.source.dim
