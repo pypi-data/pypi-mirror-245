92713ac76345d7a5540d01e5d00ed694647f1674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 15:04:29 +0000

    Add package karrio.dpdhl/2023.9.2/karrio.dpdhl-2023.9.2-py3-none-any.whl

---
+++ packages/karrio.dpdhl/karrio.dpdhl-2023.9.2-py3-none-any.whl/karrio/mappers/dpdhl/__init__.py
@@ -0,0 +1,21 @@
+from karrio.core.metadata import Metadata
+
+from karrio.mappers.dpdhl.mapper import Mapper
+from karrio.mappers.dpdhl.proxy import Proxy
+from karrio.mappers.dpdhl.settings import Settings
+import karrio.providers.dpdhl.units as units
+
+
+METADATA = Metadata(
+    id="dpdhl",
+    label="Deutsche Post DHL",
+    # Integrations
+    Mapper=Mapper,
+    Proxy=Proxy,
+    Settings=Settings,
+    # Data Units
+    services=units.ShippingService,
+    options=units.ShippingOption,
+    service_levels=units.DEFAULT_SERVICES,
+    connection_configs=units.ConnectionConfig,
+)
