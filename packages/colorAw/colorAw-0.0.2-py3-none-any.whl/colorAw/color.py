dd2af6e8a0c4f55f7c27dacf0e0148005b582196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:04:03 +0000

    Add package colorAw/0.0.2/colorAw-0.0.2-py3-none-any.whl

---
+++ packages/colorAw/colorAw-0.0.2-py3-none-any.whl/colorAw/color.py
@@ -0,0 +1,29 @@
+class text():
+    black = "\033[0;30m"
+    red = "\033[0;31m"
+    green = "\033[0;32m"
+    yellow = "\033[;33m"
+    blue = "\033[0;34m"
+    purple = "\033[0;35m"
+    cyan = "\033[0;36m"
+    white = "\033[0;37m"
+
+class bold():
+    black = "\033[1;30m"
+    red = "\033[1;31m"
+    green = "\033[1;32m"
+    yellow = "\033[1;33m"
+    blue = "\033[0;34m"
+    purple = "\033[1;35m"
+    cyan = "\033[1;36m"
+    white = "\033[1;37m"
+
+class back():
+    black = "\033[40m"
+    red = "\033[41m"
+    green = "\033[42m"
+    yellow = "\033[43m"
+    blue = "\033[44m"
+    purple = "\033[45m"
+    cyan = "\033[46m"
+    white = "\033[47m"
