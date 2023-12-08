b54ff677fdc8bb043c5f3f808761c6990d6e6b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:04:11 +0000

    Add package CS2APY/0.3/CS2APY-0.3.tar.gz

---
+++ packages/CS2APY/CS2APY-0.3.tar.gz/CS2APY-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='CS2APY',
+    version='0.3',
+    author='James Li',
+    author_email='jamesli196q@gmail.com',
+    description='A high-level Python3 wrapper module for the Counter-Strike: 2 Premier Leaderboards API.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/j4mesli/cs2leaderboard-APY',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['maps.json', 'scoreboard.proto'],
+    },
+    scripts=['CS2APY.py', 'scoreboard_pb2.py', '__init__.py'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
