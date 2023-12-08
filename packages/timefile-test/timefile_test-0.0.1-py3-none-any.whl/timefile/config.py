af499c057840c57902b10bc02fd494c580f937bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 04:03:57 +0000

    Add package timefile-test/0.0.1/timefile_test-0.0.1-py3-none-any.whl

---
+++ packages/timefile-test/timefile_test-0.0.1-py3-none-any.whl/timefile/config.py
@@ -0,0 +1,15 @@
+# Config 
+ROOT = './timefile'
+LOG_DIR = f'{ROOT}/logs'
+PLOT_DIR = f'{ROOT}/plots'
+
+# - - - - - - - - - - - - - 
+
+LOG_FILENAME = 'timelogs.log'
+
+# - - - - - - - - - - - - - 
+
+
+LOG_FILEPATH = f'{LOG_DIR}/{LOG_FILENAME}'
+
+BAD_FUNCTIONS = ["matplotlib", "matplotlib.font_manager", "matplotlib.pyplot"]
