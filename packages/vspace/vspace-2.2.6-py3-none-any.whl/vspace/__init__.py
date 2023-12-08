5dab2e450d807905607696a764cf42a29b26bb0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-08 14:03:53 +0000

    Add package vspace/2.2.6/vspace-2.2.6-py3-none-any.whl

---
+++ packages/vspace/vspace-2.2.6-py3-none-any.whl/vspace/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+@author: cwilhelm 2020
+
+vspace package init file.
+
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__version__ = "1.0.1"
+__author__ = "Caitlyn Wilhelm (cwilhelm@uw.edu)"
+__copyright__ = "Copyright 2020  Caitlyn Wilhelm"
+
+from .vspace import *
