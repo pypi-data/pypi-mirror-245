7ded8f25a5971626cbb9fcb3a44326887f0afd4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:53 +0000

    Add package red_utils/0.2.11/red_utils-0.2.11.tar.gz

---
+++ packages/red_utils/red_utils-0.2.11.tar.gz/red_utils-0.2.11/red_utils/std/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from . import (
+    context_managers,
+    dataclass_mixins,
+    dict_utils,
+    path_utils,
+    hash_utils,
+    time_utils,
+    uuid_utils,
+)
