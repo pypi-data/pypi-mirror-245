e7efb8850d1e88658025978cf5e225b135493ad6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:33 +0000

    Add package hostess/0.8.0/hostess-0.8.0-py3-none-any.whl

---
+++ packages/hostess/hostess-0.8.0-py3-none-any.whl/hostess/config/config.py
@@ -0,0 +1,24 @@
+"""
+desired settings should be placed in hostess/user_config/user_config.py.
+"""
+import os
+from pathlib import Path
+
+home = os.path.expanduser("~")
+
+GENERAL_DEFAULTS = {
+    "secrets_folders": (
+        Path(home, ".hostess", "secrets"),
+        Path(home),
+        Path(home, "Downloads"),
+        Path(home, ".ssh"),
+    ),
+    "cache_path": f"{home}/.hostess/cache",
+    "log_path": f"{home}/.hostess/logs",
+    "uname": "ubuntu",
+}
+EC2_DEFAULTS = {
+    "instance_type": "t3a.small",
+    "volume_type": "gp3",
+    "volume_size": 8,
+}
