e8af3e39f9b1997d3e469faaec252528917ec13b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:04:32 +0000

    Add package regression-meriem-mk-seds/1.0.0/regression-meriem-mk-seds-1.0.0.tar.gz

---
+++ packages/regression-meriem-mk-seds/regression-meriem-mk-seds-1.0.0.tar.gz/regression-meriem-mk-seds-1.0.0/README.md
@@ -0,0 +1,15 @@
+# package_name
+
+`regression-meriem-mk-seds` is a Python library for ..........
+
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install `package_name`.
+
+```bash
+pip regression-meriem-mk-seds
+
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ packages/regression-meriem-mk-seds/regression-meriem-mk-seds-1.0.0.tar.gz/regression-meriem-mk-seds-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+   name='regression-meriem-mk-seds',
+   version='1.0.0',
+   author='meriem-mk',
+   author_email='me.mekki@esi-sba.dz',
+   packages=['regression-meriem-mk-seds'],
+   url='http://pypi.python.org/pypi/regression-meriem-mk-seds/',
+   license='LICENSE.txt',
+   description='An awesome package that does something',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=['regression-meriem-mk-seds']
+)
