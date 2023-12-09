c3f9fd815a4e3a40089862e39fa5cd579e25ca81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:04:10 +0000

    Add package mag4/0.0.205/mag4-0.0.205-py3-none-any.whl

---
+++ packages/mag4/mag4-0.0.205-py3-none-any.whl/mag4/get_data.py
@@ -0,0 +1,45 @@
+# requirements
+import pandas as pd
+import requests
+import json
+
+base_url = 'https://raw.githubusercontent.com/Hezel2000/cosmogeochemdata/master/'
+
+# Get a specific database
+def get_data(database, property=None, type=None):
+    df_GCdata = pd.read_csv(base_url + 'GCCdata.csv')
+    fil = (df_GCdata['available datasets'] == database) | (df_GCdata['abbreviated name'] == database)
+    url = base_url + 'json/' + df_GCdata[fil]['available datasets'].values[0] + '.json'
+    resp = requests.get(url)
+    full_data = json.loads(resp.text)
+
+    if property is None:
+        if type is None or type == 'dataframe':
+            return pd.DataFrame(full_data['dataset'])
+        elif type == 'json':
+            return full_data['dataset']
+    elif property == 'properties':
+        return list(full_data.keys())[:-1]
+    else:
+        return full_data[property]
+
+
+# Convert a csv into a json file with metadata
+def write_json_file(file_name, file_path=None, description=None, references=None, source=None, license=None):
+    dataset = pd.read_csv(file_path + '/' + file_name + '.csv').dropna().to_dict()
+
+    json_file = {
+        "description": description,
+        "references": references,
+        "source": source,
+        "license": license,
+        "dataset": dataset
+    }
+
+    if file_path is not None:
+        with open(file_path + '/' + file_name + ".json", "w") as outfile:
+            json.dump(json_file, outfile)
+    else:
+        with open(os.getcwd() + '/' + file_name + ".json", "w") as outfile:
+            json.dump(json_file, outfile)
+
