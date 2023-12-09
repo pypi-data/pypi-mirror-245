e08229c871638eddc7888dca3e5fdbc25e833999
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 15:04:11 +0000

    Add package twist-academic/0.1.0/twist_academic-0.1.0-py3-none-any.whl

---
+++ packages/twist-academic/twist_academic-0.1.0-py3-none-any.whl/twist_academic/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 3.11.0
+# -*-coding:utf-8 -*-
+# @Author  : Shuang (Twist) Song
+# @Contact   : SongshGeo@gmail.com
+# GitHub   : https://github.com/SongshGeo
+# Website: https://cv.songshgeo.com/
+
+from .notify import notify_me_finished
+
+__all__ = ["notify_me_finished"]
