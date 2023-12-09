09a6b1775508ebe44ee061e9f31101c51182aced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:03:55 +0000

    Add package sintt2/2.0.0/sintt2-2.0.0.tar.gz

---
+++ packages/sintt2/sintt2-2.0.0.tar.gz/sintt2-2.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='sintt2',
+    version='2.0.0',
+    description='makes life easier',
+    author='SINTT',
+    author_email='bbhh66606@gmail.com',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
