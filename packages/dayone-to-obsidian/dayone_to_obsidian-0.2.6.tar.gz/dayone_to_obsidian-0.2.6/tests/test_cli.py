5b36c34d8a9f4c6babed806823716213f96ecfd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:05:28 +0000

    Add package dayone-to-obsidian/0.2.6/dayone_to_obsidian-0.2.6.tar.gz

---
+++ packages/dayone-to-obsidian/dayone_to_obsidian-0.2.6.tar.gz/dayone_to_obsidian-0.2.6/tests/test_cli.py
@@ -0,0 +1,11 @@
+from click.testing import CliRunner
+
+from dayone_to_obsidian import cli
+from dayone_to_obsidian.version import __version__
+
+
+def test_version():
+    runner = CliRunner()
+    result = runner.invoke(cli.main, ["version"])
+    assert result.exit_code == 0, result.output
+    assert result.output == __version__ + "\n"
