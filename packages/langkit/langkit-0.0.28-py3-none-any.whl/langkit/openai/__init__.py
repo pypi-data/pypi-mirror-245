5135b45ae7242a18a9598273c559559feea5c30e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:03:56 +0000

    Add package langkit/0.0.28/langkit-0.0.28-py3-none-any.whl

---
+++ packages/langkit/langkit-0.0.28-py3-none-any.whl/langkit/openai/__init__.py
@@ -0,0 +1,21 @@
+from .openai import (
+    ChatLog,
+    send_prompt,
+    Conversation,
+    LLMInvocationParams,
+    OpenAIAzure,
+    OpenAIDavinci,
+    OpenAIGPT4,
+    OpenAIDefault,
+)
+
+__ALL__ = [
+    ChatLog,
+    send_prompt,
+    Conversation,
+    LLMInvocationParams,
+    OpenAIAzure,
+    OpenAIDavinci,
+    OpenAIDefault,
+    OpenAIGPT4,
+]
