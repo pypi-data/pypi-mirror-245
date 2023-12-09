02cf029a649f7cdcdca9da4180ab89bf59291864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:04:40 +0000

    Add package gigawork/1.0.1/gigawork-1.0.1-py3-none-any.whl

---
+++ packages/gigawork/gigawork-1.0.1-py3-none-any.whl/gigawork/extractors.py
@@ -0,0 +1,239 @@
+"""
+Module for every extractors defined in the project.
+"""
+
+from abc import ABC, abstractmethod
+from collections import namedtuple
+import hashlib
+import logging
+from os import PathLike
+import os
+import sys
+from typing import List, NamedTuple
+import git
+
+from .change_types import ChangeTypes
+
+logger = logging.getLogger(__name__)
+
+
+class Entry(NamedTuple):
+    """A single entry in the dataset."""
+
+    commit_hash: str
+    author_name: str
+    author_email: str
+    committer_name: str
+    committer_email: str
+    committed_date: str
+    authored_date: str
+    file_path: str
+    file_hash: str
+    change_type: str
+
+
+class RenameEntry(NamedTuple):
+    """A single entry in the rename dataset."""
+
+    commit: str
+    old_name: str
+    new_name: str
+
+
+RepositoryEntry = namedtuple("RepositoryEntry", ("repository",) + Entry._fields)
+RepositoryRenameEntry = namedtuple(
+    "RepositoryRenameEntry", ("repository",) + RenameEntry._fields
+)
+
+
+class Extractor(ABC):
+    """An extractor is used to extract information from a repository."""
+
+    def __init__(self, repository: git.Repo) -> None:
+        self.repository = repository
+
+    @abstractmethod
+    def extract(self, *args, **kwargs) -> List[Entry]:
+        """Extract the information from the repository.
+
+        Returns:
+            List[Entry]: The list of entries extracted from the repository.
+        """
+
+
+class FilesExtractor(Extractor):
+    """Extract files and Entry from a repository."""
+
+    def __init__(
+        self, repository: git.Repo, directory: PathLike, save_directory: PathLike
+    ) -> None:
+        super().__init__(repository)
+        self.save_directory = save_directory
+        self.directory = directory
+        if self.save_directory != "":
+            os.makedirs(self.save_directory, exist_ok=True)
+
+    def extract(
+        self, ref="HEAD", after=None, *args, **kwargs
+    ) -> tuple[List[Entry], List[RenameEntry]]:
+        entries = []
+        rename_entries = []
+        if after is not None:
+            ref = f"{after}..{ref}"
+        # iter parents until `after` if given
+        for commit in self.repository.iter_commits(
+            ref, self.directory, **{"first-parent": True}
+        ):
+            self._extract_files(commit, entries, rename_entries)
+        return entries, rename_entries
+
+    def _extract_files(
+        self,
+        commit: git.Commit,
+        entries: List[Entry],
+        rename_entries: List[RenameEntry],
+    ):
+        """Extract the workflows from the given commit.
+
+        Args:
+            commit (git.Commit): The commit to extract the workflows from.
+            previous (Union[None, git.Commit]): The previous commit to compare to.
+            If None, compare to the first commit of the repository.
+            directory (PathLike): The directory to consider.
+            entries (List[Entry]): The list of entries representing the dataset.
+            rename_entries (List[RenameEntry]): The list of rename entries representing the
+            renaming entries of the dataset.
+        """
+        # we compare the parent commit to the current commit
+        # The order of the diff is important, because we want to know
+        # if a file was added or deleted (it might be reversed if we are not cautious)
+        # If there is no parent, we check for diff since the beginning
+        # of the repository (we are at a initial commit)
+        # we only compare with the first parent
+        # as we iter_commits with first-parent=True
+        parent = commit.parents[0] if len(commit.parents) > 0 else None
+        diffs = parent.diff(commit) if parent else commit.diff(git.NULL_TREE)
+        for diff in diffs:
+            if not diff.a_path.startswith(
+                self.directory
+            ) and not diff.b_path.startswith(self.directory):
+                continue  # a commit might contains diffs for files we do not care about
+            try:
+                self._process_diff(diff, entries, rename_entries, commit, parent)
+            except ValueError:
+                logger.error("Could not process diff %s (commit=%s)", str(diff), commit)
+                sys.exit(1)
+
+    def _get_blob_parameters(
+        self, diff: git.Diff, change_type: ChangeTypes, commit
+    ) -> List[tuple]:
+        """Returns the parameters to process a blob.
+
+        Args:
+            diff (git.Diff): The diff to process.
+            change_type (ChangeTypes): The type of change.
+            commit (_type_): The commit to which the diff belongs.
+
+        Returns:
+            List[tuple]: The parameters to process a blob.
+        """
+        if change_type == ChangeTypes.RENAMED:
+            return (
+                *self._get_blob_parameters(diff, ChangeTypes.DELETED, commit),
+                *self._get_blob_parameters(diff, ChangeTypes.ADDED, commit),
+            )
+        if change_type == ChangeTypes.DELETED:
+            blob, path = diff.a_blob, diff.a_path
+        else:
+            blob, path = diff.b_blob, diff.b_path
+        if len(commit.parents) == 0:
+            change_type = ChangeTypes.ADDED
+        return ((blob, commit, path, change_type),)
+
+    def _process_diff(
+        self,
+        diff: git.Diff,
+        entries: List[Entry],
+        rename_entries: List[RenameEntry],
+        commit: git.Commit,
+        parent: git.Commit = None,
+    ):
+        """Process a diff to extract the files and data.
+
+        Args:
+            diff (git.Diff): The diff to process.
+            entries (List[Entry]): The list of entries representing the dataset.
+            rename_entries (List[RenameEntry]): The list of rename entries representing the
+            renaming entries of the dataset.
+            commit (git.Commit): The commit to which the diff belongs.
+            parent (git.Commit, optional): The parent of the commit (if None, will try
+            to get it automatically). Defaults to None.
+        """
+        if parent is None:
+            parent = commit.parents[0] if len(commit.parents) > 0 else None
+        try:
+            change_type = ChangeTypes(diff.change_type)
+        except ValueError:
+            logger.debug(
+                "Could not process diff %s (commit=%s, change_type=%s)",
+                str(diff),
+                commit,
+                diff.change_type,
+            )
+            return  # we do not care about this diff
+
+        if change_type == ChangeTypes.RENAMED:
+            rename_entry = RenameEntry(commit, diff.a_path, diff.b_path)
+            rename_entries.append(rename_entry)
+        for params in self._get_blob_parameters(diff, change_type, commit):
+            entries.append(self._process_blob(*params))
+
+    def _process_blob(
+        self,
+        blob: git.Blob,
+        commit: git.Commit,
+        workflow_path: PathLike,
+        change_type: ChangeTypes,
+    ) -> Entry:
+        """Process a blob to extract the workflow content.
+
+        Args:
+            blob (git.Blob): The blob to process.
+
+        Returns:
+            str: The hash of the workflow content. (Its name in the save_directory)
+        """
+        if blob is None:
+            raise ValueError("Blob cannot be None")
+        data = blob.data_stream.read()
+        _hash = hashlib.sha256(data).hexdigest()
+        path = os.path.join(self.save_directory, _hash)
+        if not os.path.exists(path):
+            # if it exists, we already have the workflow
+            # (well, we might have a collision, but it is unlikely)
+            with open(path, "wb") as file:
+                file.write(data)
+        entry = Entry(
+            commit.hexsha,
+            commit.author.name,
+            commit.author.email,
+            commit.committer.name,
+            commit.committer.email,
+            commit.committed_date,
+            commit.authored_date,
+            workflow_path,
+            _hash,
+            change_type.value,
+        )
+        return entry
+
+
+class WorkflowsExtractor(FilesExtractor):
+    """Extract workflows and related Entry from a repository."""
+
+    WORKFLOWS_DIRECTORY = ".github/workflows"
+
+    def __init__(self, repository: git.Repo, save_directory: PathLike) -> None:
+        FilesExtractor.__init__(
+            self, repository, self.WORKFLOWS_DIRECTORY, save_directory
+        )
---
+++ packages/gigawork/gigawork-1.0.1-py3-none-any.whl/gigawork/main.py
@@ -0,0 +1,160 @@
+import os
+import subprocess
+import sys
+import logging
+import tempfile
+import click
+import git
+from .extractors import WorkflowsExtractor
+from .repository import clone_repository, read_repository, update_repository
+from . import utils
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@click.command()
+@click.option(
+    "--ref",
+    "--branch",
+    "-r",
+    default="HEAD",
+    help="The most recent commit reference (i.e., commit SHA or TAG) to be considered for the extraction",
+    type=str,
+)
+@click.option(
+    "--save-repository",
+    "-s",
+    help="Save the repository to the given directory in case `REPOSITORY` was distant.",
+    type=click.Path(exists=False, file_okay=False, dir_okay=True, writable=True),
+)
+@click.option(
+    "--update", "-u", help="Fetch the repository at the given path.", is_flag=True
+)
+@click.option(
+    "--after",
+    "-a",
+    help="Only consider commits after the given commit reference (i.e., commit SHA or TAG).",
+    type=str,
+)
+@click.option(
+    "--workflows",
+    "-w",
+    help="The directory where the extracted GitHub Actions workflow files will be stored.",
+    default="workflows",
+    type=click.Path(exists=False, file_okay=False, dir_okay=True, writable=True),
+)
+@click.option(
+    "--output",
+    "-o",
+    help="The output CSV file where information related to the dataset will be stored. "
+    "By default, the information will written to the standard output.",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True),
+)
+@click.option(
+    "--rename-output",
+    "-ro",
+    help="The output CSV file where information related to the renaming of workflows will be stored. "
+    "By default, this informations will not be stored.",
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, writable=True),
+)
+@click.option(
+    "--repository-name",
+    "-n",
+    help="Add a column `repository` to the output file where each value will be equal to the provided parameter.",
+    type=str,
+)
+@click.option(
+    "--headers",
+    "-h",
+    help="Create a header row for the CSV output file.",
+    is_flag=True,
+)
+@click.argument(
+    "repository",
+    type=str,
+)
+def main(
+    ref,
+    save_repository,
+    update,
+    after,
+    workflows,
+    output,
+    rename_output,
+    repository_name,
+    headers,
+    repository,
+):
+    """Extract the GitHub Actions workflows from a single Git repo.
+    The Git repository can be local or distant. In the latter case, it will be pulled
+    locally and deleted unless specified otherwise.
+
+    Example of usage:
+    gigawork myRepository -n myRepositoryName -s directory -o output.csv --headers
+    """
+    tmp_directory = None  # the temporary directory if one is created
+    repo = None  # the repository
+
+    # clone the repository if it does not exist
+    try:
+        if not os.path.exists(repository):
+            if not save_repository:
+                tmp_directory = tempfile.TemporaryDirectory(dir=".")
+                save_repository = tmp_directory.name
+            repo = clone_repository(repository, save_repository)
+        else:
+            repo = read_repository(repository)
+    except (git.exc.GitCommandError, ValueError) as exception:
+        logger.error("Could not read repository at '%s'", repository)
+        logger.debug(exception)
+        sys.exit(1)
+
+    # update it if requested
+    if update is True:
+        try:
+            update_repository(repo)
+        except git.exc.GitCommandError as exception:
+            logger.error(
+                "Could not update repository at '%s'. Keeping the current version...",
+                repository,
+            )
+            logger.debug(exception)
+
+    extractor = WorkflowsExtractor(repo, workflows)
+    entries, rename_entries = extractor.extract(ref, after)
+
+    if len(entries) > 0:
+        if output:
+            parent = os.path.dirname(output)
+            if parent != "":
+                os.makedirs(parent, exist_ok=True)
+            with open(output, "a", encoding="utf-8") as file:
+                utils.write_csv(
+                    entries, file, entries[0].__class__, headers, repository_name
+                )
+        else:
+            utils.write_csv(
+                entries, sys.stdout, entries[0].__class__, headers, repository_name
+            )
+
+    if rename_output and len(rename_entries) > 0:
+        parent = os.path.dirname(rename_output)
+        if parent != "":
+            os.makedirs(parent, exist_ok=True)
+        with open(rename_output, "a", encoding="utf-8") as file:
+            utils.write_csv(
+                rename_entries,
+                file,
+                rename_entries[0].__class__,
+                headers,
+                repository_name,
+            )
+
+    # cleanup
+    if tmp_directory:
+        tmp_directory.cleanup()
+
+
+if __name__ == "__main__":
+    main()
