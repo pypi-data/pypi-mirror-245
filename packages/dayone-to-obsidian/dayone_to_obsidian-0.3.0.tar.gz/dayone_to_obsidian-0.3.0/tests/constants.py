f5d419746ff71cfc8084166dc6b02e0804c1b0ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:30 +0000

    Add package dayone-to-obsidian/0.3.0/dayone_to_obsidian-0.3.0.tar.gz

---
+++ packages/dayone-to-obsidian/dayone_to_obsidian-0.3.0.tar.gz/dayone_to_obsidian-0.3.0/tests/constants.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+
+PROJECT_DIR = Path(__file__).parent.parent.resolve()
+TESTS_DIR = PROJECT_DIR / "tests"
+ASSETS_DIR = TESTS_DIR / "assets"
+JOURNAL_JSON_PATH = ASSETS_DIR / "MyJournal.json"
