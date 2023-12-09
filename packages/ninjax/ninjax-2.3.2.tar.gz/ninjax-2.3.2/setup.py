155c35275e4864f37f1d6d0726e395e22181f8e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:04:02 +0000

    Add package ninjax/2.3.2/ninjax-2.3.2.tar.gz

---
+++ packages/ninjax/ninjax-2.3.2.tar.gz/ninjax-2.3.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+import pathlib
+
+import ninjax
+
+
+setuptools.setup(
+    name='ninjax',
+    version=ninjax.__version__,
+    description='Flexible Modules for JAX',
+    url='http://github.com/danijar/ninjax',
+    long_description=pathlib.Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
+    packages=['ninjax'],
+    install_requires=['jax'],
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
