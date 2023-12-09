669b5c4bcf8e84031b3f66432ecfb0c4784bdcea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:03:59 +0000

    Add package adios2/0.0.2/adios2-0.0.2.tar.gz

---
+++ packages/adios2/adios2-0.0.2.tar.gz/adios2-0.0.2/README.md
@@ -0,0 +1,4 @@
+# adios2
+adios2 - The Adaptable Input Output System version 2
+
+Coming soon: ADIOS2 automatically packaged and shipped from the [source](https://github.com/ornladios/adios2).
---
+++ packages/adios2/adios2-0.0.2.tar.gz/adios2-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='adios2',
+    version="0.0.2",
+    description='adios2 - The Adaptable Input Output System version 2',
+    long_description="adios2",
+    long_description_content_type='text/markdown',
+)
