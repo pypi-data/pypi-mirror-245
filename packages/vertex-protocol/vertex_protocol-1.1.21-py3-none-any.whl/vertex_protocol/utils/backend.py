253fb679d855cc5a30e28e07628ef2e8404c2bf0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:04:02 +0000

    Add package vertex-protocol/1.1.21/vertex_protocol-1.1.21-py3-none-any.whl

---
+++ packages/vertex-protocol/vertex_protocol-1.1.21-py3-none-any.whl/vertex_protocol/utils/backend.py
@@ -0,0 +1,10 @@
+from vertex_protocol.utils.enum import StrEnum
+
+
+class VertexBackendURL(StrEnum):
+    """Enum representing different Vertex backend URLs."""
+
+    SEPOLIA_TESTNET = "https://api.sepolia-test.vertexprotocol.com"
+    MAINNET = "https://api.prod.vertexprotocol.com"
+    DEVNET_ENGINE = "http://localhost:80"
+    DEVNET_INDEXER = "http://localhost:8000"
