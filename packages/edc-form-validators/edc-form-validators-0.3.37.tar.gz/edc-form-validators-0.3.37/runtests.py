0c3942996264818a91f305f0f68aba98afd97b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:03:37 +0000

    Add package edc-form-validators/0.3.37/edc-form-validators-0.3.37.tar.gz

---
+++ packages/edc-form-validators/edc-form-validators-0.3.37.tar.gz/edc-form-validators-0.3.37/runtests.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import logging
+import os
+import sys
+from os.path import abspath, dirname
+
+import django
+from django.conf import settings
+from django.test.runner import DiscoverRunner
+from edc_test_utils import DefaultTestSettings
+
+app_name = "edc_form_validators"
+base_dir = dirname(abspath(__file__))
+
+DEFAULT_SETTINGS = DefaultTestSettings(
+    calling_file=__file__,
+    BASE_DIR=base_dir,
+    APP_NAME=app_name,
+    ETC_DIR=os.path.join(base_dir, app_name, "tests", "etc"),
+    INSTALLED_APPS=[
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "django.contrib.messages",
+        "django.contrib.sessions",
+        "django.contrib.sites",
+        "django.contrib.staticfiles",
+        # "django_crypto_fields.apps.AppConfig",
+        # "django_revision.apps.AppConfig",
+        # "multisite",
+        # "edc_dashboard.apps.AppConfig",
+        "edc_form_validators.apps.AppConfig",
+        "form_validators_app.apps.AppConfig",
+    ],
+).settings
+
+
+def main():
+    if not settings.configured:
+        settings.configure(**DEFAULT_SETTINGS)
+    django.setup()
+    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
+    failfast = True if [t for t in sys.argv if t == "--failfast"] else False
+    failures = DiscoverRunner(failfast=failfast, tags=tags).run_tests([f"{app_name}.tests"])
+    sys.exit(bool(failures))
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    main()
