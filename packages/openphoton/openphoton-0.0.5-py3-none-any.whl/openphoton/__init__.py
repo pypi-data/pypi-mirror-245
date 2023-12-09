7dd3f6784b6ec2b5d5a65767e3f493c65aeb9b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:04:42 +0000

    Add package openphoton/0.0.5/openphoton-0.0.5-py3-none-any.whl

---
+++ packages/openphoton/openphoton-0.0.5-py3-none-any.whl/openphoton/__init__.py
@@ -0,0 +1,6 @@
+from rayleigh_sommerfeld import fresnel_approx, fraunhoffer_approx
+from aperture_functions import rect, circ, jinc
+from devices import laser_beam, SLM_amplitude
+from lenses import converging_lens, diverging_lens
+# for SLM
+from PIL import Image
