c13f7a25a981bd7b4319e8222aaace6303278c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:05:51 +0000

    Add package geospacelab/0.7.11/geospacelab-0.7.11.tar.gz

---
+++ packages/geospacelab/geospacelab-0.7.11.tar.gz/geospacelab-0.7.11/geospacelab/__init__.py
@@ -0,0 +1,18 @@
+# Licensed under the BSD 3-Clause License   
+# Copyright (C) 2021 GeospaceLab (geospacelab)
+# Author: Lei Cai, Space Physics and Astronomy, University of Oulu
+
+__author__ = "Lei Cai"
+__copyright__ = "Copyright 2021, GeospaceLAB"
+__credits__ = ["Lei Cai"]
+__license__ = "BSD-3-Clause License"
+__version__ = "0.7.11"
+__maintainer__ = "Lei Cai"
+__email__ = "lei.cai@oulu.fi"
+__status__ = "Developing"
+__docformat__ = "reStructureText"
+
+
+import geospacelab.config as config
+
+preferences = config.Preferences()
