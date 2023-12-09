d7b3aa0d051f0d4e562f7e7b1c02b092b8641836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 15:04:53 +0000

    Add package gamecreator-auth-database/1.0.1/gamecreator_auth_database-1.0.1-py3-none-any.whl

---
+++ packages/gamecreator-auth-database/gamecreator_auth_database-1.0.1-py3-none-any.whl/gamecreator_auth_database/auth_secret_key.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class AuthSecretKey(models.Model):
+    "密钥管理"
+
+    id = models.AutoField(primary_key=True)
+    "唯一标识符"
+
+    type = models.CharField(max_length=255)
+    "类型"
+
+    secret_key = models.CharField(max_length=255)
+    "密钥"
+
+    class Meta:
+        db_table = "auth_secret_key"
