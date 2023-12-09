0bcc2929df5b4457d79a448a318b136deb8aa66d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:03:56 +0000

    Add package pyrusquant/0.1/pyrusquant-0.1.tar.gz

---
+++ packages/pyrusquant/pyrusquant-0.1.tar.gz/pyrusquant-0.1/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+import pyrusquant
+
+
+description_short = """
+Pyrusquant is a package for interaction with alternative data, trading API of different exchanges and brokers'    
+"""
+
+with open('README.md') as file:
+    description = file.read()
+
+with open('README.md') as file:
+    text_license = file.read()
+
+with open('requirements.txt') as file:
+    requirements = file.read().splitlines()
+
+
+setup(
+    name='pyrusquant',
+    version=pyrusquant.__version__,
+    description=description_short,
+    long_description=description,
+    long_description_content_type='text/markdown',
+    author='Arbuzov Vyacheslav, Petukhov Maksim',
+    author_email='Vyacheslav Arbuzov <noreply@github.com>',
+    url='https://github.com/arbuzovv/pyrusquant',
+    packages=find_packages(),
+    license=text_license,
+    install_requires=requirements,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires=">=3.7",
+)
