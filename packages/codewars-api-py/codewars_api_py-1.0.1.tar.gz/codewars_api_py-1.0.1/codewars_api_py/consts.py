df2c918b1218bcc2cea63f5c3efc5bb43a8bec76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:05:36 +0000

    Add package codewars-api-py/1.0.1/codewars_api_py-1.0.1.tar.gz

---
+++ packages/codewars-api-py/codewars_api_py-1.0.1.tar.gz/codewars_api_py-1.0.1/codewars_api_py/consts.py
@@ -0,0 +1,24 @@
+"""Constants for the project."""
+from pathlib import Path
+
+from platformdirs import user_config_dir, user_log_dir
+
+from . import PACKAGE, __desc__, __version__
+
+NAME = PACKAGE  # Path(__file__).name.split(".")[0]
+CONFIG_PATH = user_config_dir(appname=NAME, ensure_exists=True)
+CONFIG_FILE = Path(CONFIG_PATH).resolve() / f"{NAME}.ini"
+LOG_PATH = user_log_dir(appname=NAME, ensure_exists=True)
+LOG_FILE = Path(LOG_PATH).resolve() / f"{NAME}.log"
+VERSION = __version__
+DESC = __desc__
+
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
+
+DEBUG = False
+PROFILE = False
+
+def update_debug(value):
+    global DEBUG
+    DEBUG = value
