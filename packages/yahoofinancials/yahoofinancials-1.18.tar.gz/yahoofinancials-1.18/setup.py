f945116ab71258c54a4b3d885f4d68fada96966d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 15:04:50 +0000

    Add package yahoofinancials/1.18/yahoofinancials-1.18.tar.gz

---
+++ packages/yahoofinancials/yahoofinancials-1.18.tar.gz/yahoofinancials-1.18/setup.py
@@ -0,0 +1,42 @@
+import codecs
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with codecs.open('README.rst', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='yahoofinancials',
+    version='1.18',
+    description='A powerful financial data module used for pulling both fundamental and technical data from Yahoo Finance',
+    long_description=long_description,
+    url='https://github.com/JECSand/yahoofinancials',
+    download_url='https://github.com/JECSand/yahoofinancials/archive/1.18.tar.gz',
+    author='Connor Sanders',
+    author_email='connor@exceleri.com',
+    license='MIT',
+    keywords=['finance data', 'stocks', 'commodities', 'cryptocurrencies', 'currencies', 'forex', 'yahoo finance'],
+    packages=['yahoofinancials'],
+    install_requires=[
+        "pytz",
+        "requests>=2.26",
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Topic :: Office/Business :: Financial :: Investment',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    zip_safe=False
+)
