2ef261f3e1a1e01d43a6430c317769ab73effdc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:04:53 +0000

    Add package openphoton/0.0.7/openphoton-0.0.7.tar.gz

---
+++ packages/openphoton/openphoton-0.0.7.tar.gz/openphoton-0.0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.7'
+DESCRIPTION = 'A scalar diffraction simulation package'
+LONG_DESCRIPTION = 'A basic optics package for scalar diffraction simulation.'
+
+# Setting up
+setup(
+    name="openphoton",
+    version=VERSION,
+    author="Gilbert M. Oca",
+    author_email="<boyfriendnibluefairy@gmail.com>",
+    license="MIT",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'Pillow'],
+    keywords=['python', 'optics', 'diffraction', 'simulation', 'lens', 'SLM', 'light', 'wave', 'propagation'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
