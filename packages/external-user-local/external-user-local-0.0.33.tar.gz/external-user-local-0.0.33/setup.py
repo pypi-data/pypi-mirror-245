7e52a11b460a9b421a37d50345d380ae5538c06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:20 +0000

    Add package external-user-local/0.0.33/external-user-local-0.0.33.tar.gz

---
+++ packages/external-user-local/external-user-local-0.0.33.tar.gz/external-user-local-0.0.33/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+PACKAGE_NAME = "external-user-local"
+package_dir = PACKAGE_NAME.replace("-", "_")
+
+with open('README.md') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name=PACKAGE_NAME,  # https://pypi.org/project/external-user-local
+    version='0.0.33',
+    author="Circles",
+    author_email="info@circles.life",
+    url=f"https://github.com/circles-zone/{PACKAGE_NAME}-python-package",
+    packages=[package_dir],
+    package_dir={package_dir: f'{package_dir}/src'},
+    package_data={package_dir: ['*.py']},
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'logger-local>= 0.0.60',
+        'database-without-orm-local>=0.0.83'
+    ],
+)
