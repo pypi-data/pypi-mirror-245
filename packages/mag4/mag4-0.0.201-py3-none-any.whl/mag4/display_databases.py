f417309e3cfdafaadfe0a9b05ffb1dd24f0b8930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:03:59 +0000

    Add package mag4/0.0.201/mag4-0.0.201-py3-none-any.whl

---
+++ packages/mag4/mag4-0.0.201-py3-none-any.whl/mag4/display_databases.py
@@ -0,0 +1,8 @@
+# requirements
+import pandas as pd
+
+base_url = 'https://raw.githubusercontent.com/Hezel2000/cosmogeochemdata/master/'
+
+# Display the main database content
+def display_databases():
+    return pd.read_csv(base_url + 'GCCdata.csv')
