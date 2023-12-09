002848d8f267d0f145863e54dae6710679a0bbbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 15:04:44 +0000

    Add package natspy-lock/0.1.0/natspy_lock-0.1.0.tar.gz

---
+++ packages/natspy-lock/natspy_lock-0.1.0.tar.gz/natspy_lock-0.1.0/README.md
@@ -0,0 +1,40 @@
+# natspy-lock
+
+[![PyPI - Version](https://img.shields.io/pypi/v/natspy-lock.svg)](https://pypi.org/project/natspy-lock)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/natspy-lock.svg)](https://pypi.org/project/natspy-lock)
+
+-----
+
+`natspy-lock` is a distributed lock library using nats.
+
+**Table of Contents**
+
+- [Installation](#installation)
+- [Example](#example)
+- [License](#license)
+
+
+## Installation
+
+```console
+pip install natspy-lock
+```
+
+## Example
+
+```python
+import nats
+from natspy_lock import NatsLock
+
+async def main():
+    nc = await nats.connect("nats://127.0.0.1:4222")
+    await NatsLock.init(nc.jetstream(), "test_lock", 60)
+    async with NatsLock.get_lock("test_lock", 1):
+    #     do something
+        pass
+    await nc.drain()
+```
+
+## License
+
+`natspy-lock` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/natspy-lock/natspy_lock-0.1.0.tar.gz/natspy_lock-0.1.0/tests/test_lock.py
@@ -0,0 +1,43 @@
+import asyncio
+import multiprocessing
+
+import nats
+
+from natspy_lock import NatsLock
+
+shared_variable = multiprocessing.Value("i", 0)
+
+
+async def should_be_locked():
+    nc = await nats.connect("nats://127.0.0.1:4222")
+    await NatsLock.init(nc.jetstream(), "test_lcok", 60)
+
+    async def inner():
+        async with NatsLock.get_lock("test_lock11111", 10) as nl:
+            if nl.locked():
+                shared_variable.value += 1
+
+    await asyncio.gather(*[inner() for _ in range(250)])
+    await nc.drain()
+
+
+def run_async():
+    asyncio.run(should_be_locked())
+
+
+async def test_nats_lock():
+    p1 = multiprocessing.Process(target=run_async)
+    p2 = multiprocessing.Process(target=run_async)
+    p3 = multiprocessing.Process(target=run_async)
+    p4 = multiprocessing.Process(target=run_async)
+    p1.start()
+    p2.start()
+    p3.start()
+    p4.start()
+
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+
+    assert shared_variable.value == 1000, "Lock is not working"
