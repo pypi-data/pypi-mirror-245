fd6a77f717c8f58926f55fa512ce53448bc70dad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:03:33 +0000

    Add package anguilla-iml/0.1.0b4/anguilla_iml-0.1.0b4.tar.gz

---
+++ packages/anguilla-iml/anguilla_iml-0.1.0b4.tar.gz/anguilla_iml-0.1.0b4/src/anguilla/app/server.py
@@ -0,0 +1,172 @@
+"""
+Authors:
+  Victor Shepardson
+  Intelligent Instruments Lab 2023
+"""
+
+import anguilla
+from anguilla import IML
+from iipyper import OSC, run
+from iipyper.types import *
+import numpy as np
+from time import time
+from collections import defaultdict
+from typing import Optional
+
+def get_handle(address):
+    return ''.join(address.split('/')[3:]).strip('/')
+
+def main(
+    osc_port:int=8732,
+    osc_return_port:Optional[int]=None,
+    osc_host:str='',
+    ):
+    """
+    Args:
+        osc_port: listen for OSC controls on this port
+        osc_return_port: if supplied, reply on a different port than osc_port
+        osc_host: leave this as empty string to get all traffic on the port
+
+    OSC Routes:
+
+        /anguilla/config "emb" "Identity"
+            set embedding to Identity (the default)
+        /anguilla/config "emb" "ProjectAndSort"
+            set embedding to ProjectAndSort
+
+        /anguilla/config "interp" "Smooth"
+            set interpolator to Smooth (the default)
+        /anguilla/config "interp" "Softmax"
+            set interpolator to Softmax
+        /anguilla/config "interp" "Ripple"
+            set interpolator to Ripple
+
+        /anguilla/add "input" ... "output"... 
+            add a point to the mapping
+
+        /anguilla/remove id 
+            remove a point from the mapping by ID
+
+        /anguilla/remove_near "input" ... ["k" k]
+            remove k points from the mapping by proximity
+
+        /anguilla/map "input" ... ["k" k] ["ripple" r] ["temp" t]
+            map an input to an output using k neighbors
+            "temp" 1 > t > 0 when using Softmax interpolator
+            "ripple" r > 0 when using Ripple interpolator
+
+        /anguilla/reset
+            remove all points
+        /anguilla/reset "keep_near" ... ["k" k]
+            remove all points except the k neighbors of "keep_near"
+
+        /anguilla/load path
+            load IML from file at `path`
+        /anguilla/save path
+            save IML to file at `path`
+
+        an additional segment in a route is the name of an IML instance which
+        it will target, e.g. /anguilla/add/myinstance
+    """
+    osc = OSC(osc_host, osc_port)
+
+    instances = {}
+    configs = defaultdict(dict)
+
+    @osc.handle('/anguilla/config*')
+    def _(address, **kw):
+        k = get_handle(address)
+
+        print(k)
+        # TODO: validate input
+        configs[k].update(kw)
+        print(configs[k]) 
+
+    @osc.handle('/anguilla/add*')
+    def _(address, input:Splat[None], output:Splat[None], id:int=None):
+        key = get_handle(address)
+
+        # d = len(input)
+        # config['feature_size'] = d
+        if key not in instances:
+            # print(f'new IML object with Input dimension {d}')
+            print(f'new IML object with handle "{key}" with config {configs[key]}')
+            instances[key] = IML(**configs[key])
+
+        return '/return'+address, instances[key].add(input, output, id=id)
+    
+    @osc.handle('/anguilla/remove*')
+    def _(address, id:int):
+        key = get_handle(address)
+        if key in instances:
+            instances[key].remove(id)
+        else:
+            print(f'ERROR: anguilla: {address}: no instance "{key}" exists')
+
+    @osc.handle('/anguilla/remove_near*')
+    def _(address, input:Splat[None], k:int=None):
+        key = get_handle(address)
+        if key not in instances:
+            print(f'ERROR: anguilla: {address}: no instance "{key}" exists')
+            return
+        
+        instances[key].remove_near(input, k=k)
+
+    @osc.handle('/anguilla/map*', return_port=osc_return_port)
+    def _(address, input:Splat[None], k:int=None, **kw):
+        key = get_handle(address)
+        if key not in instances:
+            print(f'ERROR: anguilla: {address}: no instance "{key}" exists')
+            print(f'ERROR: anguilla: call {address.replace("map", "add")} at least once before {address}')
+            return
+
+        # print(f'{kw=}')
+        result = instances[key].map(input, k=k, **kw).tolist()
+
+        return '/return'+address, *result
+    
+    @osc.handle('/anguilla/reset*')
+    def _(address, keep_near:Splat[None]=None, k:int=None):
+        key = get_handle(address)
+        if key not in instances:
+            print(f'ERROR: anguilla: {address}: no instance "{key}" exists')
+            return
+    
+        instances[key].reset(keep_near, k=k)
+
+    @osc.handle('/anguilla/load*')
+    def _(address, path:str):
+        k = get_handle(address)
+        
+        assert path.endswith('.json'), \
+            "ERROR: anguilla: path should end with .json"
+        
+        if k=='':
+            print(f'loading all IML objects from {path}')
+            d = anguilla.serialize.load(path)
+            assert isinstance(d, dict)
+            print(f'found IML instances: {list(d.keys())}')
+            instances.update(d)
+        else:
+            print(f'load IML object at "{k}" from {path}')
+            instances[k] = IML.load(path)
+
+    @osc.handle('/anguilla/save*')
+    def _(address, path:str):
+        k = get_handle(address)
+        if k!='' and k not in instances:
+            print(f'ERROR: anguilla: {address}: no instance "{k}" exists')
+            return
+        
+        assert path.endswith('.json'), \
+            "ERROR: anguilla: path should end with .json"
+        
+        if k=='':
+            print(f'saving all IML objects to {path}')
+            anguilla.serialize.save(path, instances)
+        else:
+            print(f'saving IML object at "{k}" to {path}')
+            instances[k].save(path)
+
+if __name__=='__main__':
+    run(main)
---
+++ packages/anguilla-iml/anguilla_iml-0.1.0b4.tar.gz/anguilla_iml-0.1.0b4/src/anguilla/interpolate.py
@@ -0,0 +1,174 @@
+import numpy as np
+
+from .types import *
+from .serialize import JSONSerializable
+
+class Interpolate(JSONSerializable):
+    """
+    `Interpolate` combines a set of `Outputs` weighted by dissimilarity scores.
+
+    The scores depend on the `Metric` used by the `NNSearch`. 
+    They may be, for example, distances or negative cosine similarities.
+    """
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def __call__(self, targets: List[Output], scores: Scores) -> Output:
+        """
+        Args:
+            targets: [k x <batch dims> x <output dims>]
+                first dimension is neighbor dimension
+                trailing dimensions are feature dimensions
+                remaining dimensions are batch dimensions
+            scores: [k]
+
+        Returns:
+            output: [<batch dims> x <output dims>]
+        """
+        raise NotImplementedError
+
+class Nearest(Interpolate):
+    """return nearest neighbor (voronoi cell mapping)"""
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, targets, scores):
+        idx = np.argmin(scores, 0)
+        if idx.ndim > 1:
+            return [targets[i,j] for j,i in enumerate(idx)]
+        else:
+            return targets[idx]
+
+class Mean(Interpolate):
+    """mean of neighbors (piecewise constant mapping)"""
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, targets, scores):
+        return sum(targets) / len(targets)
+
+class Softmax(Interpolate):
+    """
+    Like `Mean`, but weighted toward the nearer neighbors.
+    
+    when `k` is small, has discontinuities
+    when temp is large, acts more like `Mean`.
+    -> tends to get 'washed out' for larger `k` / larger temp
+
+    when temp is small, acts more like `Nearest` (voronoi cells).
+    """
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, targets:List[Output], scores:List[float], temp:float=0.5):
+        """
+        Args:
+            targets: size [K x ...batch dims... x ...output_dims...] 
+            scores: size [K x ...batch dims...] 
+            temp: temperature of softmax
+        """
+        targets, scores = np_coerce(targets, scores)
+        # print(targets.shape, scores.shape)
+
+        if temp==0:
+            result = Nearest()(targets, scores)
+        else:
+            centered = scores - np.mean(scores, 0) # for numerical precision
+            logits = np.maximum(-centered/temp, -20)
+            # print(f'{logits=}')
+            if np.max(np.abs(logits)) > 80:
+                # NOTE: not batched properly
+                result = Nearest()(targets, scores)
+            else:
+                weights = np.exp(logits)
+                # print(f'{weights=}')
+                weights /= weights.sum(0)
+                # print(f'{weights=}')
+                # result = (np.moveaxis(targets,0,-1)*weights).sum(-1)
+                result = np.transpose((
+                    np.transpose(targets)*np.transpose(weights)).sum(-1))
+        # print(f'{result=}')
+        return result
+
+class Smooth(Interpolate):
+    """
+    Interpolate which is non-discontinuous (for `k` > 2)
+    
+    tries to prevent discontinuities while preserving the input-output mapping
+    exactly where close to data points.
+
+    works well with larger `k`.
+    out-of-domain input areas tend to be averages of many outputs.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, targets:List[Output], scores:List[float], eps:float=1e-9):
+        """
+        Args:
+            targets: size [K x ...batch dims... x ...output_dims...] 
+            scores: size [K x ...batch dims...] 
+            eps: small value preventing division by zero
+        """
+        targets, scores = np_coerce(targets, scores)
+
+        scores = scores + eps
+        assert np.min(scores) > 0
+
+        # largest scores -> 0 weight
+        # zero score -> inf weight
+        # zero first/second derivative at largest score
+        mx = np.max(scores, 0)
+        weights = 1/scores + (-3*mx*mx + 3*mx*scores - scores*scores)/(mx**3)
+
+        weights = weights + eps
+        weights = weights / weights.sum(0)
+
+        # result = (np.moveaxis(targets,0,-1)*weights).sum(-1)
+        result = np.transpose((
+            np.transpose(targets)*np.transpose(weights)).sum(-1))
+
+        return result
+    
+class Ripple(Interpolate):
+    """
+    like `Smooth` but with high-frequency ripples outside the input domain.
+    
+    useful for making random mappings in high dimensional spaces / bootstrapping expressive mappings from a few points.
+    """
+    def __init__(self):
+        super().__init__()
+
+    def __call__(self, 
+            targets:List[Output], scores:List[float], 
+            ripple:float=1, ripple_depth:float=1, eps:float=1e-9):
+        """
+        Args:
+            targets: size [K x ...output_dims...] list or ndarray
+            scores: size [K] list or ndarray
+            ripple: frequency of ripples
+            ripple_depth: amplitude of ripples
+            eps: small value preventing division by zero
+        """
+        targets, scores = np_coerce(targets, scores)
+
+        scores = scores + eps
+        assert np.min(scores) > 0
+
+        # largest scores -> 0 weight
+        # zero score -> inf weight
+        mx = np.max(scores, 0)
+        weights = 1/scores + (-3*mx*mx + 3*mx*scores - scores*scores)/(mx**3)
+        weights = weights * 2**(
+            ripple_depth * 
+            (1+np.cos(np.pi*scores/mx)*np.sin(scores*np.pi*ripple))
+            )
+
+        weights = weights + eps
+        weights = weights / weights.sum(0)
+
+        # result = (np.moveaxis(targets,0,-1)*weights).sum(-1)
+        result = np.transpose((
+            np.transpose(targets)*np.transpose(weights)).sum(-1))
+
+        return result
