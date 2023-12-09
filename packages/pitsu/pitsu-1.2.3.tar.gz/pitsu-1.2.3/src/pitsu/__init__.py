a2a846a4a19e701f452525274c712e0b18830ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:03:49 +0000

    Add package pitsu/1.2.3/pitsu-1.2.3.tar.gz

---
+++ packages/pitsu/pitsu-1.2.3.tar.gz/pitsu-1.2.3/src/pitsu/__init__.py
@@ -0,0 +1,8 @@
+class ElementError(Exception):
+    """Error by Element class"""
+class ClassError(Exception):
+    """Error by Class_List class"""
+
+from .element import *
+
+__version__ = "1.2.2"
