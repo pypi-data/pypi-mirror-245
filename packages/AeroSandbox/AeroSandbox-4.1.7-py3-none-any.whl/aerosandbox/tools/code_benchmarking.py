a5bd60570a7c23c5717ed585b5c7f2029dfde320
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:46 +0000

    Add package AeroSandbox/4.1.7/AeroSandbox-4.1.7-py3-none-any.whl

---
+++ packages/AeroSandbox/AeroSandbox-4.1.7-py3-none-any.whl/aerosandbox/tools/code_benchmarking.py
@@ -0,0 +1,78 @@
+import time
+from typing import Tuple, Any, Callable
+import aerosandbox.numpy as np
+
+
+def time_function(
+        func: Callable,
+        repeats: int = None,
+        desired_runtime: float = None,
+        runtime_reduction = np.min,
+) -> Tuple[float, Any]:
+    """
+    Runs a given callable and tells you how long it took to run, in seconds. Also returns the result of the function
+        (if any), for good measure.
+
+    Args:
+
+        func: The function to run. Should take no arguments; use a lambda function or functools.partial if you need
+            to pass arguments.
+
+        repeats: The number of times to run the function. If None, runs until desired_runtime is met.
+
+        desired_runtime: The desired runtime of the function, in seconds. If None, runs until repeats is met.
+
+        runtime_reduction: A function that takes in a list of runtimes and returns a reduced value. For example,
+            np.min will return the minimum runtime, np.mean will return the mean runtime, etc. Default is np.min.
+
+    Returns: A Tuple of (time_taken, result).
+        - time_taken is a float of the time taken to run the function, in seconds.
+        - result is the result of the function, if any.
+
+    """
+    if (repeats is not None) and (desired_runtime is not None):
+        raise ValueError("You can't specify both repeats and desired_runtime!")
+
+    def time_function_once():
+        start_ns = time.perf_counter_ns()
+        result = func()
+        return (
+            (time.perf_counter_ns() - start_ns) / 1e9,
+            result
+        )
+
+    runtimes = []
+
+    t, result = time_function_once()
+
+    if t == 0:
+        t = 1e-2
+    else:
+        runtimes.append(t)
+
+    if (desired_runtime is not None) and (repeats is None):
+        repeats = int(desired_runtime // t) - 1
+        # print(f"Running {func.__name__} {repeats} times to get a desired runtime of {desired_runtime} seconds.")
+
+    if repeats is None:
+        repeats = 0
+
+    for _ in range(repeats):
+        t, _ = time_function_once()
+        if t != 0:
+            runtimes.append(t)
+
+    if len(runtimes) == 0:
+        runtimes = [0.]
+
+    return (
+        runtime_reduction(runtimes),
+        result
+    )
+
+if __name__ == '__main__':
+
+    def f():
+        time.sleep(0.1)
+
+    print(time_function(f, desired_runtime=1))
