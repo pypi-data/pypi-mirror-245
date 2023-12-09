4a4769c964aeee34d6c4904d433368adf3c5ac9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:03:47 +0000

    Add package cybertask/1.9.0/cybertask-1.9.0.tar.gz

---
+++ packages/cybertask/cybertask-1.9.0.tar.gz/cybertask-1.9.0/cybertask/plugins/contexts.py
@@ -0,0 +1,17 @@
+from cybertask import config
+
+"""
+LetMeDoIt AI Plugin - contexts
+
+add pre-defined contexts
+"""
+
+config.predefinedContexts["Let me Summarize"] = """Provide me with a summary of the following content:
+[NO_FUNCTION_CALL]"""
+
+config.predefinedContexts["Let me Explain"] = """Explain the following content to me:
+[NO_FUNCTION_CALL]"""
+
+config.predefinedContexts["Let me Translate"] = """Assist me by acting as a translator.
+Please translate the content below:
+[NO_FUNCTION_CALL]"""
