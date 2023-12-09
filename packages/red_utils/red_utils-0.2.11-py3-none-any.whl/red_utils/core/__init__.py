00f889cc4e6ecd34cbc680af3f35dc46b87b3771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:52 +0000

    Add package red_utils/0.2.11/red_utils-0.2.11-py3-none-any.whl

---
+++ packages/red_utils/red_utils-0.2.11-py3-none-any.whl/red_utils/core/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from . import dataclass_utils, constants
+
+from .constants import (
+    DATA_DIR,
+    CACHE_DIR,
+    SERIALIZE_DIR,
+    JSON_DIR,
+    ENSURE_EXIST_DIRS,
+    LOG_DIR,
+)
