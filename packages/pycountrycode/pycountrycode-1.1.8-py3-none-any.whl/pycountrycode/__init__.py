d9f35adc21ff0c19a35adf533ca34367cb60cdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:43 +0000

    Add package pycountrycode/1.1.8/pycountrycode-1.1.8-py3-none-any.whl

---
+++ packages/pycountrycode/pycountrycode-1.1.8-py3-none-any.whl/pycountrycode/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Andile Jaden Mbele'
+__email__ = 'andilembele020@gmail.com'
+__github__ = 'www.github.com/xeroxzen/country-code'
+__package__ = 'pycountrycode'
