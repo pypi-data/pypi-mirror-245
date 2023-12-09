72a89bdda21446984867c5c2a5df333cba4ca2c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:31 +0000

    Add package parti-pytorch/0.2.0/parti-pytorch-0.2.0.tar.gz

---
+++ packages/parti-pytorch/parti-pytorch-0.2.0.tar.gz/parti-pytorch-0.2.0/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+exec(open('parti_pytorch/version.py').read())
+
+setup(
+  name = 'parti-pytorch',
+  packages = find_packages(exclude=[]),
+  version = __version__,
+  license='MIT',
+  description = 'Parti - Pathways Autoregressive Text-to-Image Model - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/parti-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'transformers',
+    'attention mechanism',
+    'text-to-image'
+  ],
+  install_requires=[
+    'einops>=0.7',
+    'einops-exts',
+    'ema-pytorch',
+    'torch>=1.6',
+    'torchvision',
+    'transformers',
+    'vector-quantize-pytorch>=1.11.8'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
