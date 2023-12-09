632c7035b2a495ef75327a9d43e7ba5e2a18accd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:42 +0000

    Add package autopub/1.0.0a8/autopub-1.0.0a8-py3-none-any.whl

---
+++ packages/autopub/autopub-1.0.0a8-py3-none-any.whl/autopub/plugins/git.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+from typing import Any
+
+from autopub.plugins import AutopubPlugin
+
+
+class GitPlugin(AutopubPlugin):
+    def post_publish(self, repository: str | None = None, **kwargs: Any) -> None:
+        self.run_command(["git", "config", "--global", "user.email", "autopub@autopub"])
+        self.run_command(["git", "config", "--global", "user.name", "autopub"])
+
+        self.run_command(["git", "add", "."])
+
+        self.run_command(["git", "commit", "-m", "🤖 autopub publish"])
+
+        self.run_command(["git", "push"])
