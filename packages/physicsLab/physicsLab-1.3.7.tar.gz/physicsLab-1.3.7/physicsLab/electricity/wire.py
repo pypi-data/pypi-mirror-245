e599647b5b96c0289cb0ed2a2ddc11e53c5c1f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:32 +0000

    Add package physicsLab/1.3.7/physicsLab-1.3.7.tar.gz

---
+++ packages/physicsLab/physicsLab-1.3.7.tar.gz/physicsLab-1.3.7/physicsLab/electricity/wire.py
@@ -0,0 +1,77 @@
+#coding=utf-8
+import physicsLab.errors as errors
+import physicsLab._fileGlobals as _fileGlobals
+
+# 电学元件引脚类
+class element_Pin:
+    __slots__ = ("element_self", "pinLabel")
+    def __init__(self, input_self, pinLabel: int) -> None:
+        self.element_self = input_self
+        self.pinLabel: int = pinLabel
+
+    # 重载减法运算符作为连接导线的语法
+    def __sub__(self, obj: "element_Pin") -> "element_Pin":
+        crt_Wire(self, obj)
+        return obj
+
+# 检查函数参数是否是导线
+def _check_typeWire(func):
+    def result(SourcePin: element_Pin, TargetPin: element_Pin, color: str = '蓝') -> None:
+        if (
+                isinstance(SourcePin, element_Pin) and
+                isinstance(TargetPin, element_Pin)
+        ):
+            # 将英文的color转换为中文
+            if color in ("black", "blue", "red", "green", "yellow"):
+                color = {"black": "黑", "blue": "蓝", "red": "红", "green": "绿", "yellow": "黄"}[color]
+
+            if (color not in ("黑", "蓝", "红", "绿", "黄")):
+                raise errors.wireColorError
+
+            _fileGlobals.check_ExperimentType(_fileGlobals.experimentType.Circuit)
+
+            func(SourcePin, TargetPin, color)
+
+    return result
+
+# 新版连接导线
+@_check_typeWire
+def crt_Wire(SourcePin: element_Pin, TargetPin: element_Pin, color: str = '蓝') -> None:
+    _fileGlobals.Wires.append({"Source": SourcePin.element_self._arguments["Identifier"], "SourcePin": SourcePin.pinLabel,
+                   "Target": TargetPin.element_self._arguments["Identifier"], "TargetPin": TargetPin.pinLabel,
+                   "ColorName": f"{color}色导线"})
+
+# 删除导线
+@_check_typeWire
+def del_Wire(SourcePin: element_Pin, TargetPin: element_Pin, color: str = '蓝') -> None:
+    a_wire = {
+        "Source": SourcePin.element_self._arguments["Identifier"], "SourcePin": SourcePin.pinLabel,
+        "Target": TargetPin.element_self._arguments["Identifier"], "TargetPin": TargetPin.pinLabel,
+        "ColorName": f"{color}色导线"
+    }
+    if a_wire in _fileGlobals.Wires:
+        _fileGlobals.Wires.remove(a_wire)
+    else:
+        a_wire = {
+            "Source": TargetPin.element_self._arguments["Identifier"], "SourcePin": TargetPin.pinLabel,
+            "Target": SourcePin.element_self._arguments["Identifier"], "TargetPin": SourcePin.pinLabel,
+            "ColorName": f"{color}色导线"
+        }
+        if a_wire in _fileGlobals.Wires:
+            _fileGlobals.Wires.remove(a_wire)
+        else:
+            raise errors.wireNotFoundError
+
+# 删除所有导线
+def clear_Wires() -> None:
+    _fileGlobals.check_ExperimentType(_fileGlobals.experimentType.Circuit)
+    _fileGlobals.Wires.clear()
+
+# 获取当前导线数
+def count_Wires() -> int:
+    _fileGlobals.check_ExperimentType(_fileGlobals.experimentType.Circuit)
+    return len(_fileGlobals.Wires)
+
+# 打印导线的json
+def print_Wires() -> None:
+    print(_fileGlobals.Wires)
---
+++ packages/physicsLab/physicsLab-1.3.7.tar.gz/physicsLab-1.3.7/physicsLab/errors.py
@@ -0,0 +1,51 @@
+#coding=utf-8
+# 用于存放自定义错误类
+# 由于有时在package外需要异常处理，故不为文件私有变量
+import physicsLab._colorUtils as _colorUtils
+
+def warning(msg: str) -> None:
+    _colorUtils.printf("Warning: " + msg, _colorUtils.COLOR.YELLOW)
+
+# 打开实验异常
+class openExperimentError(Exception):
+    def __str__(self):
+        return "open a experiment but find nothing(must open a experiment)."
+
+# 导线颜色类型异常
+class wireColorError(Exception):
+    def __str__(self):
+        return "illegal wire color."
+
+# 未找到导线异常
+class wireNotFoundError(Exception):
+    def __str__(self):
+        return "Unable to delete a nonexistent wire"
+
+# 用于模块化元件的bitLength参数
+class bitLengthError(Exception):
+    def __str__(self):
+        return "illegal bitLength number"
+
+# 创建实验已存在
+class experimentExistError(Exception):
+    def __str__(self):
+        return 'Duplicate name archives are forbidden'
+
+class crtExperimentFailError(Exception):
+    def __str__(self):
+        return "Failed to create experiment, the experiment already exists"
+
+# 打开的实验与调用的元件不符
+class experimentTypeError(Exception):
+    def __str__(self):
+        return "The type of experiment does not match the element"
+
+# 用于get_Element 获取元件引用失败
+class getElementError(Exception):
+    def __str__(self):
+        return "Index out of range"
+
+# 类实例化异常 基类无法被实例化
+class instantiateError(Exception):
+    def __str__(self):
+        return "This class cannot be instantiated"
