78c7521cb89cc05ca9911e142aac0ff6d93aec86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:04:37 +0000

    Add package aolesrtc/1.0.2/aolesrtc-1.0.2.tar.gz

---
+++ packages/aolesrtc/aolesrtc-1.0.2.tar.gz/aolesrtc-1.0.2/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import Extension, find_packages
+
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+setup(name='aolesrtc',
+      version='1.0.2',
+      description='A easy & simple rtc library supporting janus & p2p',
+      long_description=long_description,
+      author='aoles',
+      author_email='young.aoles@qq.com',
+      url='',
+      install_requires=[],
+      license='MIT License',
+      packages=find_packages(),
+      platforms=["macosx_arm64"],
+      python_requires="==3.10.13",
+      include_package_data=True,
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.10',
+          'Topic :: Software Development :: Libraries',
+          'Operating System :: MacOS'
+      ],
+    )
