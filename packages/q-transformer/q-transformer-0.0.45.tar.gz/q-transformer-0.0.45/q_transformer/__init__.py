9c5bcdb13c230efe4c9185235409e97ec2cf2487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:14 +0000

    Add package q-transformer/0.0.45/q-transformer-0.0.45.tar.gz

---
+++ packages/q-transformer/q-transformer-0.0.45.tar.gz/q-transformer-0.0.45/q_transformer/__init__.py
@@ -0,0 +1,13 @@
+from q_transformer.q_robotic_transformer import (
+    QRoboticTransformer,
+    MaxViT
+)
+
+from q_transformer.q_learner import (
+    QLearner
+)
+
+from q_transformer.agent import (
+    Agent,
+    ReplayMemoryDataset
+)
