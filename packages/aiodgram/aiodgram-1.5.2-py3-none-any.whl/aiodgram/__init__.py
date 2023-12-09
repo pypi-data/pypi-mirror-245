feaaad3804027c9ed269e753b03d455fee6aa34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:04:55 +0000

    Add package aiodgram/1.5.2/aiodgram-1.5.2-py3-none-any.whl

---
+++ packages/aiodgram/aiodgram-1.5.2-py3-none-any.whl/aiodgram/__init__.py
@@ -0,0 +1,13 @@
+from .library import TgBot, types
+from .beautiful import ColorStart, MyMessages
+from .darktube import DownloadVideo
+from .loading import Loading
+from .buttons import Button
+
+
+from .meta_data import __version__, __authors__, __title__
+
+ 
+"""
+:copyright: (c) 2023 Darkangel
+"""
