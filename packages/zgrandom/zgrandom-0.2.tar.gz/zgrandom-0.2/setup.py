6137c110117bcfb2230ff65adba056db21b70bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:47 +0000

    Add package zgrandom/0.2/zgrandom-0.2.tar.gz

---
+++ packages/zgrandom/zgrandom-0.2.tar.gz/zgrandom-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup 
+
+setup(name="zgrandom",
+      version="0.2",
+      description="Alternative Of Random Module",
+      long_description="You Can Use 'zgrandom' For Taking Any Random Input, It Can Be A Good Alternative Of Random Module. Upgrades Will Be Notified ON Telegram. ",
+      author = "Zaz Yagmi",
+      packages = ['zgrandom'],
+      install_requires=[] 
+      
+      )
---
+++ packages/zgrandom/zgrandom-0.2.tar.gz/zgrandom-0.2/zgrandom/__init__.py
@@ -0,0 +1,19 @@
+def random(val):
+    v1 = []
+    for element in val:
+        v1.append(str(element))
+
+    v3 = set(v1)
+    
+    # print(v1)cls
+    
+    res = []
+    for s in v3:
+        res.append(s)
+        
+    return res[0]
+
+
+
+
+
