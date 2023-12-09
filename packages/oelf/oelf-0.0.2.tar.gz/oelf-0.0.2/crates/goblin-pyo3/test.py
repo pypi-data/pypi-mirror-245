4bdbbe30ddbe53f69438b71b387acff984c662c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:04:08 +0000

    Add package oelf/0.0.2/oelf-0.0.2.tar.gz

---
+++ packages/oelf/oelf-0.0.2.tar.gz/oelf-0.0.2/crates/goblin-pyo3/test.py
@@ -0,0 +1,42 @@
+import oelf
+
+g = oelf.Object("test.py")
+g = oelf.Object("mylib.dylib")
+print(g.header)
+print(g.name)
+
+print("symbols")
+globsym = None
+start = -1
+end = 9999999999
+
+for sym in g.symbols():
+    if "META" in sym.name and sym.is_global:
+        if globsym is None:
+            globsym = sym
+            start = sym.meta.n_value
+
+for sym in g.symbols():
+    if sym.meta.n_value > start and sym.meta.n_value < end:
+        print(f"sym after the found one: {sym}")
+        end = sym.meta.n_value
+
+print(f"found symbol {globsym.name} from {start} to {end}, size: {end-start}")
+print(globsym)
+
+print("libs")
+print(g.libs)
+
+print("rpaths")
+print(g.rpaths)
+
+print("exports")
+print(len(g.exports()))
+
+print("imports")
+print(len(g.imports()))
+
+print("sections")
+
+for idx, section in enumerate(g.sections()):
+    print(f"{idx+1}. {section}")
