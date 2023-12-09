655a22a5c71592a89d277417806ab0e430b2935d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:05:47 +0000

    Add package Planodo/0.8/Planodo-0.8.tar.gz

---
+++ packages/Planodo/Planodo-0.8.tar.gz/Planodo-0.8/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name="Planodo",
+    version="0.8",
+    description="Utility library for creating large zoomable images",
+    author="Etienne Posthumus",
+    author_email="ep@epoz.org",
+    url="https://github.com/epoz/planodo",
+    py_modules=["planodo"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
