14ebd6a4db0d5c281524d2bc480d562c124504a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:04:03 +0000

    Add package gradio-imageslider/0.0.15/gradio_imageslider-0.0.15.tar.gz

---
+++ packages/gradio-imageslider/gradio_imageslider-0.0.15.tar.gz/gradio_imageslider-0.0.15/demo/app.py
@@ -0,0 +1,16 @@
+import gradio as gr
+from gradio_imageslider import ImageSlider
+
+
+def fn(im):
+    return im
+
+
+with gr.Blocks() as demo:
+    img1 = ImageSlider()
+    btn = gr.Button("RUN")
+    img2 = ImageSlider()
+    btn.click(fn, inputs=img1, outputs=img2)
+
+if __name__ == "__main__":
+    demo.launch()
