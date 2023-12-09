28ce937150bbe809828692521ebb306ffbf3a87f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:46 +0000

    Add package pygifdec/0.0.2/pygifdec-0.0.2.tar.gz

---
+++ packages/pygifdec/pygifdec-0.0.2.tar.gz/pygifdec-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, Extension
+
+pygifdec = Extension('pygifdec',
+                     sources=['./src/pygifdec.c', './src/gifdec.c'],
+                     include_dirs=['./src'],
+                     )
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setup(name='pygifdec',
+      version='0.0.2',
+      author='alingse',
+      author_email='alingse@foxmail.com',
+      description='a python gifdec wrapper',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      url='https://github.com/alingse/pygifdec',
+      ext_modules=[pygifdec],
+      packages=['src'],
+      package_data={'src': ['gifdec.h']},
+      )
