522dc1caba53b337dde8d35fa8d3e224e7e573b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 16:04:09 +0000

    Add package Daug/2023.12.9.1/Daug-2023.12.9.1.tar.gz

---
+++ packages/Daug/Daug-2023.12.9.1.tar.gz/Daug-2023.12.9.1/daug/cogs/channels.py
@@ -0,0 +1,54 @@
+import io
+import discord
+from time import time
+from discord.ext import commands
+from daug.utils.dpyexcept import excepter
+
+def compose_channel_tree(guild):
+    tree = []
+    for category in guild.by_category():
+        if category[0] is None:
+            tree.append('C#')
+        else:
+            tree.append(f'C# {category[0].name}')
+        for channel in category[1]:
+            if isinstance(channel, discord.channel.TextChannel):
+                tree.append(f'  T# {channel.name}')
+            if isinstance(channel, discord.channel.VoiceChannel):
+                tree.append(f'  V# {channel.name}')
+    return '\n'.join(tree)
+
+class Channels(commands.Cog):
+    """チャンネル情報取得"""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['ch'])
+    @commands.guild_only()
+    @excepter
+    async def channel_count(self, ctx):
+        count = len(ctx.guild.channels)
+        await ctx.channel.send(embed=discord.Embed(description=f'チャンネル数:{count}'))
+
+    @commands.group()
+    @commands.guild_only()
+    @excepter
+    async def channels(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send('{} = Text:{} + Voice:{} + Category:{}'.format(
+                len(ctx.guild.channels),
+                len(ctx.guild.text_channels),
+                len(ctx.guild.voice_channels),
+                len(ctx.guild.categories),
+            ))
+
+    @channels.command()
+    @commands.guild_only()
+    @excepter
+    async def tree(self, ctx):
+        await ctx.send(
+            file=discord.File(
+                io.StringIO(compose_channel_tree(ctx.guild)),
+                f'channels{int(time())}.txt',
+            )
+        )
---
+++ packages/Daug/Daug-2023.12.9.1.tar.gz/Daug-2023.12.9.1/daug/cogs/favorite.py
@@ -0,0 +1,31 @@
+import discord
+from discord.ext import commands
+from daug.utils import compose_embed_from_message
+from daug.utils.dpyexcept import excepter
+
+
+class Favorite(commands.Cog):
+    """お気に入り掲載"""
+    def __init__(self, bot, guild_id, channel_id):
+        self.bot = bot
+        self.guild_id = guild_id
+        self.channel_id = channel_id
+
+    async def dispatch_tips(self, message):
+        channel = self.bot.get_channel(self.channel_id)
+        await channel.send(embed=compose_embed_from_message(message))
+
+    @commands.Cog.listener()
+    @excepter
+    async def on_raw_reaction_add(self, payload):
+        channel = self.bot.get_channel(payload.channel_id)
+        if not isinstance(channel, discord.channel.TextChannel):
+            return
+        author = channel.guild.get_member(payload.user_id)
+        if payload.guild_id != self.guild_id:
+            return
+        if author.bot:
+            return
+        if payload.emoji.name == '⭐':
+            message = await channel.fetch_message(payload.message_id)
+            await self.dispatch_tips(message)
