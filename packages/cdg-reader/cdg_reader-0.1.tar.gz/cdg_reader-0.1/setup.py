8c4513ddcd4d95585421af00621372a02172436c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:04:25 +0000

    Add package cdg-reader/0.1/cdg_reader-0.1.tar.gz

---
+++ packages/cdg-reader/cdg_reader-0.1.tar.gz/cdg_reader-0.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+setup(
+  name = 'cdg_reader',
+  packages = ['cdg_reader'],
+  version = '0.1',
+  license='MIT',
+  description = 'Check the NCAR Climate Data Guide for information relevant to your netCDF data file',
+  author = 'YDylan Grant',
+  author_email = 'dg3311@columbia.edu',
+  url = 'https://github.com/dylangrant01/cdg-reader',
+  download_url = 'https://github.com/dylangrant01/cdg-reader/archive/refs/tags/v_01.tar.gz',
+  keywords = ['ncar', 'cdg', 'climate', 'guide', 'xarray'],
+  install_requires=[            
+          'xarray',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Science/Research',
+    'Topic :: Scientific/Engineering :: Atmospheric Science',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
