744b98ac48014c7caaa64d3bc887d794fe839a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 03:04:41 +0000

    Add package evilblade/1.1.3.0/evilblade-1.1.3.0.tar.gz

---
+++ packages/evilblade/evilblade-1.1.3.0.tar.gz/evilblade-1.1.3.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+des = "\n-----Offense without defense, \n\tunparalleled in the world.-----\n\nWelcome to the universe of N1nEmAn.\n  To find detailed usage instructions for evilblade,\n     please visit cnblogs.com/9man."
+
+setup(
+    name='evilblade',
+    version='1.1.3.0',
+    description='7 N1nEmAn\'s evilblade!!my blog:cnblogs.com/9man',
+    packages=['evilblade'],
+    long_description=des,
+)
