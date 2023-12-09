2aeb1bf6da9a560072ed627b9f8722b1435bab87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 14:03:56 +0000

    Add package chai-guanaco/1.4.2/chai_guanaco-1.4.2.tar.gz

---
+++ packages/chai-guanaco/chai_guanaco-1.4.2.tar.gz/chai_guanaco-1.4.2/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Chai Research Corp.',
+    author_email='hello@chai-research.com',
+    description='Chaiverse',
+    install_requires=['chaiverse'],
+    long_description='Use `chaiverse <https://pypi.python.org/pypi/chaiverse/>`_ instead.',
+    maintainer='UNKNOWN',
+    maintainer_email='UNKNOWN',
+    name='chai_guanaco',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/chaiverse/',
+    version='1.4.2',
+    zip_safe=False,
+)
