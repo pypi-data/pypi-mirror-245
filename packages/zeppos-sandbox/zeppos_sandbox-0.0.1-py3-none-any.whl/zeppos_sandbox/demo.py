ef184ca135258121ed2fddc0a37c6a011bb08dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:04:26 +0000

    Add package zeppos-sandbox/0.0.1/zeppos_sandbox-0.0.1-py3-none-any.whl

---
+++ packages/zeppos-sandbox/zeppos_sandbox-0.0.1-py3-none-any.whl/zeppos_sandbox/demo.py
@@ -0,0 +1,21 @@
+"""Demo module class to template purpose."""
+
+
+class Demo:
+    """Demo class as an example of you to implement zeppos pypi packages."""
+
+    def hello_world(self) -> str:
+        """Say hello world.
+
+        Returns:
+            Returns the words to say hello to the world.
+        """
+        return "Hello world."
+
+    def status(self) -> str:
+        """Give the status of the system.
+
+        Returns:
+            The status of the system `good` or `bad`
+        """
+        return "good"
