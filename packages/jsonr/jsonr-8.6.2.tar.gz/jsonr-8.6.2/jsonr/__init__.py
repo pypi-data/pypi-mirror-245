f60b217245d4da1e5bd389d68313c329ed2adb24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 15:03:59 +0000

    Add package jsonr/8.6.2/jsonr-8.6.2.tar.gz

---
+++ packages/jsonr/jsonr-8.6.2.tar.gz/jsonr-8.6.2/jsonr/__init__.py
@@ -0,0 +1,181 @@
+'''
+Highly customizable JSON objects and dataframes, and your python toolkit all in one.
+
+__all__:          tuple = (
+                        'New',
+                        'Frame',
+                        'load',
+                        'dump',
+                        'py_checker',
+                        'as_bytes',
+                        'as_unicode',
+                        'is_method',
+                        'is_function',
+                        'is_code',
+                        'get_argspec',
+                        'Bond',
+                        'Infinity',
+                        'NegativeInfinity',
+                        'PYTHON_MINIMUM_MAJOR',
+                        'PYTHON_MINIMUM_MINOR',
+                        'PYTHON_MINIMUM_MICRO',
+                        'JSON',
+                        'JSONArray',
+                        'JSONObject',
+                        'JSONValue',
+                        'Flowa',
+                        'FlowaArray',
+                        'FlowaObject',
+                        'FlowaValue',
+                        'Image',
+                        'ImageStr',
+                        'ImageObject',
+                        'ImageUrl',
+                        'Text',
+                        'TextArray',
+                        'TextObject',
+                        'TextValue',
+                        'Any',
+                        'Callable',
+                        'Dict',
+                        'List',
+                        'Optional',
+                        'Tuple',
+                        'Type',
+                        'Union',
+                        'cast',
+                        'overload',
+                        'String',
+                        'StringBuffer',
+)
+'''
+
+import json
+import pickle
+
+from . import core
+from .core import New, Frame
+
+from . import libs
+from .libs import flowa, pollinations
+
+from . import utils
+from .utils import (
+    py_checker,
+    as_bytes,
+    as_unicode,
+    is_method,
+    is_function,
+    is_code,
+    get_argspec,
+    Bond,
+    Infinity,
+    NegativeInfinity,
+)
+
+from . import typing
+from .typing import (
+    Any,
+    Callable,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Type,
+    Union,
+    cast,
+    overload,
+    String,
+    StringBuffer,
+    JSONArray,
+    JSONObject,
+    JSONValue,
+    Flowa,
+    FlowaArray,
+    FlowaObject,
+    FlowaValue,
+    Image,
+    ImageStr,
+    ImageObject,
+    ImageUrl,
+    Text,
+    TextArray,
+    TextObject,
+    TextValue
+)
+
+JSON: New = New
+
+PYTHON_MINIMUM_MAJOR: int = 3
+PYTHON_MINIMUM_MINOR: int = 7
+PYTHON_MINIMUM_MICRO: int = 0
+
+load:             object = pickle.load
+dump:             object = pickle.dump
+
+py_checker(PYTHON_MINIMUM_MAJOR, PYTHON_MINIMUM_MINOR, PYTHON_MINIMUM_MICRO)
+
+__all__:          tuple = (
+                        'New',
+                        'Frame',
+                        'load',
+                        'dump',
+                        'py_checker',
+                        'as_bytes',
+                        'as_unicode',
+                        'is_method',
+                        'is_function',
+                        'is_code',
+                        'get_argspec',
+                        'Infinity',
+                        'NegativeInfinity',
+                        'PYTHON_MINIMUM_MAJOR',
+                        'PYTHON_MINIMUM_MINOR',
+                        'PYTHON_MINIMUM_MICRO',
+                        'JSON',
+                        'JSONArray',
+                        'JSONObject',
+                        'JSONValue',
+                        'Flowa',
+                        'FlowaArray',
+                        'FlowaObject',
+                        'FlowaValue',
+                        'Image',
+                        'ImageStr',
+                        'ImageObject',
+                        'ImageUrl',
+                        'Text',
+                        'TextArray',
+                        'TextObject',
+                        'TextValue',
+                        'Any',
+                        'Callable',
+                        'Dict',
+                        'List',
+                        'Optional',
+                        'Tuple',
+                        'Type',
+                        'Union',
+                        'cast',
+                        'overload',
+                        'String',
+                        'StringBuffer',
+)
+
+__version__:      str = '8.6.2'
+__description__:  str = 'Highly customizable JSON objects and dataframes, and your python toolkit all in one.'
+__desc__:         str = 'Highly customizable JSON objects and dataframes, and your python toolkit all in one.'
+__author__:       str = 'jsonr'
+__author_email__: str = 'jsonr.py@gmail.com'
+__license__:      str = 'MIT'
+__copyright__:    str = 'Copyright (c) 2023 jsonr'
+__credits__:      str = 'jsonr'
+__maintainer__:   str = 'jsonr'
+__email__:        str = 'jsonr.py@gmail.com'
+__status__:       str = 'Development Status :: 5 - Production/Stable'
+__topic__:        str = 'Topic :: Software Development :: Build Tools'
+__license_type__: str = 'License :: OSI Approved :: MIT License'
+__url__:          str = 'https://github.com/jsonr-py/jsonr'
+__github__:       str = 'https://github.com/jsonr-py'
+__repo__ :        str = 'https://github.com/jsonr-py/jsonr'
+__keywords__:     str = 'jsonr json jsonr-py jsonr-python, python'
---
+++ packages/jsonr/jsonr-8.6.2.tar.gz/jsonr-8.6.2/setup.py
@@ -0,0 +1,38 @@
+'''
+setup jsonr > setup.py
+'''
+
+from setuptools import setup, find_packages
+import pathlib
+
+path_absolute: pathlib.Path = pathlib.Path(__file__).parent.absolute()
+
+setup(
+    name='jsonr',
+    version='8.6.2',
+    description='Highly customizable JSON objects and dataframes, and your python toolkit all in one.',
+    long_description=pathlib.Path(f"{path_absolute}/README.md").read_text(encoding="utf-8"),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jsonr-py/jsonr',
+    author='jsonr',
+    author_email='jsonr.py@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='jsonr json jsonr-py jsonr-python, python',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'requests',
+        'pathlib',
+        'pandas',
+        'google-generativeai'
+    ]
+)
