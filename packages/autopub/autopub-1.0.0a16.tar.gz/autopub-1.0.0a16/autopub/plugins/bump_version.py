15be38a7b0c1ba2b4ea15ffe8832cb427c16f097
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:04:20 +0000

    Add package autopub/1.0.0a16/autopub-1.0.0a16.tar.gz

---
+++ packages/autopub/autopub-1.0.0a16.tar.gz/autopub-1.0.0a16/autopub/plugins/bump_version.py
@@ -0,0 +1,46 @@
+from __future__ import annotations
+
+import pathlib
+
+import tomlkit
+from dunamai import Version
+
+from autopub.plugins import AutopubBumpVersionPlugin, AutopubPlugin
+from autopub.types import ReleaseInfo
+
+
+class BumpVersionPlugin(AutopubBumpVersionPlugin, AutopubPlugin):
+    current_version: str
+    new_version: str
+
+    @property
+    def pyproject_config(self) -> tomlkit.TOMLDocument:
+        content = pathlib.Path("pyproject.toml").read_text()
+
+        return tomlkit.parse(content)
+
+    def _get_version(self, config: tomlkit.TOMLDocument) -> str:
+        try:
+            return config["tool"]["poetry"]["version"]  # type: ignore
+        except KeyError:
+            return config["project"]["version"]  # type: ignore
+
+    def _update_version(self, config: tomlkit.TOMLDocument, new_version: str) -> None:
+        try:
+            config["tool"]["poetry"]["version"] = new_version  # type: ignore
+        except KeyError:
+            config["project"]["version"] = new_version  # type: ignore
+
+    def post_check(self, release_info: ReleaseInfo) -> None:
+        config = self.pyproject_config
+
+        bump_type = {"major": 0, "minor": 1, "patch": 2}[release_info.release_type]
+
+        version = Version(self._get_version(config))
+
+        self.previous_version = str(version)
+        self.new_version = version.bump(bump_type).serialize()
+
+        self._update_version(config, self.new_version)
+
+        pathlib.Path("pyproject.toml").write_text(tomlkit.dumps(config))  # type: ignore
