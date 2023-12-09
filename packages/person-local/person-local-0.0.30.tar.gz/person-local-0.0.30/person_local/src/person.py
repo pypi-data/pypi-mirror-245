f94e6f0748b05e0114722a1670637345f2a9f4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:26 +0000

    Add package person-local/0.0.30/person-local-0.0.30.tar.gz

---
+++ packages/person-local/person-local-0.0.30.tar.gz/person-local-0.0.30/person_local/src/person.py
@@ -0,0 +1,19 @@
+class Person:
+    "person details class"
+
+    def __init__(self, number: int, last_coordinate: (any, any),
+                 birthday_date: str = None, day: int = None,
+                 month: int = None, year: int = None, first_name: str = None,
+                 last_name: str = None, location_id: int = None,
+                 nickname: str = None, gender_id: int = None) -> None:
+        self.number = number
+        self.gender_id = gender_id
+        self.last_coordinate = last_coordinate
+        self.location_id = location_id
+        self.birthday_date = birthday_date
+        self.day = day
+        self.month = month
+        self.year = year
+        self.first_name = first_name
+        self.last_name = last_name
+        self.nickname = nickname
