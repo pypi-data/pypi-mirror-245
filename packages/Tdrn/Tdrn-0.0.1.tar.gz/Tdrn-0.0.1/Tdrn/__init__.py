0544bb64938192ed97b712d659c610cd663128cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 15:04:37 +0000

    Add package Tdrn/0.0.1/Tdrn-0.0.1.tar.gz

---
+++ packages/Tdrn/Tdrn-0.0.1.tar.gz/Tdrn-0.0.1/Tdrn/__init__.py
@@ -0,0 +1,6 @@
+from .tdrn import Tdrn_
+from .creat import Creat
+
+__all__=['Tdrn_','Creat']
+
+
---
+++ packages/Tdrn/Tdrn-0.0.1.tar.gz/Tdrn-0.0.1/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name = "Tdrn",                                                                                                                              
+    version = "0.0.1",
+    author = "Cheng",
+    license='MIT license',
+    author_email="kimichen2907@gmail.com",
+    description="Make a to-do record notebook",
+    url="https://github.com/ditisrmel/Tdrn", 
+    python_requires='>=3.11.4'
+)
