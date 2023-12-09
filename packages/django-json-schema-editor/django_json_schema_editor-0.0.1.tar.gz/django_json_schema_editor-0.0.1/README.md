834d7d17084ea9f1e7d3c1b8d24b64158c38dcb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 05:04:33 +0000

    Add package django-json-schema-editor/0.0.1/django_json_schema_editor-0.0.1.tar.gz

---
+++ packages/django-json-schema-editor/django_json_schema_editor-0.0.1.tar.gz/django_json_schema_editor-0.0.1/README.md
@@ -0,0 +1,5 @@
+# django-django_json_schema_editor
+
+Widget for using
+[@json-editor/json-editor](https://www.npmjs.com/package/@json-editor/json-editor)
+with Django.
---
+++ packages/django-json-schema-editor/django_json_schema_editor-0.0.1.tar.gz/django_json_schema_editor-0.0.1/django_json_schema_editor/static/django_json_schema_editor/widget.js
@@ -0,0 +1,41 @@
+/* global django, JSONEditor */
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelectorAll(
+      ".inline-related:not(.empty-form) .django_json_schema_editor",
+    )
+    .forEach((el) => {
+      DjangoJSONEditorWidget.initWidget(el)
+    })
+})
+
+document.addEventListener("DOMContentLoaded", () => {
+  django.jQuery(document).on("formset:added", (event) => {
+    event.target
+      .querySelectorAll(".django_json_schema_editor")
+      .forEach((el) => DjangoJSONEditorWidget.initWidget(el))
+  })
+})
+
+const DjangoJSONEditorWidget = {
+  initWidget: (el) => {
+    DjangoJSONEditorWidget.initEditor(el)
+  },
+  initEditor: (el) => {
+    const container = el.querySelector(".editor")
+    const input = el.querySelector("textarea")
+    const config = JSON.parse(el.dataset["editorConfig"])
+    const editor = new JSONEditor(container, config)
+
+    editor.on("ready", () => {
+      const data = input.value ? JSON.parse(input.value) : null
+      if (data) {
+        editor.setValue(data)
+      }
+    })
+
+    editor.on("change", () => {
+      input.value = JSON.stringify(editor.getValue())
+    })
+  },
+}
