636a6ea69be74db4f416ccbb4c09d05129ed6b37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 15:03:59 +0000

    Add package decora-bleak/0.2.2/decora_bleak-0.2.2.tar.gz

---
+++ packages/decora-bleak/decora_bleak-0.2.2.tar.gz/decora_bleak-0.2.2/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# -*- coding: utf-8 -*-
+# Copyright (c) 2023 Colin Campbell
+# MIT license
+# This code was inspired by https://github.com/mjg59/python-decora and https://github.com/lucapinello/pydecora_ble/tree/master
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="decora_bleak",
+        version="0.2.2"
+    )
