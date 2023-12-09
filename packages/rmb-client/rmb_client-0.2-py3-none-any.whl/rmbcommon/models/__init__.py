e8c24f9cdf08d5470729c6615b300bda6f482dc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:31 +0000

    Add package rmb-client/0.2/rmb_client-0.2-py3-none-any.whl

---
+++ packages/rmb-client/rmb_client-0.2-py3-none-any.whl/rmbcommon/models/__init__.py
@@ -0,0 +1,15 @@
+from rmbcommon.models.datasource import DataSourceCore
+from rmbcommon.models.meta import MetaData, DataField, DataTable, DataSchema
+from rmbcommon.models.chat import ChatCore, MessageCore, RunCore
+from rmbcommon.models.bi import StrucQuery, QueryResult
+
+__all__ = [
+    'MetaData',
+    'DataField',
+    'DataTable',
+    'DataSchema',
+    'DataSourceCore',
+    'ChatCore',
+    'MessageCore',
+    'RunCore'
+]
