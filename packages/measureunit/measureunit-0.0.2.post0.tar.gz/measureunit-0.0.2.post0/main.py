92c571da885f852f9645d0f3ce0e7967a87447e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-09 04:04:37 +0000

    Add package measureunit/0.0.2.post0/measureunit-0.0.2.post0.tar.gz

---
+++ packages/measureunit/measureunit-0.0.2.post0.tar.gz/measureunit-0.0.2.post0/main.py
@@ -0,0 +1,20 @@
+from measureunit.units import Length, WeightMass, Temperature
+import measureunit as msu
+import logging
+
+# print(msu.__version__)
+
+try:
+    length = Length.km2m(2)
+    logging.info(f'The result of the length value: {length}')
+    
+    mass = WeightMass.kg2g(4)
+    logging.info(f'The result of the mass value: {mass}')
+    
+    temp = Temperature.C2F('')
+    logging.info(f'The result of temp value: {temp}')
+    
+except (ValueError, TypeError) as e:
+    logging.exception(f"An error occurred due to {e}")
+except Exception as e:
+    logging.exception(f"An unexpected error occurred due to {e}. Please try again!")
---
+++ packages/measureunit/measureunit-0.0.2.post0.tar.gz/measureunit-0.0.2.post0/setup.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+from setuptools import find_packages, setup
+
+
+def get_version():
+    versionpy = (Path('measureunit') / 'version.py').read_text()
+    locals_dict = {}
+    exec(versionpy, globals(), locals_dict)
+    return locals_dict['__version__']
+
+VERSION = get_version()
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='measureunit',
+    version=VERSION,
+    description='A Python library of the measurement conversion',
+    packages=find_packages(exclude=(['test*'])),
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='v.d.anh',
+    author_email='vdanhvt2000@gmail.com',
+    url='https://github.com/v-danh/measureunit',
+    license='MIT',
+    keywords=['measureunit', 'conversion'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    extras_require={
+        'dev': ['pytest>=7.0', 'twine>=4.0.2'],
+    },
+    python_requires='>=3',
+)
