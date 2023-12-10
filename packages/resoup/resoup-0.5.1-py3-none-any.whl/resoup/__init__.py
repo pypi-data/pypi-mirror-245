62994f953a00782d9f9351ad8be1b2acb936a25e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:37 +0000

    Add package resoup/0.5.1/resoup-0.5.1-py3-none-any.whl

---
+++ packages/resoup/resoup-0.5.1-py3-none-any.whl/resoup/__init__.py
@@ -0,0 +1,28 @@
+"""
+██████╗░███████╗░██████╗░█████╗░██╗░░░██╗██████╗░
+██╔══██╗██╔════╝██╔════╝██╔══██╗██║░░░██║██╔══██╗
+██████╔╝█████╗░░╚█████╗░██║░░██║██║░░░██║██████╔╝
+██╔══██╗██╔══╝░░░╚═══██╗██║░░██║██║░░░██║██╔═══╝░
+██║░░██║███████╗██████╔╝╚█████╔╝╚██████╔╝██║░░░░░
+╚═╝░░╚═╝╚══════╝╚═════╝░░╚════╝░░╚═════╝░╚═╝░░░░░
+
+REquests + beautifulSOUP
+
+Various convenient features related to requests.
+"""
+
+__title__ = "resoup"
+__description__ = 'Various convenient features related to requests and BeautifulSoup.'
+__url__ = "https://github.com/ilotoki0804/resoup"
+__raw_source_url__ = "https://raw.githubusercontent.com/ilotoki0804/resoup/master"
+__version_info__ = (0, 5, 1)
+__version__ = str.join('.', map(str, __version_info__))
+__author__ = "ilotoki0804"
+__author_email__ = "ilotoki0804@gmail.com"
+__license__ = "MIT License"
+
+from . import requests_ as requests
+from .custom_defaults import CustomDefaults
+from .header_utils import clean_headers
+from .sessions_with_tools import Session
+from .souptools import SoupTools
