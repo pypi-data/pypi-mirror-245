8bc79ff1e463a1f5a0fde1146ec68a5c3a529aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:04:56 +0000

    Add package python-access-modifiers/1.0.0/python_access_modifiers-1.0.0-py3-none-any.whl

---
+++ packages/python-access-modifiers/python_access_modifiers-1.0.0-py3-none-any.whl/python_access_modifiers/__init__.py
@@ -0,0 +1,11 @@
+"""
+:copyright: (c) 2023-present mxngo
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = "python-access-modifiers"
+__author__ = "mxngo"
+__copyright__ = ":copyright: (c) 2023-present mxngo"
+__license__ = "MIT"
+
+from .private_modifier import private
