7127f10aafe76fe273d449afb64ff7e8433dfd13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:33 +0000

    Add package ibkrtwsapi-modules/0.0.1/ibkrtwsapi_modules-0.0.1.tar.gz

---
+++ packages/ibkrtwsapi-modules/ibkrtwsapi_modules-0.0.1.tar.gz/ibkrtwsapi_modules-0.0.1/TWSIBAPI_MODULES/Contracts.py
@@ -0,0 +1,13 @@
+from ibapi.contract import Contract
+
+
+# Make functions for Stock, Options etc
+
+
+def stock(symbol: str, currency: str = "USD", exchange: str = "SMART") -> Contract:
+    contract = Contract()
+    contract.symbol = symbol
+    contract.secType = "STK"
+    contract.currency = currency
+    contract.exchange = exchange
+    return contract
