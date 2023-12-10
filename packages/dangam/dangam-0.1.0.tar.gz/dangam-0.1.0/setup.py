170babbe8dd9ea4f370c78ec01db2772f381933f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 05:04:33 +0000

    Add package dangam/0.1.0/dangam-0.1.0.tar.gz

---
+++ packages/dangam/dangam-0.1.0.tar.gz/dangam-0.1.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dangam',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jason Heesang Lee',
+    author_email='jason.heesang.lee96@gmail.com',
+    url='https://github.com/jasonheesanglee/dangam',
+    install_requires=[
+        'torch>=1.10.0',
+        'numpy>=1.21.0',
+        'tqdm>=4.62.0',
+        'transformers>=4.10.0',
+        'scipy>=1.7.0',
+        'pandas>=1.3.0',
+        'regex>=2021.8.0'
+    ],
+    classifiers=[
+        # Choose the development status of your package
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: Science/Research',
+
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+
+        'Programming Language :: Python :: 3.10',
+
+        'Operating System :: OS Independent',  # Use specific OS classifiers if your package is OS-dependent
+
+    ]
+)
