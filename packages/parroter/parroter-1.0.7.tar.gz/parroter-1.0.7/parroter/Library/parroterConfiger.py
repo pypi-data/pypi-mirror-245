419509cb863e0328fc7c713fb316ff4a4aa5ab96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:04:45 +0000

    Add package parroter/1.0.7/parroter-1.0.7.tar.gz

---
+++ packages/parroter/parroter-1.0.7.tar.gz/parroter-1.0.7/parroter/Library/parroterConfiger.py
@@ -0,0 +1,17 @@
+from LibHanger.Library.uwGlobals import configer
+from LibHanger.Library.uwGlobals import *
+from parroter.Library.parroterGlobals import *
+
+class parroterConfiger(configer):
+    
+    """
+    parroter共通設定クラス
+    """
+    
+    def __init__(self, _tgv:parroterGlobal, _file, _configFolderName = ''):
+        
+        """
+        コンストラクタ
+        """
+        
+        pass
