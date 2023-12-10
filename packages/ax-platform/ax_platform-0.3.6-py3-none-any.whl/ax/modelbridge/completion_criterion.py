2354c06225ae14668963e73d7bfe4a1846c1bcd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:28 +0000

    Add package ax-platform/0.3.6/ax_platform-0.3.6-py3-none-any.whl

---
+++ packages/ax-platform/ax_platform-0.3.6-py3-none-any.whl/ax/modelbridge/completion_criterion.py
@@ -0,0 +1,53 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from logging import Logger
+
+from ax.modelbridge.transition_criterion import (
+    MinimumPreferenceOccurances,
+    MinimumTrialsInStatus,
+    TransitionCriterion,
+)
+from ax.utils.common.logger import get_logger
+
+logger: Logger = get_logger(__name__)
+
+
+class CompletionCriterion(TransitionCriterion):
+    """
+    Deprecated class that has been replaced by `TransitionCriterion`, and will be
+    fully reaped in a future release.
+    """
+
+    logger.warning(
+        "CompletionCriterion is deprecated, please use TransitionCriterion instead."
+    )
+    pass
+
+
+class MinimumPreferenceOccurances(MinimumPreferenceOccurances):
+    """
+    Deprecated child class that has been replaced by `MinimumPreferenceOccurances`
+    in `TransitionCriterion`, and will be fully reaped in a future release.
+    """
+
+    logger.warning(
+        "CompletionCriterion, which MinimumPreferenceOccurance inherits from, is"
+        " deprecated. Please use TransitionCriterion instead."
+    )
+    pass
+
+
+class MinimumTrialsInStatus(MinimumTrialsInStatus):
+    """
+    Deprecated child class that has been replaced by `MinimumTrialsInStatus`
+    in `TransitionCriterion`, and will be fully reaped in a future release.
+    """
+
+    logger.warning(
+        "CompletionCriterion, which MinimumTrialsInStatus inherits from, is"
+        " deprecated. Please use TransitionCriterion instead."
+    )
+    pass
