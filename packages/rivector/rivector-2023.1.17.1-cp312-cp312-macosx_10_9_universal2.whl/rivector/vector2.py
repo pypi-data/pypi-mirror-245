c4ed0901b1d5a4a6b0cdab156b384eeef91a0cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:46 +0000

    Add package rivector/2023.1.17.1/rivector-2023.1.17.1-cp312-cp312-macosx_10_9_universal2.whl

---
+++ packages/rivector/rivector-2023.1.17.1-cp312-cp312-macosx_10_9_universal2.whl/rivector/vector2.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+from rivector.wrapper import Vector2Wrapper
+
+
+class Vector2(Vector2Wrapper):
+    def __init__(self, x: float = 0, y: float = 0):
+        super().__init__(x, y)
+
+    def __repr__(self) -> str:
+        return f'<Vector2 ({self.x_coord}, {self.y_coord})>'
