8d485856a194a579cf7e45ce3914a1b18a17d9fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:26 +0000

    Add package prompt-block/0.3/prompt_block-0.3.tar.gz

---
+++ packages/prompt-block/prompt_block-0.3.tar.gz/prompt_block-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='prompt_block',
+    version='0.3',
+    description='Client for Prompt Block. Build your prompt library with ease.',
+    author='Contentify',
+    author_email='hi@contentify.app',
+    packages=['prompt_block'],
+    install_requires=['requests'],
+)
