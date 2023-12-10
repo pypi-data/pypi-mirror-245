cf13361e1556663654263d127497cbad41c08c24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:13 +0000

    Add package rdabase/2.0.5/rdabase-2.0.5-py3-none-any.whl

---
+++ packages/rdabase/rdabase-2.0.5-py3-none-any.whl/rdabase/misc.py
@@ -0,0 +1,18 @@
+"""
+MISCELLANEOUS helper functions
+"""
+
+from typing import NamedTuple
+from .constants import STATE_FIPS
+
+
+class Assignment(NamedTuple):
+    geoid: str
+    district: int | str
+
+
+def starting_seed(xx: str, N: int, K: int = 1) -> int:
+    fips: str = STATE_FIPS[xx]
+    start: int = K * N * int(fips)
+
+    return start
