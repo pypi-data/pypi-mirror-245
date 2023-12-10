098ab59c4c1a6c9bdd1a7b83536278ddd697b3de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:50 +0000

    Add package govee-py2/0.0.1/govee-py2-0.0.1.tar.gz

---
+++ packages/govee-py2/govee-py2-0.0.1.tar.gz/govee-py2-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Interacting with Govee Lights via Python'
+LONG_DESCRIPTION = 'A package that allows light device interaction with Govee API 2.0 using Python'
+
+# Setting up
+setup(
+    name="govee-py2",
+    version=VERSION,
+    author="Sxzo",
+    author_email="",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests', 'subprocess'],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
