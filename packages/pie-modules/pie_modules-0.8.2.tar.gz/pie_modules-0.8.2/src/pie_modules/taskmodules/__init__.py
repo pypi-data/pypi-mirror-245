aacab0e2d69be9890fd743aa631d8a33965a5df7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 05:04:26 +0000

    Add package pie-modules/0.8.2/pie_modules-0.8.2.tar.gz

---
+++ packages/pie-modules/pie_modules-0.8.2.tar.gz/pie_modules-0.8.2/src/pie_modules/taskmodules/__init__.py
@@ -0,0 +1,5 @@
+from .extractive_question_answering import ExtractiveQuestionAnsweringTaskModule
+from .re_text_classification_with_indices import (
+    RETextClassificationWithIndicesTaskModule,
+)
+from .token_classification import TokenClassificationTaskModule
