5d20516fce8353345dae9fee1fc6083669bb690f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:36 +0000

    Add package haupt/2.0.6rc5/haupt-2.0.6rc5-py3-none-any.whl

---
+++ packages/haupt/haupt-2.0.6rc5-py3-none-any.whl/haupt/cli/runners/streams.py
@@ -0,0 +1,28 @@
+import os
+
+from typing import Optional
+
+from haupt.cli.runners.base import start_app
+from polyaxon._env_vars.keys import ENV_KEYS_PROXY_STREAMS_TARGET_PORT
+from polyaxon._services.values import PolyaxonServices
+
+
+def start(
+    host: Optional[str] = None,
+    port: Optional[int] = None,
+    log_level: Optional[str] = None,
+    workers: Optional[int] = None,
+    per_core: bool = False,
+    uds: Optional[str] = None,
+):
+    port = port or os.environ.get(ENV_KEYS_PROXY_STREAMS_TARGET_PORT)
+    start_app(
+        app="haupt.polyconf.asgi.streams:application",
+        app_name=PolyaxonServices.STREAMS,
+        host=host,
+        port=port,
+        log_level=log_level,
+        workers=workers,
+        per_core=per_core,
+        uds=uds,
+    )
