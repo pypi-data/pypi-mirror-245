1309cb4ede072475d4cd4ec917eb97f5abedf1c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:05:09 +0000

    Add package fstrm/0.6.1/fstrm-0.6.1.tar.gz

---
+++ packages/fstrm/fstrm-0.6.1.tar.gz/fstrm-0.6.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+    
+KEYWORDS = ('framestreams fstrm encoder decoder')
+
+setuptools.setup(
+    name="fstrm",
+    version="0.6.1",
+    author="Denis MACHARD",
+    author_email="d.machard@gmail.com",
+    description="Frame Streams implementation in Python",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dmachard/python-framestream",
+    packages=['fstrm'],
+    include_package_data=True,
+    platforms='any',
+    keywords=KEYWORDS,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+    ],
+    install_requires=[]
+)
