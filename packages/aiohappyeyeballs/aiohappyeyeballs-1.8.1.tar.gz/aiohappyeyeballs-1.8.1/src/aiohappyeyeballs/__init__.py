8cd2fdc9314795dd1c22d3844651847e99b12f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:44 +0000

    Add package aiohappyeyeballs/1.8.1/aiohappyeyeballs-1.8.1.tar.gz

---
+++ packages/aiohappyeyeballs/aiohappyeyeballs-1.8.1.tar.gz/aiohappyeyeballs-1.8.1/src/aiohappyeyeballs/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "1.8.1"
+
+from .impl import start_connection
+from .types import AddrInfoType
+from .utils import pop_addr_infos_interleave, remove_addr_infos
+
+__all__ = (
+    "start_connection",
+    "AddrInfoType",
+    "remove_addr_infos",
+    "pop_addr_infos_interleave",
+)
