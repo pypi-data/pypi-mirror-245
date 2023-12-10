613f1d8fc409cf444462e81e91a2161052557c0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:57 +0000

    Add package dghs-imgutils/0.3.0/dghs-imgutils-0.3.0.tar.gz

---
+++ packages/dghs-imgutils/dghs-imgutils-0.3.0.tar.gz/dghs-imgutils-0.3.0/imgutils/validate/__init__.py
@@ -0,0 +1,13 @@
+"""
+Overview:
+    Tools for image validation and classification, which can be used to filter datasets.
+"""
+from .aicheck import *
+from .classify import *
+from .color import *
+from .monochrome import *
+from .nsfw import *
+from .portrait import *
+from .rating import *
+from .teen import *
+from .truncate import *
