ca73f0724316ba2ea28792ed64f413b0df9d899e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:45 +0000

    Add package outset/0.0.0rc48.post1/outset-0.0.0rc48.post1.tar.gz

---
+++ packages/outset/outset-0.0.0rc48.post1.tar.gz/outset-0.0.0rc48.post1/src/outset/__init__.py
@@ -0,0 +1,10 @@
+"""Outset Python Package"""
+from __future__ import annotations
+
+__version__ = "0.0.0""-rc48-post1"
+
+from outset.draw_outset_ import draw_outset_box
+
+__all__ = [
+    "draw_outset",
+]
