fecfc1bba624c66926295d4fa1259baa9344dbbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:34 +0000

    Add package augmenty/1.4.2/augmenty-1.4.2.tar.gz

---
+++ packages/augmenty/augmenty-1.4.2.tar.gz/augmenty-1.4.2/tests/test_static_embedding.py
@@ -0,0 +1,15 @@
+from augmenty.token.static_embedding_util import static_embedding
+
+from .fixtures import nlp_en_md
+
+
+def test_static_embedding_util(nlp_en_md):  # noqa F811
+    empty_embedding = static_embedding()
+
+    empty_embedding.update_from_vocab(nlp_en_md.vocab)
+    assert empty_embedding.vocab is not None
+    assert "exams".lower() in empty_embedding.most_similar("test", n=10)
+
+    emb = static_embedding.from_vocab(nlp_en_md.vocab)
+
+    assert "exams" in emb.most_similar("test", n=10)
