ec0c0422bbf75c93c24d6774363a407ffc7a35dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:17 +0000

    Add package flaxkv/0.1.7/flaxkv-0.1.7.tar.gz

---
+++ packages/flaxkv/flaxkv-0.1.7.tar.gz/flaxkv-0.1.7/flaxkv/__init__.py
@@ -0,0 +1,50 @@
+# Copyright (c) 2023 K.Y. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from .core import LevelDBDict, LMDBDict
+from .serve.client import RemoteDictDB
+
+__version__ = "0.1.7"
+
+__all__ = [
+    "dictdb",
+    "dbdict",
+    "LMDBDict",
+    "LevelDBDict",
+    "RemoteDictDB",
+]
+
+
+def dictdb(
+    path_or_url: str,
+    backend='lmdb',
+    remote=False,
+    db_name=None,
+    rebuild=False,
+    raw=False,
+    **kwargs,
+):
+    if remote:
+        return RemoteDictDB(
+            path_or_url, db_name=db_name, rebuild=rebuild, backend=backend
+        )
+    if backend == 'lmdb':
+        return LMDBDict(path_or_url, rebuild=rebuild, raw=raw, **kwargs)
+    elif backend == 'leveldb':
+        return LevelDBDict(path_or_url, rebuild=rebuild, raw=raw, **kwargs)
+    else:
+        raise ValueError(f"Unsupported DB type {backend}.")
+
+
+dbdict = dictdb
