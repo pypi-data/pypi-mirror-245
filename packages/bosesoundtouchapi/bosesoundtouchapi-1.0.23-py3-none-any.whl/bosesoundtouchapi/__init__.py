6ee1985b131feeb922ac8f3a14ccef892b89b754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 05:05:05 +0000

    Add package bosesoundtouchapi/1.0.23/bosesoundtouchapi-1.0.23-py3-none-any.whl

---
+++ packages/bosesoundtouchapi/bosesoundtouchapi-1.0.23-py3-none-any.whl/bosesoundtouchapi/__init__.py
@@ -0,0 +1,43 @@
+# include the README.md file for pdoc documentation generation.
+"""
+.. include:: ../README.md
+
+_________________
+
+<details>
+  <summary>View Change Log</summary>
+.. include:: ../CHANGELOG.md
+</details>
+"""
+
+# our package imports.
+from bosesoundtouchapi.soundtouchaudiomodes import SoundTouchAudioModes
+from bosesoundtouchapi.soundtouchclient import SoundTouchClient
+from bosesoundtouchapi.soundtouchdevice import SoundTouchDevice
+from bosesoundtouchapi.soundtouchdiscovery import SoundTouchDiscovery
+from bosesoundtouchapi.soundtoucherror import SoundTouchError
+from bosesoundtouchapi.soundtouchexception import SoundTouchException
+from bosesoundtouchapi.soundtouchhdmicecmodes import SoundTouchHdmiCecModes
+from bosesoundtouchapi.soundtouchitemtypes import SoundTouchItemTypes
+from bosesoundtouchapi.soundtouchkeys import SoundTouchKeys
+from bosesoundtouchapi.soundtouchmessage import SoundTouchMessage
+from bosesoundtouchapi.soundtouchnotifycategorys import SoundTouchNotifyCategorys
+from bosesoundtouchapi.soundtouchsources import SoundTouchSources
+from bosesoundtouchapi.soundtouchwarning import SoundTouchWarning
+
+# all classes to import when "import *" is specified.
+__all__ = [
+    'SoundTouchAudioModes',
+    'SoundTouchClient',
+    'SoundTouchDevice',
+    'SoundTouchDiscovery',
+    'SoundTouchError',
+    'SoundTouchException',
+    'SoundTouchHdmiCecModes',
+    'SoundTouchItemTypes',
+    'SoundTouchKeys',
+    'SoundTouchMessage',
+    'SoundTouchNotifyCategorys',
+    'SoundTouchSources',
+    'SoundTouchWarning'
+]
