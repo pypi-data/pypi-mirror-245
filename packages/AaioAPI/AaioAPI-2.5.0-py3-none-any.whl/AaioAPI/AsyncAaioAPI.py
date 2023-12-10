2434ef6346990d39c013dfe0317b2c3c8c65aca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:10 +0000

    Add package AaioAPI/2.5.0/AaioAPI-2.5.0-py3-none-any.whl

---
+++ packages/AaioAPI/AaioAPI-2.5.0-py3-none-any.whl/AaioAPI/AsyncAaioAPI.py
@@ -0,0 +1,148 @@
+import hashlib
+from urllib.parse import urlencode
+from requests.exceptions import ConnectTimeout, ReadTimeout
+
+import aiohttp
+
+
+class AsyncAaioAPI:
+    def __init__(self, API_KEY, SECRET_KEY, MERCHANT_ID):
+        self.API_KEY = API_KEY
+        self.SECRET_KEY = SECRET_KEY
+        self.MERCHANT_ID = MERCHANT_ID
+
+
+    async def get_balance(self):
+        """Get Balance"""
+
+        URL = 'https://aaio.io/api/balance'
+
+        headers = {
+            'Accept': 'application/json',
+            'X-Api-Key': self.API_KEY
+        }
+
+
+        try:
+            async with aiohttp.ClientSession(headers=headers) as session:
+
+                async with session.post(URL) as response:
+                    
+                    if(response.status in [200, 400, 401]):
+                        try:
+                            response_json = await response.json() # Парсинг результата
+                        except:
+                            return 'Не удалось пропарсить ответ'
+
+                        if(response_json['type'] == 'success'):
+                            return response_json
+                        else:
+                            return 'Ошибка: ' + response_json['message'] # Вывод ошибки
+                    else:
+                        return 'Response code: ' + str(response.status) # Вывод неизвестного кода ответа
+
+        except ConnectTimeout:
+            return 'ConnectTimeout' # Не хватило времени на подключение к сайту
+        
+        except ReadTimeout:
+            return 'ReadTimeout' # Не хватило времени на выполнение запроса
+
+
+    async def create_payment(self, order_id, amount, lang='ru', currency='RUB', description=None):
+        """Creating of payment"""
+
+        merchant_id = self.MERCHANT_ID # merchant id
+        secret = self.SECRET_KEY # secret key №1 from shop settings
+
+
+        sign = f':'.join([
+            str(merchant_id),
+            str(amount),
+            str(currency),
+            str(secret),
+            str(order_id)
+        ])
+
+        params = {
+            'merchant_id': merchant_id,
+            'amount': amount,
+            'currency': currency,
+            'order_id': order_id,
+            'sign': hashlib.sha256(sign.encode('utf-8')).hexdigest(),
+            'desc': description,
+            'lang': lang
+        }
+
+        url_aaio = "https://aaio.io/merchant/pay?" + urlencode(params)
+
+
+        return url_aaio
+    
+
+    async def get_payment_info(self, order_id):
+        """Get payment info"""
+
+        URL = 'https://aaio.io/api/info-pay'
+
+        params = {
+            'merchant_id': self.MERCHANT_ID,
+            'order_id': order_id
+        }
+
+        headers = {
+            'Accept': 'application/json',
+            'X-Api-Key': self.API_KEY
+
+        }
+
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post(URL, data=params) as response:
+                response_json = await response.json()
+
+                return response_json
+
+        
+    async def is_expired(self, order_id):
+        """Check status payment (expired)"""
+
+        URL = 'https://aaio.io/api/info-pay'
+
+        params = {
+            'merchant_id': self.MERCHANT_ID,
+            'order_id': order_id
+        }
+
+        headers = {
+            'Accept': 'application/json',
+            'X-Api-Key': self.API_KEY
+
+        }
+
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post(URL, data=params) as response:
+                response_json = await response.json()
+
+                return response_json['type'] == 'success' and response_json['status'] == 'expired'
+
+
+    async def is_success(self, order_id):
+        """Check status payment (success)"""
+
+        URL = 'https://aaio.io/api/info-pay'
+
+        params = {
+            'merchant_id': self.MERCHANT_ID,
+            'order_id': order_id
+        }
+
+        headers = {
+            'Accept': 'application/json',
+            'X-Api-Key': self.API_KEY
+
+        }
+
+        async with aiohttp.ClientSession(headers=headers) as session:
+            async with session.post(URL, data=params) as response:
+                response_json = await response.json()
+
+                return response_json['type'] == 'success' and response_json['status'] == 'success'
