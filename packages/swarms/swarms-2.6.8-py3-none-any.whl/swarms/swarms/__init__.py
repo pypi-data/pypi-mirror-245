166d07b565fa4c560ab3b25a27e09320ae2678dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:25 +0000

    Add package swarms/2.6.8/swarms-2.6.8-py3-none-any.whl

---
+++ packages/swarms/swarms-2.6.8-py3-none-any.whl/swarms/swarms/__init__.py
@@ -0,0 +1,10 @@
+from swarms.structs.autoscaler import AutoScaler
+from swarms.swarms.god_mode import GodMode
+from swarms.swarms.multi_agent_collab import MultiAgentCollaboration
+
+
+__all__ = [
+    "AutoScaler",
+    "GodMode",
+    "MultiAgentCollaboration",
+]
