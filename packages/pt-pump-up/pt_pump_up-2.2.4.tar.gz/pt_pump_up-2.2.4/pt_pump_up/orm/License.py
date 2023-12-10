85777016a9eeb4795c0edf3291e56b9ea4283bed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:35 +0000

    Add package pt-pump-up/2.2.4/pt_pump_up-2.2.4.tar.gz

---
+++ packages/pt-pump-up/pt_pump_up-2.2.4.tar.gz/pt_pump_up-2.2.4/pt_pump_up/orm/License.py
@@ -0,0 +1,7 @@
+from beanie import Document
+
+# TODO: Insert Licenses
+
+class License(Document):
+    name: str
+    url: str
---
+++ packages/pt-pump-up/pt_pump_up-2.2.4.tar.gz/pt_pump_up-2.2.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pt_pump_up',
+    version='2.2.4',
+    description='Hub for Portuguese NLP resources',
+    install_requires=['beanie', 'mechanize',
+                      'pandas', 'requests', 'scikit-learn', 'numpy'],
+    packages=find_packages(),
+    author='Rúben Almeida'
+)
