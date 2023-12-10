9a94c8f4fe26eebedc22800ac1b710cd79f4c43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:27 +0000

    Add package dialogflow-log-parser/0.0.4/dialogflow_log_parser-0.0.4.tar.gz

---
+++ packages/dialogflow-log-parser/dialogflow_log_parser-0.0.4.tar.gz/dialogflow_log_parser-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
+
+setuptools.setup(
+    name='dialogflow_log_parser',
+    version='0.0.4',
+    author='Peter Yusuke',
+    author_email='yyamashita1201@gmail.com',
+    description='parse dialogflow log string',
+    packages=setuptools.find_packages(exclude=['test', 'test.*']),
+    url='https://github.com/PeterYusuke/dialogflow-log-parser',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        'pyhocon==0.3.59'
+    ],
+    keywords='dialogflow parse log hocon',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+)
