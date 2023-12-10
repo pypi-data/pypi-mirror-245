f789640f0db95d68ef84e19a99314e00a50605c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:06:01 +0000

    Add package nano-fmm/0.1.3/nano_fmm-0.1.3-cp312-abi3-macosx_10_14_universal2.macosx_10_14_x86_64.macosx_11_0_arm64.macosx_11_0_universal2.whl

---
+++ packages/nano-fmm/nano_fmm-0.1.3-cp312-abi3-macosx_10_14_universal2.macosx_10_14_x86_64.macosx_11_0_arm64.macosx_11_0_universal2.whl/nano_fmm/network/match_result.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "nano_fmm/types.hpp"
+
+namespace nano_fmm
+{
+struct MatchResult
+{
+    // opt_candidates
+    // opath
+    // cpath
+    // indices
+    // geom
+};
+
+} // namespace nano_fmm
