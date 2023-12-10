1e43fe51d55e79a599859db89c9d0e237334d67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:02 +0000

    Add package obscure_stats/0.1.6/obscure_stats-0.1.6.tar.gz

---
+++ packages/obscure_stats/obscure_stats-0.1.6.tar.gz/obscure_stats-0.1.6/src/obscure_stats/skewness/__init__.py
@@ -0,0 +1,29 @@
+"""Skewness module."""
+
+from .skewness import (
+    auc_skew_gamma,
+    bickel_mode_skew,
+    bowley_skew,
+    forhad_shorna_rank_skew,
+    groeneveld_skew,
+    hossain_adnan_skew,
+    kelly_skew,
+    medeen_skew,
+    pearson_median_skew,
+    pearson_mode_skew,
+    wauc_skew_gamma,
+)
+
+__all__ = [
+    "auc_skew_gamma",
+    "wauc_skew_gamma",
+    "bowley_skew",
+    "forhad_shorna_rank_skew",
+    "groeneveld_skew",
+    "hossain_adnan_skew",
+    "kelly_skew",
+    "medeen_skew",
+    "pearson_median_skew",
+    "pearson_mode_skew",
+    "bickel_mode_skew",
+]
