5a6930ab3a1400239646450b5513283aad0fbbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:05:06 +0000

    Add package symbolicai/0.5.2/symbolicai-0.5.2-py3-none-any.whl

---
+++ packages/symbolicai/symbolicai-0.5.2-py3-none-any.whl/symai/extended/interfaces/blip_2.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from ... import core
+from ...symbol import Expression
+
+
+class blip_2(Expression):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __call__(self, image: str = None, text: List[str] = None, **kwargs) -> "blip_2":
+        @core.caption(image=image, prompt=text, **kwargs)
+        def _func(_) -> str:
+            pass
+        return self.sym_return_type(_func(self))
