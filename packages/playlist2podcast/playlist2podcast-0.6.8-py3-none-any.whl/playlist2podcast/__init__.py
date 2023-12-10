c6d2f6ecea8f80cbaff5d5ca4c826185887b686c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:23 +0000

    Add package playlist2podcast/0.6.8/playlist2podcast-0.6.8-py3-none-any.whl

---
+++ packages/playlist2podcast/playlist2podcast-0.6.8-py3-none-any.whl/playlist2podcast/__init__.py
@@ -0,0 +1,6 @@
+"""Package 'playlist2podcast' level definitions."""
+from typing import Final
+
+__version__: Final[str] = "0.6.8"
+__display_name__: Final[str] = "Playlist2Podcast"
+__package_name__: Final[str] = __display_name__.lower()
