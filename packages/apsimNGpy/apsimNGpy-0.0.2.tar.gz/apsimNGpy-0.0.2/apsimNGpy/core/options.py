f346fd5b8f4b127a91e6f311613ea64983615ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:21 +0000

    Add package apsimNGpy/0.0.2/apsimNGpy-0.0.2.tar.gz

---
+++ packages/apsimNGpy/apsimNGpy-0.0.2.tar.gz/apsimNGpy-0.0.2/apsimNGpy/core/options.py
@@ -0,0 +1,21 @@
+import os.path as osp
+import os
+import time
+from pathlib import Path
+from dataclasses import dataclass
+
+
+def _check_path(apsim_version):
+    path = True if osp.exists(apsim_version) and apsim_version.endswith("bin") and 'APSIM' in apsim_version else None
+
+
+class Options:
+    def __init__(self, path):
+        self.__path2_apsim = path
+
+    @property
+    def APSIMPATH(self):
+        return self.__path2_apsim if _check_path(self.__path2_apsim) else None
+
+
+a = Options("ye")
