8cca1accf13f4ced721b2eff803ae2c57ca661d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 15:04:47 +0000

    Add package tkadw/0.4.6/tkadw-0.4.6.tar.gz

---
+++ packages/tkadw/tkadw-0.4.6.tar.gz/tkadw-0.4.6/tkadw/windows/widgets/__init__.py
@@ -0,0 +1,5 @@
+from tkadw.windows.widgets.adw import Adw, run as adw_run, AdwRun
+from tkadw.windows.widgets.label import AdwLabel, AdwDarkLabel
+from tkadw.windows.widgets.mdi import AdwMDI
+from tkadw.windows.widgets.listbox import AdwListBox, AdwRoundListBox2, AdwRoundListBox3
+from tkadw.windows.widgets.stack import AdwStack
