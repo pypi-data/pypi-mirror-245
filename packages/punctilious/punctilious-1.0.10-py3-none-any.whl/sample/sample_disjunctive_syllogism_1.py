f507c382cb1afdfc62c81b965a767142854ff0ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:22 +0000

    Add package punctilious/1.0.10/punctilious-1.0.10-py3-none-any.whl

---
+++ packages/punctilious/punctilious-1.0.10-py3-none-any.whl/sample/sample_disjunctive_syllogism_1.py
@@ -0,0 +1,21 @@
+import punctilious as pu
+
+# Create a universe-of-discourse with basic objects for the sake of this example.
+u = pu.UniverseOfDiscourse(echo=True)
+o1 = u.o.declare()
+o2 = u.o.declare()
+o3 = u.o.declare()
+o4 = u.o.declare()
+axiom = u.a.declare(natural_language='Dummy axiom for demonstration purposes')
+
+# Elaborate a dummy theory with a set of propositions necessary for our demonstration
+t1 = u.t.declare(echo=True)
+theory_axiom = t1.include_axiom(a=axiom)
+phi1 = t1.i.axiom_interpretation.infer_formula_statement(a=theory_axiom,
+    p=(o1 | u.c1.implies | o3) | u.c1.lor | (o1 | u.c1.implies | o4), lock=False)
+phi2 = t1.i.axiom_interpretation.infer_formula_statement(a=theory_axiom, p=u.c1.lnot((o1 | u.c1.implies | o3)),
+    lock=True)
+
+# And finally, use the conjunction-introduction inference-rule:
+proposition_of_interest = t1.i.disjunctive_syllogism_1.infer_formula_statement(p_or_q=phi1, not_p=phi2,
+    subtitle='The proposition of interest')
---
+++ packages/punctilious/punctilious-1.0.10-py3-none-any.whl/theory/__init__.py
@@ -0,0 +1,7 @@
+"""This python module contains mathematical theories formalized with *punctilious*."""
+__version__ = "1.0.10"
+
+from theory.theory_tao_2006_the_peano_axioms import Tao2006ThePeanoAxioms
+from theory.theory_mgz_2021_minimal_logic_m0 import MGZ2021MinimalLogicM0
+from theory.theory_mgz_2021_intuitionistic_logic_j0 import MGZ2021IntuitionisticLogicJ0
+from theory.theory_mgz_2021_classical_logic_j0 import MGZ2021ClassicalLogicK0
