71785d952886a7421cb2292e75aaa0f22ea9a066
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:52 +0000

    Add package charidotella/3.0.0/charidotella-3.0.0-cp310-cp310-win_amd64.whl

---
+++ packages/charidotella/charidotella-3.0.0-cp310-cp310-win_amd64.whl/charidotella/tasks/spectrogram.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+import importlib.resources
+import pathlib
+import subprocess
+import typing
+
+from .. import utilities
+
+EXTENSION = ".png"
+
+
+def run(
+    input: pathlib.Path,
+    output: pathlib.Path,
+    begin: int,
+    end: int,
+    parameters: dict[str, typing.Any],
+):
+    arguments = [
+        str(utilities.asset_path("spectrogram")),
+        str(input),
+        str(output),
+        str((output.parent / output.stem).with_suffix(".json")),
+        f"--begin={begin}",
+        f"--end={end}",
+        f"--tau={parameters['tau']}",
+        f"--mode={parameters['mode']}",
+        f"--maximum={parameters['maximum']}",
+        f"--frequencies={parameters['frequencies']}",
+        f"--times={parameters['times']}",
+        f"--gamma={parameters['gamma']}",
+    ]
+    if "minimum" in parameters:
+        arguments.append(f"--minimum={parameters['minimum']}")
+    if "region-of-interest" in parameters:
+        arguments.append(f"--left={parameters['region-of-interest'][0]}")
+        arguments.append(f"--bottom={parameters['region-of-interest'][1]}")
+        arguments.append(f"--width={parameters['region-of-interest'][2]}")
+        arguments.append(f"--height={parameters['region-of-interest'][3]}")
+    subprocess.run(
+        arguments,
+        check=True,
+    )
