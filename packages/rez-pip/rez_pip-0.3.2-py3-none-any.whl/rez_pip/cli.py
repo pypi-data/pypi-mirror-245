a3ba00d9b7049eda208d8d84d68d40d2ee79fc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:34 +0000

    Add package rez-pip/0.3.2/rez_pip-0.3.2-py3-none-any.whl

---
+++ packages/rez-pip/rez_pip-0.3.2-py3-none-any.whl/rez_pip/cli.py
@@ -0,0 +1,332 @@
+import os
+import sys
+import json
+import shutil
+import typing
+import logging
+import argparse
+import textwrap
+import pathlib
+import tempfile
+import subprocess
+
+if sys.version_info >= (3, 10):
+    import importlib.metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+import rich
+import rich.text
+import rich.panel
+import rich.markup
+import rich.logging
+import rez.vendor.version.version
+
+import rez_pip.pip
+import rez_pip.rez
+import rez_pip.data
+import rez_pip.install
+import rez_pip.download
+import rez_pip.exceptions
+
+_LOG = logging.getLogger("rez_pip.cli")
+
+__all__ = ["run"]
+
+
+def __dir__() -> typing.List[str]:
+    return __all__
+
+
+def _createParser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description="Ingest and convert python packages to rez packages.",
+        prog=__package__.replace("_", "-"),
+        add_help=False,
+    )
+    parser.add_argument("packages", nargs="*", help="Packages to install.")
+
+    generalGroup = parser.add_argument_group(title="general options")
+    generalGroup.add_argument(
+        "-r",
+        "--requirement",
+        action="append",
+        metavar="<file>",
+        help="Install from the given requirements file. This option can be used multiple times.",
+    )
+    generalGroup.add_argument(
+        "-c",
+        "--constraint",
+        action="append",
+        metavar="<file>",
+        help="Constrain versions using the given constraints file. This option can be used multiple times.",
+    )
+    generalGroup.add_argument(
+        "-p",
+        "--prefix",
+        metavar="<path>",
+        help="Custom repository path (can be any directory, even non rez repository path) (default: configured local_packages_path)",
+    )
+    generalGroup.add_argument(
+        "--release",
+        action="store_true",
+        help="Release the converted packages (default: configured release_packages_path)",
+    )
+
+    generalGroup.add_argument(
+        "--python-version",
+        # 3.7+ because that's what pip supports
+        default="3.7+",
+        metavar="<version>",
+        help="Range of python versions. It can also be a single version, any valid rez version specifier or range or 'latest' (default: 3.7+)",
+    )
+    generalGroup.add_argument(
+        "--pip",
+        default=rez_pip.pip.getBundledPip(),
+        metavar="<path>",
+        help="Standalone pip (https://pip.pypa.io/en/stable/installation/#standalone-zip-application) (default: bundled).",
+    )
+
+    # Manually define just to keep the style consistent (capital letters, dot, etc.)
+    generalGroup.add_argument(
+        "-h", "--help", action="help", help="Show this help message and exit."
+    )
+
+    debugGroup = parser.add_argument_group(title="debug options")
+    debugGroup.add_argument(
+        "-l",
+        "--log-level",
+        default="info",
+        choices=["info", "debug", "warning", "error"],
+        help="Logging level.",
+    )
+
+    debugGroup.add_argument(
+        "--keep-tmp-dirs",
+        action="store_true",
+        help="Keep some temporary directories at the end of the process for further inspection.",
+    )
+
+    debugGroup.add_argument(
+        "--debug-info",
+        action="store_true",
+        help="Print debug information that you can use when reporting an issue on GitHub.",
+    )
+
+    parser.usage = f"""
+
+  %(prog)s [options] <package(s)>
+  %(prog)s <package(s)> [-- [pip options]]
+"""
+    return parser
+
+
+def _parseArgs(
+    args: typing.List[str],
+) -> typing.Tuple[argparse.Namespace, typing.List[str]]:
+    parser = _createParser()
+
+    knownArgs = []
+    pipArgs = []
+    if "--" in args:
+        # anything after -- will be passed as is to pip.
+        splitIndex = args.index("--")
+        knownArgs = args[:splitIndex]
+        pipArgs = args[splitIndex + 1 :]
+    else:
+        knownArgs = args
+
+    ownArgs = parser.parse_args(knownArgs)
+
+    return ownArgs, pipArgs
+
+
+def _validateArgs(args: argparse.Namespace) -> None:
+    if not args.pip.endswith(".pyz"):
+        raise rez_pip.exceptions.RezPipError(
+            f"[bold red]{args.pip!r} does not look like a valid zipapp. A zipapp should end with '.pyz'.[/]\n\n"
+            "  [grey74]Standalone pip documentation[/]: https://pip.pypa.io/en/stable/installation/#standalone-zip-application\n"
+            "  [grey74]zipapp documentation[/]: https://docs.python.org/3/library/zipapp.html"
+        )
+
+    if not os.path.exists(args.pip):
+        raise rez_pip.exceptions.RezPipError(rf"zipapp at {args.pip!r} does not exist")
+
+    if not args.packages and not args.requirement:
+        raise rez_pip.exceptions.RezPipError(
+            "no packages were passed and --requirements was not used. At least one of them must be passed."
+        )
+
+
+def _run(args: argparse.Namespace, pipArgs: typing.List[str], pipWorkArea: str) -> None:
+    pythonVersions = rez_pip.rez.getPythonExecutables(
+        args.python_version, packageFamily="python"
+    )
+
+    for pythonVersion, pythonExecutable in pythonVersions.items():
+        _LOG.info(
+            f"[bold underline]Installing requested packages for Python {pythonVersion}"
+        )
+
+        wheelsDir = os.path.join(pipWorkArea, "wheels")
+        os.makedirs(wheelsDir, exist_ok=True)
+
+        # Suffix with the python version because we loop over multiple versions,
+        # and package versions, content, etc can differ for each Python version.
+        installedWheelsDir = os.path.join(pipWorkArea, "installed", pythonVersion)
+        os.makedirs(installedWheelsDir, exist_ok=True)
+
+        with rich.get_console().status(
+            f"[bold]Resolving dependencies for {rich.markup.escape(', '.join(args.packages))} (python-{pythonVersion})"
+        ):
+            packages = rez_pip.pip.getPackages(
+                args.packages,
+                args.pip,
+                pythonVersion,
+                os.fspath(pythonExecutable),
+                args.requirement or [],
+                args.constraint or [],
+                pipArgs,
+            )
+
+        _LOG.info(f"Resolved {len(packages)} dependencies for python {pythonVersion}")
+
+        # TODO: Should we postpone downloading to the last minute if we can?
+        _LOG.info("[bold]Downloading...")
+        wheels = rez_pip.download.downloadPackages(packages, wheelsDir)
+        _LOG.info(f"[bold]Downloaded {len(wheels)} wheels")
+
+        dists: typing.Dict[importlib_metadata.Distribution, bool] = {}
+
+        with rich.get_console().status(
+            f"[bold]Installing wheels into {installedWheelsDir!r}"
+        ):
+            for package, wheel in zip(packages, wheels):
+                _LOG.info(f"[bold]Installing {package.name}-{package.version} wheel")
+                dist, isPure = rez_pip.install.installWheel(
+                    package, pathlib.Path(wheel), installedWheelsDir
+                )
+
+                dists[dist] = isPure
+
+        distNames = [dist.name for dist in dists.keys()]
+
+        with rich.get_console().status("[bold]Creating rez packages..."):
+            for dist, package in zip(dists, packages):
+                isPure = dists[dist]
+                rez_pip.rez.createPackage(
+                    dist,
+                    isPure,
+                    rez.vendor.version.version.Version(pythonVersion),
+                    distNames,
+                    installedWheelsDir,
+                    wheelURL=package.download_info.url,
+                    prefix=args.prefix,
+                    release=args.release,
+                )
+
+
+def _debug(
+    args: argparse.Namespace, console: rich.console.Console = rich.get_console()
+) -> None:
+    """Print debug information"""
+    prefix = "  "
+    console.print(
+        f"[bold]rez-pip version[/]: {importlib_metadata.version(__package__)}"
+    )
+
+    console.print(f"[bold]rez version[/]: {importlib_metadata.version('rez')}")
+
+    console.print(f"[bold]python version[/]: {sys.version}", highlight=False)
+    console.print(f"[bold]python executable[/]: {sys.executable}", highlight=False)
+
+    pip = args.pip or rez_pip.pip.getBundledPip()
+
+    console.print(f"[bold]pip[/]: {pip}", highlight=False)
+
+    completedProcess = subprocess.run(
+        [sys.executable, pip, "--version"],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+
+    console.print(
+        f"[bold]pip version[/]: {completedProcess.stdout.strip()}", highlight=False
+    )
+
+    completedProcess = subprocess.run(
+        [sys.executable, pip, "config", "debug"],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+
+    console.print("[bold]rez-pip provided arguments[/]:")
+    print(textwrap.indent(json.dumps(vars(args), indent=4), prefix))
+
+    console.print(f"[bold]pip config debug[/]:", highlight=False)
+    print(textwrap.indent(completedProcess.stdout.strip(), "  "))
+
+    completedProcess = subprocess.run(
+        [sys.executable, pip, "config", "list"],
+        stdout=subprocess.PIPE,
+        text=True,
+    )
+
+    console.print(f"[bold]pip config list[/]:", highlight=False)
+    print(
+        textwrap.indent(completedProcess.stdout.strip(), prefix)
+        or f"{prefix}Returned nothing"
+    )
+
+    console.print(f"[bold]rez python packages[/]:", highlight=False)
+    for pythonVersion, pythonExecutable in rez_pip.rez.getPythonExecutables(
+        args.python_version
+    ).items():
+        print(textwrap.indent(f"{pythonExecutable} ({pythonVersion})", prefix))
+
+    print()
+
+    rich.print(
+        rich.panel.Panel(
+            rich.text.Text(
+                "Please redact any sensitive information before giving this output to someone else!\n\n"
+                "Don't remove things, only redact/replace. Look for IP addresses, domain names, passwords, etc.",
+                justify="center",
+            ),
+            title="[bold red]WARNING!",
+            expand=False,
+            border_style="yellow",
+        ),
+        file=sys.stderr,
+    )
+
+
+def run() -> int:
+    pipWorkArea = tempfile.mkdtemp(prefix="rez-pip-target")
+    args, pipArgs = _parseArgs(sys.argv[1:])
+
+    try:
+        _validateArgs(args)
+
+        handler = rich.logging.RichHandler(
+            show_time=False, markup=True, show_path=False
+        )
+        handler.setFormatter(logging.Formatter(fmt="%(message)s"))
+
+        rootLogger = logging.getLogger("rez_pip")
+        rootLogger.addHandler(handler)
+        rootLogger.setLevel(args.log_level.upper())
+
+        if args.debug_info:
+            _debug(args)
+            return 0
+
+        _run(args, pipArgs, pipWorkArea)
+        return 0
+    except rez_pip.exceptions.RezPipError as exc:
+        rich.get_console().print(exc, soft_wrap=True)
+        return 1
+    finally:
+        if not args.keep_tmp_dirs:
+            _LOG.debug(f"Removing {pipWorkArea}")
+            shutil.rmtree(pipWorkArea)
---
+++ packages/rez-pip/rez_pip-0.3.2-py3-none-any.whl/rez_pip/rez.py
@@ -0,0 +1,265 @@
+import os
+import sys
+import copy
+import shutil
+import typing
+import logging
+import pathlib
+
+if sys.version_info >= (3, 10):
+    import importlib.metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+import rez.config
+import rez.packages
+import rez.package_maker
+import rez.resolved_context
+import rez.vendor.version.version
+
+import rez_pip.pip
+import rez_pip.utils
+
+_LOG = logging.getLogger(__name__)
+
+
+def createPackage(
+    dist: importlib_metadata.Distribution,
+    isPure: bool,
+    pythonVersion: rez.vendor.version.version.Version,
+    nameCasings: typing.List[str],
+    installedWheelsDir: str,
+    wheelURL: str,
+    prefix: typing.Optional[str] = None,
+    release: bool = False,
+) -> None:
+    _LOG.info(f"Creating rez package for {dist.name}")
+    name = rez_pip.utils.pythontDistributionNameToRez(dist.name)
+    version = rez_pip.utils.pythonDistributionVersionToRez(dist.version)
+
+    requirements = rez_pip.utils.getRezRequirements(dist, pythonVersion, isPure, [])
+
+    requires = requirements.requires
+    variant_requires = requirements.variant_requires
+    metadata = requirements.metadata
+
+    if prefix:
+        packagesPath = prefix
+    else:
+        packagesPath = (
+            rez.config.config.release_packages_path
+            if release
+            else rez.config.config.local_packages_path
+        )
+
+    def make_root(variant: rez.packages.Variant, path: str) -> None:
+        """Using distlib to iterate over all installed files of the current
+        distribution to copy files to the target directory of the rez package
+        variant
+        """
+        formattedRequirements = ", ".join(str(req) for req in variant.variant_requires)
+
+        _LOG.info(
+            rf"Installing {variant.qualified_package_name} \[{formattedRequirements}]"
+        )
+        if not dist.files:
+            raise RuntimeError(
+                f"{dist.name} package has no files registered! Something is wrong maybe?"
+            )
+
+        wheelsDirAbsolute = pathlib.Path(installedWheelsDir).resolve()
+        for src in dist.files:
+            srcAbsolute = src.locate().resolve()
+            dest = os.path.join(path, srcAbsolute.relative_to(wheelsDirAbsolute))
+            if not os.path.exists(os.path.dirname(dest)):
+                os.makedirs(os.path.dirname(dest))
+
+            _LOG.debug(f"Copying {str(srcAbsolute)!r} to {str(dest)!r}")
+            shutil.copyfile(srcAbsolute, dest)
+            shutil.copystat(srcAbsolute, dest)
+
+    with rez.package_maker.make_package(
+        name, packagesPath, make_root=make_root, skip_existing=True, warn_on_skip=False
+    ) as pkg:
+        pkg.version = version
+
+        # requirements and variants
+        if requires:
+            pkg.requires = requires
+
+        if variant_requires:
+            pkg.variants = [variant_requires]
+
+        # commands
+        commands = ["env.PYTHONPATH.append('{root}/python')"]
+
+        console_scripts = [
+            ep.name for ep in dist.entry_points if ep.group == "console_scripts"
+        ]
+        if console_scripts:
+            pkg.tools = console_scripts
+            # TODO: Don't hardcode scripts here.
+            commands.append("env.PATH.append('{root}/scripts')")
+
+        pkg.commands = "\n".join(commands)
+
+        # Make the package use hashed variants. This is required because we
+        # can't control what ends up in its variants, and that can easily
+        # include problematic chars (>, +, ! etc).
+        # TODO: #672 (shortlinks for variants)
+        pkg.hashed_variants = True
+
+        pkg.pip = {
+            "name": dist.name,
+            "version": dist.version,
+            "is_pure_python": metadata["is_pure_python"],
+            "wheel_url": wheelURL,
+            "rez_pip_version": importlib_metadata.version("rez-pip"),
+        }
+
+        # Take all the metadata that can be converted and put it
+        # in the rez package definition.
+        convertedMetadata, remainingMetadata = _convertMetadata(dist)
+        for key, values in convertedMetadata.items():
+            setattr(pkg, key, values)
+
+        pkg.pip["metadata"] = remainingMetadata
+
+    _LOG.info(
+        f"[bold]Created {len(pkg.installed_variants)} variants and skipped {len(pkg.skipped_variants)}"
+    )
+
+
+def _convertMetadata(
+    dist: importlib_metadata.Distribution,
+) -> typing.Tuple[typing.Dict[str, typing.Any], typing.Dict[str, typing.Any]]:
+    metadata = {}
+    originalMetadata = copy.deepcopy(dist.metadata.json)
+    del originalMetadata["metadata_version"]
+    del originalMetadata["name"]
+    del originalMetadata["version"]
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#summary
+    if dist.metadata["Summary"]:
+        metadata["summary"] = dist.metadata["Summary"]
+        del originalMetadata["summary"]
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#description
+    if dist.metadata["Description"]:
+        metadata["description"] = dist.metadata["Description"]
+        del originalMetadata["description"]
+
+    authors = []
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#author
+    author = dist.metadata["Author"]
+    if author:
+        authors.append(author)
+        del originalMetadata["author"]
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#author-email
+    authorEmail = dist.metadata["Author-email"]
+    if authorEmail:
+        authors.extend([email.strip() for email in authorEmail.split(",")])
+        del originalMetadata["author_email"]
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#maintainer
+    maintainer = dist.metadata["Maintainer"]
+    if maintainer:
+        authors.append(maintainer)
+        del originalMetadata["maintainer"]
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#maintainer-email
+    maintainerEmail = dist.metadata["Maintainer-email"]
+    if maintainerEmail:
+        authors.extend([email.strip() for email in maintainerEmail.split(",")])
+        del originalMetadata["maintainer_email"]
+
+    if authors:
+        metadata["authors"] = authors
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#license
+    # Prefer the License field and fallback to classifiers if one is present.
+    if dist.metadata["License"]:
+        metadata["license"] = dist.metadata["License"]
+        del originalMetadata["license"]
+    else:
+        licenseClassifiers = [
+            item.split("::")[-1].strip()
+            for item in dist.metadata.get_all("classifier", [])
+            if item.startswith("License ::")
+        ]
+
+        # I don't know what to do in this case, so just skip license if more than one
+        # classifier is found.
+        if len(licenseClassifiers) == 1:
+            metadata["license"] = licenseClassifiers[0]
+
+    helpLinks = []
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#home-page
+    if dist.metadata["Home-page"]:
+        helpLinks.append(["Home-page", dist.metadata["Home-page"]])
+        del originalMetadata["home_page"]
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#project-url-multiple-use
+    if dist.metadata["Project-URL"]:
+        urls = [
+            url.strip()
+            for value in dist.metadata.get_all("Project-URL")
+            for url in value.split(",")
+        ]
+        helpLinks.extend([list(entry) for entry in zip(urls[::2], urls[1::2])])
+        del originalMetadata["project_url"]
+
+    # https://packaging.python.org/en/latest/specifications/core-metadata/#download-url
+    if dist.metadata["Download-URL"]:
+        helpLinks.append(["Download-URL", dist.metadata["Download-URL"]])
+        del originalMetadata["download_url"]
+
+    if helpLinks:
+        metadata["help"] = helpLinks
+
+    return metadata, originalMetadata
+
+
+def getPythonExecutables(
+    range_: typing.Optional[str], packageFamily: str = "python"
+) -> typing.Dict[str, pathlib.Path]:
+    """
+    Get the available python executable from rez packages.
+
+    :param range_: version specifier
+    :param packageFamily: Name of the rez package family for the python package. This allows ot support PyPy, etc.
+    :returns: Dict where the keys are the python versions and values are abolute paths to executables.
+    """
+    packages = sorted(
+        rez.packages.iter_packages(
+            packageFamily, range_=range_ if range_ != "latest" else None
+        ),
+        key=lambda x: x.version,
+    )
+
+    if range_ == "latest":
+        packages = [packages[-1]]
+
+    pythons: typing.Dict[str, pathlib.Path] = {}
+    for package in packages:
+        resolvedContext = rez.resolved_context.ResolvedContext(
+            [f"{package.name}=={package.version}"]
+        )
+
+        # Make sure that system PATH doens't interfere with the "which" method.
+        resolvedContext.append_sys_path = False
+
+        for trimmedVersion in map(package.version.trim, [2, 1, 0]):
+            path = resolvedContext.which(f"python{trimmedVersion}", parent_environ={})
+            if path:
+                pythons[str(package.version)] = pathlib.Path(path)
+                break
+        else:
+            _LOG.warning(
+                f"Failed to find a Python executable in the {package.qualified_name!r} rez package"
+            )
+
+    return pythons
