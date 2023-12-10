e280d89553f9ad812d9984dd972144717c80f571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 05:05:03 +0000

    Add package qwertypy/0.1.0/qwertypy-0.1.0.tar.gz

---
+++ packages/qwertypy/qwertypy-0.1.0.tar.gz/qwertypy-0.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='qwertypy',
+    version='0.1.0',
+    author='Vipul Sharma',
+    description='My personal utilities library.',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
