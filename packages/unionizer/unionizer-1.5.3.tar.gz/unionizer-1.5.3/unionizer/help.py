b391f7dd97e4ebdff7fa7d1c202a6639a4b73944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:37 +0000

    Add package unionizer/1.5.3/unionizer-1.5.3.tar.gz

---
+++ packages/unionizer/unionizer-1.5.3.tar.gz/unionizer-1.5.3/unionizer/help.py
@@ -0,0 +1,45 @@
+string = """
+V1.5.2
+
+Unionizer helps developers pair objects together.
+
+For local pairs:
+    ```
+    bond: unionizer.Bond = unionizer.Bond(size=10) # Limit bond memory to 10.
+
+    bond.assign(object_1, object_2) # Tie object_1 and object_2 together.
+
+    # bond.delete(object_1, object_2) # Untie object_1 and object_2 from
+
+    bond.filter(object_2) # Filter all pairs that are tied to object_2.
+
+    bond.groups() # Return all pairs.
+
+    bond.isinstance(object_1, object_2) # Check if object_1 and object_2 are paired.
+    ```
+
+For global pairs:
+    ```
+    unionizer.core # Stored memory with no size limit unless changed
+
+    # Changing core params
+    #     unionizer.core(unique=True, size=10) # Limit core memory to 10 and make all keys be instance variables.
+
+    #     unionizer.set(unique=True, size=10) # Limit core memory to 10 and make all keys be instance variables.
+
+    unionizer.assign(object_1, object_2) # Tie object_1 and object_2 together.
+
+    unionizer.get(object_1) # Get object_1's tied object.
+
+    unionizer.delete(object_1) # Untie object_1 from its tied object.
+
+    unionizer.filter(object_2) # Filter all pairs that are tied to object_2.
+
+    # Properties
+    #     unionizer.vars # Return all instance variables.
+    #     unionizer.groups # Return all pairs.
+    #     unionizer.rules # Return all rules.
+    #     unionizer.memory # Return all memory.
+"""
+
+print(string)
