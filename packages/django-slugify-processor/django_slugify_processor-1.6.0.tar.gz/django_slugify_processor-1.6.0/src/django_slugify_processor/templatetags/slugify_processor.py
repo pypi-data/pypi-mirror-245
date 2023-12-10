ffb53343c3c2540f413f4f811553c92639c06391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:13 +0000

    Add package django-slugify-processor/1.6.0/django_slugify_processor-1.6.0.tar.gz

---
+++ packages/django-slugify-processor/django_slugify_processor-1.6.0.tar.gz/django_slugify_processor-1.6.0/src/django_slugify_processor/templatetags/slugify_processor.py
@@ -0,0 +1,25 @@
+"""Slugify django template filter."""
+from django import template
+from django.template.defaultfilters import stringfilter
+
+from ..text import slugify as _slugify
+
+register = template.Library()
+
+
+@register.filter(is_safe=True)
+@stringfilter
+def slugify(value: str) -> str:
+    """Template filter intended to override django 1.11+'s default slugify.
+
+    This can be installed via a builtin, or via ``{% load slugify_processor %}``.
+
+    Usage in a Django template:
+
+    .. code-block:: django
+
+       {% load slugify_processor %}  {# unless you added it to builtins %}
+       {{variable|slugify}}  {# assuming "variable" is in context %}
+       {{"C++"|slugify}}
+    """
+    return _slugify(value)
