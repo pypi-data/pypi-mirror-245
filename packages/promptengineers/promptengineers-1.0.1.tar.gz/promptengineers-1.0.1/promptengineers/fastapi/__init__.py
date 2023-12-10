cd6a14fd3d818da35f284a5f4795725975bcfee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:26 +0000

    Add package promptengineers/1.0.1/promptengineers-1.0.1.tar.gz

---
+++ packages/promptengineers/promptengineers-1.0.1.tar.gz/promptengineers-1.0.1/promptengineers/fastapi/__init__.py
@@ -0,0 +1,13 @@
+from .chat import router as chat_router
+from .history import router as history_router
+from .storage import router as storage_router
+from .retrieval import router as retrieval_router
+from .prompt import router as prompt_router
+
+__all__ = [
+    'chat_router',
+    'history_router',
+    'storage_router',
+    'retrieval_router',
+    'prompt_router'
+]
