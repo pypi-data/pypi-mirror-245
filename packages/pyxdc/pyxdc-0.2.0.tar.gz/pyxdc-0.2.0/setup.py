3561f9ef3c1994e68cb3f7576a200270a0fde5f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:29 +0000

    Add package pyxdc/0.2.0/pyxdc-0.2.0.tar.gz

---
+++ packages/pyxdc/pyxdc-0.2.0.tar.gz/pyxdc-0.2.0/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from setuptools import (
+    setup, find_packages
+)
+
+# README.md
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description: str = readme.read()
+
+# requirements.txt
+with open("requirements.txt", "r") as _requirements:
+    requirements: list = list(map(str.strip, _requirements.read().split("\n")))
+
+setup(
+    name="pyxdc",
+    version="0.2.0",
+    description="Python library with tools for XinFin blockchain.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author="Meheret Tesfaye Batu",
+    author_email="meherett.batu@gmail.com",
+    url="https://github.com/meherett/pyxdc",
+    keywords=["xinfin", "wallet", "protocol", "blockchain"],
+    python_requires=">=3.6,<4",
+    packages=find_packages(),
+    install_requires=requirements,
+    extras_require={
+        "tests": [
+            "pytest>=7.4.3,<8",
+            "pytest-cov>=4.1.0,<5"
+        ],
+        "docs": [
+            "sphinx>=3.5.4,<4",
+            "sphinx-rtd-theme>=0.5.2,<1"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
