7b83c4f4543117270faafe8ed03057d336551890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:05:08 +0000

    Add package tf2-data/0.0.5/tf2_data-0.0.5-py3-none-any.whl

---
+++ packages/tf2-data/tf2_data-0.0.5-py3-none-any.whl/tf2_data/__init__.py
@@ -0,0 +1,8 @@
+__title__ = "tf2-data"
+__author__ = "offish"
+__version__ = "0.0.5"
+__license__ = "MIT"
+
+from .schema import Schema, SchemaItems, IEconItems
+from .static import *
+from .files import EFFECTS, SCHEMA_ITEMS, DEFINDEX_NAMES
