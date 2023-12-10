c0372408025b335265e2fb1da33f44e1fb31ac0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:22 +0000

    Add package pyautogan/0.1.1rc0/pyautogan-0.1.1rc0.tar.gz

---
+++ packages/pyautogan/pyautogan-0.1.1rc0.tar.gz/pyautogan-0.1.1rc0/autogan/agents/__init__.py
@@ -0,0 +1,17 @@
+from .agent_switch import AgentSwitch
+from .human_agent import HumanAgent
+from .tool_agent_code_execution import ToolAgentCodeExecution
+from .tool_agent_file import ToolAgentFile
+from .tool_agent_mail import ToolAgentMail
+from .tool_agent_search import ToolAgentSearch
+from .universal_agent import UniversalAgent
+
+__all__ = [
+    "AgentSwitch",
+    "HumanAgent",
+    "ToolAgentCodeExecution",
+    "ToolAgentFile",
+    "ToolAgentMail",
+    "ToolAgentSearch",
+    "UniversalAgent",
+]
