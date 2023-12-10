a26f25ef6bfac58a89e50d15cf4169a60d8ce986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:29 +0000

    Add package financepy/0.34/financepy-0.34-py3-none-any.whl

---
+++ packages/financepy/financepy-0.34-py3-none-any.whl/financepy/products/fx/fx_fixed_lookback_option.py
@@ -0,0 +1,250 @@
+##############################################################################
+# Copyright (C) 2018, 2019, 2020 Dominic O'Kane
+##############################################################################
+
+from math import exp, log, sqrt
+import numpy as np
+
+
+from ...utils.math import N
+from ...utils.global_vars import gDaysInYear, gSmall
+from ...utils.error import FinError
+from ...models.gbm_process_simulator import FinGBMProcess
+from ...utils.helpers import check_argument_types
+from ...utils.date import Date
+from ...utils.global_types import OptionTypes
+from ...market.curves.discount_curve import DiscountCurve
+
+##########################################################################
+# TODO: Attempt control variate adjustment to monte carlo
+# TODO: Sobol for Monte Carlo
+# TODO: TIGHTEN UP LIMIT FOR W FROM 100
+# TODO: Vectorise the analytical pricing formula
+##########################################################################
+
+##########################################################################
+# FIXED STRIKE LOOKBACK CALL PAYS MAX(SMAX-K,0)
+# FIXED STRIKE LOOKBACK PUT PAYS MAX(K-SMIN,0)
+##########################################################################
+
+
+class FXFixedLookbackOption:
+    """ The Class for FX Fixed Strike Lookback options. """
+
+    def __init__(self,
+                 expiry_dt: Date,
+                 option_type: OptionTypes,
+                 optionStrike: float):
+        """ Create option with expiry date, option type and the option strike
+        """
+
+        check_argument_types(self.__init__, locals())
+
+        self._expiry_dt = expiry_dt
+        self._option_type = option_type
+        self._optionStrike = optionStrike
+
+##########################################################################
+
+    def value(self,
+              value_dt: Date,
+              stock_price: float,
+              dom_discount_curve: DiscountCurve,
+              for_discount_curve: DiscountCurve,
+              volatility: float,
+              stock_min_max: float):
+        """ Value FX Fixed Lookback Option using Black Scholes model and
+        analytical formulae. """
+
+        if isinstance(value_dt, Date) is False:
+            raise FinError("Valuation date is not a Date")
+
+        if value_dt > self._expiry_dt:
+            raise FinError("Valuation date after expiry date.")
+
+        if dom_discount_curve._value_dt != value_dt:
+            raise FinError(
+                "Domestic Curve valuation date not same as option value date")
+
+        if for_discount_curve._value_dt != value_dt:
+            raise FinError(
+                "Foreign Curve valuation date not same as option value date")
+
+        t = (self._expiry_dt - value_dt) / gDaysInYear
+
+        df = dom_discount_curve.df(self._expiry_dt)
+        r = -np.log(df)/t
+
+        dq = for_discount_curve.df(self._expiry_dt)
+        q = -np.log(dq)/t
+
+        v = volatility
+        s0 = stock_price
+        k = self._optionStrike
+        smin = 0.0
+        smax = 0.0
+
+        if self._option_type == OptionTypes.EUROPEAN_CALL:
+            smax = stock_min_max
+            if smax < s0:
+                raise FinError(
+                    "The Smax value must be >= the stock price.")
+        elif self._option_type == OptionTypes.EUROPEAN_PUT:
+            smin = stock_min_max
+            if smin > s0:
+                raise FinError(
+                    "The Smin value must be <= the stock price.")
+
+        # There is a risk of an overflow in the limit of q=r which
+        # we remove by adjusting the value of the dividend
+        if abs(r - q) < gSmall:
+            q = r + gSmall
+
+        df = exp(-r * t)
+        dq = exp(-q * t)
+        b = r - q
+        u = v * v / 2.0 / b
+        w = 2.0 * b / v / v
+        expbt = exp(b * t)
+
+        # Taken from Hull Page 536 (6th edition) and Haug Page 143
+        if self._option_type == OptionTypes.EUROPEAN_CALL:
+
+            if k > smax:
+                d1 = (log(s0/k) + (b+v*v/2.0)*t)/v/sqrt(t)
+                d2 = d1 - v * sqrt(t)
+
+                if s0 == k:
+                    term = -N(d1 - 2.0 * b * sqrt(t) / v) + expbt * N(d1)
+                elif s0 < k and w > 100:
+                    term = expbt * N(d1)
+                else:
+                    term = (-(s0 / k)**(-w)) * N(d1 - 2.0 *
+                                                 b * sqrt(t) / v) + expbt * N(d1)
+
+                v = s0 * dq * N(d1) - k * df * N(d2) + s0 * df * u * term
+
+            else:
+                e1 = (log(s0/smax) + (b+v*v/2.0)*t) / v / sqrt(t)
+                e2 = e1 - v * sqrt(t)
+
+                if s0 == smax:
+                    term = -N(e1 - 2.0 * b * sqrt(t) / v) + expbt * N(e1)
+                elif s0 < smax and w > 100:
+                    term = expbt * N(e1)
+                else:
+                    term = (-(s0 / smax)**(-w)) * \
+                        N(e1 - 2.0 * b * sqrt(t) / v) + expbt * N(e1)
+
+                v = df * (smax - k) + s0 * dq * N(e1) - \
+                    smax * df * N(e2) + s0 * df * u * term
+
+        elif self._option_type == OptionTypes.EUROPEAN_PUT:
+
+            if k >= smin:
+                f1 = (log(s0 / smin) + (b + v * v / 2.0) * t) / v / sqrt(t)
+                f2 = f1 - v * sqrt(t)
+
+                if s0 == smin:
+                    term = N(-f1 + 2.0 * b * sqrt(t) / v) - expbt * N(-f1)
+                elif s0 > smin and w < -100:
+                    term = -expbt * N(-f1)
+                else:
+                    term = ((s0 / smin)**(-w)) * N(-f1 + 2.0 *
+                                                   b * sqrt(t) / v) - expbt * N(-f1)
+
+                v = df * (k - smin) - s0 * dq * N(-f1) + \
+                    smin * df * N(-f2) + s0 * df * u * term
+
+            else:
+                d1 = (log(s0 / k) + (b + v * v / 2) * t) / v / sqrt(t)
+                d2 = d1 - v * sqrt(t)
+                if s0 == k:
+                    term = N(-d1 + 2.0 * b * sqrt(t) / v) - expbt * N(-d1)
+                elif s0 > k and w < -100:
+                    term = -expbt * N(-d1)
+                else:
+                    term = ((s0 / k)**(-w)) * N(-d1 + 2.0 *
+                                                b * sqrt(t) / v) - expbt * N(-d1)
+
+                v = k * df * N(-d2) - s0 * dq * N(-d1) + s0 * df * u * term
+
+        else:
+            raise FinError("Unknown lookback option type:" +
+                           str(self._option_type))
+
+        return v
+
+###############################################################################
+
+    def value_mc(self,
+                 value_dt: Date,
+                 spot_fx_rate: float,  # FORDOM
+                 domestic_curve: DiscountCurve,
+                 foreign_curve: DiscountCurve,
+                 volatility: float,
+                 spot_fx_rateMinMax: float,
+                 num_paths: int = 10000,
+                 num_steps_per_year: int = 252,
+                 seed: int = 4242):
+        """ Value FX Fixed Lookback option using Monte Carlo. """
+
+        t = (self._expiry_dt - value_dt) / gDaysInYear
+        S0 = spot_fx_rate
+
+        df = domestic_curve._df(t)
+        r_d = -np.log(df)/t
+
+        dq = foreign_curve._df(t)
+        r_f = -np.log(dq)/t
+
+        mu = r_d - r_f
+
+        num_time_steps = int(t * num_steps_per_year)
+
+        option_type = self._option_type
+        k = self._optionStrike
+
+        smin = 0.0
+        smax = 0.0
+
+        if self._option_type == OptionTypes.EUROPEAN_CALL:
+            smax = spot_fx_rateMinMax
+            if smax < S0:
+                raise FinError(
+                    "Smax must be greater than or equal to the stock price.")
+        elif self._option_type == OptionTypes.EUROPEAN_PUT:
+            smin = spot_fx_rateMinMax
+            if smin > S0:
+                raise FinError(
+                    "Smin must be less than or equal to the stock price.")
+
+        model = FinGBMProcess()
+        Sall = model.get_paths(
+            num_paths,
+            num_time_steps,
+            t,
+            mu,
+            S0,
+            volatility,
+            seed)
+
+        # Due to antithetics we have doubled the number of paths
+        num_paths = 2 * num_paths
+        payoff = np.zeros(num_paths)
+
+        if option_type == OptionTypes.EUROPEAN_CALL:
+            SMax = np.max(Sall, axis=1)
+            smaxs = np.ones(num_paths) * smax
+            payoff = np.maximum(SMax - k, 0.0)
+            payoff = np.maximum(payoff, smaxs - k)
+        elif option_type == OptionTypes.EUROPEAN_PUT:
+            SMin = np.min(Sall, axis=1)
+            smins = np.ones(num_paths) * smin
+            payoff = np.maximum(k - SMin, 0.0)
+            payoff = np.maximum(payoff, k - smins)
+        else:
+            raise FinError("Unknown lookback option type:" + str(option_type))
+
+        v = payoff.mean() * exp(-r_d*t)
+        return v
---
+++ packages/financepy/financepy-0.34-py3-none-any.whl/tests_golden/TestFinBondYieldCurve.py
@@ -0,0 +1,113 @@
+###############################################################################
+# Copyright (C) 2018, 2019, 2020 Dominic O'Kane
+###############################################################################
+
+import datetime as dt
+import os
+
+import sys
+sys.path.append("..")
+
+from FinTestCases import FinTestCases, globalTestCaseMode
+from financepy.products.bonds.yield_curve_model import CurveFitNelsonSiegelSvensson
+from financepy.products.bonds.yield_curve_model import CurveFitNelsonSiegel
+from financepy.products.bonds.yield_curve_model import CurveFitBSpline
+from financepy.products.bonds.yield_curve_model import CurveFitPolynomial
+from financepy.products.bonds.yield_curve import BondYieldCurve
+from financepy.products.bonds.bond import Bond
+from financepy.utils.date import Date, from_datetime
+from financepy.utils.day_count import DayCountTypes
+from financepy.utils.frequency import FrequencyTypes
+
+
+
+test_cases = FinTestCases(__file__, globalTestCaseMode)
+
+###############################################################################
+###############################################################################
+
+
+def test_BondYieldCurve():
+
+    ###########################################################################
+
+    import pandas as pd
+    path = os.path.join(os.path.dirname(__file__), './data/giltBondPrices.txt')
+    bondDataFrame = pd.read_csv(path, sep='\t')
+    bondDataFrame['mid'] = 0.5*(bondDataFrame['bid'] + bondDataFrame['ask'])
+
+    freq_type = FrequencyTypes.SEMI_ANNUAL
+    dc_type = DayCountTypes.ACT_ACT_ICMA
+    settlement = Date(19, 9, 2012)
+
+    bonds = []
+    ylds = []
+
+    for _, bond in bondDataFrame.iterrows():
+
+        date_string = bond['maturity']
+        mat_date_time = dt.datetime.strptime(date_string, '%d-%b-%y')
+        maturity_dt = from_datetime(mat_date_time)
+        issue_dt = Date(maturity_dt._d, maturity_dt._m, 2000)
+        coupon = bond['coupon']/100.0
+        clean_price = bond['mid']
+        bond = Bond(issue_dt, maturity_dt, coupon, freq_type, dc_type)
+        yld = bond.yield_to_maturity(settlement, clean_price)
+        bonds.append(bond)
+        ylds.append(yld)
+
+###############################################################################
+
+    curveFitMethod = CurveFitPolynomial()
+    fitted_curve1 = BondYieldCurve(settlement, bonds, ylds, curveFitMethod)
+#    fitted_curve1.display("GBP Yield Curve")
+
+    curveFitMethod = CurveFitPolynomial(5)
+    fitted_curve2 = BondYieldCurve(settlement, bonds, ylds, curveFitMethod)
+#    fitted_curve2.display("GBP Yield Curve")
+
+    curveFitMethod = CurveFitNelsonSiegel()
+    fitted_curve3 = BondYieldCurve(settlement, bonds, ylds, curveFitMethod)
+#    fitted_curve3.display("GBP Yield Curve")
+
+    curveFitMethod = CurveFitNelsonSiegelSvensson()
+    fitted_curve4 = BondYieldCurve(settlement, bonds, ylds, curveFitMethod)
+#    fitted_curve4.display("GBP Yield Curve")
+
+    curveFitMethod = CurveFitBSpline()
+    fitted_curve5 = BondYieldCurve(settlement, bonds, ylds, curveFitMethod)
+#    fitted_curve5.display("GBP Yield Curve")
+
+###############################################################################
+
+    test_cases.header("PARAMETER", "VALUE")
+    test_cases.print("values", fitted_curve1._curveFit._coeffs)
+
+    test_cases.header("PARAMETER", "VALUE")
+    test_cases.print("values", fitted_curve2._curveFit._coeffs)
+
+    test_cases.header("PARAMETER", "VALUE")
+    test_cases.print("beta_1", fitted_curve3._curveFit._beta_1)
+    test_cases.print("beta_2", fitted_curve3._curveFit._beta_2)
+    test_cases.print("beta_3", fitted_curve3._curveFit._beta_3)
+    test_cases.print("tau", fitted_curve3._curveFit._tau)
+
+    test_cases.header("PARAMETER", "VALUE")
+    test_cases.print("beta_1", fitted_curve4._curveFit._beta_1)
+    test_cases.print("beta_2", fitted_curve4._curveFit._beta_2)
+    test_cases.print("beta_3", fitted_curve4._curveFit._beta_3)
+    test_cases.print("beta_4", fitted_curve4._curveFit._beta_4)
+    test_cases.print("tau_1", fitted_curve4._curveFit._tau_1)
+    test_cases.print("tau_2", fitted_curve4._curveFit._tau_2)
+
+###############################################################################
+
+    maturity_dt = Date(19, 9, 2030)
+    interpolated_yield = fitted_curve5.interpolated_yield(maturity_dt)
+    test_cases.print(maturity_dt, interpolated_yield)
+
+###############################################################################
+
+
+test_BondYieldCurve()
+test_cases.compareTestCases()
