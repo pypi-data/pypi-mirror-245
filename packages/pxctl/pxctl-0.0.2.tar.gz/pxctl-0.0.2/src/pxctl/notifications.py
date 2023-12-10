90b3e6cbcc80ecdd641adb7c246f5d811eac4be8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:35 +0000

    Add package pxctl/0.0.2/pxctl-0.0.2.tar.gz

---
+++ packages/pxctl/pxctl-0.0.2.tar.gz/pxctl-0.0.2/src/pxctl/notifications.py
@@ -0,0 +1,29 @@
+import subprocess
+import sys
+
+from printer import PrintingInfo
+
+
+class Notifications:
+    def __init__(self, on_success_hook: str):
+        self.__notified_latch = False
+        self.__on_success_hook = on_success_hook
+
+    def update_state(self, info_optional: tuple[bool, PrintingInfo]):
+        if self.__notified_latch:
+            return
+
+        connection_ok, info = info_optional
+
+        if connection_ok:
+            if info.state == "SUCCESS":
+                self.run_success()
+                self.__notified_latch = True
+
+    def run_success(self):
+        if self.__on_success_hook is not None:
+            try:
+                subprocess.run(self.__on_success_hook, shell=True)
+            except Exception as e:
+                print(e, file=sys.stderr)
+
