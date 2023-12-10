4292fd0e936accbabc3f1d7b395d52e397f83820
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:41 +0000

    Add package distributions-azi/0.1/distributions_azi-0.1.tar.gz

---
+++ packages/distributions-azi/distributions_azi-0.1.tar.gz/distributions_azi-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_azi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_azi'],
+      author = 'Abdulaziz A. Hawaej',
+      author_email = 'abdulaziz602842@gmail.com',
+      zip_safe=False)
