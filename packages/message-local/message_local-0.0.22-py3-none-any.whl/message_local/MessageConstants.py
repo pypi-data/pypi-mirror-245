d62da1a09e4458ea84fcbf35adec6a3fb988c5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:50 +0000

    Add package message-local/0.0.22/message_local-0.0.22-py3-none-any.whl

---
+++ packages/message-local/message_local-0.0.22-py3-none-any.whl/message_local/MessageConstants.py
@@ -0,0 +1,18 @@
+
+from logger_local.Logger import Logger
+from logger_local.LoggerComponentEnum import LoggerComponentEnum
+
+MESSAGE__LOCAL_PYTHON_COMPONENT_ID = ""
+MESSAGE_LOCAL_PYTHON_COMPONENT_COMPONENT_NAME = ""
+DEVELOPER_EMAIL = 'jenya.b@circ.zone'
+object_message = {
+    'component_id': MESSAGE__LOCAL_PYTHON_COMPONENT_ID,
+    'component_name': MESSAGE_LOCAL_PYTHON_COMPONENT_COMPONENT_NAME,
+    'component_category': LoggerComponentEnum.ComponentCategory.Code.value,
+    'developer_email': DEVELOPER_EMAIL
+}
+logger = Logger.create_logger(object=object_message)
+
+DEFAULT_HEADERS = {"Content-Type": "application/json"}
+
+TEST_API_TYPE_ID = 4
