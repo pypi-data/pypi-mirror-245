8f3b1a2ec892a4f1e7050ce8ab1f43fe6eaf9a74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 14:04:51 +0000

    Add package degirum-tools/0.3.0/degirum_tools-0.3.0-py3-none-any.whl

---
+++ packages/degirum-tools/degirum_tools-0.3.0-py3-none-any.whl/degirum_tools/math_support.py
@@ -0,0 +1,126 @@
+#
+# math_support.py: miscellaneous math/geometry functions
+#
+# Copyright DeGirum Corporation 2023
+# All rights reserved
+#
+
+# MIT License
+#
+# Copyright (c) 2022 Roboflow
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import numpy as np
+from typing import Union
+
+
+def area(box: np.ndarray) -> np.ndarray:
+    """
+    Computes bbox(es) area: is vectorized.
+
+    Parameters
+    ----------
+    box : np.array
+        Box(es) in format (x0, y0, x1, y1)
+
+    Returns
+    -------
+    np.array
+        area(s)
+    """
+    return (box[..., 2] - box[..., 0]) * (box[..., 3] - box[..., 1])
+
+
+def intersection(boxA: np.ndarray, boxB: np.ndarray) -> Union[float, np.ndarray]:
+    """
+    Compute area of intersection of two boxes
+
+    Parameters
+    ----------
+    boxA : np.array
+        First box
+    boxB : np.array
+        Second box
+
+    Returns
+    -------
+    float64
+        Area of intersection
+    """
+    xA = np.fmax(boxA[..., 0], boxB[..., 0])
+    xB = np.fmin(boxA[..., 2], boxB[..., 2])
+    dx = np.fmax(xB - xA, 0)
+
+    yA = np.fmax(boxA[..., 1], boxB[..., 1])
+    yB = np.fmin(boxA[..., 3], boxB[..., 3])
+    dy = np.fmax(yB - yA, 0)
+
+    # compute the area of intersection rectangle
+    return dx * dy
+
+
+def xyxy2xywh(x: np.ndarray) -> np.ndarray:
+    """
+    Convert bounding box coordinates from (x1, y1, x2, y2) format to (x, y, width, height) format where (x1, y1) is the
+    top-left corner and (x2, y2) is the bottom-right corner.
+
+    Args:
+        x (np.ndarray): The input bounding box coordinates in (x1, y1, x2, y2) format.
+
+    Returns:
+        y (np.ndarray): The bounding box coordinates in (x, y, width, height) format.
+    """
+    y = np.copy(x)
+    y[:, 0] = (x[:, 0] + x[:, 2]) / 2  # x center
+    y[:, 1] = (x[:, 1] + x[:, 3]) / 2  # y center
+    y[:, 2] = x[:, 2] - x[:, 0]  # width
+    y[:, 3] = x[:, 3] - x[:, 1]  # height
+    return y
+
+
+def box_iou_batch(boxes_true: np.ndarray, boxes_detection: np.ndarray) -> np.ndarray:
+    """
+    Compute Intersection over Union (IoU) of two sets of bounding boxes -
+        `boxes_true` and `boxes_detection`. Both sets
+        of boxes are expected to be in `(x_min, y_min, x_max, y_max)` format.
+
+    Args:
+        boxes_true (np.ndarray): 2D `np.ndarray` representing ground-truth boxes.
+            `shape = (N, 4)` where `N` is number of true objects.
+        boxes_detection (np.ndarray): 2D `np.ndarray` representing detection boxes.
+            `shape = (M, 4)` where `M` is number of detected objects.
+
+    Returns:
+        np.ndarray: Pairwise IoU of boxes from `boxes_true` and `boxes_detection`.
+            `shape = (N, M)` where `N` is number of true objects and
+            `M` is number of detected objects.
+    """
+
+    def box_area(box):
+        return (box[2] - box[0]) * (box[3] - box[1])
+
+    area_true = box_area(boxes_true.T)
+    area_detection = box_area(boxes_detection.T)
+
+    top_left = np.maximum(boxes_true[:, None, :2], boxes_detection[:, :2])
+    bottom_right = np.minimum(boxes_true[:, None, 2:], boxes_detection[:, 2:])
+
+    area_inter = np.prod(np.clip(bottom_right - top_left, a_min=0, a_max=None), 2)
+    return area_inter / (area_true[:, None] + area_detection - area_inter)
