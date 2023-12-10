854ed952522c9fc7677a0120b5ad43c91e3cd5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:05:14 +0000

    Add package calclib/0.0.1/calclib-0.0.1.tar.gz

---
+++ packages/calclib/calclib-0.0.1.tar.gz/calclib-0.0.1/calclib/__init__.py
@@ -0,0 +1,11 @@
+def add_(num1, num2):
+    return num1 + num2
+
+def sub_(num1, num2):
+    return num1 - num2
+
+def mul_(num1, num2):
+    return num1 * num2
+
+def div_(num1, num2):
+    return num1 / num2
