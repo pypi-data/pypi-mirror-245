1b508150589f8d3deff7fbd98e85979130bb5ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:34 +0000

    Add package augmenty/1.4.2/augmenty-1.4.2-py3-none-any.whl

---
+++ packages/augmenty/augmenty-1.4.2-py3-none-any.whl/augmenty/character/casing.py
@@ -0,0 +1,52 @@
+import random
+from functools import partial
+from typing import Callable, Iterator
+
+import spacy
+from spacy.language import Language
+from spacy.training import Example
+
+from ..augment_utilities import make_text_from_orth
+
+
+def random_casing_augmenter_v1(
+    nlp: Language,
+    example: Example,
+    level: float,
+) -> Iterator[Example]:  # type: ignore
+    def __casing(c):
+        if random.random() < level:
+            return c.lower() if random.random() < 0.5 else c.upper()
+        return c
+
+    example_dict = example.to_dict()
+    example_dict["token_annotation"]["ORTH"] = [
+        "".join(__casing(c) for c in t.text) for t in example.y
+    ]
+    text = make_text_from_orth(example_dict)
+    doc = nlp.make_doc(text)
+    yield Example.from_dict(doc, example_dict)
+
+
+@spacy.registry.augmenters("random_casing_v1")  # type: ignore
+def create_random_casing_augmenter_v1(
+    level: float,
+) -> Callable[[Language, Example], Iterator[Example]]:  # type: ignore
+    """Create an augment that randomly changes the casing of the document.
+
+    Args:
+        level (float): The percentage of character that will have its casing changed.
+
+    Returns:
+        Callable[[Language, Example], Iterator[Example]]: The augmenter.
+
+    Example:
+        >>> import augmenty
+        >>> from spacy
+        >>> nlp = spacy.blank("en")
+        >>> random_casing_augmenter = augmenty.load("random_casing_v1", level=0.1)
+        >>> texts = ["A sample text"]
+        >>> list(augmenty.texts(texts, random_casing_augmenter, nlp))
+        ["A saMple texT"]
+    """
+    return partial(random_casing_augmenter_v1, level=level)
---
+++ packages/augmenty/augmenty-1.4.2-py3-none-any.whl/augmenty/util.py
@@ -0,0 +1,143 @@
+"""Utility functions for the package."""
+
+from typing import Callable, Dict, Iterable, Iterator, List
+
+import catalogue  # type: ignore
+import spacy  # type: ignore
+import thinc  # type: ignore
+from spacy.language import Language  # type: ignore
+from spacy.tokens import Doc  # type: ignore
+from spacy.training import Example  # type: ignore
+
+
+class registry(thinc.registry):
+    keyboards = catalogue.create("augmenty", "keyboards", entry_points=True)
+
+
+def docs(
+    docs: Iterable[Doc],  # type: ignore
+    augmenter: Callable[[Language, Example], Iterator[Example]],  # type: ignore
+    nlp: Language,
+) -> Iterator[Doc]:  # type: ignore
+    """Augments an iterable of spaCy Doc.
+
+    Args:
+        docs (Iterable[Doc]): A iterable of spaCy Docs
+        augmenter (Callable[[Language, Example], Iterator[Example]]): An augmenter
+        nlp (Language): A spaCy language pipeline.
+
+    Return:
+        Iterator[Doc]: An iterator of the augmented Docs.
+
+    Yields:
+        Doc: The augmented Docs.
+
+    Example:
+        >>> from spacy.tokens import Doc
+        >>> from spacy.lang.en import English
+        >>> nlp = English()
+        >>> docs = [Doc(words=["Fine", "by", "me"])]
+        >>> augmenter = augmenty.load("upper_case_v1", level=1)
+        >>> augmented_docs = augmenty.docs(docs, augmenter, nlp)
+    """
+    if isinstance(docs, Doc):
+        docs = [docs]
+    for doc in docs:
+        example = Example(doc, doc)
+        examples = augmenter(nlp, example)
+        for e in examples:
+            yield e.y
+
+
+def texts(
+    texts: Iterable[str],  # type: ignore
+    augmenter: Callable[[Language, Example], Iterator[Example]],  # type: ignore
+    nlp: Language,
+) -> Iterable[str]:  # type: ignore
+    """Augments an list of texts.
+
+    Args:
+        texts (Iterable[str]): A iterable of strings
+        augmenter (Callable[[Language, Example], Iterator[Example]]): An augmenter
+        nlp (Language): A spaCy language pipeline.
+
+    Return:
+        Iterator[str]: An iterator of the augmented texts.
+
+    Yields:
+        str: The augmented text.
+    """
+    if isinstance(texts, str):
+        texts = [texts]
+
+    def __gen() -> Iterable[Doc]:  # type: ignore
+        for text in texts:
+            yield nlp(text)
+
+    for doc in docs(__gen(), augmenter=augmenter, nlp=nlp):
+        yield doc.text
+
+
+def augmenters() -> Dict[str, Callable]:  # type: ignore
+    """A utility function to get an overview of all augmenters.
+
+    Returns:
+        Dict[str, Callable]: Dictionary of all augmenters
+
+    Example:
+    >>> augmenters = augmenty.augmenters()
+    >>> "upper_case_v1" in augmenters
+    True
+    """
+    return spacy.registry.augmenters.get_all()  # type: ignore
+
+
+def load(augmenter: str, **kwargs) -> Callable:  # type: ignore
+    """A utility functionload an augmenter.
+
+    Returns:
+        Dictionary of all augmenters
+
+    Example:
+    >>> from spacy.lang.en import English
+    >>> nlp = English()
+    >>> upper_case_augmenter = augmenty.load("upper_case_v1", level = 1)
+    >>> texts = ["hello there!"]
+    >>> list(augmenty.texts(texts, upper_case_augmenter, nlp))
+    ["HELLO THERE!"]
+    """
+    aug = spacy.registry.augmenters.get(augmenter)  # type: ignore
+    return aug(**kwargs)
+
+
+def keyboards() -> List[str]:  # type: ignore
+    """A utility function to get an overview of all keyboards.
+
+    Returns:
+        List[str]]: List of all keyboards
+
+    Example:
+    >>> keyboards = augmenty.keyboards()
+    """
+    return list(registry.keyboards.get_all().keys())
+
+
+def meta() -> Dict[str, dict]:  # type: ignore
+    """Returns a a dictionary containing metadata for each augmenter.
+
+    Returns:
+        Dict[str, dict]: A dictionary of meta data
+
+    Example:
+    >>> metadata = augmenty.meta()
+    >>> metadata["token_swap_v1"]
+    """
+    import json
+    import os
+    import pathlib
+
+    p = pathlib.Path(__file__).parent.resolve()
+    p = os.path.join(p, "meta.json")  # type: ignore
+    with open(p) as f:  # type: ignore
+        r = json.load(f)
+    return r
