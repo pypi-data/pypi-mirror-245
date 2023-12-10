35593d5b217a192838e14ecb4059c84b69dd759f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 03:06:03 +0000

    Add package nano-fmm/0.1.3/nano_fmm-0.1.3-cp38-cp38-win32.whl

---
+++ packages/nano-fmm/nano_fmm-0.1.3-cp38-cp38-win32.whl/nano_fmm/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from ._core import *  # noqa: F403
+from ._core import __version__  # noqa: F401
+
+
+def pure_python_func(
+    arg1: int = 42,
+    arg2: float = 3.14,
+    arg3: str = "you shall not pass",
+) -> str:
+    return f"int: {arg1}, float: {arg2}, str: {arg3}"
