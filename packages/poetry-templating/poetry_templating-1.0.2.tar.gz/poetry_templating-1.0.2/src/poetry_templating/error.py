181834f85726d7da66c30ce2a85ad679452f9a32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-10 04:04:36 +0000

    Add package poetry-templating/1.0.2/poetry_templating-1.0.2.tar.gz

---
+++ packages/poetry-templating/poetry_templating-1.0.2.tar.gz/poetry_templating-1.0.2/src/poetry_templating/error.py
@@ -0,0 +1,23 @@
+import logging
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from poetry_templating.engine import EvaluationContext
+
+_log = logging.getLogger(__name__)
+
+
+class TemplatingError(Exception):
+    ...
+
+
+class EvaluationError(TemplatingError):
+    def __init__(self, ctx: "EvaluationContext", message) -> None:
+        super().__init__(
+            f"Error evaluating template: {message}\n  "
+            + (
+                f"Line {ctx.line}"
+                if ctx.path is None
+                else f'File "{ctx.path}", line {ctx.line}'
+            )
+        )
