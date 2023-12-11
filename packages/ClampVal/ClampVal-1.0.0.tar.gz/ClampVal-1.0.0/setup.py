3191d0e1b1765275c883cfec8a78d7dffadf6579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:37 +0000

    Add package ClampVal/1.0.0/ClampVal-1.0.0.tar.gz

---
+++ packages/ClampVal/ClampVal-1.0.0.tar.gz/ClampVal-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ClampVal",
+    version="1.0.0",
+    packages=find_packages(),
+    author="LittleBread69",
+    author_email="chelouminatis+pypi@gmail.com",
+    description="Clamp your vars",
+    url="https://github.com/LittleBread69/Clampy",
+    license="MIT",
+)
