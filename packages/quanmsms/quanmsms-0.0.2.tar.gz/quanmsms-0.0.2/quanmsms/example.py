5cfb3c1a316e261c2d5ee71c3f5cc38f066548c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:46 +0000

    Add package quanmsms/0.0.2/quanmsms-0.0.2.tar.gz

---
+++ packages/quanmsms/quanmsms-0.0.2.tar.gz/quanmsms-0.0.2/quanmsms/example.py
@@ -0,0 +1,5 @@
+# this is a Example file
+# You must first execute:pip install quanmsms
+import quanmsms
+
+print(quanmsms.sdk())
---
+++ packages/quanmsms/quanmsms-0.0.2.tar.gz/quanmsms-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "quanmsms",
+    version = "0.0.2",
+    packages=find_packages(),
+    description="dev.quanmwl.com/ability_sms --> Python版SDK",
+    url="https://gitee.com/chengdu-quanming-network/quanmsms-python",
+    author="qiu peng",
+    author_email="qp@quanmwl.com",
+)
