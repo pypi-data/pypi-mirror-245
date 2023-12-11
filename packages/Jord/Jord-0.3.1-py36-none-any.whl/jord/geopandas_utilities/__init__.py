10c7667631b7f0222c3420686f5fba533ab1af4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:37 +0000

    Add package Jord/0.3.1/Jord-0.3.1-py36-none-any.whl

---
+++ packages/Jord/Jord-0.3.1-py36-none-any.whl/jord/geopandas_utilities/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from .geometry_filtering import *
+from .importing import *
---
+++ packages/Jord/Jord-0.3.1-py36-none-any.whl/jord/qgis_utilities/conversion/parsing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+def sanitise_wkb() -> str:
+    ...
+
+
+def sanitise_wkt() -> str:
+    ...
+
+
+def explode_geometry_collection():
+    ...
