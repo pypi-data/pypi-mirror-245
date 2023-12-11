e5c86f7cf418b89e054415a622971690954cb588
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:00 +0000

    Add package pygif-converter-test/1.0.1/pygif_converter_test-1.0.1.tar.gz

---
+++ packages/pygif-converter-test/pygif_converter_test-1.0.1.tar.gz/pygif_converter_test-1.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="pygif_converter_test",
+    version="1.0.1",
+    description="Test package for distribution",
+    author="quasarhub",
+    author_email="quasarhub@gmail.com",
+    url="",
+    download_url="",
+    install_requires=["pillow"],
+    include_package_data=True,
+    packages=find_packages(),
+    keywords=["GIFCONVERTER", "gifconverter"],
+    python_requires=">=3",
+    zip_safe=False,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
