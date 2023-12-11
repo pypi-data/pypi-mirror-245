baa836d66343653be0c1c92f26e45cde6b5f110a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:00 +0000

    Add package unitxt/1.2.0/unitxt-1.2.0-py3-none-any.whl

---
+++ packages/unitxt/unitxt-1.2.0-py3-none-any.whl/unitxt/__init__.py
@@ -0,0 +1,16 @@
+import os
+import random
+
+from . import logging
+from .catalog import add_to_catalog
+from .load import load_dataset
+from .register import register_all_artifacts, register_local_catalog
+
+register_all_artifacts()
+random.seed(0)
+dataset_file = os.path.join(os.path.dirname(__file__), "dataset.py")
+metric_file = os.path.join(os.path.dirname(__file__), "metric.py")
+local_catalog_path = os.path.join(os.path.dirname(__file__), "catalog")
+
+dataset_url = "unitxt/data"
+metric_url = "unitxt/metric"
