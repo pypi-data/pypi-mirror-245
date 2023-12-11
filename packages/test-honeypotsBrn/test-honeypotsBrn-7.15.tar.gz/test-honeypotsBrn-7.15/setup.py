985adb018c281a420c20eb54bea862c11d0655e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:22 +0000

    Add package test-honeypotsBrn/7.15/test-honeypotsBrn-7.15.tar.gz

---
+++ packages/test-honeypotsBrn/test-honeypotsBrn-7.15.tar.gz/test-honeypotsBrn-7.15/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+with open("README.rst", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='test-honeypotsBrn',
+    author='BRi',
+    long_description=long_description,
+    version='7.15',
+    license="AGPL-3.0",
+    license_files=('LICENSE'),
+    url="https://github.com/qeeqbox/honeypots",
+    packages=['honeypots'],
+    entry_points={
+        "console_scripts": [
+            'honeypots=honeypots.__main__:main_logic'
+        ]
+    },
+    include_package_data=True,
+    install_requires=[
+        'twisted==21.7.0',
+        'psutil==5.9.0',
+        'psycopg2-binary==2.9.3',
+        'pycrypto==2.6.1',
+        'requests==2.28.2',
+        'requests[socks]==2.28.2',
+        'impacket==0.9.24',
+        'paramiko==3.1.0',
+        'scapy==2.4.5',
+        'service_identity==21.1.0',
+        'netifaces==0.11.0'
+    ],
+    extras_require={
+        'test': ['redis', 'mysql-connector', 'elasticsearch', 'pymssql', 'ldap3', 'pysnmp']
+    },
+    python_requires='>=3.5'
+)
