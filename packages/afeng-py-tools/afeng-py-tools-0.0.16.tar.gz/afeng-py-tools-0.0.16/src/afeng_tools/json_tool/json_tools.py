4ce2551f340942b8980a8f2bbb850b53a3cb1ec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 15:05:16 +0000

    Add package afeng-py-tools/0.0.16/afeng-py-tools-0.0.16.tar.gz

---
+++ packages/afeng-py-tools/afeng-py-tools-0.0.16.tar.gz/afeng-py-tools-0.0.16/src/afeng_tools/json_tool/json_tools.py
@@ -0,0 +1,59 @@
+import datetime
+
+import json
+from array import array
+from enum import Enum
+
+from _decimal import Decimal
+
+
+class JsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, datetime.datetime):
+            return obj.strftime("%Y-%m-%d %H:%M:%S")
+        elif isinstance(obj, datetime.date):
+            return obj.strftime('%Y-%m-%d')
+        elif isinstance(obj, bytes):
+            return str(obj, encoding='utf-8')
+        elif isinstance(obj, int):
+            return int(obj)
+        elif isinstance(obj, float):
+            return float(obj)
+        elif isinstance(obj, array):
+            return obj.tolist()
+        elif isinstance(obj, Decimal):
+            return str(obj)
+        elif isinstance(obj, Enum):
+            return obj.value
+        else:
+            return super(JsonEncoder, self).default(obj)
+
+
+def to_json(obj_data):
+    """将Python中的对象转换为JSON中的字符串对象"""
+    return json.dumps(obj_data, cls=JsonEncoder, indent=4)
+
+
+def to_obj(str_data):
+    """将JSON中的字符串对象转换为Python中的对象"""
+    return json.loads(str_data)
+
+
+def model_to_dict(model):
+    """Model实例转dict"""
+    model_dict = dict(model.__dict__)
+    del model_dict['_sa_instance_state']
+    return model_dict
+
+
+def model_to_dict2(model):
+    """单个对象转dict(效果等同上面的那个)"""
+    return {c.name: getattr(model, c.name) for c in model.__table__.columns}
+
+
+def model_to_json(model) -> str:
+    """model或model集合转换为json字符串"""
+    if isinstance(model, list):
+        return to_json([model_to_dict2(tmp) for tmp in model])
+    else:
+        return to_json(model_to_dict(model))
