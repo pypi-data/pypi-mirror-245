41def36cf5a78af20b83865eb25c1ab3a4911542
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:43 +0000

    Add package mrx-link/1.2.0/mrx_link-1.2.0-cp39-cp39-win_amd64.whl

---
+++ packages/mrx-link/mrx_link-1.2.0-cp39-cp39-win_amd64.whl/mrx_link/converter/templates/__init__.py
@@ -0,0 +1,21 @@
+#  MAKINAROCKS CONFIDENTIAL
+#  ________________________
+#
+#  [2017] - [2021] MakinaRocks Co., Ltd.
+#  All Rights Reserved.
+#
+#  NOTICE:  All information contained herein is, and remains
+#  the property of MakinaRocks Co., Ltd. and its suppliers, if any.
+#  The intellectual and technical concepts contained herein are
+#  proprietary to MakinaRocks Co., Ltd. and its suppliers and may be
+#  covered by U.S. and Foreign Patents, patents in process, and
+#  are protected by trade secret or copyright law. Dissemination
+#  of this information or reproduction of this material is
+#  strictly forbidden unless prior written permission is obtained
+#  from MakinaRocks Co., Ltd.
+#
+from .kfp_component_template import KfpComponentTemplate
+from .kfp_pipeline_template import KfpPipelineTemplate
+from .pycode_template import PyCodeTemplate
+
+__all__ = ["KfpComponentTemplate", "KfpPipelineTemplate", "PyCodeTemplate"]
