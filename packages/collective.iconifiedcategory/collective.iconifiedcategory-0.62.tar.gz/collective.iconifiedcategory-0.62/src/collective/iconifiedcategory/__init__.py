def3068800a5513614a626c42ebe62aa7da292f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 15:05:28 +0000

    Add package collective.iconifiedcategory/0.62/collective.iconifiedcategory-0.62.tar.gz

---
+++ packages/collective.iconifiedcategory/collective.iconifiedcategory-0.62.tar.gz/collective.iconifiedcategory-0.62/src/collective/iconifiedcategory/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from zope.i18nmessageid import MessageFactory
+
+# enable :json type converter
+import imio.helpers.converters  # noqa
+import logging
+
+
+logger = logging.getLogger('collective.iconifiedcategory')
+
+CAT_SEPARATOR = '_-_'
+CSS_SEPARATOR = '-'
+
+_ = MessageFactory('collective.iconifiedcategory')
