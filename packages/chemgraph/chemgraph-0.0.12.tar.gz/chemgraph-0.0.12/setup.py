37fd08ff11bdb7dc309f888ce75f847a4d235c4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:37 +0000

    Add package chemgraph/0.0.12/chemgraph-0.0.12.tar.gz

---
+++ packages/chemgraph/chemgraph-0.0.12.tar.gz/chemgraph-0.0.12/setup.py
@@ -0,0 +1,17 @@
+# mypy: ignore-errors
+from setuptools import setup
+
+# Read deps
+with open('requirements.txt', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='chemgraph',
+    version='0.0.12',
+    description='Chemical data plotting',
+    author='Eduardo Bogado',
+    author_email='eduardob1999@gmail.com',
+    url='https://github.com/eduardob999/chemgraph',
+    py_modules=['chemgraph', 'chemgraph.plot_module'],
+    install_requires=requirements
+)
