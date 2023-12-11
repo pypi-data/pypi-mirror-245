cdac7b615baea45f0ba768879fa1755b160f08ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:04:48 +0000

    Add package mds-cashbook-report/6.0.9/mds_cashbook_report-6.0.9-py3-none-any.whl

---
+++ packages/mds-cashbook-report/mds_cashbook_report-6.0.9-py3-none-any.whl/trytond/modules/cashbook_report/templates.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# This file is part of the cashbook-module from m-ds for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the
+# full copyright notices and license terms.
+
+cashbook_types = [
+    'cashbooks', 'cashbooks_gldiff', 'cashbooks_glperc',
+    'cashbooks_glvalue', 'cashbooks_glyield']
+category_types = [
+    'categories', 'category_gldiff', 'category_glvalue',
+    'category_glperc', 'category_glyield']
+booktype_types = [
+    'types', 'types_gldiff', 'types_glvalue',
+    'types_glperc', 'types_glyield']
+
+template_view_line = '<field name="balance" fill="%(fill)s" ' + \
+    'empty="0" string="%(string)s"/>'
+
+template_view_graph = """<?xml version="1.0"?>
+<graph type="%(type)s" legend="%(legend)s" %(colscheme)s %(bgcol)s>
+    <x>
+        <field name="name"/>
+    </x>
+    <y>
+        %(lines)s
+    </y>
+</graph>
+"""
