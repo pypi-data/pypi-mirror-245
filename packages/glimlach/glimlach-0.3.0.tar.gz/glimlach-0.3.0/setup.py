727fe5e766175b88763ae9a4fa60a63f05bbe877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:04:58 +0000

    Add package glimlach/0.3.0/glimlach-0.3.0.tar.gz

---
+++ packages/glimlach/glimlach-0.3.0.tar.gz/glimlach-0.3.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glimlach',
+    version='0.3.0',
+    packages=find_packages(),
+    install_requires=[
+        'docker>=4.4.4'
+    ],
+     entry_points={
+        'console_scripts': [
+            'run-docker-images = cli:run_docker_images_main',
+        ],
+    },
+)
