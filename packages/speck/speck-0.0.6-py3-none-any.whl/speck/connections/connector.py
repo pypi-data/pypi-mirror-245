05c0e02b34411516427aa427e1ac1f5e07fc2c49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:06 +0000

    Add package speck/0.0.6/speck-0.0.6-py3-none-any.whl

---
+++ packages/speck/speck-0.0.6-py3-none-any.whl/speck/connections/connector.py
@@ -0,0 +1,28 @@
+from abc import ABC
+
+from ..chat.entities import ChatLogger, Prompt, Response
+from ..logs.logger import universal_format_log
+from .providers import Providers
+
+
+class IConnector(ABC):
+    def __init__(self, provider: Providers):
+        self.provider = provider
+
+    # @abstractmethod
+    # def process_message(self, messages: Messages, model: str) -> str:
+    #     pass
+
+    def _get_log_kwargs(self, prompt: Prompt, response: Response, **kwargs):
+        return {
+            "provider": self.provider,
+            "prompt": prompt,
+            "config": kwargs,
+            "response": response,
+        }
+
+    def log(self, prompt: Prompt, model: str, response: Response, **kwargs):
+        ChatLogger.log(**self._get_log_kwargs(prompt, response, **kwargs))
+
+    def __str__(self):
+        return f"Client({self.provider.value})"
