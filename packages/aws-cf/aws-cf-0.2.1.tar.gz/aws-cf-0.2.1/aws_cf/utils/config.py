375915d3e9a32a5dccfa70c88efda192a81826c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:46 +0000

    Add package aws-cf/0.2.1/aws-cf-0.2.1.tar.gz

---
+++ packages/aws-cf/aws-cf-0.2.1.tar.gz/aws-cf-0.2.1/aws_cf/utils/config.py
@@ -0,0 +1,52 @@
+from typing import List
+from pydantic import BaseModel
+import yaml
+import os
+from .context import Context
+
+class Stack(BaseModel):
+    path: str
+    name: str
+
+    @property
+    def _path(self):
+        return self.path.replace("$root", Context.get_root())
+        
+    @property
+    def _yml(self):
+        class SafeLoaderIgnoreUnknown(yaml.SafeLoader):
+            def ignore_unknown(self, node):
+                return None 
+
+        SafeLoaderIgnoreUnknown.add_constructor(None, SafeLoaderIgnoreUnknown.ignore_unknown)
+
+        return yaml.load(open(self._path).read(), Loader=SafeLoaderIgnoreUnknown)
+
+    @property
+    def resources(self):
+        return self._yml.get("Resources", {})
+
+class Enviroment(BaseModel):
+    name: str
+    profile: str
+    region: str
+    artifacts: str
+
+
+
+class Config(BaseModel):
+    Enviroments: List[Enviroment]
+    Stacks: List[Stack]
+
+    @staticmethod
+    def parse(path: str = None):
+        try:
+            data = yaml.safe_load(open(path))
+            return Config(**data)
+        except:
+            raise IOError("Not able to find file at path: " + '"' + path + '"')
+
+    def setup_env(self, env=None):
+        if not env:
+            os.environ["AWS_PROFILE"] = self.Enviroments[0].profile
+            os.environ["AWS_DEFAULT_REGION"] = self.Enviroments[0].region
