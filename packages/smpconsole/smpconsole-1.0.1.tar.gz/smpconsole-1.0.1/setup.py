ea99a5a64dcd6cd4e88365f5926dee6f48ad7808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 15:04:32 +0000

    Add package smpconsole/1.0.1/smpconsole-1.0.1.tar.gz

---
+++ packages/smpconsole/smpconsole-1.0.1.tar.gz/smpconsole-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='smpconsole',
+    version='1.0.1',
+    author='khiemndd',
+    description='best console experience',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    url = 'https://github.com/khimnguynnn/smpConsole',
+    install_requires=[
+        'colorama'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.0',
+)
