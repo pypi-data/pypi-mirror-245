abab1dc6d13ea851e285f9741e6d0b37bf8dae96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:40 +0000

    Add package mymx/0.0.2/mymx-0.0.2.tar.gz

---
+++ packages/mymx/mymx-0.0.2.tar.gz/mymx-0.0.2/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(name='mymx',
+      version='0.0.2',
+      description='just test python pip module',
+      url='https://github.com/huangweibbk001/mymx',
+      author='huangweigang',
+      author_email='huangweibbk001@googlemail.com',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
