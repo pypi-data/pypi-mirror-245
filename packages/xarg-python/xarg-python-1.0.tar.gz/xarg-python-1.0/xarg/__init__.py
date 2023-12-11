23549972e4bf2ffb069515ffe049dd8e70079a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:22 +0000

    Add package xarg-python/1.0/xarg-python-1.0.tar.gz

---
+++ packages/xarg-python/xarg-python-1.0.tar.gz/xarg-python-1.0/xarg/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+from .util import __version__
+
+from .actuator import Namespace
+from .actuator import commands
+from .actuator import add_command
+from .actuator import run_command
+
+from .parser import argp
+
+from .scanner import scanner
---
+++ packages/xarg-python/xarg-python-1.0.tar.gz/xarg-python-1.0/xarg/util.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# coding=utf-8
+
+__package_name__ = "xarg-python"
+__prog_name__ = "xarg"
+__prog_complete__ = "xargcomplete"
+__version__ = "1.0"
+
+URL_PROG = "https://github.com/bondbox/xarg-python"
+
+
+def singleton(cls):
+    instance = {}
+
+    def _singleton_wrapper(*args, **kwargs):
+        if cls not in instance:
+            instance[cls] = cls(*args, **kwargs)
+        return instance[cls]
+
+    return _singleton_wrapper
