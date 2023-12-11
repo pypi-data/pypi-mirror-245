69115980f841432662c930718a494ab30c916a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:37 +0000

    Add package CheckListProce/0.0.4/CheckListProce-0.0.4.tar.gz

---
+++ packages/CheckListProce/CheckListProce-0.0.4.tar.gz/CheckListProce-0.0.4/CheckListProce/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  8 15:00:28 2023
+
+@author: QianYang
+"""
+
+from CheckListProce import CheckListProce 
+from CRFCheckListCreator import CRFCheckListCreator 
