e025b8238c95ba79c842e460db3e9ae7f8168006
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 15:05:12 +0000

    Add package prstat/0.5.1/prstat-0.5.1.tar.gz

---
+++ packages/prstat/prstat-0.5.1.tar.gz/prstat-0.5.1/README.md
@@ -0,0 +1,8 @@
+# Your Package Name
+
+## 설명
+
+부산대학교 확률과 통계 수업을 위한 도구
+
+## 설치 방법
+pip install your-package-name
---
+++ packages/prstat/prstat-0.5.1.tar.gz/prstat-0.5.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prstat',
+    version='0.5.1',
+    license='MIT',
+    url='https://github.com/karinysis/prstat',
+    author='youngjae lee',
+    author_email='leeyoungjae@pusan.ac.kr',
+    description='tools for pnu probabilities and statistics class',
+    packages=find_packages(),    
+    install_requires=[],
+)
