9a4bcd706015ea937dc808ce67467327f417188b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:04:58 +0000

    Add package pypi-templates/0.1.8/pypi_templates-0.1.8.tar.gz

---
+++ packages/pypi-templates/pypi_templates-0.1.8.tar.gz/pypi_templates-0.1.8/README.md
@@ -0,0 +1,2 @@
+# pypi_templates (pyproject-setuptools)
+python package template for publish to pypi
---
+++ packages/pypi-templates/pypi_templates-0.1.8.tar.gz/pypi_templates-0.1.8/tests/test_main.py
@@ -0,0 +1,5 @@
+from pypi_templates.main import add_one
+
+
+def test_add_one():
+    assert add_one(3) == 4
