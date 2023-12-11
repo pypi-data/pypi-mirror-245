bb7200497050e97128d2b4707e336261b773b130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:46 +0000

    Add package quanmsms/0.0.4/quanmsms-0.0.4.tar.gz

---
+++ packages/quanmsms/quanmsms-0.0.4.tar.gz/quanmsms-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+setup(
+    name = "quanmsms",
+    version = "0.0.4",
+    packages=find_packages(),
+    description="发送短信,dev.quanmwl.com/ability_sms --> Python版SDK",
+    url="https://gitee.com/chengdu-quanming-network/quanmsms-python",
+    author="qiu peng",
+    author_email="qp@quanmwl.com",
+)
