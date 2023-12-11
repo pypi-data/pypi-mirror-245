8d57faa42b361a81bfa308e0e90eac450bfa021f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:43 +0000

    Add package disease-detection/0.1.8/disease_detection-0.1.8.tar.gz

---
+++ packages/disease-detection/disease_detection-0.1.8.tar.gz/disease_detection-0.1.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='disease_detection',
+    version='0.1.8',
+    packages=find_packages(),
+    package_data={'disease_detection': ['datasets/cancer.csv', 'datasets/diabetes.csv']},
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'pandas',
+        'scikit-learn',
+        'seaborn',
+        'pytest',
+        'pkg_resources'
+    ],
+    
+    author='Niraj Prasad',
+    author_email='nirajsaiprasad@gmail.com',
+    description='A package for disease detection',
+    #long_description=open('README.md').read(),
+    #long_description_content_type='text/markdown',
+    url='https://github.com/nirajprasad12/ds5010-disease-detection',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
