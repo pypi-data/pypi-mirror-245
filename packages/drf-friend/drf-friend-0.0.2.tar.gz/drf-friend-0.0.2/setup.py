82d029212e08e92f43956995ce75d7ed9ddcf6fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:38 +0000

    Add package drf-friend/0.0.2/drf-friend-0.0.2.tar.gz

---
+++ packages/drf-friend/drf-friend-0.0.2.tar.gz/drf-friend-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drf-friend',
+    version='0.0.2',
+    author='Mostafa Kamal',
+    author_email='hiremostafa@gmail.com',
+    description='A description of your package',
+    long_description='A longer description if needed',
+    url='https://github.com/code4mk/drf-friend',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
