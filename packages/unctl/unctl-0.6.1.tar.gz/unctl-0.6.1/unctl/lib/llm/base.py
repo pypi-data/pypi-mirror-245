191702861b832f249b915ced811cb04623cf74d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:04:46 +0000

    Add package unctl/0.6.1/unctl-0.6.1.tar.gz

---
+++ packages/unctl/unctl-0.6.1.tar.gz/unctl-0.6.1/unctl/lib/llm/base.py
@@ -0,0 +1,39 @@
+from abc import ABC, abstractmethod
+
+
+class LanguageModel(ABC):
+    @abstractmethod
+    async def initiate_session(self, data: list[str] | None = None) -> str:
+        """
+        This function is used to start LLM session
+
+        Returns:
+            str: identifier of LLM context
+        """
+        pass
+
+    @abstractmethod
+    async def push_info(self, session_id: str, data: str):
+        """
+        Add additional information to session context
+
+        Args:
+            session_id (str): identifier of LLM context
+            data (str): additional info needed to describe problem
+        """
+        pass
+
+    @abstractmethod
+    async def get_recommendation(
+        self, session_id: str, instructions: str | None = None
+    ) -> str:
+        """
+        Requests LLM to get recommendations based on provided information
+
+        Args:
+            session_id (str): identifier of LLM context
+
+        Returns:
+            str: contains general recommendations based on provided information
+        """
+        pass
