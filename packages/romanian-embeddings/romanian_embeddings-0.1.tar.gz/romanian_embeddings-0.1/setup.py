cc25d26825374751f6df4e733031a471c960c97f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:40 +0000

    Add package romanian-embeddings/0.1/romanian_embeddings-0.1.tar.gz

---
+++ packages/romanian-embeddings/romanian_embeddings-0.1.tar.gz/romanian_embeddings-0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='romanian_embeddings',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'transformers',
+        'numpy',
+        'onnxruntime',
+        'multiprocessing',
+        'queue'
+    ],
+    author='Mihai Ilie',
+    author_email='ilie.mihai92@gmail.com',
+    description='Romanian language embeddings using transformers and ONNX.',
+    keywords='nlp transformers onnx romanian embeddings',
+)
+
