ecd9cbdf24386c33f6fc99f4bccb72fe14a302c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:37 +0000

    Add package llm-loop/0.1/llm-loop-0.1.tar.gz

---
+++ packages/llm-loop/llm-loop-0.1.tar.gz/llm-loop-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='llm-loop',
+    version='0.01',
+    packages=find_packages(),
+    description='A utility package for querying language models with pattern matching and retry logic',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'ctransformers',
+    ],
+    url='https://github.com/chigwell/llm-loop',
+    author='Evgenii Evstafev',
+    author_email='chigwel@gmail.com',
+)
