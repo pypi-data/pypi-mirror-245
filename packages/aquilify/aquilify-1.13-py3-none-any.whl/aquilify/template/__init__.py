61dd93d80131853ba8005462511dddbecf103268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:04:49 +0000

    Add package aquilify/1.13/aquilify-1.13-py3-none-any.whl

---
+++ packages/aquilify/aquilify-1.13-py3-none-any.whl/aquilify/template/__init__.py
@@ -0,0 +1,9 @@
+
+# refer @noql -> 5391
+
+from .templates import (
+    Template as Template,
+    TemplateError as TemplateError,
+    TemplateNotFound as TemplateNotFound,
+    TemplateResponse as TemplateResponse
+)
