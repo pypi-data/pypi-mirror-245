b8edf691e7c24c288f470c052d9bedbabb55dd82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:04:25 +0000

    Add package CRFActions/1.8/CRFActions-1.8.tar.gz

---
+++ packages/CRFActions/CRFActions-1.8.tar.gz/CRFActions-1.8/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 00:07:44 2023
+
+@author: Stacey.Yang
+"""
+from setuptools import setup
+
+setup(
+    name='CRFActions',
+    version='1.8',
+    scripts=['CheckListProce.py'],
+)
