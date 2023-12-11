35b735fa1fd78be4b58793dfa8be9af258d2e50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:36 +0000

    Add package Jord/0.3.0/Jord-0.3.0-py36-none-any.whl

---
+++ packages/Jord/Jord-0.3.0-py36-none-any.whl/jord/shapely_utilities/rings.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from shapely import LinearRing
+
+__all__ = [
+    "ensure_ccw_ring",
+    "ensure_cw_ring",
+    "make_projected_ring",
+    "make_extruded_ring",
+]
+
+
+def ensure_ccw_ring(ring: LinearRing) -> LinearRing:
+    if not ring.is_ccw:
+        return LinearRing(list(ring.coords)[::-1])
+    return ring
+
+
+def ensure_cw_ring(ring: LinearRing) -> LinearRing:
+    if ring.is_ccw:
+        return LinearRing(list(ring.coords)[::-1])
+    return ring
+
+
+from typing import Iterable, Union
+
+from shapely import MultiLineString, LineString, Point, LinearRing
+
+from jord.shapely_utilities.projection import (
+    project_point_to_line,
+    line_line_intersection,
+)
+
+
+def make_projected_ring(
+    lines: Union[MultiLineString, Iterable[LineString]], ccw: bool = True
+) -> LinearRing:
+    points = []
+
+    if isinstance(lines, MultiLineString):
+        lines = lines.geoms
+
+    num_lines = len(lines)
+
+    if ccw:
+        lines = lines[::-1]
+
+    for n in range(num_lines):
+        points.append(project_point_to_line(Point(lines[n - 1].coords[-1]), lines[n]))
+
+    ring = LinearRing(points)
+
+    assert ring.is_closed
+    assert ring.is_ring
+
+    return ring
+
+
+def make_extruded_ring(
+    lines: Union[MultiLineString, Iterable[LineString]], ccw: bool = True
+) -> LinearRing:
+    points = []
+
+    if isinstance(lines, MultiLineString):
+        lines = lines.geoms
+
+    num_lines = len(lines)
+
+    if ccw:
+        lines = lines[::-1]
+
+    for n in range(num_lines):
+        points.append(Point(line_line_intersection(lines[n - 1], lines[n]).coords[-1]))
+
+    ring = LinearRing(points)
+
+    assert ring.is_closed
+    assert ring.is_ring
+
+    return ring
+
+
+if __name__ == "__main__":
+
+    def juijh():
+        r"""
+
+            0   1   2
+
+        0   0---0   0
+                    |
+        1   0       0
+            |
+        2   0   0---0
+
+        to become
+
+
+            0   1   2
+
+        0   0---0---0
+            |       |
+        1   0       0
+            |       |
+        2   0---0---0
+
+        :return:
+        """
+
+        lines = [
+            LineString([[0, 0], [1, 0]]),
+            LineString([[2, 0], [2, 1]]),
+            LineString([[2, 2], [1, 2]]),
+            LineString([[0, 2], [0, 1]]),
+        ]
+
+        print(make_extruded_ring(lines))
+
+    def juijh2():
+        r"""
+
+            0   1   2   3
+
+        0   0---0---0---0
+                    |
+        1   0       0
+            |
+        2   0   0---0
+
+        to become
+
+
+            0   1   2
+
+        0   0---0---0
+            |       |
+        1   0       0
+            |       |
+        2   0---0---0
+
+        :return:
+        """
+
+        lines = [
+            LineString([[0, 0], [3, 0]]),
+            LineString([[2, 0], [2, 1]]),
+            LineString([[2, 2], [1, 2]]),
+            LineString([[0, 2], [0, 1]]),
+        ]
+
+        print(make_extruded_ring(lines))
+
+    juijh2()
