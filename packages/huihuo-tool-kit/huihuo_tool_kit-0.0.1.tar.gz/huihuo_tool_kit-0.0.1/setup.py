cc07dbd1d0cfb40b102804babf162098843f9c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 15:04:43 +0000

    Add package huihuo-tool-kit/0.0.1/huihuo_tool_kit-0.0.1.tar.gz

---
+++ packages/huihuo-tool-kit/huihuo_tool_kit-0.0.1.tar.gz/huihuo_tool_kit-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='huihuo_tool_kit',  # 项目名称
+    description="huihuo tool kit",  # 项目描述
+    url='https://github.com/hui-huo',  # 项目主页
+    version='0.0.1',  # 版本
+    author='huihuo',  # 作者
+    # packages=['huihuo_tool_kit'],# 项目包列表
+    packages=find_packages(),  # 自动查找包
+    install_requires=[  # 项目依赖
+        "build"
+    ],
+    entry_points={  # 入口点
+        'console_scripts': [  # 脚本入口
+            'excel2case = huihuo_tool_kit.excel_tool:ExcelTool.excel2case',  # 命令名=包名.模块名:类名.函数名
+        ],
+    }
+)
