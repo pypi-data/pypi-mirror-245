48e1bf475194bbd2388b7129f92a2ea87aba523f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 14:08:08 +0000

    Add package charidotella/3.1.0/charidotella-3.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/charidotella/charidotella-3.1.0-cp312-cp312-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/charidotella/tasks/colourtime.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+import pathlib
+import typing
+
+import colourtime
+import event_stream
+import matplotlib
+import matplotlib.colors
+import PIL.Image
+
+from .. import formats
+
+EXTENSION = ".png"
+
+
+def run(
+    input: pathlib.Path,
+    output: pathlib.Path,
+    begin: int,
+    end: int,
+    parameters: dict[str, typing.Any],
+):
+    if "cycle" in parameters:
+        time_mapping = colourtime.generate_cyclic_time_mapping(
+            duration=parameters["cycle"],
+            begin=begin,
+        )
+    else:
+        time_mapping = colourtime.generate_linear_time_mapping(begin=begin, end=end)
+    with formats.Decoder(input) as decoder:
+        image = colourtime.convert(
+            begin=begin,
+            end=end,
+            width=decoder.width,
+            height=decoder.height,
+            decoder=decoder,  # type: ignore
+            colormap=matplotlib.colormaps[parameters["colormap"]],  # type: ignore
+            time_mapping=time_mapping,
+            alpha=parameters["alpha"],
+            background_colour=matplotlib.colors.to_rgba(parameters["background_color"]),  # type: ignore
+        )
+        image.resize(
+            size=(
+                image.width * parameters["scale"],
+                image.height * parameters["scale"],
+            ),
+            resample=PIL.Image.Resampling.NEAREST,  # type: ignore
+        ).save(
+            str(output),
+            compress_level=parameters["png_compression_level"],
+            format="png",
+        )
