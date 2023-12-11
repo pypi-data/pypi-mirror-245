16b958e3965f126bf00c0811cade7671c0f196ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:17 +0000

    Add package glimlach/0.4.1/glimlach-0.4.1.tar.gz

---
+++ packages/glimlach/glimlach-0.4.1.tar.gz/glimlach-0.4.1/run_docker_images/__main__.py
@@ -0,0 +1,5 @@
+# __main__.py
+from run_docker_images.cli import run_docker_images_main
+
+if __name__ == "__main__":
+    run_docker_images_main("path/to/your_config_file.json")
---
+++ packages/glimlach/glimlach-0.4.1.tar.gz/glimlach-0.4.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glimlach',
+    version='0.4.1',
+    description='Glimlach is a project that allows you to easily run Docker images based on a configuration file.',
+    long_description='''\
+   Glimlach is a cybersecurity automation tool designed to streamline the execution of multiple open-source security tools in a single scan. The project is initiated to support Rauli's research, enabling automated complex security scans with commonly used tools like Nmap, Testssl.sh, and Ssh-audit. This tool is a Python script that allows you to run Docker tools in parallel based on a JSON configuration file. It is designed to make it easy to automate the execution of Docker images with customizable parameters.
+
+The primary objective is to facilitate the execution of various cybersecurity tools as a single scan without the need for individual installations and configurations. Glimlach focuses on the containerized execution (OCI) of tools, allowing for isolation and reproducibility.
+    ''',
+    author="'Emmanuel Ikwunna', 'Arttu Juntunen', 'Piyumi Weebadu Arachchige'",
+    url='https://github.com/firstnuel/Glimlach',
+    packages=find_packages(),
+    install_requires=[
+        'docker>=4.4.4'
+    ],
+     entry_points={
+        'console_scripts': [
+            'run-docker-images = run_docker_images.cli:run_docker_images_main',
+        ],
+    },
+)
