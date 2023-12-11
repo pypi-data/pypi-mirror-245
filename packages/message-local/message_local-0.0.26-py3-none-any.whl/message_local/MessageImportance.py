295271d2cfe97271858bc7556440220438cd5e19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 04:04:47 +0000

    Add package message-local/0.0.26/message_local-0.0.26-py3-none-any.whl

---
+++ packages/message-local/message_local-0.0.26-py3-none-any.whl/message_local/MessageImportance.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+#from .MessageConstants import logger
+
+class MessageImportance(Enum):
+    """Enum class"""
+    #logger.start()
+    LOW = 10
+    MEDIUM = 20
+    HIGH = 30
+    #logger.end()
