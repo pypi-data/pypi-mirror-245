448534c983d7eaa164dc02fd2adb1107c9c108ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 03:05:40 +0000

    Add package antarctic-plots/0.1.0/antarctic_plots-0.1.0-py3-none-any.whl

---
+++ packages/antarctic-plots/antarctic_plots-0.1.0-py3-none-any.whl/antarctic_plots/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022 The Antarctic-Plots Developers.
+# Distributed under the terms of the MIT License.
+# SPDX-License-Identifier: MIT
+#
+# This code is part of the package:
+# Antarctic-plots (https://github.com/mdtanker/antarctic_plots)
+#
+"""Functions to automate Antarctic data visualization"""
+
+from __future__ import annotations
+
+__version__ = "0.1.0"
