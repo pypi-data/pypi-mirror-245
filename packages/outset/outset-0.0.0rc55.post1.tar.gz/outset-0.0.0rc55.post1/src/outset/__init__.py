125f26c71141766e70fe355d4319070b3a228d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 15:05:19 +0000

    Add package outset/0.0.0rc55.post1/outset-0.0.0rc55.post1.tar.gz

---
+++ packages/outset/outset-0.0.0rc55.post1.tar.gz/outset-0.0.0rc55.post1/src/outset/__init__.py
@@ -0,0 +1,17 @@
+"""Outset Python Package"""
+from __future__ import annotations
+
+__version__ = "0.0.0""-rc55-post1"
+
+from .draw_outset_ import draw_outset
+from .mark_arrow_ import mark_arrow
+from .mark_inlaid_asterisk_ import mark_inlaid_asterisk
+from .mark_magnifying_glass_ import mark_magnifying_glass
+
+__all__ = [
+    "draw_outset",
+    "mark_arrow",
+    "mark_inlaid_asterisk",
+    "mark_magnifying_glass",
+    "outsetplot",
+]
