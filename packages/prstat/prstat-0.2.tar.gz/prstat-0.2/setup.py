e865546e2a495cc746f276ebf4ae62ae2b91605c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 15:04:45 +0000

    Add package prstat/0.2/prstat-0.2.tar.gz

---
+++ packages/prstat/prstat-0.2.tar.gz/prstat-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prstat',
+    version='0.2',
+    url='https://github.com/karinysis/prstat',
+    author='youngjae lee',
+    author_email='leeyoungjae@pusan.ac.kr',
+    description='tools for pnu probabilities and statistics class',
+    packages=find_packages(),    
+    install_requires=[],
+)
