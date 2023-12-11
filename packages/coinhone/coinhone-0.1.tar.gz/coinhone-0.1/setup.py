93f81489f407ee90517fac34240f59af5f445b75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 15:05:30 +0000

    Add package coinhone/0.1/coinhone-0.1.tar.gz

---
+++ packages/coinhone/coinhone-0.1.tar.gz/coinhone-0.1/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='coinhone',
+    version='0.1',
+    author='wade',
+    author_email='weishadev@gmail.com',
+    description='coinhone of lucky',
+    packages=find_packages()
+)
