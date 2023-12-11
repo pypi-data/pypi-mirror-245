d7c501d798b705be9f59efb242d0cdbc0fc2667b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-11 04:04:37 +0000

    Add package spopt/0.6.0/spopt-0.6.0-py3-none-any.whl

---
+++ packages/spopt/spopt-0.6.0-py3-none-any.whl/spopt/tests/test_spenc.py
@@ -0,0 +1,37 @@
+import geopandas
+import libpysal
+import numpy
+
+from spopt.region import Spenc
+
+
+# Empirical tests
+RANDOM_STATE = 123456
+# -- Mexican states
+pth = libpysal.examples.get_path("mexicojoin.shp")
+MEXICO = geopandas.read_file(pth)
+
+
+class TestSpenc:
+    def setup_method(self):
+        # Mexico
+        self.mexico = MEXICO.copy()
+        self.w_mexico = libpysal.weights.Queen.from_dataframe(self.mexico)
+        self.default_attrs_mexico = [f"PCGDP{year}" for year in range(1950, 2010, 10)]
+        self.non_default_mexico = [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 2]
+        self.non_default_mexico += [1, 2, 2, 2, 1, 2, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2]
+
+    def test_spenc_non_defaults(self):
+        self.mexico["count"] = 1
+        numpy.random.seed(RANDOM_STATE)
+        model = Spenc(
+            self.mexico,
+            self.w_mexico,
+            self.default_attrs_mexico,
+            random_state=RANDOM_STATE,
+            gamma=0,
+            n_clusters=3,
+        )
+        model.solve()
+
+        numpy.testing.assert_equal(model.labels_, self.non_default_mexico)
