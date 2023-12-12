fdcd8177499eba9a6df14393db4cd81d0c83ebcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:04:34 +0000

    Add package crocodile/13.7/crocodile-13.7-py3-none-any.whl

---
+++ packages/crocodile/crocodile-13.7-py3-none-any.whl/crocodile/__init__.py
@@ -0,0 +1,13 @@
+
+"""Info
+"""
+
+__version__ = "13.7"
+__author__ = "Alex Al-Saffar"
+__date__ = "2023-12-12"
+docs = r"https://crocodile.readthedocs.io/en/latest/"
+git = r"https://github.com/thisismygitrepo/crocodile"
+pypi = r"https://pypi.org/project/crocodile"
+release_notes = """
+write_text now creates the file if it doesn't exist.
+"""
