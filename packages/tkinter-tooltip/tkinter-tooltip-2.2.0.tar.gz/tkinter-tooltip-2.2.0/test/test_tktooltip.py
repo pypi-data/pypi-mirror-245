7e0fd232a2a7c1a882d964467b176029e39db5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 05:05:04 +0000

    Add package tkinter-tooltip/2.2.0/tkinter-tooltip-2.2.0.tar.gz

---
+++ packages/tkinter-tooltip/tkinter-tooltip-2.2.0.tar.gz/tkinter-tooltip-2.2.0/test/test_tktooltip.py
@@ -0,0 +1,81 @@
+import tkinter as tk
+from typing import Callable
+
+import pytest
+
+from tktooltip import ToolTip
+
+
+@pytest.fixture()
+def widget():
+    widget = tk.Entry()
+    widget.pack()
+    widget.update()
+    yield widget
+    widget.destroy()
+
+
+def test_tooltip(widget: tk.Widget):
+    tooltip = ToolTip(
+        widget,
+        msg="Test Tooltip",
+        delay=0.5,
+        follow=False,
+        refresh=0.5,
+        x_offset=20,
+        y_offset=20,
+        parent_kwargs={"bg": "red", "padx": 2, "pady": 2},
+        fg="white",
+        bg="red",
+        font=("Helvetica", 12),
+    )
+
+    assert tooltip.msg == "Test Tooltip"
+    assert tooltip.delay == 0.5
+    assert not tooltip.follow
+    assert tooltip.refresh == 0.5
+    assert tooltip.x_offset == 20
+    assert tooltip.y_offset == 20
+
+
+@pytest.mark.parametrize(
+    "msg",
+    [
+        1,
+        (["text 1", 2]),
+    ],
+)
+def test_tooltip_exceptions(
+    widget: tk.Widget,
+    msg: str | list[str] | Callable[[], str | list[str]],
+):
+    with pytest.raises(TypeError):
+        ToolTip(widget, msg=msg)  # type: ignore
+
+
+def test_tooltip_follow(widget: tk.Widget):
+    tooltip = ToolTip(widget, msg="Test", follow=False)
+    widget.event_generate("<Enter>")
+    assert not tooltip.follow
+
+
+@pytest.mark.parametrize(
+    "msg",
+    [
+        (lambda: "Callable Tooltip"),
+        "text",
+        (["text 1", "text 2"]),
+    ],
+)
+def test_tooltip_show(
+    widget: tk.Widget,
+    msg: str | list[str] | Callable[[], str | list[str]],
+):
+    tooltip = ToolTip(widget, msg=msg)
+    assert tooltip.status == "outside"
+    widget.event_generate("<Enter>")
+    assert tooltip.status == "inside"
+    tooltip._show()
+    assert tooltip.status == "visible"
+    widget.event_generate("<Leave>")
+    assert tooltip.status == "outside"
