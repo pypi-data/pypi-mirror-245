3e0ef2bb7aed99036ed2f0d380f1bbd386d49205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:09 +0000

    Add package neumai-tools/0.0.19/neumai_tools-0.0.19.tar.gz

---
+++ packages/neumai-tools/neumai_tools-0.0.19.tar.gz/neumai_tools-0.0.19/neumai_tools/SemanticHelpers/__init__.py
@@ -0,0 +1,4 @@
+#Semantic Helpers
+from .semantic_chunking import semantic_chunking_code, semantic_chunking
+from .semantic_metadata import fields_for_metadata, fields_to_embed
+from .semantic_retrieval import metadata_attributes_for_retrieval
