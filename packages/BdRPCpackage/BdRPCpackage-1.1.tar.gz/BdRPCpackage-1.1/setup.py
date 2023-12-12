b196c461e2a9b6c58849c128dcdc37b81bcdba97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:05:00 +0000

    Add package BdRPCpackage/1.1/BdRPCpackage-1.1.tar.gz

---
+++ packages/BdRPCpackage/BdRPCpackage-1.1.tar.gz/BdRPCpackage-1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='BdRPCpackage',
+    version='1.1',
+    author='mabin',
+    author_email='595470377@qq.com',
+    description='Phylogenetic new sample placement software.',
+    long_description='./README.md',
+    url='https://github.com/Bin-Ma/bd-rpc'
+    #python_requires= '>=3.6',
+    #install_requires=['biopython','pandas','numpy','scipy','scikit-learn','rpy2'],
+
+)
+
+import BdRPCpackage
