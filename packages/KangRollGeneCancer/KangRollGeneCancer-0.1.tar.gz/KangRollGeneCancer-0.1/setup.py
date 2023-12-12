482507a668fe58e41a51ee621281b52422d32cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:04:46 +0000

    Add package KangRollGeneCancer/0.1/KangRollGeneCancer-0.1.tar.gz

---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.1.tar.gz/KangRollGeneCancer-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="KangRollGeneCancer",
+    version="0.1",
+    author="Your Name",
+    description="A brief description of your package",
+    packages=find_packages(),
+    install_requires=[
+        # 依赖项列表，例如 'numpy>=1.18.5'
+    ],
+)
