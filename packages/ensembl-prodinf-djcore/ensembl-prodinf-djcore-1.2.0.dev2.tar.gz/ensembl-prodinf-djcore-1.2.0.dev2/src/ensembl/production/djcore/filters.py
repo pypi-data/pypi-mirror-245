e26a84b9b879de10bc7efbe60c8836af96a1fcb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 03:08:16 +0000

    Add package ensembl-prodinf-djcore/1.2.0.dev2/ensembl-prodinf-djcore-1.2.0.dev2.tar.gz

---
+++ packages/ensembl-prodinf-djcore/ensembl-prodinf-djcore-1.2.0.dev2.tar.gz/ensembl-prodinf-djcore-1.2.0.dev2/src/ensembl/production/djcore/filters.py
@@ -0,0 +1,29 @@
+# See the NOTICE file distributed with this work for additional information
+#   regarding copyright ownership.
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#       http://www.apache.org/licenses/LICENSE-2.0
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+from django.contrib.admin import SimpleListFilter
+
+
+class BackEndListFilter(SimpleListFilter):
+    _lookups = ()
+
+    def lookups(self, request, model_admin):
+        return self._lookups
+
+    def choices(self, cl):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': self.value() == lookup,
+                'query_string': cl.get_query_string({
+                    self.parameter_name: lookup,
+                }, []),
+                'display': title,
+            }
