d47d551ef932dc8be9b7a2dcb087c0aff38e9c65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 06:04:36 +0000

    Add package pysdcp-cteillet/0.0.4/pysdcp_cteillet-0.0.4.tar.gz

---
+++ packages/pysdcp-cteillet/pysdcp_cteillet-0.0.4.tar.gz/pysdcp_cteillet-0.0.4/test.py
@@ -0,0 +1,27 @@
+from pysdcp_cteillet import *
+
+if __name__ == '__main__':
+    # b = Projector()
+    # b.find_projector(timeout=1)
+    b = Projector("192.168.1.11")
+    print(b.list_inputs())
+    # b.set_HDMI_input(1)
+    # print(b.get_power())
+    # print(b.get_aspect_ratio())
+    # print(b.get_lamp_timer())
+    # b.set_aspect_ratio('NORMAL')
+    # print(b.get_aspect_ratio())
+    # print(b.get_selected_input())
+    # print(b.get_calibrated_preset())
+    # print(b.get_lamp_control())
+    # b.set_aspect_ratio('STRETCH')
+    # b.get_aspect_ratio()
+    # print(b.get_power())
+    # # #
+    # print(b.set_power(False))
+    # # import time
+    # # time.sleep(7)
+    # print (b.set_HDMI_input(1))
+    # # time.sleep(7)
+    # # print (b.set_HDMI_input(2))
+    pass
