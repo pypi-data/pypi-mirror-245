3fd5348c2d1a0a4d3f825f45cb9808b8f65d323f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:05:20 +0000

    Add package breeze-historical-options/1.0/breeze-historical-options-1.0.tar.gz

---
+++ packages/breeze-historical-options/breeze-historical-options-1.0.tar.gz/breeze-historical-options-1.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+# Function to read the contents of the requirements file
+def read_requirements():
+    with open('requirements.txt', encoding='utf-8') as f:
+        return [line.strip() for line in f if line.strip()]
+    
+    
+setup(
+    name='breeze-historical-options',
+    version='1.0',
+    author='Mayank Rai',
+    author_email='mrai748@gmail.com',
+    description='A Python package for easily downloading historical options data',
+    long_description='A Python package for easily downloading second-level historical options data',
+    url='https://github.com/madmay247/breeze-historical-options',
+    packages=find_packages(),
+    install_requires=read_requirements(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.9',
+    )
