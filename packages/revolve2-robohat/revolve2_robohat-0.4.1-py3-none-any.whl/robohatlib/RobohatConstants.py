6c78e5b55fdcecb85b70a94a1b735f46a35f977c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:05:09 +0000

    Add package revolve2-robohat/0.4.1/revolve2_robohat-0.4.1-py3-none-any.whl

---
+++ packages/revolve2-robohat/revolve2_robohat-0.4.1-py3-none-any.whl/robohatlib/RobohatConstants.py
@@ -0,0 +1,17 @@
+"""!
+Robohatlib (2022-2823-01)
+Copyright © 2023 Vrije Universiteit Amsterdam
+Electronica-Beta-VU
+A. Denker (a.denker@vu.nl)
+
+This file consists of the Constants for the Robohat lib
+do not alter
+"""
+
+ROBOHAT_LIB_VERSION_STR = "0.4.3"                    # version nr of library
+ROBOHAT_BUILD_DATE_STR = "7-dec-2023"               # build date of library YYMMDD
+
+
+"""
+0.4.0 was from 30-aug-2023
+"""
