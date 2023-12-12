2921461108d242d210a3128e1e8cb25d6d447341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 03:05:49 +0000

    Add package blissdata/1.0.0/blissdata-1.0.0-py3-none-any.whl

---
+++ packages/blissdata/blissdata-1.0.0-py3-none-any.whl/blissdata/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of the bliss project
+#
+# Copyright (c) 2015-2023 Beamline Control Unit, ESRF
+# Distributed under the GNU LGPLv3. See LICENSE for more info.
+__version__ = "1.0.0"
