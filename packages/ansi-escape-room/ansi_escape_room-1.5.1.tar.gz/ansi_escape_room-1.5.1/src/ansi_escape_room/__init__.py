4e984ecafca12d644aef90c790c468fce88fb55a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:05:01 +0000

    Add package ansi-escape-room/1.5.1/ansi_escape_room-1.5.1.tar.gz

---
+++ packages/ansi-escape-room/ansi_escape_room-1.5.1.tar.gz/ansi_escape_room-1.5.1/src/ansi_escape_room/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from __future__ import print_function
+
+from .back import *  # noqa: F401, F403
+from .colored import *  # noqa: F401, F403
+from .fore import *  # noqa: F401, F403
+from .style import *  # noqa: F401, F403
+
+__version_info__ = (1, 4, 2)
+__version__ = "1.5.1".format(*__version_info__)
