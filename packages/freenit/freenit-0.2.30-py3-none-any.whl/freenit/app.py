28e0647857eea0ae774cad105ea24ccbe2173e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 06:04:33 +0000

    Add package freenit/0.2.30/freenit-0.2.30-py3-none-any.whl

---
+++ packages/freenit/freenit-0.2.30-py3-none-any.whl/freenit/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+import freenit.config
+
+from .api import api
+
+config = freenit.config.getConfig()
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    if not config.database.is_connected:
+        await config.database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    if config.database.is_connected:
+        await config.database.disconnect()
+
+
+app.mount(config.api_root, api)
