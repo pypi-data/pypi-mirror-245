5797666cb3c581a7987efb715530cfa0499ace8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:05:10 +0000

    Add package my-latex-generator-nikita-itmo-mhs/1.0.0/my_latex_generator_nikita_itmo_mhs-1.0.0.tar.gz

---
+++ packages/my-latex-generator-nikita-itmo-mhs/my_latex_generator_nikita_itmo_mhs-1.0.0.tar.gz/my_latex_generator_nikita_itmo_mhs-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='my_latex_generator_nikita_itmo_mhs',
+    version='1.0.0',
+    description='Latex table and img generator library',
+    packages=['tsk_second'],
+    install_requires=[
+    ],
+    author_email='nik-stepanov-2001@bk.ru'
+)
