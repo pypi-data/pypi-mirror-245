6594241a7805e13897f97c57a68ffc2f3554c10a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:05:28 +0000

    Add package opencompass/0.2.0/opencompass-0.2.0.tar.gz

---
+++ packages/opencompass/opencompass-0.2.0.tar.gz/opencompass-0.2.0/opencompass/summarizers/__init__.py
@@ -0,0 +1,5 @@
+from .circular import CircularSummarizer  # noqa: F401
+from .corev2 import Corev2Summarizer  # noqa: F401
+from .creationv01 import Creationv01Summarizer  # noqa: F401
+from .default import DefaultSummarizer  # noqa: F401
+from .subjective import SubjectiveSummarizer  # noqa: F401
