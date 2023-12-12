2e1d803f0736c9243cf934604cb71c0fedca160f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:04:50 +0000

    Add package flaskz/1.6.5rc1/flaskz-1.6.5rc1-py3-none-any.whl

---
+++ packages/flaskz/flaskz-1.6.5rc1-py3-none-any.whl/flaskz/utils/_private.py
@@ -0,0 +1,31 @@
+"""
+only for internal use
+"""
+
+
+def get_dict(d, default=None):
+    """
+     Get the dict instance
+    -- If obj is a dict, just return
+    -- If obj is not a dict, return default or {}
+
+    :param d:
+    :param default:
+    :return:
+    """
+    return d if type(d) == dict else (default or {})
+
+
+def contains_any(collection, targets):
+    """
+    Checks whether the collection contains any item
+    :param collection:
+    :param targets:
+    :return:
+    """
+    collection_type = type(collection)
+    if collection_type is dict:
+        return any(key in collection for key in targets)
+    elif collection_type is list:
+        return any(element in collection for element in targets)
+    return False
