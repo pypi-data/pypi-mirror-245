4a5d087cfd9a01225125da01877a2f9f7617200a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 03:05:51 +0000

    Add package userhub/0.7/userhub-0.7-py3-none-any.whl

---
+++ packages/userhub/userhub-0.7-py3-none-any.whl/userhub/__init__.py
@@ -0,0 +1,15 @@
+"""
+Initializing the Python package
+"""
+
+from .main import detect_type, auth, token
+
+
+__version__ = '0.7'
+
+__all__ = (
+    '__version__',
+    'detect_type',
+    'auth',
+    'token',
+)
