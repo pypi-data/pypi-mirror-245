516d359659517af9a5f09e86ccfeb7adb07aa690
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 06:04:33 +0000

    Add package graphite-bridge/0.0.2/graphite-bridge-0.0.2.tar.gz

---
+++ packages/graphite-bridge/graphite-bridge-0.0.2.tar.gz/graphite-bridge-0.0.2/graphite_bridge/server.py
@@ -0,0 +1,16 @@
+import requests
+
+class GraphiteServerConnector:
+    def __init__(self, serverUrl, strategy_id):
+        self.__serverUrl = serverUrl
+        self.__strategy_id = strategy_id
+
+    def get_client_config(self):
+        response = requests.get(self.__serverUrl + "/client/config?strategyId=" + self.__strategy_id)
+        return response
+    
+    def upload_state(self, state):
+        data = { 'strategyId': self.__strategy_id, 'state': state }
+        response = requests.post(self.__serverUrl + "/client/upload", json=data)
+        return response
+    
