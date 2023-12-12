f246d0aba23d135e7e3ab3b013d90b97bd084919
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 06:04:45 +0000

    Add package tutor-indigo/17.0.0/tutor-indigo-17.0.0.tar.gz

---
+++ packages/tutor-indigo/tutor-indigo-17.0.0.tar.gz/tutor-indigo-17.0.0/tutorindigo/plugin.py
@@ -0,0 +1,93 @@
+from __future__ import annotations
+
+import os
+import typing as t
+
+import pkg_resources
+from tutor import hooks
+from tutor.__about__ import __version_suffix__
+
+from .__about__ import __version__
+
+# Handle version suffix in nightly mode, just like tutor core
+if __version_suffix__:
+    __version__ += "-" + __version_suffix__
+
+
+################# Configuration
+config: t.Dict[str, t.Dict[str, t.Any]] = {
+    # Add here your new settings
+    "defaults": {
+        "VERSION": __version__,
+        "WELCOME_MESSAGE": "The place for all your online learning",
+        "PRIMARY_COLOR": "#3b85ff",  # cool blue
+        # Footer links are dictionaries with a "title" and "url"
+        # To remove all links, run:
+        # tutor config save --set INDIGO_FOOTER_NAV_LINKS=[] --set INDIGO_FOOTER_LEGAL_LINKS=[]
+        "FOOTER_NAV_LINKS": [
+            {"title": "About", "url": "/about"},
+            {"title": "Contact", "url": "/contact"},
+        ],
+        "FOOTER_LEGAL_LINKS": [
+            {"title": "Terms of service", "url": "/tos"},
+            {
+                "title": "Indigo theme for Open edX",
+                "url": "https://github.com/overhangio/tutor-indigo",
+            },
+        ],
+    },
+    "unique": {},
+    "overrides": {},
+}
+
+# Theme templates
+hooks.Filters.ENV_TEMPLATE_ROOTS.add_item(
+    pkg_resources.resource_filename("tutorindigo", "templates")
+)
+# This is where the theme is rendered in the openedx build directory
+hooks.Filters.ENV_TEMPLATE_TARGETS.add_items(
+    [
+        ("indigo", "build/openedx/themes"),
+    ],
+)
+
+# Force the rendering of scss files, even though they are included in a "partials" directory
+hooks.Filters.ENV_PATTERNS_INCLUDE.add_item(
+    r"indigo/lms/static/sass/partials/lms/theme/"
+)
+
+# init script: set theme automatically
+with open(
+    os.path.join(
+        pkg_resources.resource_filename("tutorindigo", "templates"),
+        "indigo",
+        "tasks",
+        "init.sh",
+    ),
+    encoding="utf-8",
+) as task_file:
+    hooks.Filters.CLI_DO_INIT_TASKS.add_item(("lms", task_file.read()))
+
+
+# Override openedx docker image name
+@hooks.Filters.CONFIG_DEFAULTS.add()
+def _override_openedx_docker_image(
+    items: list[tuple[str, t.Any]]
+) -> list[tuple[str, t.Any]]:
+    image = ""
+    for k, v in items:
+        if k == "DOCKER_IMAGE_OPENEDX":
+            image = v
+    if image:
+        items.append(("DOCKER_IMAGE_OPENEDX", f"{image}-indigo"))
+    return items
+
+
+# Load all configuration entries
+hooks.Filters.CONFIG_DEFAULTS.add_items(
+    [(f"INDIGO_{key}", value) for key, value in config["defaults"].items()]
+)
+hooks.Filters.CONFIG_UNIQUE.add_items(
+    [(f"INDIGO_{key}", value) for key, value in config["unique"].items()]
+)
+hooks.Filters.CONFIG_OVERRIDES.add_items(list(config["overrides"].items()))
