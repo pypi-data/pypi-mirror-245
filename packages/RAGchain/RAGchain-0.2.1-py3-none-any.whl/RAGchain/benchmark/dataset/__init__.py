e6a385a45d92296d8b3e305607a7e79832d7cd19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:04:50 +0000

    Add package RAGchain/0.2.1/RAGchain-0.2.1-py3-none-any.whl

---
+++ packages/RAGchain/RAGchain-0.2.1-py3-none-any.whl/RAGchain/benchmark/dataset/__init__.py
@@ -0,0 +1,9 @@
+from .beir import (BeirFEVEREvaluator, BeirFIQAEvaluator,
+                   BeirHOTPOTQAEvaluator, BeirQUORAEvaluator,
+                   BeirSCIDOCSEvaluator, BeirSCIFACTEvaluator)
+from .ko_strategy_qa import KoStrategyQAEvaluator
+from .mr_tydi import MrTydiEvaluator
+from .msmarco import MSMARCOEvaluator
+from .natural_qa import NaturalQAEvaluator
+from .qasper import QasperEvaluator
+from .strategy_qa import StrategyQAEvaluator
---
+++ packages/RAGchain/RAGchain-0.2.1-py3-none-any.whl/RAGchain/preprocess/loader/__init__.py
@@ -0,0 +1,7 @@
+from .dataset_loader import KoStrategyQALoader
+from .excel_loader import ExcelLoader
+from .file_loader import FileLoader
+from .hwp_loader import HwpLoader
+from .mathpix_markdown_loader import MathpixMarkdownLoader
+from .nougat_pdf_loader import NougatPDFLoader
+from .deepdoctection_loader import DeepdoctectionPDFLoader
