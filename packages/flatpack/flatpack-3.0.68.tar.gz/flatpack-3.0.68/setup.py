663f583e0c45b2028d0ab815a44a33b0fbadf037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 03:05:52 +0000

    Add package flatpack/3.0.68/flatpack-3.0.68.tar.gz

---
+++ packages/flatpack/flatpack-3.0.68.tar.gz/flatpack-3.0.68/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="flatpack",
+    version="3.0.68",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography",
+        "gradio",
+        "httpx",
+        "lida",
+        "llmx",
+        "numpy",
+        "requests",
+        "toml",
+        "torch"
+    ],
+    author="Romlin Group AB",
+    author_email="hello@romlin.com",
+    description="Ready-to-assemble AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "flatpack=flatpack.main:main"
+        ],
+    }
+)
