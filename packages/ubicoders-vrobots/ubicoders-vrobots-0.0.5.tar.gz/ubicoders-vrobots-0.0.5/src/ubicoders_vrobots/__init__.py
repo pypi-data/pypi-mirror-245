9717d2754a5816b3bf23fcd5462e2041f43d8bc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:18 +0000

    Add package ubicoders-vrobots/0.0.5/ubicoders-vrobots-0.0.5.tar.gz

---
+++ packages/ubicoders-vrobots/ubicoders-vrobots-0.0.5.tar.gz/ubicoders-vrobots-0.0.5/src/ubicoders_vrobots/__init__.py
@@ -0,0 +1,5 @@
+from .multirotor.multirotor import Multirotor
+from .helicopter.heli import Helicopter
+from .vrobots_client.clientutils import System
+from .vrobots_bridge.vrobots_bridge_gui import gui_app_run
+from .vrobots_bridge.vrobots_bridge import run_bridge_server
