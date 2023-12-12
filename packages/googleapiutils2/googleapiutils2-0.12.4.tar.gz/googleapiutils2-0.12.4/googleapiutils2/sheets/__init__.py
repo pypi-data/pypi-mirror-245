22b89da1df0a297c90f56c4066cb3885a86eed36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 05:04:37 +0000

    Add package googleapiutils2/0.12.4/googleapiutils2-0.12.4.tar.gz

---
+++ packages/googleapiutils2/googleapiutils2-0.12.4.tar.gz/googleapiutils2-0.12.4/googleapiutils2/sheets/__init__.py
@@ -0,0 +1,13 @@
+from .misc import (
+    HorizontalAlignment,
+    InsertDataOption,
+    SheetSliceT,
+    TextDirection,
+    ValueInputOption,
+    ValueRenderOption,
+    VerticalAlignment,
+    WrapStrategy,
+)
+from .sheets import Sheets
+from .sheets_slice import SheetSlice, to_sheet_slice
+from .sheets_value_range import SheetsValueRange
