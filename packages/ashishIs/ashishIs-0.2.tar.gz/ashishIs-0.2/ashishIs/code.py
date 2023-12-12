975c85f807c595f60cd6cadd27084b66b2a266d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 03:05:52 +0000

    Add package ashishIs/0.2/ashishIs-0.2.tar.gz

---
+++ packages/ashishIs/ashishIs-0.2.tar.gz/ashishIs-0.2/ashishIs/code.py
@@ -0,0 +1,6 @@
+# rsa_code.py
+
+def rsaCode():
+
+    return "Your text goes here..."
+
---
+++ packages/ashishIs/ashishIs-0.2.tar.gz/ashishIs-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ashishIs',
+    version='0.2',  # Increment the version number
+    packages=find_packages(),
+    install_requires=[
+        # Specify any dependencies your library might have
+    ],
+)
