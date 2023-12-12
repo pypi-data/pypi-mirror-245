6ea4429548c88b14b2978fbd628d81ff120603fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 05:04:44 +0000

    Add package micrOSDevToolKit/1.42.0/micrOSDevToolKit-1.42.0.tar.gz

---
+++ packages/micrOSDevToolKit/micrOSDevToolKit-1.42.0.tar.gz/micrOSDevToolKit-1.42.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+# https://towardsdatascience.com/create-your-custom-python-package-that-you-can-pip-install-from-your-git-repository-f90465867893
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='micrOSDevToolKit',
+    version='1.42.0',
+    author='Marcell Ban',
+    author_email='miros.framework@gmail.com',
+    description='Development and deployment environment for micrOS, the diy micropython automation OS (IoT)',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/BxNxM/micrOS',
+    project_urls={
+        "Bug Tracker": "https://github.com/BxNxM/micrOS/issues",
+        "GitHub Discussions": "https://github.com/BxNxM/micrOS/discussions"
+    },
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=['adafruit-ampy', 'esptool', 'ipaddress', 'mpy-cross==1.20.0', 'netaddr',
+                      'pylint', 'PyQt5', 'pyserial', 'resources', 'flask', 'flask_restful', 'numpy',
+                      'matplotlib', 'json2html'],
+    scripts=['devToolKit.py'],
+    include_package_data=True,
+    use_scm_version=True
+    #setup_requires=['setuptools_scm']
+)
