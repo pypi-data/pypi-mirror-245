b0cce405afd5f53ddf35c6987c12ef84f1a40dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:05:00 +0000

    Add package tlid/0.1.0/tlid-0.1.0.tar.gz

---
+++ packages/tlid/tlid-0.1.0.tar.gz/tlid-0.1.0/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup
+
+setup(
+    name='tlid',
+    version='0.1.0',
+    description='Module for creating datetime-based strings for file naming.',
+    author='jgwill',
+    packages=['tlid'],
+    install_requires=[],
+)
