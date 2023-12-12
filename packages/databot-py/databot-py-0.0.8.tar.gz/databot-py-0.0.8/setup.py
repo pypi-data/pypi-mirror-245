3d50623490ece92e327bb5f6a2088c7c5e72dcb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:17 +0000

    Add package databot-py/0.0.8/databot-py-0.0.8.tar.gz

---
+++ packages/databot-py/databot-py-0.0.8.tar.gz/databot-py-0.0.8/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from glob import glob
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='databot-py',
+    version='0.0.8',
+    packages=['databot'],
+    include_package_data=True,
+    package_data={
+        'media': glob('media/*'),
+        'data': glob('data/*'),
+        'web': glob('web/*')
+    },
+    url='https://github.com/dbaldwin/databot-py',
+    license='MIT',
+    author='Pat Ryan, Dennis Baldwin',
+    author_email='theyoungsoul@gmail.com, db@droneblocks.io',
+    description='databot Python Library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'bleak==0.21.1'
+    ]
+)
