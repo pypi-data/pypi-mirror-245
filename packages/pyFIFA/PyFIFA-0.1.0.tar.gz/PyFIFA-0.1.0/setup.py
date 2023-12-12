fddb099fe951f6d666162f6ee8ecb2b07dcc70ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:04:55 +0000

    Add package pyFIFA/0.1.0/PyFIFA-0.1.0.tar.gz

---
+++ packages/pyFIFA/PyFIFA-0.1.0.tar.gz/PyFIFA-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyFIFA',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pyfiglet>=0.8.post1'
+    ],
+)
