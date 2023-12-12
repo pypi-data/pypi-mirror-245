804762331adbf9a42a733f6cd79aa5e16c1e67ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 06:04:37 +0000

    Add package shepherd-data/2023.12.1/shepherd_data-2023.12.1.tar.gz

---
+++ packages/shepherd-data/shepherd_data-2023.12.1.tar.gz/shepherd_data-2023.12.1/shepherd_data/__init__.py
@@ -0,0 +1,16 @@
+"""shepherd.datalib
+~~~~~
+Provides classes for storing and retrieving sampled IV data to/from
+HDF5 files.
+
+"""
+from shepherd_core import Writer
+
+from .reader import Reader
+
+__version__ = "2023.12.1"
+
+__all__ = [
+    "Reader",
+    "Writer",
+]
