c31f0cc8445034b7bea613de53c8734ffd88ee4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 03:07:09 +0000

    Add package flatpack/3.0.75/flatpack-3.0.75.tar.gz

---
+++ packages/flatpack/flatpack-3.0.75.tar.gz/flatpack-3.0.75/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="flatpack",
+    version="3.0.75",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography",
+        "httpx",
+        "numpy",
+        "requests",
+        "toml",
+        "torch"
+    ],
+    author="Romlin Group AB",
+    author_email="hello@romlin.com",
+    description="Ready-to-assemble AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "flatpack=flatpack.main:main"
+        ],
+    }
+)
