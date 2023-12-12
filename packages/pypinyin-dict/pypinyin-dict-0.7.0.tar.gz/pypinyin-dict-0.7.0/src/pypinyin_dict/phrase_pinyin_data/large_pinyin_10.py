c4cf773c835f413e7841048d02673077e71bdf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 03:06:05 +0000

    Add package pypinyin-dict/0.7.0/pypinyin-dict-0.7.0.tar.gz

---
+++ packages/pypinyin-dict/pypinyin-dict-0.7.0.tar.gz/pypinyin-dict-0.7.0/src/pypinyin_dict/phrase_pinyin_data/large_pinyin_10.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+# Warning: Auto-generated file, don't edit.
+phrases_dict = {
+    '𫫇嗪': [['è'], ['qín']],
+    '𬂩提': [['jiā'], ['tí']],
+    '𬟁草': [['yì'], ['cǎo']],
+    '𰻝𰻝面': [['biáng'], ['biáng'], ['miàn']],
+}
+
+
+from pypinyin import load_phrases_dict
+
+
+def load():
+    load_phrases_dict(phrases_dict)
