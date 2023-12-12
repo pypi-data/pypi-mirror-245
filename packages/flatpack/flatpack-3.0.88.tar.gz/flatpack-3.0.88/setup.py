ef2d4eba6ef4c8b38b504e945e0a47b4a2fb6704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:04:47 +0000

    Add package flatpack/3.0.88/flatpack-3.0.88.tar.gz

---
+++ packages/flatpack/flatpack-3.0.88.tar.gz/flatpack-3.0.88/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="flatpack",
+    version="3.0.88",
+    license="Apache Software License (Apache 2.0)",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography==41.0.7",
+        "gradio==4.8.0",
+        "httpx==0.25.2",
+        "lida==0.0.10",
+        "llmx==0.0.18a0",
+        "requests==2.31.0",
+        "tensorflow-probability==0.23.0",
+        "toml==0.10.2"
+    ],
+    author="Romlin Group AB",
+    author_email="hello@romlin.com",
+    description="Ready-to-assemble AI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    entry_points={
+        "console_scripts": [
+            "flatpack=flatpack.main:main"
+        ],
+    }
+)
