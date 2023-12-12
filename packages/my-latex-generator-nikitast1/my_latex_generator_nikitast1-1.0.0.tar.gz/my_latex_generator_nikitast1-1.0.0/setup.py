1b4c477ea67c49a0b1de6df3625dd622a6aaae48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:05:22 +0000

    Add package my-latex-generator-nikitast1/1.0.0/my_latex_generator_nikitast1-1.0.0.tar.gz

---
+++ packages/my-latex-generator-nikitast1/my_latex_generator_nikitast1-1.0.0.tar.gz/my_latex_generator_nikitast1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='my_latex_generator_nikitast1',
+    version='1.0.0',
+    description='Latex table and img generator library',
+    packages=['tsk_second'],
+    install_requires=[
+    ],
+    author_email='nik-stepanov-2001@bk.ru'
+)
