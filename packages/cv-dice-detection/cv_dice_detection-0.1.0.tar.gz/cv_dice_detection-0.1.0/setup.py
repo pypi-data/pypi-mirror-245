4ad90e146ff7e81c20ae8993132adc61fd7a9509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:02 +0000

    Add package cv-dice-detection/0.1.0/cv_dice_detection-0.1.0.tar.gz

---
+++ packages/cv-dice-detection/cv_dice_detection-0.1.0.tar.gz/cv_dice_detection-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cv_dice_detection',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An API developed to identify and sum the values of the total dices.',
+    author='Arthur Chieppe, Luiza Valezim, Vinicius Eller',
+    license='MIT',
+    zip=False
+)
