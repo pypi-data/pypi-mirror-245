4b2708913a85b570771d4177925cd7048a10fbd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 05:04:49 +0000

    Add package advanced-pw-gen/1.1/advanced-pw-gen-1.1.tar.gz

---
+++ packages/advanced-pw-gen/advanced-pw-gen-1.1.tar.gz/advanced-pw-gen-1.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+setup(
+    name="advanced-pw-gen",
+    version="1.1",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    package_data={'': ['input_tables/*']},
+    requires=[
+        ],
+    extras_require={
+        'dev': [
+            'twine',
+            'wheel',
+            'pytest',
+        ]
+    },
+    author="Arthur Edmiston",
+    author_email="arthur.edmiston@soledify.com",
+    description="An advanced password generator",
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    url="https://gitlab.com/darksidevt/pw_gen",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    maintainer="Arthur Edmiston",
+    maintainer_email="arthur.edmiston@soledify.com"
+)
