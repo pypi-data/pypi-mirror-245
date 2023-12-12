d891999afe21bb56a9378fbf989b936ba001166e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:05:17 +0000

    Add package myLatexGeneratorNikitaStepanovItmoMhs2023/1.0.0/myLatexGeneratorNikitaStepanovItmoMhs2023-1.0.0.tar.gz

---
+++ packages/myLatexGeneratorNikitaStepanovItmoMhs2023/myLatexGeneratorNikitaStepanovItmoMhs2023-1.0.0.tar.gz/myLatexGeneratorNikitaStepanovItmoMhs2023-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='myLatexGeneratorNikitaStepanovItmoMhs2023',
+    version='1.0.0',
+    description='Latex table and img generator library',
+    packages=['myLatexGeneratorNikitaStepanovItmoMhs2023'],
+    install_requires=[
+    ],
+    author_email='nik-stepanov-2001@bk.ru'
+)
