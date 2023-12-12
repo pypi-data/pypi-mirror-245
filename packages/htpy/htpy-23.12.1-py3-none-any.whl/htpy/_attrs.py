41346c3c287d0ffaa1898abde3f2a231a543928e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 05:04:51 +0000

    Add package htpy/23.12.1/htpy-23.12.1-py3-none-any.whl

---
+++ packages/htpy/htpy-23.12.1-py3-none-any.whl/htpy/_attrs.py
@@ -0,0 +1,56 @@
+from ._safestring import mark_safe, to_html
+
+
+# Inspired by https://www.npmjs.com/package/classnames
+def class_names(value):
+    if isinstance(value, list | tuple | set):
+        return mark_safe(" ".join(to_html(x, quote=True) for x in value if x))
+
+    if isinstance(value, dict):
+        return mark_safe(
+            " ".join(to_html(k, quote=True) for k, v in value.items() if v)
+        )
+
+    return mark_safe(to_html(value, quote=True))
+
+
+def id_classnames_from_css_str(x):
+    if "#" in x and "." in x and x.find("#") > x.find("."):
+        raise ValueError("id (#) must be specified before classes (.)")
+
+    parts = x.split(".")
+    ids = [part.removeprefix("#") for part in parts if part.startswith("#")]
+    classes = [part for part in parts if not part.startswith("#") if part]
+
+    assert len(ids) in (0, 1)
+
+    result = {}
+    if ids:
+        result["id"] = ids[0]
+
+    if classes:
+        result["class"] = " ".join(classes)
+
+    return result
+
+
+def kwarg_attribute_name(name):
+    # Make _hyperscript (https://hyperscript.org/) work smoothly
+    if name == "_":
+        return "_"
+
+    return name.removesuffix("_").replace("_", "-")
+
+
+def generate_attrs(raw_attrs):
+    for key, value in raw_attrs.items():
+        if key == "class":
+            value = class_names(value)
+
+        if value is False:
+            continue
+
+        elif value is not True:
+            value = to_html(value, quote=True)
+
+        yield to_html(key, quote=True), value
