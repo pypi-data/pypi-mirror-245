98ffb5739ba161fff31ebbc290d89268faa510f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 14:05:07 +0000

    Add package tablepic/0.1.0/tablepic-0.1.0.tar.gz

---
+++ packages/tablepic/tablepic-0.1.0.tar.gz/tablepic-0.1.0/src/tablepic/__init__.py
@@ -0,0 +1,10 @@
+'''
+Author: xinyan
+Date: 2023-06-13 17:12:19
+LastEditors: xinyan
+LastEditTime: 2023-12-01 10:35:01
+Description: file content
+'''
+
+
+from .main import generate_table_pic, combine_multiple_pic
