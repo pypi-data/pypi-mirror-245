b3ef6d076d935812c428bbcda50b820dd7a8f529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 06:04:36 +0000

    Add package dcpim/1.0.5/dcpim-1.0.5.tar.gz

---
+++ packages/dcpim/dcpim-1.0.5.tar.gz/dcpim-1.0.5/dcpim/__init__.py
@@ -0,0 +1,6 @@
+""" DCPIM is a general purpose Python 3.x library that contains a lot of
+	commonly done operations inside of a single package. (C) 2018-2023
+	Patrick Lambert - http://dendory.net - Provided under the MIT License
+"""
+
+from .dcpim import *
