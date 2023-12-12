10560224d0373ec0e6593a6591c906cbd8cc9df3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 03:05:51 +0000

    Add package djk-bootstrap5/2.2.1/djk_bootstrap5-2.2.1-py2.py3-none-any.whl

---
+++ packages/djk-bootstrap5/djk_bootstrap5-2.2.1-py2.py3-none-any.whl/djk_ui/conf.py
@@ -0,0 +1,10 @@
+LAYOUT_CLASSES = {
+    '': {
+        'label': 'col-md-3',
+        'field': 'col-md-7',
+    },
+    'display': {
+        'label': 'w-25 table-info',
+        'field': 'w-100 table-light',
+    },
+}
