0b8f96dc96034a7448c39247e3e824c0d94315a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 05:05:01 +0000

    Add package COAsT/3.3.0/COAsT-3.3.0.tar.gz

---
+++ packages/COAsT/COAsT-3.3.0.tar.gz/COAsT-3.3.0/coast/__init__.py
@@ -0,0 +1,36 @@
+from .data.coast import Coast, setup_dask_client
+from ._utils.mask_maker import MaskMaker
+from .data.gridded import Gridded
+from .diagnostics.transect import Transect, TransectF, TransectT
+from .diagnostics.contour import Contour, ContourF, ContourT
+from .diagnostics.eof import compute_eofs, compute_hilbert_eofs
+from .diagnostics.gridded_stratification import GriddedStratification
+from .diagnostics.climatology import Climatology
+from ._utils import logging_util, general_utils, plot_util, crps_util, seasons
+
+# from .diagnostics.annual_hydrographic_climatology import Annual_Climatology
+from .data.index import Indexed
+from .data.profile import Profile
+from .diagnostics.profile_analysis import ProfileAnalysis
+from .data.track import Track
+from .data.lagrangian import Lagrangian
+from .data.oceanparcels import Oceanparcels
+from .data.glider import Glider
+from .data.argos import Argos
+from .data.altimetry import Altimetry
+from .data.timeseries import Timeseries
+from .data.tidegauge import Tidegauge
+from .diagnostics.tidegauge_analysis import TidegaugeAnalysis
+from .data.config_parser import ConfigParser
+from ._utils.xesmf_convert import xesmf_convert
+from ._utils.process_data import Process_data, ProcessData
+from .data.opendap import OpendapInfo
+from .data.copernicus import Copernicus, Product
+from ._utils.experiments_file_handling import experiments
+from ._utils.experiments_file_handling import nemo_filename_maker
+from .diagnostics.circulation import CurrentsOnT
+
+# Set default for logging level when coast is imported
+import logging
+
+logging_util.setup_logging(level=logging.CRITICAL)
