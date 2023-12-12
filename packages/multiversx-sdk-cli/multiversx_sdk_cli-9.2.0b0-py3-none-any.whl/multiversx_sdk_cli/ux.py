e0f06c9887c413aad81118af504d2c89f48f0a21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:04:58 +0000

    Add package multiversx-sdk-cli/9.2.0b0/multiversx_sdk_cli-9.2.0b0-py3-none-any.whl

---
+++ packages/multiversx-sdk-cli/multiversx_sdk_cli-9.2.0b0-py3-none-any.whl/multiversx_sdk_cli/ux.py
@@ -0,0 +1,14 @@
+from rich import print
+from rich.panel import Panel
+
+
+def show_message(message: str):
+    print(Panel(f"[green]{message}"))
+
+
+def show_critical_error(message: str):
+    print(Panel(f"[red]{message}"))
+
+
+def show_warning(message: str):
+    print(Panel(f"[yellow]{message}"))
