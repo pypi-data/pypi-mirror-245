eb25cad8cb8607b4bce6add68115eee929e0b91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 04:05:16 +0000

    Add package edvinspythonlib/0.2.0/edvinspythonlib-0.2.0.tar.gz

---
+++ packages/edvinspythonlib/edvinspythonlib-0.2.0.tar.gz/edvinspythonlib-0.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+from codecs import open
+from os import path
+
+#python3 setup.py sdist bdist_wheel
+#twine check dist/*
+#twine upload dist/*
+#twine upload --skip-existing dist/*
+#
+# The directory containing this file
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='edvinspythonlib',
+    packages=find_packages(include=['edvinspythonlib']),
+    version='0.2.0',
+    description='My first Python library',
+    author='Me',
+    install_requires=['numpy','pandas','matplotlib','scikit-learn','imageio','scikit-image'],
+    tests_require=['pytest == 4.4.1'],
+    setup_requires=['pytest-runner == 4.4'],
+    test_suite='tests',
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
---
+++ packages/edvinspythonlib/edvinspythonlib-0.2.0.tar.gz/edvinspythonlib-0.2.0/tests/test_machinelearning.py
@@ -0,0 +1,11 @@
+from edvinspythonlib import machinelearning as ml
+import unittest
+
+class TestMachineLearning(unittest.TestCase):
+    def test_standardize(self):
+        '''Tests standardize function from machinelearning library'''
+        assert True
+
+if __name__ == '__main__':
+    unittest.main()
+
