604d4ddb9d2228fee19838d0f7d3171b70d45cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-12 15:04:44 +0000

    Add package pylyzer/0.0.51/pylyzer-0.0.51-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/pylyzer/pylyzer-0.0.51-cp310-cp310-manylinux_2_17_i686.manylinux2014_i686.whl/.erg/lib/std/_erg_std_prelude.py
@@ -0,0 +1,25 @@
+from _erg_range import (
+    Range,
+    LeftOpenRange,
+    RightOpenRange,
+    OpenRange,
+    ClosedRange,
+    RangeIterator,
+)
+from _erg_result import Error, is_ok
+from _erg_float import Float, FloatMut
+from _erg_int import Int, IntMut
+from _erg_nat import Nat, NatMut
+from _erg_bool import Bool
+from _erg_bytes import Bytes
+from _erg_str import Str, StrMut
+from _erg_array import Array, UnsizedArray
+from _erg_dict import Dict
+from _erg_set import Set
+from _erg_contains_operator import contains_operator
+from _erg_mutate_operator import mutate_operator
+
+Record = tuple
+
+class Never:
+    pass
